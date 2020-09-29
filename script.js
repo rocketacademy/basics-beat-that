d54ec15781702e2cb3f8e83424b139709e7b2720
cynthcxy <cynthcxy@gmail.com>
2020-09-29 10:44:49 +0800

    added variable number of dice logic for 1 player

--- script.js
+++ script.js
@@ -5,65 +5,80 @@ var getRandomDice = function () {
   return randomInteger;
 };
 
-var gameMode = 'player 1 roll';
+// game modes
+var chooseNumberOfDice = 'choose number of dice';
 var player1Roll = 'player 1 roll';
 var player2Roll = 'player 2 roll';
 var player1Choice = 'player 1 choice';
 var player2Choice = 'player 2 choice';
-var continueGame = 'continue';
 var endGame = 'end';
-var dice1Roll;
-var dice2Roll;
-var dice1 = 'dice 1';
-var dice2 = 'dice 2';
+
+// initialise game mode
+var gameMode = chooseNumberOfDice;
+
+var dices = [];
+var positions = [];
+var numberOfDice;
+var diceArray = [];
+var dicePosition = [];
+var selectedPosition = [];
+var playerNumber = 0;
 var player1Number;
 var player2Number;
 
+var getNumberOfDiceAndRoll = function (diceCount) {
+  var index = 0;
+  while (index < diceCount) {
+    dices.push(getRandomDice());
+    index = index + 1;
+  }
+  return dices;
+};
+
+var getDicePosition = function (diceCount) {
+  var index = 0;
+  while (index < diceCount) {
+    positions.push(index);
+    index = index + 1;
+  }
+  return positions;
+};
+
 var main = function (input) {
   var myOutputValue = 'hello world';
-  if (gameMode == player1Roll) {
-    // when player click submit, roll 2 dice
-    dice1Roll = getRandomDice();
-    dice2Roll = getRandomDice();
+
+  if (gameMode == chooseNumberOfDice) {
+    numberOfDice = input;
+    gameMode = player1Roll;
+    myOutputValue = 'You have selected ' + numberOfDice + ' dice. Click Submit to roll.';
+  } else if (gameMode == player1Roll) {
+    // get dice array and positions
+    diceArray = getNumberOfDiceAndRoll(numberOfDice);
+    dicePosition = getDicePosition(numberOfDice);
     // go to player 1 choice
-    myOutputValue = 'Welcome Player 1. <br><br> You rolled <br>Dice 1: ' + dice1Roll + ' and Dice 2: ' + dice2Roll + '. <br><br> Choose the order of the dice by typing in dice 1 or dice 2, then click submit.';
+    myOutputValue = 'Welcome Player 1. <br><br> You rolled: <br>------dice: ' + diceArray + '<br>positions: ' + dicePosition + '<br><br>Enter the order of dice you want by entering the dice position.';
     gameMode = player1Choice;
   } else if (gameMode == player1Choice) {
-    // if player 1 chose dice 1
-    if (input == dice1) {
-      player1Number = (dice1Roll * 10) + dice2Roll;
-      myOutputValue = 'Player 1, you chose ' + dice1 + ' first. <br>Your number is ' + player1Number + '. <br> It is now player 2\'s turn. <br><br>Click Submit to roll.';
-      // go to player 2 mode
-      gameMode = player2Roll;
-    } else
-    // if player 1 chose dice 2
-    if (input == dice2) {
-      player1Number = (dice2Roll * 10) + dice1Roll;
-      myOutputValue = 'Player 1, you chose ' + dice2 + ' first. <br>Your number is ' + player1Number + '. <br> It is now player 2\'s turn. <br><br>Click Submit to roll.';
-      // go to player 2 mode
-      gameMode = player2Roll;
+    // for each number in user input, create player number
+    selectedPosition = input.split('');
+    console.log('selected position ' + selectedPosition);
+    console.log('dice array ' + diceArray);
+
+    var numberExponential = numberOfDice - 1;
+    // player number is 0 + selected dice multiplied by 10^numberofdice -1
+    // for each loop the power value decreases
+    var index = 0;
+    while (index < numberOfDice) {
+      playerNumber = playerNumber + (diceArray[selectedPosition[index]]) * 10 ** numberExponential;
+      console.log('dice number ' + playerNumber);
+      index = index + 1;
+      numberExponential = numberExponential - 1;
     }
-  } else if (gameMode == player2Roll) {
-    // when player click submit, roll 2 dice
-    dice1Roll = getRandomDice();
-    dice2Roll = getRandomDice();
-    // go to player 2 choice
-    myOutputValue = 'Welcome Player 2. <br><br> You rolled <br>Dice 1: ' + dice1Roll + ' and Dice 2: ' + dice2Roll + '. <br><br> Choose the order of the dice by typing in dice 1 or dice 2, then click submit.';
-    gameMode = player2Choice;
-  } else if (gameMode == player2Choice) {
-    if (input == dice1) {
-      player2Number = (dice1Roll * 10) + dice2Roll;
-      myOutputValue = 'Player 2, you chose ' + dice1 + ' first. <br>Your number is ' + player2Number + '. <br> Click Submit to see the results!';
-      // go to player 2 mode
-      gameMode = endGame;
-    } else
-    // if player 1 chose dice 2
-    if (input == dice2) {
-      player2Number = (dice2Roll * 10) + dice1Roll;
-      myOutputValue = 'Player 2, you chose ' + dice2 + ' first. <br>Your number is ' + player2Number + '. <br> Click Submit to see the results!';
-      // go to player 2 mode
-      gameMode = endGame;
-    }
+    // set player 1 number as this number
+    player1Number = playerNumber;
+    myOutputValue = 'Player 1, you chose the positions' + selectedPosition + '. <br>Your number is ' + player1Number + '. <br> It is now player 2\'s turn. <br><br>Click Submit to roll.';
+    // go to player 2 mode
+    gameMode = player2Roll;
   } else if (gameMode == endGame) {
     if (player1Number > player2Number) {
       myOutputValue = 'Player 1 won! Player 1\'s number was ' + player1Number + ' and Player 2\'s number was ' + player2Number;
