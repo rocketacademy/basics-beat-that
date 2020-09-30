fe29995c8910691f03271c597442a9c48f81c1d4
cynthcxy <cynthcxy@gmail.com>
2020-09-30 18:28:27 +0800

    added score logic

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ var player1Roll = 'player 1 roll';
 var player2Roll = 'player 2 roll';
 var player1Choice = 'player 1 choice';
 var player2Choice = 'player 2 choice';
-var endGame = 'end';
+var continueGame = 'continue game';
+var endGame = 'end game';
 
 // initialise game mode
 var gameMode = chooseNumberOfDice;
@@ -25,6 +26,8 @@ var selectedPosition = [];
 var playerNumber = 0;
 var player1Number;
 var player2Number;
+var player1Score = 0;
+var player2Score = 0;
 
 var getNumberOfDiceAndRoll = function (diceCount) {
   var index = 0;
@@ -80,6 +83,7 @@ var main = function (input) {
     }
     // set player 1 number as this number
     player1Number = playerNumber;
+    player1Score = player1Score + player1Number;
     myOutputValue = 'Player 1, you chose the positions' + selectedPosition + '. <br>Your number is ' + player1Number + '. <br> It is now player 2\'s turn. <br><br>Click Submit to roll.';
     // go to player 2 mode
     gameMode = player2Roll;
@@ -113,16 +117,26 @@ var main = function (input) {
     }
     // set player 2 number as this number
     player2Number = playerNumber;
-    myOutputValue = 'Player 2, you chose the positions' + selectedPosition + '. <br>Your number is ' + player2Number + '. <br> It is the end of the game. <br><br>Click Submit to see the results.';
-    // go to end game mode
-    gameMode = endGame;
+    player2Score = player2Score + player2Number;
+    myOutputValue = 'Player 2, you chose the positions' + selectedPosition + '. <br>Your number is ' + player2Number + '. <br><br>To continue the game, type continue and click Submit. <br><br>To end the game, type end and click Submit.';
+    gameMode = continueGame;
+  } else if (gameMode == continueGame) {
+    if (input == 'continue') {
+      // continue game mode
+      myOutputValue = 'Click submit to roll';
+      gameMode = player1Roll;
+    } else if (input == 'end') {
+      // go to end game mode
+      myOutputValue = 'Click submit to see results.';
+      gameMode = endGame;
+    }
   } else if (gameMode == endGame) {
     if (player1Number > player2Number) {
-      myOutputValue = 'Player 1 won! Player 1\'s number was ' + player1Number + ' and Player 2\'s number was ' + player2Number;
+      myOutputValue = 'Player 1 won! Player 1\'s score was ' + player1Score + ' and Player 2\'s score was ' + player2Score;
     } else if (player1Number < player2Number) {
-      myOutputValue = 'Player 2 won! Player 1\'s number was ' + player1Number + ' and Player 2\'s number was ' + player2Number;
+      myOutputValue = 'Player 2 won! Player 1\'s score was ' + player1Score + ' and Player 2\'s score was ' + player2Score;
     } else if (player1Number == player2Number) {
-      myOutputValue = 'It was a draw! Player 1\'s number was ' + player1Number + ' and Player 2\'s number was ' + player2Number;
+      myOutputValue = 'It was a draw! Player 1\'s score was ' + player1Score + ' and Player 2\'s score was ' + player2Score;
     }
   }
   return myOutputValue;
