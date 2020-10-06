03c54ef32f0484569342f2b9e2fc1749c348b1fd
Marie <mariewong.art@gmail.com>
2020-10-06 11:49:35 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Get random dice function
+// Get random dice function
 var getRandomDiceNumber = function () {
   // return float between 0 to 5.9999
   var randomFloat = Math.random() * 6;
@@ -7,17 +7,17 @@ var getRandomDiceNumber = function () {
   return randomDiceRoll;
 };
 
-//Global Variable for Game modes
+// Global Variable for Game modes
 var currentGameState = 'waiting to start game...';
-//var player1Roll = 'Player 1 rolls twice';
-//var player2Roll = 'Player 2 rolls twice';
+// var player1Roll = 'Player 1 rolls twice';
+// var player2Roll = 'Player 2 rolls twice';
 var P1DiceNo = [];
 var P2DiceNo = [];
 var p1ChosenNumber = 0;
 var p2ChosenNumber = 0;
-//var gameMode = player1Roll;
+// var gameMode = player1Roll;
 
-//Allow the user to roll the dice twice and get a random number between 1 - 6
+// Allow the user to roll the dice twice and get a random number between 1 - 6
 
 var main = function (input) {
   var myOutputValue = '';
@@ -28,9 +28,7 @@ var main = function (input) {
     // Change game mode to player 1 roll
     currentGameState = 'P1 StartGame';
     myOutputValue = 'Player 1 start rolling the dice by clicking enter';
-  }
-
-  else if (currentGameState == 'P1 StartGame') {
+  } else if (currentGameState == 'P1 StartGame') {
     var counter = 0;
     while (counter < 2) {
       P1DiceNo[counter] = getRandomDiceNumber();
@@ -40,16 +38,12 @@ var main = function (input) {
     myOutputValue = 'Player 1: You have rolled <br> Dice #1: ' + P1DiceNo[0] + ' <br>Dice #2: ' + P1DiceNo[1] + '<br> Input your winning number';
 
     currentGameState = 'P1 Choose number';
-  }
-
-  else if (currentGameState == 'P1 Choose number') {
-    //allow p1 to input their winning number
+  } else if (currentGameState == 'P1 Choose number') {
+    // allow p1 to input their winning number
     p1ChosenNumber = input;
-    myOutputValue = 'Player 2, Roll the dice by pressing enter'
+    myOutputValue = 'Player 2, Roll the dice by pressing enter';
     currentGameState = 'P2 StartGame';
-  }
-
-  else if (currentGameState == 'P2 StartGame') {
+  } else if (currentGameState == 'P2 StartGame') {
     var counter = 0;
     while (counter < 2) {
       P2DiceNo[counter] = getRandomDiceNumber();
@@ -58,17 +52,12 @@ var main = function (input) {
     }
     myOutputValue = 'Player 2: You have rolled <br> Dice #1: ' + P2DiceNo[0] + ' <br>Dice #2: ' + P2DiceNo[1] + '<br> Input your winning number';
     currentGameState = 'P2 Choose number';
-  }
-
-  else if (currentGameState == 'P2 Choose number') {
-    //allow p2 to input their winning number
+  } else if (currentGameState == 'P2 Choose number') {
+    // allow p2 to input their winning number
     p2ChosenNumber = input;
-    myOutputValue = 'Lets calculate the results'
+    myOutputValue = 'Lets calculate the results';
     currentGameState = 'calculating results';
-  }
-
-  else if (currentGameState == 'calculating results') {
-
+  } else if (currentGameState == 'calculating results') {
     if (p1ChosenNumber == p2ChosenNumber) {
       myOutputValue == 'Its a draw';
     }
@@ -79,9 +68,7 @@ var main = function (input) {
       myOutputValue = 'Player 2 Wins!';
     }
     currentGameState = 'waiting to start game...';
-
   }
-
 
   return myOutputValue;
 };
