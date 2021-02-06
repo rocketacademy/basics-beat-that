60b0fd84364bbd22fed30b456e532f30950ec881
Aidil-Shahril <mdaidilshahril@gmail.com>
2021-02-06 13:15:34 +0800

    updated 1

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var myOutputValue;
 var playerTurn = 'player1';
 var player1Final = 0;
 var player2Final = 0;
-var diceRolls = [];
+var player1DiceRoll = [];
+var player2DiceRoll = [];
 
 var rollDice = function () {
   // produces a decimal between 0 and 3
@@ -15,72 +16,58 @@ var rollDice = function () {
   return numberRolled;
 };
 
-// var gameRound = function () {
-//   if (playerTurn == 'player1') {
-//     var p1Dice1 = rollDice().toString();
-//     var p1Dice2 = rollDice().toString();
-//     player1DiceRoll.push(p1Dice1, p1Dice2);
+var gameRound = function () {
+  if (playerTurn == 'player1') {
+    // logs player 1 dice rolls into array
+    var p1Dice1 = rollDice().toString();
+    var p1Dice2 = rollDice().toString();
+    player1DiceRoll.push(p1Dice1, p1Dice2);
 
-//     console.log(player1DiceRoll);
-//     myOutputValue = 'Player 1 rolled ' + p1Dice1 + ' & ' + p1Dice2 + '. It is now player 2s turn';
-//   } else if (playerTurn == 'player2') {
-//     var p2Dice1 = rollDice().toString();
-//     var p2Dice2 = rollDice().toString();
-//     player2DiceRoll.push(p2Dice1, p2Dice2);
+    console.log(player1DiceRoll);
+    myOutputValue = 'Player 1 rolled ' + p1Dice1 + ' & ' + p1Dice2 + '. It is now player 2s turn';
+  } else if (playerTurn == 'player2') {
+    // logs player 2 dice roll into array
+    var p2Dice1 = rollDice().toString();
+    var p2Dice2 = rollDice().toString();
+    player2DiceRoll.push(p2Dice1, p2Dice2);
 
-//     myOutputValue = 'Player 2 rolled ' + p2Dice1 + ' & ' + p2Dice2 + '. it is now a new round.';
-//   }
-// };
+    console.log(player2DiceRoll);
+    myOutputValue = 'Player 2 rolled ' + p2Dice1 + ' & ' + p2Dice2 + '. it is now a new round.';
+  }
+};
 
 var turnRotation = function () {
-  console.log('turning turns');
   if (playerTurn == 'player1') {
+    playerTurn = 'player1ChoiceRound';
+  } else if (playerTurn == 'player1ChoiceRound') {
     playerTurn = 'player2';
   } else if (playerTurn == 'player2') {
+    playerTurn = 'player2ChoiceRound';
+  } else if (playerTurn == 'player2ChoiceRound') {
     playerTurn = 'player1';
   }
+  console.log('check: ' + playerTurn);
 };
 
-// var playerChoices = function (input) {
-//   console.log('Running player choice method');
-//   if (playerTurn == 'player1ChoiceRound') {
-//     if ((input - 1) == 0) {
-//       player1Final = player1DiceRoll[0] + player2DiceRoll[1];
-//       console.log(player1Final);
-//     }
-//   }
-// };
 var handlePlayerChoice = function (input) {
-  var finalResult;
-  if ((input - 1) == 0) {
-    finalResult = diceRolls[0] + diceRolls[1];
-  } else {
-    finalResult = diceRolls[1] + diceRolls[0];
+  if (playerTurn == 'player1ChoiceRound' && (input - 1) == 0) {
+    player1Final = player1DiceRoll[0] + player1DiceRoll[1];
+    console.log('check');
+  } else if (playerTurn == 'player1ChoiceRound' && (input - 1) == 1) {
+    player1Final = player1DiceRoll[1] + player1DiceRoll[0];
   }
-  console.log(finalResult);
-  return finalResult;
+  if (playerTurn == 'player2ChoiceRound' && (input - 1) == 0) {
+    player2Final = player2DiceRoll[0] + player2DiceRoll[1];
+  } else if (playerTurn == 'player2ChoiceRound' && (input - 1) == 1) {
+    player2Final = player2DiceRoll[1] + player2DiceRoll[0];
+  }
+  console.log(player1Final, player2Final);
 };
-
-var rollingDice = function () {
-  var diceArray = [];
-  var dice1 = rollDice().toString();
-  var dice2 = rollDice().toString();
-  diceArray.push(dice1, dice1);
-
-  console.log('player:' + playerTurn + ' options: ' + diceArray);
-  return diceArray;
-};
-
-diceRolls = rollingDice();
 
 var main = function (input) {
-  if (playerTurn == 'player1') {
-    player1Final = handlePlayerChoice(input);
-  } else {
-    player2Final = handlePlayerChoice(input);
-  }
+  gameRound();
   turnRotation();
-  diceRolls = rollingDice();
+  handlePlayerChoice(input);
 
   if (player1Final > player2Final) {
     myOutputValue = 'Player 1 winning';
@@ -88,8 +75,5 @@ var main = function (input) {
     myOutputValue = 'Player 2 winning';
   }
 
-  console.log('who is playing ', playerTurn);
-  console.log('Player 1 result ', player1Final);
-  console.log('Player 2 result ', player2Final);
   return myOutputValue;
 };
