dd5ce1bdb7fcef5bcf91a0beabcffda835e3409f
Aidil-Shahril <mdaidilshahril@gmail.com>
2021-02-06 14:29:30 +0800

    final base working

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ var gameRound = function () {
     var p1Dice2 = rollDice().toString();
     player1DiceRoll.push(p1Dice1, p1Dice2);
 
-    console.log(player1DiceRoll);
-    myOutputValue = 'Player 1 rolled ' + p1Dice1 + ' & ' + p1Dice2 + '. It is now player 2s turn';
+    console.log('player 1 has rolled: ' + player1DiceRoll);
+    myOutputValue = 'Player 1 rolled ' + p1Dice1 + ' & ' + p1Dice2 + '. <br><br>Player 1 choose which dice is first, type 1 or 2.';
   } else if (playerTurn == 'player2') {
     // logs player 2 dice roll into array
     var p2Dice1 = rollDice().toString();
@@ -32,7 +32,7 @@ var gameRound = function () {
     player2DiceRoll.push(p2Dice1, p2Dice2);
 
     console.log(player2DiceRoll);
-    myOutputValue = 'Player 2 rolled ' + p2Dice1 + ' & ' + p2Dice2 + '. it is now a new round.';
+    myOutputValue = 'Player 2 rolled ' + p2Dice1 + ' & ' + p2Dice2 + '. <br><br>Player 2 choose which dice is first. type 1 or 2.';
   }
 };
 
@@ -44,34 +44,40 @@ var turnRotation = function () {
   } else if (playerTurn == 'player2') {
     playerTurn = 'player2ChoiceRound';
   } else if (playerTurn == 'player2ChoiceRound') {
+    playerTurn = 'winningRound';
+  } else if (playerTurn == 'winningRound') {
     playerTurn = 'player1';
   }
   console.log('check: ' + playerTurn);
 };
 
 var handlePlayerChoice = function (input) {
-  if (playerTurn == 'player1ChoiceRound' && (input - 1) == 0) {
+  if (playerTurn == 'player1ChoiceRound' && input == '1') {
     player1Final = player1DiceRoll[0] + player1DiceRoll[1];
-    console.log('check');
-  } else if (playerTurn == 'player1ChoiceRound' && (input - 1) == 1) {
+    myOutputValue = 'Player 1, your current combination is ' + player1Final + '. <br><br>Player 2, click submit to roll your dice.';
+  } else if (playerTurn == 'player1ChoiceRound' && input == '2') {
     player1Final = player1DiceRoll[1] + player1DiceRoll[0];
+    myOutputValue = 'Player 1, your current combination is ' + player1Final + '. <br><br>Player 2, click submit to roll your dice.';
   }
+
   if (playerTurn == 'player2ChoiceRound' && (input - 1) == 0) {
     player2Final = player2DiceRoll[0] + player2DiceRoll[1];
+    myOutputValue = 'Player 2, your current combination is ' + player2Final + '. <br><br>Player 1, click submit to roll your dice.';
   } else if (playerTurn == 'player2ChoiceRound' && (input - 1) == 1) {
     player2Final = player2DiceRoll[1] + player2DiceRoll[0];
+    myOutputValue = 'Player 2, your current combination is ' + player2Final + '. <br><br>Player 1, click submit to roll your dice.';
   }
   console.log(player1Final, player2Final);
 };
 
 var main = function (input) {
   gameRound();
-  turnRotation();
   handlePlayerChoice(input);
+  turnRotation();
 
-  if (player1Final > player2Final) {
+  if (playerTurn == 'winningRound' && (player1Final > player2Final)) {
     myOutputValue = 'Player 1 winning';
-  } else {
+  } else if (player2Final > player1Final) {
     myOutputValue = 'Player 2 winning';
   }
 
