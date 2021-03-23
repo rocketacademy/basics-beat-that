fdae628cfc95ba362a17613493ae65091888d2d0
isaac-prog <panda_mm88@yahoo.com>
2021-03-23 12:58:37 +0800

    Improved-version

--- script.js
+++ script.js
@@ -1,62 +1,83 @@
-// main function
-// arrays of players die
 var main = function (input) {
-  const player1RanRolls = [];
-  const player2RanRolls = [];
-  // creating the largest value of the 2 dice that was rolled
-  player1RanRolls.sort((a, b) => a - b);
-  player2RanRolls.sort((a, b) => a - b);
+  var dice = [];
+  var player1Score = [];
+  var player2Score = [];
+  var mode = 0;
 
-  // if player 1 is going to start, player1's dice will roll
-  if (input == 'Player1') {
-    var player1Dice1 = Math.floor(Math.random() * 6) + 1;
-    var player1Dice2 = Math.floor(Math.random() * 6) + 1;
-    console.log(player1Dice1);
-    console.log(player1Dice2);
-    player1RanRolls.push(player1Dice1);
-    player1RanRolls.push(player1Dice2);
-    console.log(player1RanRolls);
-    return player1RanRolls;
+  // mode selection
+  if (input == 'LargeMode') {
+ 		var mode = 0;
   }
 
-  // if player 2 is going to start, player2's dice will roll
-  if (input == 'Player2') {
-    var player2Dice1 = Math.floor(Math.random() * 6) + 1;
-    var player2Dice2 = Math.floor(Math.random() * 6) + 1;
-    console.log(player2Dice1);
-    console.log(player2Dice2);
-    player2RanRolls.push(player2Dice1);
-    player2RanRolls.push(player2Dice2);
-    console.log(player2RanRolls);
-    return player2RanRolls;
+  if (input == 'SmallMode') {
+  	var mode = 1;
   }
 
-  // this is to determine which player will win
-  if (input == 'vs') {
-    if (player1Dice1 > player2Dice1 || player1Dice1 == player2Dice1 && player1Dice2 > player2Dice2) {
-      return 'player1 wins';
-    }
+  // if player 1 is going to start, player1's dice will roll for largemode
+  if (input == 'Player1' && mode == 0) {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+ 		// pushing value into array
+    dice.push(dice1);
+    dice.push(dice2);
+    // auto-generator (largest value)
+    dice.sort((a, b) => b - a);
 
-    if (player1Dice1 < player2Dice1 || player1Dice1 == player2Dice1 && player1Dice2 < player2Dice2) {
-      return 'player2 wins';
-    }
+    const player1Result = +dice.join('');
+    player1Score.push(player1Result);
+    // clear dice array
+    var dice = [];
+    return player1Result;
+  }
 
-    if (player1Dice1 == player2Dice1 && player1Dice2 == player2Dice2) {
-      return 'draw';
-    }
+  // if player 2 is going to start, player1's dice will roll for largemode
+  if (input == 'Player2' && mode == 0) {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+ 		// pushing value into array
+    dice.push(dice1);
+    dice.push(dice2);
+    // auto-generator (largest value)
+    dice.sort((a, b) => b - a);
+
+    const player2Result = +dice.join('');
+    player1Score.push(player2Result);
+    // clear dice array
+    var dice = [];
+    return player2Result;
+  }
+
+  // if player 1 is going to start, player1's dice will roll for smallmode
+  if (input == 'Player1' && mode == 1) {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+ 		// pushing value into array
+    dice.push(dice1);
+    dice.push(dice2);
+    // auto-generator (smallest value)
+    dice.sort((a, b) => a - b);
+
+    const player1Result = +dice.join('');
+    player1Score.push(player1Result);
+    // clear dice array
+    var dice = [];
+    return player1Result;
+  }
+
+ 	// if player 2 is going to start, player1's dice will roll for smallmode
+  if (input == 'Player2' && mode == 1) {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+ 		// pushing value into array
+    dice.push(dice1);
+    dice.push(dice2);
+    // auto-generator (smallest value)
+    dice.sort((a, b) => a - b);
+
+    const player2Result = +dice.join('');
+    player1Score.push(player2Result);
+    // clear dice array
+    var dice = [];
+    return player2Result;
   }
 };
-
-// Dice roll
-var diceRoll = function () {
-  var randRoll = Math.floor(Math.random() * 6) + 1;
-  return diceRoll;
-};
-
-// Arrays of players' rolls
-const player1DiceList = [];
-const player2DiceList = [];
-
-// default number of dice rolls before game starts
-var player1BaseRoll = 0;
-var player2BaseRoll = 0;
