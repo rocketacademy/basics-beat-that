40b2982eb06acaa91c34bdee07023087912685c4
Hazel Lim <Hazel@Admins-MacBook-Pro-3.local>
2021-06-14 16:07:05 +0800

    base version 1_pseudocode

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+//Variable to store players' results
+
+
 var main = function (input) {
   var myOutputValue = 'hello world';
+
+  //Mode 1: player 1 
+    //roll 2 dices
+
+
+    //Output
+      //myOutputValue = `You rolled __ for Dice 1 and __ for Dice 2`
+
+    //input order of dice
+
+  //Mode 2: player 2
+    //roll 2 dices
+
+
+    //Output
+      //myOutputValue = `You rolled __ for Dice 1 and __ for Dice 2`
+
+    //input order of dice
+  
+  //If player 1's dice numbers > player 2's dice numbers, player 1 wins.
+
+  //If player 2's dice numbers > player 1's dice numbers, player 2 wins.
+
+  //Else, it's a draw.
+
   return myOutputValue;
 };
