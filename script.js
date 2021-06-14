99621402b07a000e5f20d291eb0724c876c99ffc
JustinWong98 <justinwong8991@gmail.com>
2021-06-14 13:52:03 +0800

    Lowest Combined Mode option added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mode = "player1start";
+var mode = "start";
 var combinedDiceRoll1 = 0;
 var combinedDiceRoll2 = 0;
 var outcome1 = 0;
@@ -7,7 +7,17 @@ var p1Choice = 0;
 var p2Choice = 0;
 var p1Score = 0;
 var p2Score = 0;
+var noOfPlayers = 0;
 var main = function (input) {
+  if (mode == "start") {
+    if (input == "normal") {
+      mode = "player1startnormal";
+    } else if (input == "lowest") {
+      mode = "player1startlowest";
+    } else {
+      return "please only enter normal or lowest!";
+    }
+  }
   var twoDiceRolls = dicerolltwice();
   var pickingprocess = pickprocess(twoDiceRolls, outcome1, outcome2);
   return pickingprocess;
@@ -20,13 +30,24 @@ var diceroll = function () {
 };
 
 var faceoff = function (p1Choice, p2Choice) {
-  mode = "player1start";
-  if (p1Choice > p2Choice) {
-    return `Player 1 wins with a value of ${p1Choice} over ${p2Choice}!`;
-  } else if (p2Choice > p1Choice) {
-    return `Player 2 wins with a value of ${p2Choice} over ${p1Choice}!`;
-  } else {
-    return "There is no winner!";
+  if (mode == "player2startnormal") {
+    mode = "start";
+    if (p1Choice > p2Choice) {
+      return `Player 1 wins with a value of ${p1Choice} over ${p2Choice}!`;
+    } else if (p2Choice > p1Choice) {
+      return `Player 2 wins with a value of ${p2Choice} over ${p1Choice}!`;
+    } else {
+      return "There is no winner!";
+    }
+  } else if ("player2startlowest") {
+    mode = "start";
+    if (p2Choice > p1Choice) {
+      return `Player 1 wins as the value of ${p1Choice} is lower than ${p2Choice}!`;
+    } else if (p1Choice > p2Choice) {
+      return `Player 2 wins as the value of ${p2Choice} is lower than ${p1Choice}!`;
+    } else {
+      return "There is no winner!";
+    }
   }
 };
 
@@ -35,13 +56,15 @@ var dicerolltwice = function () {
   var rollDice2 = diceroll();
   outcome1 = rollDice1 * 10 + rollDice2;
   outcome2 = rollDice2 * 10 + rollDice1;
+  console.log(outcome1);
+  console.log(outcome2);
   var myOutputValue =
     `You have rolled ${rollDice1} and ${rollDice2}!` +
     "<br>" +
     `1: ${outcome1}` +
     "<br>" +
     `2: ${outcome2}`;
-  if (mode == "player1start") {
+  if (mode == "player1startnormal" || mode == "player1startlowest") {
     myOutputValue = "Player 1:" + "<br>" + myOutputValue;
   } else {
     myOutputValue = "Player 2:" + "<br>" + myOutputValue;
@@ -51,8 +74,8 @@ var dicerolltwice = function () {
 
 var pickprocess = function (myOutputValue, outcome1, outcome2) {
   if (outcome1 > outcome2) {
-    if (mode == "player1start") {
-      mode = "player2start";
+    if (mode == "player1startnormal") {
+      mode = "player2startnormal";
       p1Choice = outcome1;
       p1Score = p1Score + p1Choice;
       return (
@@ -60,7 +83,8 @@ var pickprocess = function (myOutputValue, outcome1, outcome2) {
         "<br>" +
         `Player 1, your highest roll is ${outcome1}. Now, player 2 will roll.`
       );
-    } else {
+    }
+    if (mode == "player2startnormal") {
       p2Choice = outcome1;
       p2Score = p2Score + p2Choice;
       var winnerDecided = faceoff(p1Choice, p2Choice);
@@ -74,9 +98,33 @@ var pickprocess = function (myOutputValue, outcome1, outcome2) {
         leaderboardScore
       );
     }
+    if (mode == "player1startlowest") {
+      mode = "player2startlowest";
+      p1Choice = outcome2;
+      p1Score = p1Score + p1Choice;
+      return (
+        `${myOutputValue}` +
+        "<br>" +
+        `Player 1, your lowest roll is ${outcome2}. Now, player 2 will roll.`
+      );
+    }
+    if (mode == "player2startlowest") {
+      p2Choice = outcome2;
+      p2Score = p2Score + p2Choice;
+      var winnerDecided = faceoff(p1Choice, p2Choice);
+      var leaderboardScore = leaderboard(p1Score, p2Score);
+      return (
+        `${myOutputValue}` +
+        "<br>" +
+        `Player 2, your lowest roll is ${outcome2}.` +
+        "<br>" +
+        winnerDecided +
+        leaderboardScore
+      );
+    }
   } else {
-    if (mode == "player1start") {
-      mode = "player2start";
+    if (mode == "player1startnormal") {
+      mode = "player2startnormal";
       p1Choice = outcome2;
       p1Score = p1Score + p1Choice;
       return (
@@ -84,11 +132,13 @@ var pickprocess = function (myOutputValue, outcome1, outcome2) {
         "<br>" +
         `Player 1, your highest roll is ${outcome2}. Now, player 2 will roll.`
       );
-    } else {
+    }
+    if (mode == "player2startnormal") {
       p2Choice = outcome2;
       p2Score = p2Score + p2Choice;
       var winnerDecided = faceoff(p1Choice, p2Choice);
       var leaderboardScore = leaderboard(p1Score, p2Score);
+      console.log(leaderboardScore);
       return (
         `${myOutputValue}` +
         "<br>" +
@@ -98,60 +148,85 @@ var pickprocess = function (myOutputValue, outcome1, outcome2) {
         leaderboardScore
       );
     }
+    if (mode == "player1startlowest") {
+      mode = "player2startlowest";
+      p1Choice = outcome1;
+      p1Score = p1Score + p1Choice;
+      return (
+        `${myOutputValue}` +
+        "<br>" +
+        `Player 1, your lowest roll is ${outcome1}. Now, player 2 will roll.`
+      );
+    }
+    if (mode == "player2startlowest") {
+      p2Choice = outcome1;
+      console.log(p2Choice);
+      p2Score = p2Score + p2Choice;
+      console.log(p2Score);
+      var winnerDecided = faceoff(p1Choice, p2Choice);
+      var leaderboardScore = leaderboard(p1Score, p2Score);
+      console.log(leaderboardScore);
+      return (
+        `${myOutputValue}` +
+        "<br>" +
+        `Player 2, your lowest roll is ${outcome1}.` +
+        "<br>" +
+        winnerDecided +
+        leaderboardScore
+      );
+    }
   }
 };
 
 var leaderboard = function (p1Score, p2Score) {
-  if (p1Score > p2Score) {
-    return (
-      "<br>" +
-      `1st Place - Player 1 Total score: ${p1Score}` +
-      "<br>" +
-      `2nd Place - Player 2 Total score: ${p2Score}` +
-      "<br> <br>" +
-      "Player 1, click submit to roll again!"
-    );
-  } else if (p2Score > p1Score) {
-    return (
-      "<br>" +
-      `1st Place - Player 2 Total score: ${p2Score}` +
-      "<br>" +
-      `2nd Place - Player 1 Total score: ${p1Score}` +
-      "<br> <br>" +
-      "Player 1, click submit to roll again!"
-    );
+  var myOutputValue = "";
+  if (mode == "player1startnormal") {
+    if (p1Score > p2Score) {
+      myOutputValue =
+        "<br>" +
+        `1st Place - Player 1 Total score: ${p1Score}` +
+        "<br>" +
+        `2nd Place - Player 2 Total score: ${p2Score}` +
+        "<br> <br>" +
+        "Player 1, type normal to play Normal mode again or type lowest to play reverse!";
+    } else if (p2Score > p1Score) {
+      myOutputValue =
+        "<br>" +
+        `1st Place - Player 2 Total score: ${p2Score}` +
+        "<br>" +
+        `2nd Place - Player 1 Total score: ${p1Score}` +
+        "<br> <br>" +
+        "Player 1, type normal to play Normal mode again or type lowest to play reverse!";
+    }
+  } else if (mode == "player1startlowest") {
+    if (p1Score > p2Score) {
+      myOutputValue =
+        "<br>" +
+        `1st Place - Player 2 Total score: ${p2Score}` +
+        "<br>" +
+        `2nd Place - Player 1 Total score: ${p1Score}` +
+        "<br> <br>" +
+        "Player 1, type lowest to play Reverse mode again or type normal to play normal dice!";
+    } else if (p2Score > p1Score) {
+      myOutputValue =
+        "<br>" +
+        `1st Place - Player 1 Total score: ${p1Score}` +
+        "<br>" +
+        `2nd Place - Player 2 Total score: ${p2Score}` +
+        "<br> <br>" +
+        "Player 1, type lowest to play Reverse mode again or type normal to play normal dice!";
+    }
   }
+  return myOutputValue;
 };
 
-var leaderboard = function (p1Score, p2Score) {
-  if (p1Score > p2Score) {
-    return (
-      "<br>" +
-      `1st Place - Player 1 Total score: ${p1Score}` +
-      "<br>" +
-      `2nd Place - Player 2 Total score: ${p2Score}` +
-      "<br> <br>" +
-      "Player 1, click submit to roll again!"
-    );
-  } else if (p2Score > p1Score) {
-    return (
-      "<br>" +
-      `1st Place - Player 2 Total score: ${p2Score}` +
-      "<br>" +
-      `2nd Place - Player 1 Total score: ${p1Score}` +
-      "<br> <br>" +
-      "Player 1, click submit to roll again!"
-    );
-  }
-};
 var inputDisplay = function () {
-  if (mode == "player1start") {
-    return "Player 1, please roll.";
-  } else if (mode == "player1pick") {
-    return "Player 1, please enter which dice you wish to put in front.";
-  } else if (mode == "player2start") {
-    return "Player 2, please roll.";
-  } else if (mode == "player2pick") {
-    return "Player 2, please enter which dice you wish to put in front.";
+  // change to new lowest and normal modes
+  if (mode == "start") {
+    return "Please input whether you would like to play lowest or normal mode.";
+  } else if (mode == "player1startnormal" || mode == "player1startlowest") {
+    return "It is Player 1's turn.";
+  } else if (mode == "player2startnormal" || mode == "player2startlowest") {
+    return "It is Player 2's turn.";
   }
 };
