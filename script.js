78bd4a614e69fa67d1239a647caea273a65813e5
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2021-06-14 18:16:59 +0700

    added rollDie function

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
+
+// die rolling function setup
+function getRandomInteger(max) {
+  var randomDecimal = Math.random();
+  var randomInteger = Math.floor(randomDecimal * max);
+  return randomInteger;
+}
+
+function rollDie() {
+  var randomDieFace = getRandomInteger(6) + 1;
+  return randomDieFace;
+}
