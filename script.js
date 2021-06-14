f4016247c6c260252b0a41427b0f3d9c62a01a10
Ameeralih <ameerali.hassanali@gmail.com>
2021-06-14 17:33:52 +0800

    Added variable number of dice to play with and auto-generation of combined number

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // (e.g. If the two dice rolls 2 and 5,  The user will decide 25 or 52). The player with the higher combined number wins.
 // Create an array of player rolls, an array of players' combined scores, and two check arrays -- to check if a player has rolled and to check if a player has selected his/her combined score
 
-var numberOfDiceRolls = 2;
+var numberOfDiceRolls = 0;
 var gameStatus = "chooseNumberPlayers";
 var numberOfPlayers = 0;
 var playerRollCheckArray = [];
@@ -25,16 +25,34 @@ var rollXdice = function (numberOfDiceToRoll) {
   return diceRolls;
 };
 
+// Function to autogenerate combined number to get highest number
+var autoGenerateCombinedNumber = function (diceRollNumbersArray) {
+  diceRollNumbersArray.sort();
+  diceRollNumbersArray.reverse();
+  var sortedCombinedNumber = "";
+  for (var counter = 0; counter < diceRollNumbersArray.length; counter += 1) {
+    sortedCombinedNumber = sortedCombinedNumber + diceRollNumbersArray[counter];
+  }
+  sortedCombinedNumber = Number(sortedCombinedNumber);
+  return sortedCombinedNumber;
+};
+
 var main = function (input) {
   if (gameStatus == "chooseNumberPlayers") {
     numberOfPlayers = input;
-    gameStatus = "gamePlay";
+    gameStatus = "chooseNumberOfDiceToRoll";
     for (var player = 0; player < numberOfPlayers; player += 1) {
       // Create two arrays containing check-values for each player -- one array to check if each player has rolled the dice. The other array to check if each player has chosen whch dice roll of theirs they want to choose as their first to create the combined number.
       playerRollCheckArray.push(0);
       playersCombinedNumberCheckArray.push(0);
     }
-    return "Player 1 - click submit to roll your two dice!";
+    return "Choose the number of dice each player rolls";
+  }
+
+  if (gameStatus == "chooseNumberOfDiceToRoll") {
+    numberOfDiceRolls = input;
+    gameStatus = "gamePlay";
+    return "Player 1 - click submit to roll your dice!";
   }
 
   if (gameStatus == "gamePlay") {
@@ -43,33 +61,35 @@ var main = function (input) {
       if (playerRollCheckArray[player] == 0) {
         playerRollsArray.push(rollXdice(numberOfDiceRolls));
         playerRollCheckArray[player] = 1;
-        // After player rolls the two dice, make him choose what dice goes first and create the player's combined number
-        return (
-          "Player " +
-          (player + 1) +
-          ", you rolled a " +
-          playerRollsArray[player][0] +
-          " and " +
-          playerRollsArray[player][1] +
-          ".<br><br>" +
-          "Choose which dice roll to go first."
-        );
-      }
-      if (playersCombinedNumberCheckArray[player] == 0) {
-        var firstRoll = input;
-        var secondRoll = 2;
-        if (firstRoll == 2) {
-          secondRoll = 1;
+        var playerRollMessage = "Player " + (player + 1) + ", you rolled a<br>";
+        for (var counter = 0; counter < numberOfDiceRolls; counter += 1) {
+          playerRollMessage =
+            playerRollMessage + playerRollsArray[player][counter] + "<br>";
         }
-        console.log(firstRoll, secondRoll);
-        playersCombinedNumberArray[player] = Number(
-          "" +
-            playerRollsArray[player][firstRoll - 1] +
-            playerRollsArray[player][secondRoll - 1]
+        // After player rolls the two dice, make him choose what dice goes first and create the player's combined number (not applicable for autogenerate number version)
+
+        playersCombinedNumberArray[player] = autoGenerateCombinedNumber(
+          playerRollsArray[player]
         );
-        playersCombinedNumberCheckArray[player] = 1;
-        return "Your  combined number is " + playersCombinedNumberArray[player];
+        return playerRollMessage;
       }
+
+      // // Players select their own combined number (only for two dice rolls)
+      // if (playersCombinedNumberCheckArray[player] == 0) {
+      //   var firstRoll = input;
+      //   var secondRoll = 2;
+      //   if (firstRoll == 2) {
+      //     secondRoll = 1;
+      //   }
+      //   console.log(firstRoll, secondRoll);
+      //   playersCombinedNumberArray[player] = Number(
+      //     "" +
+      //       playerRollsArray[player][firstRoll - 1] +
+      //       playerRollsArray[player][secondRoll - 1]
+      //   );
+      //   playersCombinedNumberCheckArray[player] = 1;
+      //   return "Your  combined number is " + playersCombinedNumberArray[player];
+      // }
 
       if (player + 1 == numberOfPlayers) {
         // If the last player is reached and they have rolled and selected their number to go first, show the scoreboard
@@ -79,9 +99,15 @@ var main = function (input) {
     }
   }
   var scoreboard = "";
+  var winner = 0;
   if (gameStatus == "scoreboard") {
     for (var player = 0; player < numberOfPlayers; player += 1) {
       // Creating the scoreboard
+      if (
+        playersCombinedNumberArray[player] > playersCombinedNumberArray[winner]
+      ) {
+        winner = player;
+      }
       scoreboard =
         scoreboard +
         "Player " +
@@ -90,6 +116,12 @@ var main = function (input) {
         playersCombinedNumberArray[player] +
         "<br><br>";
     }
-    return scoreboard;
+
+    return (
+      "Player " +
+      (winner + 1) +
+      " wins!<br><br><br><b>Scoreboard</b><br><br>" +
+      scoreboard
+    );
   }
 };
