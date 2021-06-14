d6805e72c5913deb5b03581b59e4b1b8a3053919
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2021-06-14 23:29:28 +0700

    completed base of exercise

--- script.js
+++ script.js
@@ -1,40 +1,109 @@
 var die1;
 var die2;
-var playersResultingNumbers = [];
-var playerNumber = 0;
+var player1Results;
+var player2Results;
+var player = 1;
 var mode = "dice roll";
 
 var main = function (input) {
-  if (mode == "dice roll") {
-    var diceResult = roll2Dice();
-    mode = "choose dice order";
-    playerNumber = playerNumber + 1;
-    console.log(die1 + die2);
-    return `Welcome Player ${playerNumber}. <br>
-     ${diceResult}`;
-  } else if (mode == "choose dice order") {
-    var resultingNumber;
-    if (Number(input) == 1) {
-      concatDiceResult = `${die1}`.concat(`${die2}`);
-    } else if (Number(input) == 2) {
-      concatDiceResult = `${die2}`.concat(`${die1}`);
+  var outputMessage;
+  while (player == 1) {
+    if (mode == "dice roll") {
+      var diceResult = roll2Dice();
+      mode = "choose dice order";
+      outputMessage = `Welcome Player 1. <br>
+     ${diceResult} <br>Choose the order of the dice`;
+    } else if (mode == "choose dice order") {
+      var concatDiceResult;
+      if (Number(input) == 1) {
+        concatDiceResult = `${die1}`.concat(`${die2}`);
+      } else if (Number(input) == 2) {
+        concatDiceResult = `${die2}`.concat(`${die1}`);
+      }
+      player1Results = concatDiceResult;
+      player = 2;
+      mode = "dice roll";
+      outputMessage = `Player 1, you chose Dice ${input} first. <br>Your number is ${player1Results}. <br>It is now Player 2's turn.`;
     }
-    playersResultingNumbers.push(concatDiceResult);
-    mode = "dice roll";
-    return `Player ${playerNumber}, you chose Dice ${input} first. <br>Your number is ${concatDiceResult}. <br>It is now Player 2's turn.`;
+    return outputMessage;
   }
+  while (player == 2) {
+    if (mode == "dice roll") {
+      var diceResult = roll2Dice();
+      mode = "choose dice order";
+      outputMessage = `Welcome Player 2. <br>
+     ${diceResult} <br>Choose the order of the dice.`;
+    } else if (mode == "choose dice order") {
+      var concatDiceResult;
+      if (Number(input) == 1) {
+        concatDiceResult = `${die1}`.concat(`${die2}`);
+      } else if (Number(input) == 2) {
+        concatDiceResult = `${die2}`.concat(`${die1}`);
+      }
+      player2Results = concatDiceResult;
 
-  if (mode == "player2 dice roll") {
+      mode = "dice roll";
+      if (player2Results > player1Results) {
+        var winnerIs = "Player 2 wins!";
+      } else if (player2Results < player1Results) {
+        winnerIs = "Player 1 wins!";
+      } else if (player2Results == player1Results) {
+        WinnerIs = "It's a draw!";
+      }
+      outputMessage = `Player 2, you chose Dice ${input} first. <br>Your number is ${player2Results}. <br>${winnerIs}`;
+    }
+    return outputMessage;
   }
-  var myOutputValue = diceOutput;
-  return myOutputValue;
 };
+
+// var main = function (input) {
+//   console.log("top" + playersResultingNumbers);
+
+//   // Mode : dice roll
+
+//   if (mode == "dice roll") {
+//     var diceResult = roll2Dice();
+
+//     if (playerNumber == 1) {
+//       var diceRollMessage = `Welcome Player 1. <br>
+//      ${diceResult} <br>Choose the order of the dice.`;
+//       mode = "choose dice order";
+//     } else if (playerNumber == 2) {
+//       diceRollMessage = `Welcome Player 2. <br>
+//      ${diceResult} <br>Choose the order of the dice.`;
+//       mode = "choose dice order player 2";
+//     }
+//     return diceRollMessage;
+//   }
+
+//   // Mode : choose dice order
+//   else if (mode == "choose dice order player 1") {
+//     var concatDiceResult;
+//     if (Number(input) == 1) {
+//       concatDiceResult = `${die1}`.concat(`${die2}`);
+//     } else if (Number(input) == 2) {
+//       concatDiceResult = `${die2}`.concat(`${die1}`);
+//     }
+//     playersResultingNumbers.push(Number(concatDiceResult));
+//     mode = "dice roll";
+//     var diceOrderMessage = `Player ${playerNumber}, you chose Dice ${input} first. <br>Your number is ${concatDiceResult}. <br>It is now Player ${
+//       playerNumber + 1
+//     }'s turn.`;
+//     playerNumber = playerNumber + 1;
+//     return diceOrderMessage;
+//   }
+
+//   if (mode == "player2 dice roll") {
+//   }
+//   var myOutputValue = diceOutput;
+//   return myOutputValue;
+// };
 
 // roll both dice
 function roll2Dice() {
   die1 = rollDie();
   die2 = rollDie();
-  var diceOutput = `You rolled ${die1} for Dice 1 and ${die2} for Dice 2. <br>Choose the order of the dice.`;
+  var diceOutput = `You rolled ${die1} for Dice One and ${die2} for Dice Two.`;
   return diceOutput;
 }
 
