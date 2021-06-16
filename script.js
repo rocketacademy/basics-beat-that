f16652f84711507889d43eccb7947372124c2951
gw98 <glenwee1998@gmail.com>
2021-06-16 17:08:26 +0800

    added score and leaderboard features

--- script.js
+++ script.js
@@ -7,19 +7,15 @@ var Dice1 = "";
 var Dice2 = "";
 var player1Number = "";
 var player2Number = "";
-// var player1Score = "";
-// var player2Score = "";
-// var player1NumRounds = 0;
-// var player2NumRounds = 0;
-// var player1NumWins = 0;
-// var player2NumWins = 0;
+var player1Score = 0;
+var player2Score = 0;
 var ROLL_DICE_MODE = "ROLL DICE";
 var CHOOSE_ORDER_MODE = "CHOOSE ORDER";
+var LOWEST_NUMBER_MODE = "LOWEST NUMBER";
 var gameMode = ROLL_DICE_MODE;
-// var input = "";
 var winner = "";
 
-// Start with Player 1
+// The game always starts with Player 1
 
 var main = function (input) {
   var myOutputValue = "";
@@ -33,9 +29,9 @@ var basicGame = function (input) {
     Dice1 = rollDice();
     Dice2 = rollDice();
     gameMode = CHOOSE_ORDER_MODE;
-    return `Player ${currentPlayer}: You have rolled ${Dice1} and ${Dice2}. <br> Please choose which dice you would like first: "1" for ${Dice1}${Dice2} and "2" for ${Dice2}${Dice1}.`;
+    return `Player ${currentPlayer}: You have rolled ${Dice1} and ${Dice2}. <br><br> Please choose the order of the dice: "1" for ${Dice1}${Dice2} and "2" for ${Dice2}${Dice1}.`;
   }
-  var currPlayerNum = "";
+  var currPlayerNum = 0;
   var chosenOrder = Number(input);
 
   // Concatenate dice rolls based on user choice
@@ -49,11 +45,14 @@ var basicGame = function (input) {
 
   if (currentPlayer == PLAYER1) {
     player1Number = currPlayerNum;
+    player1Score += Number(currPlayerNum);
     myOutputValue = `${chosenOutputMessage}. It is now Player 2's turn. Please click submit to play.`;
   } else if (currentPlayer == PLAYER2) {
     player2Number = currPlayerNum;
+    player2Score += Number(currPlayerNum);
     winner = generateWinner(player1Number, player2Number);
-    myOutputValue = `${chosenOutputMessage}. ${winner} Click submit to play again.`;
+    leader = generateLeaderboard(player1Score, player2Score);
+    myOutputValue = `${chosenOutputMessage}. ${winner} <br> <br> ${leader} <br> Click submit to play again.`;
   }
 
   // Switches to other player and restarts game
@@ -69,6 +68,17 @@ var generateWinner = function (player1Number, player2Number) {
   if (player1Number > player2Number) {
     return `Player ${PLAYER1} wins!`;
   } else return `Player ${PLAYER2} wins!`;
+};
+
+// Lists the 2 players and their scores in decreasing order
+var generateLeaderboard = function (player1Score, player2Score) {
+  if (player1Score >= player2Score) {
+    return `Leaderboard <br> Player 1: ${player1Score} <br> 
+    Player 2: ${player2Score}`;
+  } else {
+    return `Leaderboard <br> Player 2: ${player2Score} <br> 
+    Player 1: ${player1Score}`;
+  }
 };
 
 // Simple Dice Function
