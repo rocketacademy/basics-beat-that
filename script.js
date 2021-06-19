362a2f6e4d16730221903ce6bdaec049e86a5d06
fruitflyy <yingyi.chew@gmail.com>
2021-06-19 17:01:27 +0800

    WorksNow

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ var gameMode = "Roll Dice";
 
 var player1Dice = [];
 var player2Dice = [];
-var currentPlayer;
+var dice1;
+var dice2;
+var currentPlayer = 1;
 
 //When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
 var main = function (input) {
+  console.log("game mode is " + gameMode);
   if (gameMode == "Roll Dice") {
-    currentPlayer = input;
-    var dice1 = rollDice();
-    var dice2 = rollDice();
+    console.log("The current player is " + currentPlayer);
+    dice1 = rollDice();
+    dice2 = rollDice();
 
     if (currentPlayer == 1) {
-      player1Dice[(dice1, dice2)];
-    } else {
-      player2Dice[(dice1, dice2)];
+      player1Dice.push(dice1);
+      player1Dice.push(dice2);
+    } else if (currentPlayer == 2) {
+      player2Dice.push(dice1);
+      player2Dice.push(dice2);
     }
     console.log("dice 1:" + dice1);
     console.log("dice 2:" + dice2);
@@ -26,29 +31,71 @@ var main = function (input) {
     gameMode = "Dice Order";
 
     myOutputValue =
-      "player " +
+      "Player " +
       input +
       " rolled " +
       dice1 +
       " for Dice 1 and " +
       dice2 +
-      " for Dice 2" +
-      "now enter 1 or 2 to decide if you want to have the dice1/dice2 as your first digit";
-  } else {
-    //Dice order mode
+      " for Dice 2. " +
+      "<br>Action required: Please enter 1 or 2 to decide if you want to have the dice1 or dice2 as your first digit";
+    //The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
+    //"Choose the order of the dice."
+  } else if (gameMode == "Dice Order") {
     diceOrder = input;
-
-    if (diceOrder == 1) {
-      console.log("nothing changed");
-      return "Your number combination is:" + dice1 + dice2;
-    } else {
-      dice1 = player2Dice[0];
-      dice2 = player2Dice[1];
-      //player1Dice = player1Dice[(dice2, dice1)];
-      return "Your number combination is:" + dice2 + dice1;
+    var dices = getPlayerDice(currentPlayer);
+    if (currentPlayer == 1) {
+      if (diceOrder == 1) {
+        myOutputValue =
+          "Your number combination is: " +
+          dices.join("") +
+          "<br>Now it is Player 2's turn to roll the dice. ";
+      } else if (diceOrder == 2) {
+        dices = [dices[1], dices[0]];
+        storePlayerDice(currentPlayer, dices);
+        myOutputValue =
+          "Your number combination is: " +
+          dices.join("") +
+          "<br>Now it is Player 2's turn to roll the dice. ";
+      }
+      currentPlayer = 2;
+      gameMode = "Roll Dice";
+    } else if (currentPlayer == 2) {
+      if (diceOrder == 1) {
+        myOutputValue = "Your number combination is: " + dices.join("");
+      } else if (diceOrder == 2) {
+        dices = [dices[1], dices[0]];
+        if (currentPlayer == 1) {
+          player1Dice = dices;
+        } else if (currentPlayer == 2) {
+          player2Dice = dices;
+        }
+        myOutputValue = "Your number combination is: " + dices.join("");
+      }
+      winningPlayer = whoWins();
+      myOutputValue = myOutputValue + "<br> Player1: " + player1Dice.join("");
+      myOutputValue = myOutputValue + "<br> Player2: " + player2Dice.join("");
+      myOutputValue =
+        myOutputValue + "<br>The Winning player is: " + winningPlayer;
     }
   }
   return myOutputValue;
+};
+
+var getPlayerDice = function (currentPlayer) {
+  if (currentPlayer == 1) {
+    return player1Dice;
+  } else if (currentPlayer == 2) {
+    return player2Dice;
+  }
+};
+
+var storePlayerDice = function (currentPlayer, dices) {
+  if (currentPlayer == 1) {
+    player1Dice = dices;
+  } else if (currentPlayer == 2) {
+    player2Dice = dices;
+  }
 };
 
 var rollDice = function () {
@@ -59,7 +106,15 @@ var rollDice = function () {
   return diceNumber;
 };
 
-//The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
-//"Choose the order of the dice."
+//trying to replicate the above for player2
+//currentPlayer == "Player 2";
+//gameMode = "Roll Dice";
+//return "It is now Player 2's turn. Press Submit to roll Player's 2 dice";
 
 //after both players have rolled and chosen dice order, the player with the higher combined number wins.
+var whoWins = function () {
+  if (player1Dice.join("") > player2Dice.join("")) {
+    return "Play 1 won";
+  }
+  return "Play 2 won";
+};
