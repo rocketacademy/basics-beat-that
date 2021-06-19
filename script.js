a75f0676f799cc9c29ce4bb9bc2fe4d6d9b7d04a
FionaNFauna <fionaandfauna@gmail.com>
2021-06-19 15:32:40 +0800

    finished variable number of dices and errors

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ var rollDice = function () {
 };
 
 var mode = "choosinggamemode";
-var user1Number = "";
-var user2Number = "";
-var user1dice1 = "";
-var user1dice2 = "";
-var user2dice1 = "";
-var user2dice2 = "";
+var user1Number = 0;
+var user2Number = 0;
+var user1Numbers = [];
+var user2Numbers = [];
+
 var user1SCORE = 0;
 var user2SCORE = 0;
 var gamemode = "";
+var NumberofDices = 0;
 
 var main = function (input) {
   var myOutputValue = " ";
@@ -26,7 +26,10 @@ var main = function (input) {
     mode = "gamemodechosen";
   } else if (mode == "gamemodechosen") {
     myOutputValue = gamemodechosen(input);
+    mode = "chooseNumofDice";
+  } else if (mode == "chooseNumofDice") {
     mode = "part1";
+    return NumofDice(input);
   } else if (mode == "part1") {
     myOutputValue = part1();
     mode = "part2";
@@ -34,10 +37,15 @@ var main = function (input) {
     myOutputValue = part2();
     mode = "outcome";
   } else if (mode == "outcome") {
-    if (gamemode == "Normal") {
-      myOutputValue = normalOutcome(user1Number, user2Number);
-    } else if (gamemode == "Reverse") {
-      myOutputValue = reverseOutcome(user1Number, user2Number);
+    console.log(gamemode);
+    if (gamemode == "Reverse") {
+      myOutputValue =
+        reverseOutcome(user1Number, user2Number) +
+        reverseleaderboard(user1SCORE, user2SCORE);
+    } else if (gamemode == "Normal") {
+      myOutputValue =
+        normalOutcome(user1Number, user2Number) +
+        normalleaderboard(user1SCORE, user2SCORE);
     }
     mode = "part1";
   }
@@ -52,89 +60,98 @@ var choosinggamemode = function () {
 var gamemodechosen = function (input) {
   if (input == "Normal") {
     gamemode = "Normal";
-    return "The game mode you chose was Normal. <br>The player with the highest possible number will win.";
-  }
-  if (input == "Reverse") {
+    return "The game mode you chose was Normal. <br>The player with the highest possible number will win. <br>Please now type in the Number of Dices.";
+  } else if (input == "Reverse") {
     gamemode = "Reverse";
-    return "The game mode you chose was Reverse.<br>The player with the lowest possible number will win.";
+    return "The game mode you chose was Reverse.<br>The player with the lowest possible number will win. <br>Please now type in the Number of Dices.";
   } else {
+    mode = "choosinggamemode";
     return "Please type in either 'Normal' or 'Reverse' ";
+  }
+};
+
+var NumofDice = function (input) {
+  if (isNaN(input) || input == 0) {
+    mode = "chooseNumofDice";
+    return "Please type in a number.";
+  } else {
+    NumberofDices = Number(input);
+    return `You've chosen to roll ${NumberofDices} dices. <br>Click the submit button.`;
   }
 };
 
 ///roll the dice for user1's two numbers
 var part1 = function () {
-  user1dice1 = rollDice();
-  user1dice2 = rollDice();
+  var roll = 0;
+  while (roll < NumberofDices) {
+    var user1Num = rollDice();
+    user1Numbers.push(user1Num);
+    roll += 1;
+  }
+
   if (gamemode == "Normal") {
-    if (user1dice1 > user1dice2) {
-      user1Number = user1dice1 * 10 + user1dice2;
-    } else if (user1dice2 >= user1dice1) {
-      user1Number = user1dice2 * 10 + user1dice1;
+    user1Numbers = user1Numbers.sort(function (a, b) {
+      return b - a;
+    });
+
+    var counter = 0;
+    while (counter < user1Numbers.length) {
+      user1Number +=
+        user1Numbers[counter] * 10 ** (NumberofDices - 1 - counter);
+      counter += 1;
     }
-  } else {
-    if (user1dice1 > user1dice2) {
-      user1Number = user1dice2 * 10 + user1dice1;
-    } else if (user1dice2 >= user1dice1) {
-      user1Number = user1dice1 * 10 + user1dice2;
+  } else if (gamemode == "Reverse") {
+    user1Numbers = user1Numbers.sort(function (a, b) {
+      return a - b;
+    });
+    var counter = 0;
+    while (counter < user1Numbers.length) {
+      user1Number +=
+        user1Numbers[counter] * 10 ** (NumberofDices - 1 - counter);
+      counter += 1;
     }
   }
   user1SCORE = user1SCORE + user1Number;
   console.log(user1SCORE);
 
-  return `User 1, your numbers are ${user1dice1} and ${user1dice2}. <br>Please choose the order of the dice. <br>Your number is ${user1Number}`;
+  return `User 1, your number is ${user1Number}`;
 };
 
 ///roll the dice for user2's two numbers
 var part2 = function () {
-  user2dice1 = rollDice();
-  user2dice2 = rollDice();
+  var roll = 0;
+  while (roll < NumberofDices) {
+    var user2Num = rollDice();
+    user2Numbers.push(user2Num);
+    roll += 1;
+  }
+
   if (gamemode == "Normal") {
-    if (user2dice1 > user2dice2) {
-      user2Number = user2dice1 * 10 + user2dice2;
-    } else if (user2dice2 >= user2dice1) {
-      user2Number = user2dice2 * 10 + user2dice1;
+    user2Numbers = user2Numbers.sort(function (a, b) {
+      return b - a;
+    });
+
+    var counter = 0;
+    while (counter < user2Numbers.length) {
+      user2Number +=
+        user2Numbers[counter] * 10 ** (NumberofDices - 1 - counter);
+      counter += 1;
     }
-  } else {
-    if (user2dice1 > user2dice2) {
-      user2Number = user2dice2 * 10 + user2dice1;
-    } else if (user2dice1 >= user2dice1) {
-      user2Number = user2dice1 * 10 + user2dice1;
+  } else if (gamemode == "Reverse") {
+    user2Numbers = user2Numbers.sort(function (a, b) {
+      return a - b;
+    });
+    var counter = 0;
+    while (counter < user2Numbers.length) {
+      user2Number +=
+        user2Numbers[counter] * 10 ** (NumberofDices - 1 - counter);
+      counter += 1;
     }
   }
   user2SCORE = user2SCORE + user2Number;
   console.log(user2SCORE);
-  return `User 2, your numbers are ${user2dice1} and ${user2dice2}. <br> Your number is ${user2Number}.`;
-};
 
-///get the leaderboard for the normal mode.
-var normalleaderboard = function (user1SCORE, user2SCORE) {
-  var message = "";
-  if (user1SCORE >= user2SCORE) {
-    message = `1. User1's Score: ${user1SCORE} <br>2. User2's Score: ${user2SCORE}`;
-  } else if (user2SCORE > user1SCORE) {
-    message = `1. User2's Score: ${user2SCORE} <br>2. User1's Score: ${user1SCORE}`;
-  }
-  return message;
-};
-
-///get the outcome for the normal mode (e.g. win or lose)
-var normalOutcome = function (user1Number, user2Number) {
-  var message = "";
-  if (user1Number > user2Number) {
-    message = `User 1 won! Try again later User 2. <br>`;
-  }
-  if (user2Number > user1Number) {
-    message = `User 2 won! Try again later User 1. <br>`;
-  }
-  if (user1Number == user2Number) {
-    message = `It's a tie! <br>Better luck next time. <br>`;
-  }
-  message =
-    message +
-    normalleaderboard(user1SCORE, user2SCORE) +
-    '<br><br>Click the "submit" button to replay. <br>To change Game Modes, please refresh page.';
-  return message;
+  return `User 2, your number is ${user2Number}`;
 };
 
 ///get the leaderboard for the reverse mode.
@@ -145,23 +162,52 @@ var reverseleaderboard = function (user1SCORE, user2SCORE) {
   } else if (user2SCORE < user1SCORE) {
     message = `1. User2's Score: ${user2SCORE} <br>2. User1's Score: ${user1SCORE}`;
   }
-  return message;
+  return (
+    message +
+    '<br><br>Click the "submit" button to replay. <br>To change Game Modes, please refresh page.'
+  );
 };
 
 ///get the outcome for the reverse mode (e.g. win or lose)
 var reverseOutcome = function (user1Number, user2Number) {
+  var message = `Player 1's number is ${user1Number} while Player 2's number is ${user2Number}. <br>`;
+
   if (user1Number < user2Number) {
-    var message = `User 1 won! Try again later User 2. <br>`;
+    message += `User 1 won! Try again later User 2. <br>`;
+  } else if (user2Number > user1Number) {
+    message += `User 2 won! Try again later User 1. <br>`;
+  } else if (user1Number == user2Number) {
+    message += `It's a tie! <br>Better luck next time. <br>`;
   }
-  if (user2Number > user1Number) {
-    var message = `User 2 won! Try again later User 1. <br>`;
+  message += `<br>`;
+  return message;
+};
+
+///get the leaderboard for the normal mode.
+var normalleaderboard = function (user1SCORE, user2SCORE) {
+  var message = "";
+  if (user1SCORE >= user2SCORE) {
+    message = `1. User1's Score: ${user1SCORE} <br>2. User2's Score: ${user2SCORE}`;
+  } else if (user2SCORE > user1SCORE) {
+    message = `1. User2's Score: ${user2SCORE} <br>2. User1's Score: ${user1SCORE}`;
   }
-  if (user1Number == user2Number) {
-    var message = `It's a tie! <br>Better luck next time. <br>`;
-  }
-  message =
+  return (
     message +
-    reverseleaderboard(user1SCORE, user2SCORE) +
-    '<br><br>Click the "submit" button to replay. <br>To change Game Modes, please refresh page.';
+    '<br><br>Click the "submit" button to replay. <br>To change Game Modes, please refresh page.'
+  );
+};
+
+///get the outcome for the normal mode (e.g. win or lose)
+var normalOutcome = function (user1Number, user2Number) {
+  var message = `Player 1's number is ${user1Number} while Player 2's number is ${user2Number}. <br>`;
+
+  if (user1Number > user2Number) {
+    message += `User 1 won! Try again later User 2. <br>`;
+  } else if (user2Number > user1Number) {
+    message += `User 2 won! Try again later User 1. <br>`;
+  } else if (user1Number == user2Number) {
+    message += `It's a tie! <br>Better luck next time. <br>`;
+  }
+  message += `<br>`;
   return message;
 };
