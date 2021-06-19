8d4e520dfd772c4e308352d1f3a2b49d686db4c7
yapjunyi <yapjunyi95@gmail.com>
2021-06-19 15:37:27 +0800

    remove comments

--- script.js
+++ script.js
@@ -171,32 +171,6 @@ var main = function (input) {
   } else return `Choose your gamemode: normal, reversed or knockout.`;
 };
 
-// arrangeOrder = true;
-
-// }
-
-// if (arrangeOrder == true) {
-//   switchPlayercalculateScore(playerNum, combinedScoring);
-//   return output;
-// }
-// //Arranging the order of dice
-// if (arrangeOrder == true) {
-//   var scoring = arrangeDice(input);
-//   var statementOne = scoring;
-//   if (!isNaN(scoring)) {
-//     switchPlayercalculateScore(playerNum, scoring);
-
-//     if (playerOneScore > playerTwoScore) {
-//       statementOne = `Player ${currentplayer}, You chose order ${input} first <br> Your number is ${scoring}. <br><br> Player 1 score: ${playerOneScore}<br>Player 2 score: ${playerTwoScore}<br><br>It is now Player ${playerNum}'s turn.`;
-//       return statementOne;
-//     }
-//     if (playerTwoScore > playerOneScore) {
-//       statementOne = `Player ${currentplayer}, You chose order ${input} first <br> Your number is ${scoring}. <br><br> Player 2 score: ${playerTwoScore}<br>Player 1 score: ${playerOneScore}<br><br>It is now Player ${playerNum}'s turn.`;
-//       return statementOne;
-//     }
-//   } else return statementOne;
-// }
-
 //Roll the dice
 var diceroll = function () {
   var randomDecimal = Math.random() * 6;
@@ -362,24 +336,3 @@ var finalScoreboardStatement = function (finalList) {
 
   return statement;
 };
-// Arrange dice
-// var arrangeDice = function (input) {
-//   if (input == 1) {
-//     console.log("read");
-//     firstNum = diceOne;
-//     secondNum = diceTwo;
-//     arrangeOrder = false;
-//   }
-//   if (input == 2) {
-//     firstNum = diceTwo;
-//     secondNum = diceOne;
-//     arrangeOrder = false;
-//   }
-//   if (input != 1 && input != 2) {
-//     return `Please enter 1 or 2.`;
-//   }
-//   score = firstNum * 10 + secondNum;
-//   return score;
-// };
-
-//roll the dice
