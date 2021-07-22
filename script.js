4b5b03d633a382703c910e674f3dba9cfb2c375a
whyexis <chongyingxiang@gmail.com>
2021-07-22 10:17:50 +0800

    Added scorekeeping with output

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ var playerTurn = 0;
 var playerCombi = [];
 var numOfDice = 2;
 var diceRolls = [];
+var scoreBoard = [];
 
 // Main
 var main = function (input) {
+  // Generate score board
+  if (scoreBoard.length != numOfPlayers) {
+    generateScoreBoard();
+  }
+
   // Roll dice if dice rolls are empty and ask player to choose combination
   if (diceRolls.length == 0) {
     getDiceRolls(numOfDice);
     return chooseCombi(playerNumber);
   } else {
-    // Show a player's combination or ends the game
+    // Show a player's combination or ends the round
     return generateCombi(input);
   }
 };
@@ -32,10 +38,8 @@ function rollDie() {
 
 // Dice Rolls
 function getDiceRolls(numOfDice) {
-  var counter = 0;
-  while (counter < numOfDice) {
+  for (var counter = 0; counter < numOfDice; counter += 1) {
     diceRolls.push(rollDie());
-    counter += 1;
   }
 }
 
@@ -56,6 +60,7 @@ function generateCombi(input) {
   } else {
     playerCombi.push(Number(String(diceRolls[1]) + String(diceRolls[0])));
   }
+  scoreBoard[playerTurn] += playerCombi[playerTurn];
   var message = `Player ${playerNumber}, you chose Die ${input} first.<br>Your number is ${playerCombi[playerTurn]}.<br>`;
 
   // Check if everyone has played
@@ -67,10 +72,20 @@ function generateCombi(input) {
     message += `It is now Player ${playerNumber}'s turn.`;
     return message;
   } else {
-    message += endGame();
+    message += endRound();
     return message;
   }
 }
+
+// Generate scoreboard
+function generateScoreBoard() {
+  for (var counter = 0; counter < numOfPlayers; counter += 1) {
+    scoreBoard.push(0);
+  }
+}
+
+// Update scoreboard
+function updateScoreBoard() {}
 
 // Get max in array
 function arrayMax(array) {
@@ -86,14 +101,23 @@ function resetRound() {
 }
 
 // Game Ending
-function endGame() {
+function endRound() {
   var message = "All players have played, now for the results!<br>";
   for (player in playerCombi) {
     message += `Player ${Number(player) + 1} got ${playerCombi[player]}.<br>`;
   }
   var highestCombi = arrayMax(playerCombi);
   var winner = playerCombi.indexOf(highestCombi) + 1;
-  message += `The winner is Player ${winner}!`;
+  message += `The winner is Player ${winner}!<br>`;
+  message += `Here are the players' cumulative rolls.<br>`;
+  for (score in scoreBoard) {
+    message += `Player ${Number(score) + 1}: ${scoreBoard[score]}<br>`;
+  }
   resetRound();
   return message;
+}
+
+// Reset Game
+function resetGame() {
+  scoreBoard = [];
 }
