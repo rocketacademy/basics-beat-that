c081cb9d671eec120c950ad374609ff7a26d99bd
tanzheen <ntan418@gmail.com>
2021-07-24 23:19:51 +0800

    added variable number of dice and variable number of players and also leaderboard

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
 var gametype = "nil";
-var gamestage = "start1";
-var player1name = "username";
-var player2name = "username";
-var player1number = "";
-var player2number = "";
-var playerArray = "";
-var player1score = 0;
-var player2score = 0;
-var playcount = playcount + 1;
+var gamestage = "nil";
+var numberOfDice = 0;
+var numberOfPlayers = 0;
+var PlayerNames = [];
+var PlayerGeneratedNumbers = [];
+var PlayerScores = [];
+var playcount = 0;
+var numberOfPlayersPlayed = 0;
+var outputvalue = "please enter the number of players";
+// i need to put the player numbers into an array using a loop
 
 var main = function (input) {
-  if (gametype == "nil") {
+  if (gamestage == "nil") {
+    if (isNaN(input) || input == 0 || input == "") {
+      return "invalid input! " + outputvalue;
+    }
+    outputvalue = 'pls type "lowest" or "highest" to determine how the winner should be picked';
+    numberOfPlayers = `${input}`;
+    gamestage = "gametype";
+    console.log(gamestage);
+  } else if (gamestage == "gametype") {
     gametype = `${input}`;
-    console.log(`gametype = ${input}`);
+    if (gametype != "lowest" && gametype != "highest") {
+      return "invalid input! " + outputvalue;
+    }
+
+    gamestage = "start1";
+    console.log(gametype);
     outputvalue = "pls enter username for player 1";
   }
 
   // start of the game
   else if (gamestage == "start1") {
-    player1name = `${input}`;
-    gamestage = "start2";
-    outputvalue = "pls enter username for player 2";
-  } else if (gamestage == "start2") {
-    player2name = `${input}`;
-    gamestage = "turn1";
-    outputvalue = `${player1name}, pls click submit to generate your number`;
-
-    //player 1 will generate his numbers and arrange the order
-  } else if (gamestage == "turn1") {
-    playerArray = playerNumber();
-    console.log(playerArray);
-    if (gametype == "lowest") {
-      var numberGenerated = chooseOrderOfNumberAutoLow();
-    } else if (gametype == "highest") {
-      var numberGenerated = chooseOrderOfNumberAutoHigh();
+    numberOfPlayersPlayed = numberOfPlayersPlayed + 1;
+    PlayerNames.push(input);
+    PlayerScores.push(0);
+    console.log(`player names = ${PlayerNames}`);
+    console.log(`Player scores = ${PlayerScores}`);
+    if (numberOfPlayers > numberOfPlayersPlayed) {
+      outputvalue = `pls enter username for player ${numberOfPlayersPlayed + 1}`;
+    } else {
+      numberOfPlayersPlayed = 0;
+      gamestage = "enternumberofdice";
+      outputvalue = "pls input the number of dice to be played in the game";
     }
-
-    player1number = numberGenerated;
-    outputvalue = player1number + ". it is now the next player's turn! click 'submit' to continue";
-    gamestage = "welcomeplayer2";
-  } else if (gamestage == "welcomeplayer2") {
-    gamestage = "turn2";
-
-    outputvalue = `${player2name}, pls click submit to generate your number`;
-    // player 2 generate numbers and choose the order of  the numbers
-  } else if (gamestage == "turn2") {
-    playerArray = playerNumber();
-    console.log(playerArray);
-    if (gametype == "lowest") {
-      var numberGenerated = chooseOrderOfNumberAutoLow();
-    } else if (gametype == "highest") {
-      var numberGenerated = chooseOrderOfNumberAutoHigh();
+  } else if (gamestage == "enternumberofdice") {
+    if (isNaN(input) || input == 0 || input == "") {
+      return "invalid input! " + outputvalue;
     }
-    player2number = numberGenerated;
-    outputvalue = player2number + '. click "submit" to compare the 2 numbers';
+    numberOfDice = `${input}`;
+    gamestage = "turns";
+    outputvalue = `${PlayerNames[0]}, pls click 'submit' to generate your number`;
+  }
 
-    gamestage = "comparison";
-    // compare the 2 numbers generated and find the winner
+  //players will generate their number
+  else if (gamestage == "turns") {
+    numberOfPlayersPlayed = numberOfPlayersPlayed + 1;
+    var number = playerNumber();
+    PlayerGeneratedNumbers.push(number);
+    console.log(PlayerGeneratedNumbers);
+    if (numberOfPlayers > numberOfPlayersPlayed) {
+      outputvalue = number + `, it is now ${PlayerNames[numberOfPlayersPlayed]}'s turn to generate his/her number. Pls click "submit" to continue`;
+    } else {
+      gamestage = "comparison";
+      outputvalue = number + ", pls click submit to compare the numbers";
+    }
+    // compare the  numbers generated and find the winner
   } else if (gamestage == "comparison") {
     playcount = playcount + 1;
     if (gametype == "highest") {
-      var whoWins = CompareTheNumbersHigh(player1number, player2number);
+      var whoWins = CompareTheNumbersHigh();
     } else if (gametype == "lowest") {
-      var whoWins = CompareTheNumbersLow(player1number, player2number);
+      var whoWins = CompareTheNumbersLow();
     }
-
-    var leaderboard = makeLeaderboard(player1score, player2score);
-    outputvalue = whoWins + `${player1name}'s number is ${player1number} while ${player2name}'s number is ${player2number}.` + leaderboard + `<br>click submit to play again starting from ${player1name}'s turn`;
-    gamestage = "turn1";
+    leaderboard = makeLeaderboard();
+    outputvalue = whoWins + leaderboard + `<br>click "submit" to start another round beginning from ${PlayerNames[0]}`;
+    gamestage = "turns";
+    numberOfPlayersPlayed = 0;
+    PlayerGeneratedNumbers = [];
   }
 
   return outputvalue;
@@ -79,58 +89,43 @@ var diceRoll = function () {
   return integer;
 };
 var playerNumber = function () {
-  var number1 = `${diceRoll()}`;
-  var number2 = `${diceRoll()}`;
-  var numArray = [number1, number2];
-  return numArray;
-};
-var chooseOrderOfNumberAutoLow = function () {
-  if (playerArray[0] < playerArray[1]) {
-    numberGenerated = playerArray[0] + playerArray[1];
-  } else if (playerArray[0] > playerArray[1]) {
-    numberGenerated = playerArray[1] + playerArray[0];
+  var numArray = [];
+  var numberOfDiceRolled = 0;
+  while (numberOfDice > numberOfDiceRolled) {
+    var diceRolled = diceRoll();
+    numArray.push(diceRolled);
+    numberOfDiceRolled = numberOfDiceRolled + 1;
   }
+  var outputarray = numArray.sort();
+  if (gametype == "highest") {
+    outputarray = outputarray.reverse();
+  }
+  numberGenerated = outputarray.join("");
   return numberGenerated;
 };
-var chooseOrderOfNumberAutoHigh = function () {
-  if (playerArray[0] > playerArray[1]) {
-    numberGenerated = playerArray[0] + playerArray[1];
-  } else if (playerArray[0] < playerArray[1]) {
-    numberGenerated = playerArray[1] + playerArray[0];
-  }
-  return numberGenerated;
+
+var CompareTheNumbersHigh = function () {
+  var highestNumber = `${Math.max.apply(null, PlayerGeneratedNumbers)}`;
+  var numericalPositionOfthatNumber = Number(PlayerGeneratedNumbers.indexOf(highestNumber));
+  console.log(`number position = ${numericalPositionOfthatNumber}`);
+  var playerWithThatNumber = PlayerNames[numericalPositionOfthatNumber];
+  PlayerScores[numericalPositionOfthatNumber] = Number(PlayerScores[numericalPositionOfthatNumber]) + 1;
+  return playerWithThatNumber + " is the winner!";
 };
-var CompareTheNumbersHigh = function (player1number, player2number) {
-  if (player1number > player2number) {
-    player1score = player1score + 1;
-    return `${player1name} is the winner! `;
-  }
-  if (player1number < player2number) {
-    player2score = player2score + 1;
-    return `${player2name} is the winner! `;
-  }
-  if (player1number == player2number) {
-    return "It is a draw! ";
-  }
+var CompareTheNumbersLow = function () {
+  var lowestNumber = `${Math.min.apply(null, PlayerGeneratedNumbers)}`;
+  var numericalPositionOfthatNumber = Number(PlayerGeneratedNumbers.indexOf(lowestNumber));
+  console.log(`number position = ${numericalPositionOfthatNumber}`);
+  var playerWithThatNumber = PlayerNames[numericalPositionOfthatNumber];
+  PlayerScores[numericalPositionOfthatNumber] = Number(PlayerScores[numericalPositionOfthatNumber]) + 1;
+  return playerWithThatNumber + " is the winner!";
 };
-var CompareTheNumbersLow = function (player1number, player2number) {
-  if (player1number < player2number) {
-    player1score = player1score + 1;
-    return `${player1name} is the winner! `;
+var makeLeaderboard = function () {
+  var counter = 0;
+  var leaderboard = "";
+  while (numberOfPlayers > counter) {
+    leaderboard = leaderboard + `<br>${PlayerNames[counter]}'score = ${PlayerScores[counter]}`;
+    counter = counter + 1;
   }
-  if (player1number > player2number) {
-    player2score = player2score + 1;
-    return `${player2name} is the winner! `;
-  }
-  if (player1number == player2number) {
-    return "It is a draw! ";
-  }
-};
-var makeLeaderboard = function (player1score, player2score) {
-  if (player1score > player2score) {
-    var tableDisplay = `<br> ${player1name}'s score = ${player1score}<br>${player2name}' score = ${player2score}`;
-  } else {
-    var tableDisplay = `<br> ${player2name}'s score = ${player2score}<br>${player1name}' score = ${player1score}`;
-  }
-  return tableDisplay;
+  return leaderboard;
 };
