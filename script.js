10fb20ad954c684db6a883c25028b4b49a1f294a
annaleongmy <annaleongmy@gmail.com>
2021-07-25 20:15:33 +0800

    allowed for variable number of dice with dice roll arrays and auto-sorting of array to auto-generate number

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var RESULTS_MODE_LOWEST = `lowest`
 var currentPlayer = PLAYER_1;
 
 //keep track of dice rolls
-var dice1 = ``;
-var dice2 = ``;
+var player1DiceRolls = [];
+var player2DiceRolls = [];
 
 //keep track of list of numbers for each player
 var player1Numbers = [];
@@ -24,6 +24,12 @@ var player2RunningScore = 0;
 //keep track of current results mode - start with default mode
 var currentResultsMode = RESULTS_MODE_DEFAULT;
 
+//keep track of start of round
+var isStartOfRound = true;
+
+//keep track of number of dice to roll
+var numDiceToRoll = 0;
+
 //generates random dice number
 var rollDice = function () {
   var randomDecimal = Math.random() * 6;
@@ -34,14 +40,36 @@ var rollDice = function () {
 
 //DICE ROLL GAME MODE LOGIC
 //rolls two dice and changes mode to `choose dice`
-var generateTwoDiceRolls = function(){
-  dice1 = rollDice().toString();
-  dice2 = rollDice().toString();
-  // currentMode = GAME_MODE_CHOOSE_DICE_ORDER;
-  return `Welcome ${currentPlayer}.<br>
-  You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br>`
-  // Choose the order of the dice.
+// var generateTwoDiceRolls = function(){
+//   dice1 = rollDice().toString();
+//   dice2 = rollDice().toString();
+//   return `Welcome ${currentPlayer}.<br>
+//   You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br>`
+// };
+
+//returns current player's dice rolls array based on current player
+var currentPlayerDiceRolls = function(currentPlayer){
+  if (currentPlayer == PLAYER_1){
+    return player1DiceRolls;
+  }else{
+    if(currentPlayer == PLAYER_2){
+      return player2DiceRolls;
+    };
+  };
 };
+
+//rolls n number of dice and pushes into player's dice roll array
+//returns player's dice roll array
+var generateDiceRolls = function(numDiceToRoll){
+  var counter = 0;
+  while(counter < numDiceToRoll){
+    var diceRoll = rollDice().toString(); 
+    currentPlayerDiceRolls(currentPlayer).push(diceRoll);
+    counter += 1;
+  };
+  return `${currentPlayer} dice rolls: ${currentPlayerDiceRolls(currentPlayer)}`;
+};
+
 
 //returns current player's number array based on current player
 var currentPlayerNumbers = function(currentPlayer){
@@ -54,34 +82,50 @@ var currentPlayerNumbers = function(currentPlayer){
   };
 };
 
+// //Auto-generate highest combined number from dice rolls
+// var autoGenerateHighestNum = function(){
+//   var highestNumber = 0;
+//   if(dice1 >= dice2){
+//     highestNumber = dice1 + dice2;
+//     currentPlayerNumbers(currentPlayer).push(highestNumber);
+//   }else{
+//     if(dice2 > dice1){
+//       highestNumber = dice2 + dice1;
+//       currentPlayerNumbers(currentPlayer).push(highestNumber);
+//     };
+//   }
+//   return `${currentPlayer} highest number is ${highestNumber}.`;
+// };
+
 //Auto-generate highest combined number from dice rolls
 var autoGenerateHighestNum = function(){
-  var highestNumber = 0;
-  if(dice1 >= dice2){
-    highestNumber = dice1 + dice2;
-    currentPlayerNumbers(currentPlayer).push(highestNumber);
-  }else{
-    if(dice2 > dice1){
-      highestNumber = dice2 + dice1;
-      currentPlayerNumbers(currentPlayer).push(highestNumber);
-    };
-  }
-  return `${currentPlayer} highest number is ${highestNumber}.`;
+  var sortDiceRollsDescending = currentPlayerDiceRolls(currentPlayer).sort((a,b)=>b-a);
+  var highestNumber = Number(sortDiceRollsDescending.join(``));
+  currentPlayerNumbers(currentPlayer).push(highestNumber);
+  return `${currentPlayer} highest number is ${highestNumber}.`
 };
+
+// //Auto-generate lowest combined number from dice rolls
+// var autoGenerateLowestNum = function(){
+//   var lowestNumber = 0;
+//   if(dice1 >= dice2){
+//     lowestNumber = dice1 + dice2;
+//     currentPlayerNumbers(currentPlayer).push(lowestNumber);
+//   }else{
+//     if(dice2 > dice1){
+//       lowestNumber = dice2 + dice1;
+//       currentPlayerNumbers(currentPlayer).push(lowestNumber);
+//     };
+//   };
+//   return `${currentPlayer} lowest number is ${lowestNumber}.`;
+// };
 
 //Auto-generate lowest combined number from dice rolls
 var autoGenerateLowestNum = function(){
-  var lowestNumber = 0;
-  if(dice1 >= dice2){
-    lowestNumber = dice1 + dice2;
-    currentPlayerNumbers(currentPlayer).push(lowestNumber);
-  }else{
-    if(dice2 > dice1){
-      lowestNumber = dice2 + dice1;
-      currentPlayerNumbers(currentPlayer).push(lowestNumber);
-    };
-  };
-  return `${currentPlayer} lowest number is ${lowestNumber}.`;
+  var sortDiceRollsAscending = currentPlayerDiceRolls(currentPlayer).sort((a,b)=>a-b);
+  var lowestNumber = Number(sortDiceRollsAscending.join(``));
+  currentPlayerNumbers(currentPlayer).push(lowestNumber);
+  return `${currentPlayer} lowest number is ${lowestNumber}.`
 };
 
 //GAME RESULTS
@@ -233,28 +277,50 @@ var main = function (input) {
       currentResultsMode = RESULTS_MODE_LOWEST;
         return`You have changed game mode to ${currentResultsMode}.`;
     }else{
-    var resultsModeMessage = `Current result mode: ${currentResultsMode}`;
-    var chosenNumMessage = generateTwoDiceRolls() + currentAutoGeneratedNum(currentResultsMode);
-    var leaderboard = currentLeaderboard(currentResultsMode);
-    //Player 1 turn
-    if(currentPlayer == PLAYER_1){
-      myOutputValue = `${resultsModeMessage}<br><br>
-      ${chosenNumMessage}<br>
-      It is now Player 2's turn.<br><br>
-      ${leaderboard}`;
-      currentPlayer = PLAYER_2;
-    }else{
-      //Player 2 turn
-      if(currentPlayer == PLAYER_2){
-        var gameResults = currentGameResult(currentResultsMode);
-        myOutputValue = `${resultsModeMessage}<br><br>
-        ${chosenNumMessage}<br>
-        ${gameResults}<br><br>
-        ${leaderboard}`;
-        currentPlayer = PLAYER_1;
-      };
-    };
-   };     
-  };  
-  return myOutputValue;
+      if(isStartOfRound == true){
+        //input validation for number of dice to roll
+        if (
+          Number.isNaN(Number(input)) ||
+          input == ``
+          ){
+          return 'Please enter the number of dice you want to roll.';
+          }else{
+            //choose number of dice to roll this round
+            numDiceToRoll = input;
+            isStartOfRound = false;
+            return `You have chosen to roll ${numDiceToRoll} dice this round.`
+          };
+        };
+      }
+      if(isStartOfRound == false){
+          var resultsModeMessage = `Current result mode: ${currentResultsMode}`;
+          var chosenNumMessage = generateDiceRolls(numDiceToRoll) +`<br>`+ currentAutoGeneratedNum(currentResultsMode);
+          var leaderboard = currentLeaderboard(currentResultsMode);
+          //Player 1 turn
+          if(currentPlayer == PLAYER_1){
+            myOutputValue = `${resultsModeMessage}<br><br>
+            ${chosenNumMessage}<br>
+            It is now Player 2's turn.<br><br>
+            ${leaderboard}`;
+            currentPlayer = PLAYER_2;
+          }else{
+            //Player 2 turn
+            if(currentPlayer == PLAYER_2){
+              var gameResults = currentGameResult(currentResultsMode);
+              myOutputValue = `${resultsModeMessage}<br><br>
+              ${chosenNumMessage}<br>
+              ${gameResults}<br><br>
+              ${leaderboard}`;
+              currentPlayer = PLAYER_1;
+
+              //end of round so clear dice roll variables
+              isStartOfRound = true;
+              numDiceToRoll = 0;
+              player1DiceRolls = [];
+              player2DiceRolls = [];
+            };
+      };   
+    };    
+  };
+return myOutputValue;
 };
