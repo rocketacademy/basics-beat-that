8a0c4bc723cd29d68a790d46d6cecfd9ca0e7021
quahzhengjie <quahzhengjie@gmail.com>
2021-07-31 16:50:50 +0800

    added git push instructions

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
+//git add (script.js)
+//git commit -m (comment of what changes you added)
+//add the new changes to github, git push origin master
+//$ git push https://github.com/quahzhengjie/basics-beat-that
