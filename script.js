afd795670bfcb901077d61e00ba62f1e1a47423e
diyanarmln <diyana.rmln@gmail.com>
2021-07-31 11:44:44 +0800

    variable number of dice

--- script.js
+++ script.js
@@ -38,18 +38,6 @@ var userAutoRoll = function (input) {
   }
 };
 
-// // function to concantate if user input value of order of dice -- VALID
-// var concatenateMain = function (input) {
-//   // convert input value to number data type
-//   var orderSeq = Number(input);
-//   // pull diceRoll array index as specified in input/order selected (minus one as array index starts at zero)
-//   var firstDigit1 = playerdiceRoll.splice(orderSeq - 1, 1);
-//   // assign remaining value in array as the subsequent digit
-//   var secondDigit1 = playerdiceRoll.pop();
-//   // assign and store the final number sequence for player 1
-//   return Number(`${firstDigit1}${secondDigit1}`);
-// };
-
 // function to auto generate combined value -- VALID
 var autoGenHigh = function (playerdiceRoll) {
   var combinedValue = 0;
@@ -74,41 +62,6 @@ var autoGenHigh = function (playerdiceRoll) {
     index += 1;
   }
   playerdiceRoll.splice(0, playerdiceRoll.length);
-  // set combinedValue variable as number
-  // var combinedValue = 0;
-  // var largest = 0;
-  // var index = 0;
-  // // run loop to compare array and stop once its pulled largest number
-
-  // while (playerdiceRoll.length > 0) {
-  //   while (index < numDice) {
-  //     if (playerdiceRoll[index] > largest) {
-  //       largest = playerdiceRoll[index];
-  //     }
-  //     index += 1;
-  //   }
-  //   // remove the value from the array that has been assigned to the first digit
-  //   var indexLargest = playerdiceRoll.indexOf(largest);
-  //   combinedValue = `${combinedValue}${playerdiceRoll.splice(indexLargest, 1)}`;
-  //   largest = 0;
-  //   index = 0;
-  // }
-  // combinedValue = `${combinedValue}${playerdiceRoll.pop}`;
-  // console.log(playerdiceRoll);
-  // console.log(largest);
-  // console.log(playerdiceRoll);
-  // console.log(indexLargest);
-
-  //   // return and assign the last element in array to the second digit
-  //   var secondDigit = playerdiceRoll.pop();
-  //   // arrange combined value according to highest element first in regular game mode
-  //   if (currentGameMode == "regular") {
-  //     combinedValue = `${firstDigit}${secondDigit}`;
-  //   }
-  //   // arrange combined value according to lowest element first in reverse game mode
-  //   if (currentGameMode == "reverse") {
-  //     combinedValue = `${secondDigit}${firstDigit}`;
-  //   }
 
   return combinedValue;
 };
@@ -118,6 +71,7 @@ var concatenatePlayer1 = function (input) {
   var messageCon1 = "";
   if (input == "") {
     player1 = Number(autoGenHigh(playerdiceRoll));
+    player1score += Number(player1);
     messageCon1 = `Player 1, your number is ${player1}.<br><br>It is now Player 2's turn.<br>Click 'Submit' to start.`;
     // } else if (input == 1 || input == 2) {
     //   player1 = concatenateMain(input);
@@ -129,10 +83,8 @@ var concatenatePlayer1 = function (input) {
   } else if (input == "regular") {
     currentGameMode = input;
     resetGame();
-    messageCon1 = `Regular gameplay initiated.<br>Win  by having the highest dice number.<br><br>Click Submit to start game.`;
-  } else
-    messageCon1 = `Please enter either 1 or 2 in input box. Or click Submit to autogenerate combined value`;
-  player1score += player1;
+    messageCon1 = `Regular gameplay initiated.<br>Win  by having the highest dice number.<br><br>Enter the number of dice you wish to roll for this round.`;
+  } else messageCon1 = `Please click Submit to autogenerate combined value`;
   // set return message
   return messageCon1;
 };
@@ -143,16 +95,13 @@ var concatenatePlayer2 = function (input) {
   var messageCon2 = "";
   if (input == "") {
     player2 = Number(autoGenHigh(playerdiceRoll));
+    player2score += Number(player2);
     if (currentGameMode == "regular") {
       messageCon2 = `${getWinMessage()}`;
     }
-    messageCon2 = `${getWinMessageReverse()}`;
-    // } else if (input == 1 || input == 2) {
-    //   player2 = concatenateMain(input);
-    //   if (currentGameMode == "regular") {
-    //     messageCon2 = `${getWinMessage()}`;
-    //   }
-    //   messageCon2 = `${getWinMessageReverse()}`;
+    if (currentGameMode == "reverse") {
+      messageCon2 = `${getWinMessageReverse()}`;
+    }
   } else if (input == "reverse") {
     currentGameMode = input;
     resetGame();
@@ -160,11 +109,8 @@ var concatenatePlayer2 = function (input) {
   } else if (input == "regular") {
     currentGameMode = input;
     resetGame();
-    messageCon2 = `Regular gameplay initiated.<br>Win  by having the highest dice number.<br><br>Click Submit to start game.`;
-  } else
-    messageCon2 = `Please enter either 1 or 2 in input box. Or click Submit to autogenerate combined value`;
-
-  player2score += player2;
+    messageCon2 = `Regular gameplay initiated.<br>Win  by having the highest dice number.<br><br>Enter the number of dice you wish to roll for this round.`;
+  } else messageCon2 = `Please click Submit to autogenerate combined value`;
   return messageCon2;
 };
 console.log(concatenatePlayer2);
@@ -185,19 +131,22 @@ var getLeaderboardReverse = function () {
 
 // compare the 2 numbers and decide who won -- VALID
 var getWinMessage = function () {
+  console.log(player1);
+  console.log(player2);
   // if player 1 number was higher than player 2
   if (player1 > player2) {
     // winning message for player 1
     return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    Congratulations Player 1, you won!<br><br>${getLeaderboard()}`;
+    Congratulations Player 1, you won!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
   }
   if (player1 == player2) {
     return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    It's a tie for this round!<br><br>${getLeaderboard()}`;
+    It's a tie for this round!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
   }
   // winning message for player 2
-  return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    Congratulations Player 2, you won!<br><br>${getLeaderboard()}`;
+  else
+    return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
+    Congratulations Player 2, you won!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
 };
 
 // compare the 2 numbers and decide who won for reverse game mode -- VALID
@@ -206,15 +155,15 @@ var getWinMessageReverse = function () {
   if (player1 < player2) {
     // winning message for player 1
     return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    Congratulations Player 1, you won!<br><br>${getLeaderboard()}`;
+    Congratulations Player 1, you won!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
   }
   if (player1 == player2) {
     return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    It's a tie for this round!<br><br>${getLeaderboard()}`;
+    It's a tie for this round!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
   }
   // winning message for player 2
   return `Player 1's number was ${player1}.<br>Player 2's number was ${player2}.<br>
-    Congratulations Player 2, you won!<br><br>${getLeaderboard()}`;
+    Congratulations Player 2, you won!<br><br>${getLeaderboard()}<br><br>Now enter the number of dice you wish to roll to begin the next round.`;
 };
 
 // main function -- VALID
@@ -239,7 +188,7 @@ var inputVal = function (input) {
   if (input == "regular" || input == "") {
     currentGameMode = "regular";
     resetGame();
-    messageInputVal = `Regular gameplay initiated.<br>Win  by having the higher dice number.<br><br>Click Submit to start game.`;
+    messageInputVal = `Regular gameplay initiated.<br>Win  by having the higher dice number.<br><br>Enter the number of dice you wish to roll for this round.`;
   } else if (input == "reverse") {
     currentGameMode = "reverse";
     resetGame();
@@ -249,13 +198,29 @@ var inputVal = function (input) {
   return messageInputVal;
 };
 
+var waitingNumDice = function (input) {
+  if (isNaN(input) || input == "") {
+    return `Please input a number to indicate number of dice you wish to play for this round.`;
+  }
+  currentGameFunction = "player 1 diceroll";
+  numDice = Number(input);
+  return `You have chosen to play ${input} number of dices.
+  Click on SUBMIT to begin game.`;
+};
+
 // game function for regular game mode
 var gamePlay = function (input) {
+  console.log(currentGameFunction);
   if (input == "reverse") {
     currentGameMode = "reverse";
     resetGame();
     return `Reverse gameplay initiated.<br>Win  by having the lowest dice number.<br><br>Click Submit to start game.`;
   }
+
+  if (currentGameFunction == "new round") {
+    return waitingNumDice(input);
+  }
+
   if (currentGameFunction == "player 1 diceroll") {
     currentGameFunction = "player 1 waiting for input on order of dice";
     playerName = "Player 1";
@@ -274,7 +239,7 @@ var gamePlay = function (input) {
   }
 
   if (currentGameFunction == "player 2 waiting for input on order of dice") {
-    currentGameFunction = "player 1 diceroll";
+    currentGameFunction = "new round";
     return `${concatenatePlayer2(input)}`;
   }
 };
@@ -285,8 +250,13 @@ var gamePlayReverse = function (input) {
   if (input == "regular") {
     currentGameMode = "regular";
     resetGame();
-    myOutputValue = `Regular gameplay initiated.<br>Win  by having the higher dice number.<br><br>Click Submit to start game.`;
+    myOutputValue = `Regular gameplay initiated.<br>Win  by having the higher dice number.<br><br>Enter the number of dice you wish to roll for this round`;
   }
+
+  if (currentGameFunction == "new round") {
+    return waitingNumDice(input);
+  }
+
   if (currentGameFunction == "player 1 diceroll") {
     currentGameFunction = "player 1 waiting for input on order of dice";
     playerName = "Player 1";
@@ -308,30 +278,11 @@ var gamePlayReverse = function (input) {
   console.log(playerdiceRoll);
 
   if (currentGameFunction == "player 2 waiting for input on order of dice") {
-    currentGameFunction = "player 1 diceroll";
+    currentGameFunction = "new round";
     return `${concatenatePlayer2(input)}`;
   }
   console.log(playerdiceRoll);
 };
-
-// // only allow switching of game modes during game function player 2 waiting for input on order of dice
-// var checkInput = function () {
-//   if (
-//     (currentGameMode == "regular" || currentGameMode == "reverse") &&
-//     currentGameFunction == "player 1 diceroll"
-//   ) {
-//     if (input == "regular") {
-//       currentGameMode = "regular";
-//       return `Regular gameplay initiated.<br>Win  by having the highest dice number.<br><br>Click Submit to start game.`;
-//     }
-//     if (input == "reverse") {
-//       currentGameMode = "reverse";
-//       return `Reverse gameplay initiated.<br>Win  by having the lowest dice number.<br><br>Click Submit to start game.`;
-//     }
-//   } else {
-//     return `Please enter dice order or click Submit to continue game play. You can only switch game modes after each round.`;
-//   }
-// };
 
 // function to reset variables when new game mode is initialised
 var resetGame = function () {
@@ -341,7 +292,7 @@ var resetGame = function () {
   player2score = 0;
   player1Name = "";
   player2Name = "";
-  currentGameFunction = "player 1 diceroll";
+  currentGameFunction = "new round";
 };
 
 // all the global variables created to store values
@@ -355,41 +306,6 @@ var dice1 = 0;
 var dice2 = 0;
 var playerdiceRoll = [];
 var numOfDice = 0;
-var currentGameFunction = "player 1 diceroll";
+var currentGameFunction = "new round";
 var currentGameMode = "waiting for game mode";
 var playerName = "";
-
-// [
-//   "waiting for player 1 username",
-//   "waiting for player 2 username",
-//   "waiting for number of dice to play",
-//   "diceroll",
-//   "waiting for input on order of dice",
-// ];
-
-// function for random dice
-// global variable for 2 dices
-// global variable for 2 player modes
-//default mode is player 1
-// after number is finalised, mode changes to player 2
-// dice roll is auto upon submission
-// input is for player to decide which dice to put in front
-// convert input to number
-// concantate 2 dice rolls together
-// allow user to input which dice goes first
-// store final concantated dice number
-// compare the 2 stored dice numbers between each modes
-// output message on who won.
-
-// global variable for score
-// add concantate values to each player score
-
-// output leaderboard for each message for both players in decreasing order
-
-// add new game mode for lowest combined number
-
-// add function to auto generate combined number
-
-// create variable for an array for each round's score
-
-// output the array in descending order every output
