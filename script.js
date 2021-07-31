e97ea1168f8b9186ade0c5db22bfdb2df9870ecb
waterworld1 <hongxi_lim@yahoo.com.sg>
2021-07-31 13:42:46 +0800

    First Draft

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+//Dice Roll function
+var diceRoll = function () {
+  return Math.ceil(Math.random() * 6);
+};
+
+//Player roll 2 dices
+var playerRoll = [diceRoll(), diceRoll()];
+
+//Player 1 roll
+
+//Player 2 roll
+
+//Give 2 choices
+
+//Combine 2 numbers
+
+//Keep score
+
+//Check winner
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
