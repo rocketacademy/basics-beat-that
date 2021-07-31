5c47329f5f6ccb2fa324db3cb3bcefac57871382
jaihiu <jiahui97@yahoo.com>
2021-07-31 20:51:26 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// Game modes
+var gameModeDiceRoll = "game mode dice roll";
+var gameModeChooseDice = "game mode choose dice";
+
+var gameMode = gameModeDiceRoll;
+
+// generate dice number from 1-6
+var diceRoll = function () {
+  var randomDiceRoll = Math.ceil(Math.random() * 6);
+  return randomDiceRoll;
+};
+
+// player to get random number
+var playerOne = [diceRoll(), diceRoll()];
+
+var playerTwo = [diceRoll(), diceRoll()];
+
+// Game mode 2
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
