3746855d58320bc04f86fff243b7c2b43d5c4961
solihinf <solihinf@hotmail.com>
2021-08-02 21:05:56 +0700

    Add combine function and choose winner function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 2 Players take turns
 // Keep track variables for player 1
-var diceVal1 = [];
+var diceVal1 = 0;
 
 // Keep track variables for player 2
 var diceVal2 = [];
@@ -24,7 +24,7 @@ var roll2Dice = function (){
       while (counter <2) {
       var twoDiceRolls = rollDice ();
       diceValue.push (Number(twoDiceRolls));
-      console.log(diceValue);
+      //console.log(diceValue);
 
       counter = counter + 1;
       }
@@ -32,44 +32,116 @@ var roll2Dice = function (){
       
     };
 
-  // joinValue = function (){
-  //   playerOneValueString = diceValueOne.join ('');
-  //   //   var playerOneValueNum = Number (diceValueOne.join (''));
-  //   //   return playerOneValueNum;
-  // }
-var main = function (input){
-  var myOutputValue = "";
-  console.log (gameMode);
-  if (currentPlayer == 1){
-    if (gameMode == 'DICE ROLL'){
-      roll2Dice()
-  
-      player1Score = diceValue[0] + diceValue[1];
-      gameMode = 'CHOOSE ORDER';
-
-      return 'Welcome Player '+ currentPlayer + ' ! <br> You rolled:<br> Dice 1 = ' + diceValue[0] + '<br> Dice 2 = ' + diceValue[1] + '<br> Choose the order of the dice please!. <br> Choose: <br> 1 - for Dice 1 <br> 2 - for Dice 2. Player one score ' + player1Score;
-    }
-    
-    else if (gameMode == 'CHOOSE ORDER'){
-      
-      console.log (gameMode);
-      myOutputValue = combineVal(input);
-
-    }
-    
-    return myOutputValue;
-  }
-};
- //if gamemode = dice roll => run the rollTwoDice function
+ //Function to concetenate number from the dice rolls after the player choose order
 
 var combineVal = function (input){
   var firstIndex = (input - 1);
   var firstDigit = (diceValue[firstIndex]);
   diceValue.splice(firstIndex,1);
   var secondDigit = (diceValue.pop());
-  return Number(`${firstDigit}${secondDigit}`);
+  return Number (`${firstDigit}${secondDigit}`);
+  };
+ 
+// Function to decide the winner
 
+var decideWinner = function (){
+  if (diceVal1 > diceVal2){
+  return 1;
+}
+  return 2;
+};
+
+
+var main = function (input){
+var myOutputValue = "";
+ console.log (gameMode);
+  if (gameMode == 'DICE ROLL'){
+    roll2Dice()
+   
+    gameMode = 'CHOOSE ORDER';
+    counter = 0;
+    return 'Welcome Player '+ currentPlayer + ' ! <br> You rolled:<br> Dice 1 = ' + diceValue[0] + '<br> Dice 2 = ' + diceValue[1] + '<br> Choose the order of the dice please!. <br> Choose: <br> 1 - for Dice 1 <br> 2 - for Dice 2. ';
+  
+  } if (gameMode == 'CHOOSE ORDER' ){
+        if (currentPlayer == 1) {    
+        
+        diceVal1 = combineVal(input);
+        console.log ('dice value');
+        console.log (diceVal1);
+        gameMode = 'DICE ROLL';
+        currentPlayer = 2;
+        
+
+        myOutputValue = 'Player 1, your dice value is ' + diceVal1 + '<br> Now, it is player 2 turn. Please press the submit button.';
+
+      }  else { 
+      diceVal2 = combineVal(input);
+      
+      gameMode = 'CHOOSE WINNER';
+      
+    
+      myOutputValue = 'Player 2, your dice Value is '+ diceVal2;
+      }
+  if (gameMode == 'CHOOSE WINNER'){
+      myOutputValue = 'Player ' + decideWinner() + ' won!'
+      gameMode = 'DICE ROLL'
+      currentPlayer = 1;
   }
+
+
+
+   return myOutputValue;
+}
+};
+  
+  
+  
+  
+  // currentPlayer == 1){
+  //   combineVal ();
+  //   diceVal1 = playerOutput;
+  //   myOutputValue = diceVal1;
+  
+  // } else (gameMode = 'CHOOSE ORDER' && currentPlayer == 2){
+
+  // }
+
+
+
+
+
+
+//   console.log (gameMode);
+//   //if (currentPlayer == 1 || currentPlayer == 2){
+//      if (gameMode == 'DICE ROLL'){
+//       roll2Dice()
+//       console.log ('dice');
+//       console.log (roll2Dice);
+  
+//       //player1Score = diceValue[0] + diceValue;
+//       gameMode = 'CHOOSE ORDER';
+
+//       
+//     }
+    
+//     else if (gameMode == 'CHOOSE ORDER' && currentPlayer == 1){
+      
+//       console.log (gameMode);
+//       var playerDiceNum = combineVal (input);
+
+//       myOutputValue = 'Player ' + currentPlayer + ' dice value is '+ playerDiceNum;
+//       diceVal1 = playerDiceNum;
+//       currentPlayer = 2;
+//       gameMode = 'DICE ROLL';
+//       console.log (gameMode);
+
+
+//     }
+    
+//     return myOutputValue;
+//   //}
+// };
+
 
 
 
