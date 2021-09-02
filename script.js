e700593588ac5c6eafe52a0feef4fb2dbece6325
wzjustuslim <weizhengjustus.lim@gmail.com>
2021-09-02 18:15:29 +0800

    create beat-that

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+var currentPlayer = 1;
+var gameMode = "order";
+
+var main = function (input) {};
+
+// select order function
+var selectOrder = function (input, diceOne, diceTwo) {
+  if (input == "one") {
+    var str = String(diceOne) + String(diceTwo);
+    return str;
+  } else if (input == "two") {
+    var str = String(diceTwo) + String(diceOne);
+    return str;
+  } else {
+    return "invalid input";
+  }
 };
+
+// dice function
+var rollDice = function () {
+  randomDecimal = Math.random() * 6;
+  randomInteger = Math.ceil(randomDecimal);
+  return randomInteger;
+};
+
+// player turn function
+var selectPlayer = function () {
+  if (currentPlayer == 1) {
+    return (currentPlayer = 2);
+  } else {
+    return (currentPlayer = 1);
+  }
+};
+
+/*
+
+After both players have rolled and chosen dice order, the player with the higher combined number wins.
+*/
