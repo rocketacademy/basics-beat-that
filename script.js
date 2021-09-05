be1d0ad1dd6f1daf0539025baf7997bc15a54e5d
thereseLYR <thereselau1902@gmail.com>
2021-09-05 23:11:25 +0800

    added input validation

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ var currentScore = 0;
 var scoreArray = [[], []];
 // ^this is meant to be a nested array - index 0 corresponds to player 1's scores, etc.
 
+var validateInput = function (input) {
+  var validCommands = ["start", "Roll", "dice 1", "dice 2"];
+  if (validCommands.includes(input)) {
+    return true;
+  }
+  console.log("Invalid Input.");
+  return false;
+};
+
 var rollDice = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
@@ -12,7 +21,7 @@ var rollDice = function () {
 var playerTurnRoll = function (playerNumber) {
   currentPlayerArray[0] = rollDice();
   currentPlayerArray[1] = rollDice();
-  // console.log(`Rolled ${currentPlayerArray[0]} and ${currentPlayerArray[1]}`);
+  console.log(`Rolled ${currentPlayerArray[0]} and ${currentPlayerArray[1]}`);
   var returnstring =
     `It is now Player ${playerNumber}'s turn.` +
     "<br><br>" +
@@ -40,37 +49,40 @@ var playerTurnSelection = function (input) {
     `${activePlayer}, you chose ${input} to be the first numeral.` +
     "<br><br>" +
     `The number you generated is: ${score}`;
-  // console.log(`Score Generated: ${score}`);
+  console.log(`Score Generated: ${score}`);
   return returnstring;
 };
 
 var writePlayerScore = function () {
-  scoreArray[activePlayer - 1].push(currentScore); // i hope this works
-  // console.log("Score recorded.");
+  var index = activePlayer - 1;
+  scoreArray[index].push(currentScore); // i hope this works
+  console.log(scoreArray[index]);
+  console.log("Score recorded.");
 };
 
 var switchPlayer = function () {
-  activePlayer = activePlayer + 1;
-  activePlayer = (activePlayer % 2) + 1;
+  var previousPlayer = activePlayer;
+  activePlayer++;
+  activePlayer = ((activePlayer + 1) % 2) + 1;
   // ^toggles between 1 and 2, conveniently assumes 2 players
-  // console.log(`New Player Turn: ${activePlayer}`);
-  return `${activePlayer}'s turn has ended. Moving to next player, type 'Roll' to continue.`;
+  console.log(`New Player Turn: ${activePlayer}`);
+  return `Player ${previousPlayer}'s turn has ended. Moving to next player, type 'Roll' to continue.`;
 };
 
 var main = function (input) {
-  if (currentMode == "waiting to start" || input == "start") {
-    currentMode = "playing";
-    // console.log("Started Playing");
-    return "Hello Player 1. Enter 'Roll' to continue.";
-  }
-  if (input == "Roll") {
-    return playerTurnRoll(activePlayer);
-  }
-  if (input == "dice 1" || input == "dice 2") {
+  if (validateInput(input) == true) {
+    // this catches all non-valid input
+    if (currentMode == "waiting to start" && input == "start") {
+      currentMode = "playing";
+      console.log("Started Playing");
+      return "Hello Player 1. Enter 'Roll' to continue.";
+    }
+    if (input == "Roll") {
+      return playerTurnRoll(activePlayer);
+    }
     playerTurnSelection(input);
     writePlayerScore();
     return switchPlayer();
-  } else {
-    return "Looks like you entered something invalid.";
   }
+  return "Looks like you submitted something invalid.";
 };
