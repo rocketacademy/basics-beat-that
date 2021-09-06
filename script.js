95e508ff01404700027fc2321f86bc5e23854af8
Eunice321 <xeuniceyeex@gmail.com>
2021-09-06 20:09:48 +0800

    Clean up code a little

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-var player1Dice = [];
-var player2Dice = [];
-var player1Num;
-var player2Num;
 var myOutputValue = " ";
 var gameMode = "Get player 1 dice roll";
 
-// roll a random number
+// roll a random number between 1-6
 var diceRoll = function () {
   var randomDecimal = Math.random() * 6;
   var randomInteger = Math.floor(randomDecimal);
@@ -15,72 +11,65 @@ var diceRoll = function () {
 
 var main = function (input) {
   if (gameMode == "Get player 1 dice roll") {
+    player1Dice = [];
+    player2Dice = [];
+    currPlayer = 1;
     myOutputValue = getRandomDiceRoll();
     gameMode = "Choose dice order";
   } else if (gameMode == "Choose dice order") {
     myOutputValue = chooseDiceOrder(input);
     gameMode = "Get player 2 dice roll";
   } else if (gameMode == "Get player 2 dice roll") {
-    myOutputValue = getRandomDiceRoll2();
+    currPlayer = 2;
+    myOutputValue = getRandomDiceRoll();
     gameMode = "Choose dice order 2";
   } else if (gameMode == "Choose dice order 2") {
-    gameMode = "Results";
-    myOutputValue = chooseDiceOrder2(input);
-  } else if (gameMode == "Results") {
-    console.log("hi");
-    myOutputValue = results();
+    myOutputValue = chooseDiceOrder(input);
+    gameMode = "Get player 1 dice roll";
   }
 
   console.log(gameMode);
+  console.log(`Player ${currPlayer}`);
   return myOutputValue;
 };
 
 var getRandomDiceRoll = function () {
-  player1Dice.push(diceRoll(), diceRoll());
-  console.log(player1Dice);
-  return `Welcome Player 1. <br>
+  if (currPlayer == 1) {
+    player1Dice.push(diceRoll(), diceRoll());
+    console.log(player1Dice);
+    return `Welcome Player ${currPlayer}. <br>
   The 2 dice rolls are: ${player1Dice}. <br>
   Choose the order of the dice (Enter 1 or 2)`;
-};
-
-var getRandomDiceRoll2 = function () {
-  player2Dice.push(diceRoll(), diceRoll());
-  console.log(player2Dice);
-  return `Welcome Player 2. <br>
-  The 2 dice rolls are: ${player2Dice}. <br>
-  Choose the order of the dice (Enter 1 or 2)`;
-};
-
-var chooseDiceOrder = function (input) {
-  if (input == 1) {
-    player1Num = Number(`${player1Dice[0]}${player1Dice[1]}`);
-    return `Player 1, you chose Dice 1 first. <br>
-    Your number is ${player1Num}. <br>
-    It is now Player 2's turn. `;
-  } else {
-    player1Num = Number(`${player1Dice[1]}${player1Dice[0]}`);
-
-    return `Player 1, you chose Dice 2 first. <br>
-    Your number is ${player1Num}.<br>
-    It is now Player 2's turn. `;
+  } else if (currPlayer == 2) {
+    player2Dice.push(diceRoll(), diceRoll());
+    console.log(player2Dice);
+    return `Welcome Player ${currPlayer}. <br>
+      The 2 dice rolls are: ${player2Dice}. <br>
+      Choose the order of the dice (Enter 1 or 2)`;
   }
 };
 
-var chooseDiceOrder2 = function (input) {
-  if (input == 1) {
-    console.log(player2Dice[0]);
-    console.log(player2Dice[1]);
-    player2Num = Number(`${player2Dice[0]}${player2Dice[1]}`);
-    return `Player 2, you chose Dice 1 first. <br>
+var chooseDiceOrder = function (input) {
+  if (currPlayer == 1 && input == 1) {
+    player1Num = `${player1Dice[0]}${player1Dice[1]}`;
+    return `Player ${currPlayer}, you chose Dice 1 first. <br>
+    Your number is ${player1Num}. <br>
+    It is now Player 2's turn. `;
+  } else if (currPlayer == 1 && input == 2) {
+    player1Num = `${player1Dice[1]}${player1Dice[0]}`;
+    return `Player ${currPlayer}, you chose Dice 2 first. <br>
+    Your number is ${player1Num}.<br>
+    It is now Player 2's turn. `;
+  } else if (currPlayer == 2 && input == 1) {
+    player2Num = `${player2Dice[0]}${player2Dice[1]}`;
+    return `Player ${currPlayer}, you chose Dice 1 first. <br>
     Your number is ${player2Num}. <br>
-    Click 'Submit' to see results.`;
-  } else {
-    player2Num = Number(`${player2Dice[1]}${player2Dice[0]}`);
-    console.log(player2Dice[1]);
-    console.log(player2Dice[0]);
-    return `Player 2, you chose Dice 2 first. <br>
+    ${results()} `;
+  } else if (currPlayer == 2 && input == 2) {
+    player2Num = `${player2Dice[1]}${player2Dice[0]}`;
+    return `Player ${currPlayer}, you chose Dice 2 first. <br>
     Your number is ${player2Num}.<br>
-    Click 'Submit' to see results.`;
+    ${results()} `;
   }
 };
 
