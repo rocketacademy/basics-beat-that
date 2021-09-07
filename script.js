80ed644a31aaab75ccdd40522ef570335fdc9b51
lim-jiahao <lim.jiahaoo@gmail.com>
2021-09-07 16:41:13 +0800

    changed to ask for user input using html elements, refactor code

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-// constants for game modes and output strings
-const SELECT_NUMBER_MODE = "number";
-const SELECT_WINNER_MODE = "winner";
-const NUM_PLAYER_MODE = "player";
-const NUM_DICE_MODE = "dice";
-const ROLL_MODE = "roll";
-
-const NUMBER_MODE_INVALID_MSG = `Invalid selection.<br><br>Type 1 for Highest Combined Number mode.<br>Type 2 for Lowest Combined Number mode.`;
-const NUMBER_MODE_VALID_MSG = `Good choice! Now...<br><br>Type 1 for running mode.<br>Type 2 for knockout mode.<br><br>In running mode, the game goes on indefinitely and there is no permanent winner, only a temporary leader.<br><br>In knockout mode, two players will be selected at random and the loser will be eliminated. This goes on until there is one player left, who is the ultimate winner.`;
-const WINNER_MODE_INVALID_MSG = `Invalid selection.<br><br>Type 1 for running mode.<br>Type 2 for knockout mode.`;
-const WINNER_MODE_VALID_MSG = `Good choice!<br><br>How many players would like to play?`;
-const INPUT_DICE_MSG = "How many dice would you like to play with?";
-const NUM_PLAYER_INPUT_MSG = "Please input an integer starting from 2.";
-const NUM_DICE_INPUT_MSG = "Please input an integer starting from 2.";
-const STARTING_GAME_MSG = `Starting game. It is Player 1's turn. Press Submit to roll.`;
-
 // state variables
-var curGameMode = SELECT_NUMBER_MODE;
-var highestNumberMode = true;
-var knockoutMode = false;
-var numDice = 0;
+var highestNumberMode, knockoutMode, numDice, numPlayers;
 var diceRolls = [];
 var curPlayer = 1;
-var numPlayers = 0;
 var playerScores = [];
 
 // knockout mode state variables
@@ -30,59 +10,15 @@ var players = [];
 var scores = [];
 var playerIndex = 0;
 
-var setNumberMode = function (input) {
-  // set highest combined number mode or lowest combined number mode, and return msg with instructions
-  if (input != 1 && input != 2) return NUMBER_MODE_INVALID_MSG;
-  if (input == 2) {
-    highestNumberMode = false;
-  }
-  curGameMode = SELECT_WINNER_MODE;
-  return NUMBER_MODE_VALID_MSG;
-};
-
-var setWinnerMode = function (input) {
-  // set running mode or knockout mode, return msg with instructions
-  if (input != 1 && input != 2) return WINNER_MODE_INVALID_MSG;
-  if (input == 2) {
-    knockoutMode = true;
-  }
-  curGameMode = NUM_PLAYER_MODE;
-  return WINNER_MODE_VALID_MSG;
-};
-
-var setNumberOfPlayers = function (num) {
-  // set number of players, return msg with instructions
-  if (!Number.isInteger(Number(num)) || num < 2) return NUM_PLAYER_INPUT_MSG;
-  numPlayers = Number(num);
-  playerScores = new Array(numPlayers).fill(0); // create a playerScores array with numPlayers elements all set to 0
-
-  if (knockoutMode) {
-    // fill the playerChoices array with the player nums which will be used in gameplay later
-    for (var i = 1; i <= numPlayers; i += 1) {
-      playerChoices.push(i);
-    }
-  }
-  curGameMode = NUM_DICE_MODE;
-  return INPUT_DICE_MSG;
-};
-
-var setNumberOfDice = function (num) {
-  // set number of dice, return msg with instructions which will vary depending on running/knockout mode
-  if (!Number.isInteger(Number(num)) || num < 2) return NUM_DICE_INPUT_MSG;
-  numDice = num;
-  curGameMode = ROLL_MODE;
-  return knockoutMode ? randomSelectTwoPlayers() : STARTING_GAME_MSG;
-};
-
 var getDiceRolls = function () {
   // generate a player's dice rolls and store it in diceRolls array, output msg for each roll
-  var rolls = "";
+  var rollsMsg = "";
   for (var i = 0; i < numDice; i += 1) {
     var diceRoll = Math.ceil(Math.random() * 6);
     diceRolls.push(diceRoll);
-    rolls += `You rolled ${diceRoll} for Dice ${i + 1}.<br>`;
+    rollsMsg += `You rolled ${diceRoll} for Dice ${i + 1}.<br>`;
   }
-  return rolls;
+  return rollsMsg;
 };
 
 var getScore = function () {
@@ -112,24 +48,6 @@ var generateEndTurnOutput = function () {
   var winningScore = getWinningScore(playerScores);
   var winner = playerScores.indexOf(winningScore) + 1;
   output += `The current leader is Player ${winner}!`;
-  return output;
-};
-
-var playRunningMode = function () {
-  var output = `Welcome Player ${curPlayer}.<br>`;
-
-  output += getDiceRolls();
-
-  // calculate score for cur player and add to playerScores array
-  var score = getScore();
-  playerScores[curPlayer - 1] += score;
-  diceRolls = [];
-
-  output += `<br>Your number is ${score}.`;
-  curPlayer += 1; // next player
-  if (curPlayer > numPlayers) curPlayer = 1; // go back to player 1
-  output += `<br>It is now Player ${curPlayer}'s turn.`;
-  output += generateEndTurnOutput();
   return output;
 };
 
@@ -163,49 +81,79 @@ var generateEndKnockoutRoundOutput = function () {
   if (playerChoices.length == 1) {
     // knockout game over, there is a winner
     output += `<br><br>After a long fought battle, the ultimate winner of the knockout round is player ${playerChoices[0]}! Congratulations!`;
-    output += `<br><br>To start a new game:<br>Type 1 for Highest Combined Number mode.<br>Type 2 for Lowest Combined Number mode.`;
-    curGameMode = SELECT_NUMBER_MODE;
-    playerChoices = [];
+    resetGame();
   } else {
     output += `<br><br>Remaining players: ${playerChoices}`;
   }
   return output;
 };
 
-var playKnockoutMode = function () {
-  curPlayer = players[playerIndex];
-  var output = `Welcome Player ${curPlayer}.<br>`;
+var setGameState = function (players, dice, calcScore, gameMode) {
+  numPlayers = Number(players);
+  numDice = Number(dice);
+  highestNumberMode = calcScore == "highest";
+  knockoutMode = gameMode == "knockout";
 
-  output += getDiceRolls();
+  playerScores = new Array(numPlayers).fill(0); // create a playerScores array with numPlayers elements all set to 0
 
-  // calculate score for cur player and add to scores array
-  var score = getScore();
-  scores.push(score);
-  diceRolls = [];
-
-  output += `<br>Your number is ${score}.`;
-
-  if (playerIndex < players.length - 1) {
-    playerIndex += 1; // next player
-    output += `<br>It is now Player ${players[playerIndex]}'s turn.`;
-  } else {
-    output += generateEndKnockoutRoundOutput();
+  if (knockoutMode) {
+    // fill the playerChoices array with the player nums which will be used in gameplay later
+    for (var i = 1; i <= numPlayers; i += 1) {
+      playerChoices.push(i);
+    }
   }
-  return output;
+
+  return knockoutMode
+    ? randomSelectTwoPlayers()
+    : `Starting game. It is Player 1's turn. Press Submit to roll.`;
 };
 
-var main = function (input) {
-  if (curGameMode == SELECT_NUMBER_MODE) return setNumberMode(input);
+var resetGame = function () {
+  diceRolls = [];
+  curPlayer = 1;
+  playerScores = [];
+  playerChoices = [];
+  players = [];
+  scores = [];
+  playerIndex = 0;
 
-  if (curGameMode == SELECT_WINNER_MODE) return setWinnerMode(input);
+  document.querySelector("#start-game-button").disabled = false;
+  document.querySelector("#continue-button").style.visibility = "hidden";
+  document.querySelector("#quit-button").style.visibility = "hidden";
+  document.querySelector("#num-player").disabled = false;
+  document.querySelector("#num-dice").disabled = false;
+  document.querySelector("#calc-score").disabled = false;
+  document.querySelector("#game-mode").disabled = false;
+};
 
-  if (curGameMode == NUM_PLAYER_MODE) return setNumberOfPlayers(input);
+var main = function () {
+  if (knockoutMode && players.length == 0) return randomSelectTwoPlayers();
 
-  if (curGameMode == NUM_DICE_MODE) return setNumberOfDice(input);
+  curPlayer = knockoutMode ? players[playerIndex] : curPlayer;
+  var output = `Welcome Player ${curPlayer}.<br>`;
+  output += getDiceRolls();
+  var score = getScore();
+  diceRolls = [];
+  output += `<br>Your number is ${score}.`;
 
-  if (curGameMode == ROLL_MODE) {
-    if (!knockoutMode) return playRunningMode();
-    if (players.length == 0) return randomSelectTwoPlayers();
-    return playKnockoutMode();
+  if (!knockoutMode) {
+    // calculate score for cur player and add to playerScores array
+    playerScores[curPlayer - 1] += score;
+
+    curPlayer += 1; // next player
+    if (curPlayer > numPlayers) curPlayer = 1; // go back to player 1
+    output += `<br>It is now Player ${curPlayer}'s turn.`;
+    output += generateEndTurnOutput();
+  } else {
+    // calculate score for cur player and add to scores array
+    scores.push(score);
+
+    if (playerIndex < players.length - 1) {
+      playerIndex += 1; // next player
+      output += `<br>It is now Player ${players[playerIndex]}'s turn.`;
+    } else {
+      output += generateEndKnockoutRoundOutput();
+    }
   }
+  return output;
 };
