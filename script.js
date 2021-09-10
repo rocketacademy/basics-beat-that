a441f6268558c53ec4bcabba77683df2f3dbe18c
nehtelee <nehtelee@gmail.com>
2021-09-10 22:55:20 +0800

    completed More Comfortable Variable Dice

--- script.js
+++ script.js
@@ -1,168 +1,64 @@
-// create a 2 player mode and dice roll/dice order mode
-var playersMode = "player 1";
-var gameMode = "dice roll";
-var otherModes = "highest number wins";
-var player1;
-var player2;
-var myOutputValue;
-var roll1;
-var roll2;
-var player1score = 0;
-console.log("Player 1's score:", player1score);
-var player2score = 0;
-console.log("Player 2's score:", player2score);
+var player1 = [];
+var player2 = [];
+var player1Score;
+var player2Score;
 
-// generate random dice rolls
-var rollDice = function () {
+//ask players how many dice they wish to play with
+var main = function (input) {
+  return rollDice(input);
+};
+
+//generate equivalent number of randomized dice rolls and store them in an array
+var generateRandomNumber1 = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var randomNumber = randomInteger + 1;
+  return randomNumber;
+};
+var generateRandomNumber2 = function () {
   var randomDecimal = Math.random() * 6;
   var randomInteger = Math.floor(randomDecimal);
   var randomNumber = randomInteger + 1;
   return randomNumber;
 };
 
-// return result of dice rolls and invite dice ordering
-var diceRoll = function () {
-  var rollOne = rollDice();
-  roll1 = String(rollOne);
-  console.log("Roll 1: " + rollOne);
-  var rollTwo = rollDice();
-  roll2 = String(rollTwo);
-  console.log("Roll 2: " + rollTwo);
-  if (playersMode == "player 1") {
-    gameMode = "dice order";
-    return (
-      "Nice rollin', Player 1!" +
-      "<br><br>" +
-      `Dice One: ${roll1}` +
-      "<br>" +
-      `Dice Two: ${roll2}` +
-      "<br><br>" +
-      "Please indicate the order you want your dice roll to be recorded in. If you want dice 1 to be recorded first, enter '1'. If you want dice 2 to be recorded first, enter '2'."
-    );
-  }
-  if (playersMode == "player 2") {
-    gameMode = "dice order";
-    return (
-      "Nice rollin', Player 2!" +
-      "<br><br>" +
-      `Dice One: ${roll1}` +
-      "<br>" +
-      `Dice Two: ${roll2}` +
-      "<br><br>" +
-      "Please indicate the order you want your dice roll to be recorded in. If you want dice 1 to be recorded first, enter '1'. If you want dice 2 to be recorded first, enter '2'."
-    );
-  }
-};
-
-var runningScore = function () {
-  if (player1score > player2score || player1score == player2score) {
-    var outputValue =
-      "<br><br>" +
-      "<b>Running Score</b>" +
-      "<br>" +
-      `Player 1: ${player1score}` +
-      "<br>" +
-      `Player 2: ${player2score}`;
-  }
-  if (player2score > player1score) {
-    outputValue =
-      "<br><br>" +
-      "<b>Running Score</b>" +
-      "<br>" +
-      `Player 2: ${player2score}` +
-      "<br>" +
-      `Player 1: ${player1score}`;
-  }
-  return outputValue;
-};
-
-var declareWinner = function () {
-  myOutputValue =
-    `Player 1, you got ${player1}. Player 2, you got ${player2}.` + "<br><br>";
-
-  if (otherModes == "highest number wins") {
-    if (player1 > player2) {
-      myOutputValue =
-        myOutputValue + `Player 1 wins this round!` + runningScore();
-    }
-    if (player1 < player2) {
-      myOutputValue =
-        myOutputValue + `Player 2 wins this round!` + runningScore();
-    }
-    if (player1 == player2) {
-      myOutputValue =
-        myOutputValue + `It's a draw this round!` + runningScore();
-    }
+var rollDice = function (numberOfRolls) {
+  counter = 0;
+  while (counter < numberOfRolls) {
+    var randomNumber1 = generateRandomNumber1();
+    var randomNumber2 = generateRandomNumber2();
+    player1.push(randomNumber1);
+    console.log("player1", player1);
+    player2.push(randomNumber2);
+    console.log("player2", player2);
+    counter += 1;
   }
 
-  if (otherModes == "lowest number wins") {
-    if (player1 > player2) {
-      myOutputValue =
-        myOutputValue + `Player 2 wins this round!` + runningScore();
-    }
-    if (player1 < player2) {
-      myOutputValue =
-        myOutputValue + `Player 1 wins this round!` + runningScore();
-    }
-    if (player1 == player2) {
-      myOutputValue = myOutputValue + `It's a draw!` + runningScore();
-    }
+  //auto-generate the optimal combined number based on each player's dice rolls to determine the winner
+  var sortBigToSmall1 = player1.sort((a, b) => b - a);
+  console.log("sortBigToSmall1", sortBigToSmall1);
+  var sortBigToSmall2 = player2.sort((a, b) => b - a);
+  console.log("sortBigToSmall2", sortBigToSmall2);
+  player1Score = Number(sortBigToSmall1.join(""));
+  console.log("Player 1:", player1Score);
+  player2Score = Number(sortBigToSmall2.join(""));
+  console.log("Player 2:", player2Score);
+
+  var playersDiceRolls =
+    `Player 1's dice rolls: ${player1}` +
+    "<br>" +
+    `Player 2's dice rolls: ${player2}` +
+    "<br><br>" +
+    `Player 1's highest combined number is ${player1Score}. Player 2's highest combined number is ${player2Score}. `;
+
+  if (player1Score > player2Score) {
+    myOutputValue = playersDiceRolls + "Player 1 wins!";
   }
-  gameMode = "dice roll";
-  playersMode = "player 1";
+  if (player2Score > player1Score) {
+    myOutputValue = playersDiceRolls + "Player 2 wins!";
+  }
+  if (player1Score == player2Score) {
+    myOutputValue = playersDiceRolls + "It's a draw!";
+  }
   return myOutputValue;
-};
-
-// return results of dice ordering and announce winner
-var diceOrder = function (input) {
-  if (playersMode == "player 1") {
-    if (input == 1) {
-      player1 = roll1 + roll2;
-      player1score = player1score + Number(player1);
-      playersMode = "player 2";
-      gameMode = "dice roll";
-      console.log("Player 1:", player1);
-      return `Your recorded dice roll is ${roll1}${roll2}. Player 2 may roll now.`;
-    }
-    if (input == 2) {
-      player1 = roll2 + roll1;
-      player1score = player1score + Number(player1);
-      playersMode = "player 2";
-      gameMode = "dice roll";
-      console.log("Player 1:", player1);
-      return `Your recorded dice roll is ${roll2}${roll1}. Player 2 may roll now.`;
-    }
-  }
-  if (playersMode == "player 2") {
-    if (input == 1) {
-      player2 = roll1 + roll2;
-      player2score = player2score + Number(player2);
-      console.log("Player 2:", player2);
-      return declareWinner();
-    }
-    if (input == 2) {
-      player2 = roll2 + roll1;
-      player2score = player2score + Number(player2);
-      console.log("Player 2:", player2);
-      return declareWinner();
-    }
-  }
-  return "Invalid input. Please indicate '1' if you want dice 1 to be recorded first, and '2' if you want dice 2 to be recorded first.";
-};
-
-var main = function (input) {
-  if (input == "lowest") {
-    otherModes = "lowest number wins";
-    return "Starting now, the lowest number wins. To revert back to having the highest number win, enter 'highest'.";
-  }
-  if (input == "highest") {
-    otherModes = "highest number wins";
-    return "Starting now, the highest number wins. To have the lowest number win, enter 'lowest'.";
-  }
-  if (gameMode == "dice roll") {
-    return diceRoll();
-  }
-  if (gameMode == "dice order") {
-    return diceOrder(input);
-  }
 };
