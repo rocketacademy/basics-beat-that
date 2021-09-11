cdcdb83136584b350076891e2257b347c3128658
muthusalami <matthewyangjiefeng@gmail.com>
2021-09-12 01:59:41 +0800

    Mathew Yang Beat That! Base

--- script.js
+++ script.js
@@ -1,52 +1,84 @@
+// dices
+var dice11 = 0;
+var dice12 = 0;
+var dice21 = 0;
+var dice22 = 0;
+
 // game mode
-var gameMode1 = `user roll dice`;
-var gameMode2 = `user choose dice`;
+var gameMode1 = `user1 roll dice`;
+var gameMode2 = `user1 choose dice`;
+var gameMode3 = `user2 roll dice`;
+var gameMode4 = `user2 choose dice`;
 var gameMode = gameMode1;
 
 // player mode
-var playerTurn1 = `Player 1`;
-var playerTurn2 = `Player 2`;
-var playerTurn = playerTurn1;
-
-// player dice array
-var player1 = [];
-var player2 = [];
+var player1Turn = `Player 1`;
+var player2Turn = `Player 2`;
+var playerTurn = player1Turn;
 
 // chosen numbers
 var player1ChosenNo = ``;
-var player2ChcosenNo = ``;
+var player2ChosenNo = ``;
 
 // number generator. 1 - 6
 var genDiceRoll = function () {
-  return Math.floor(Math.random() * 7);
-};
-
-// return two dice rolls
-var genTwoDiceRolls = function () {
-  var twoDiceNum = [genDiceRoll(), genDiceRoll()];
-
-  if (playerTurn == playerTurn1) {
-    player1 = twoDiceNum;
-  } else {
-    player2 = twoDiceNum;
-  }
-
-  return twoDiceNum;
+  var randomDice = Math.floor(Math.random() * 6);
+  return randomDice + 1;
 };
 
 var main = function (input) {
-  var numberOrder = ``;
+  var dice11 = genDiceRoll();
+  var dice12 = genDiceRoll();
+  var dice21 = genDiceRoll();
+  var dice22 = genDiceRoll();
 
   if (gameMode == gameMode1) {
-    var diceRoll = genTwoDiceRolls();
-    gameMode == gameMode2;
-    return `${playerTurn} <br><br> Dice numbers: ${diceRoll}. <br><br> Please choose the order of dice by entering 1 or 2.`;
+    player1Dice1 = dice11;
+    player1Dice2 = dice12;
+    gameMode = gameMode2;
+    return `${playerTurn} <br><br> Dice numbers: ${dice11}, ${dice12}. <br><br> Please choose the order of dice by entering 1 or 2.`;
   }
 
   if (gameMode == gameMode2) {
-    var numberOrder = Number(input);
-    player1ChosenNo = Number(player1[1], player1[0]);
-    gameMode = gameMode1;
-    playerTurn = playerTurn2;
+    if (input == 1) {
+      player1ChosenNo = [player1Dice1] + [player1Dice2];
+      playerTurn = player2Turn;
+      gameMode = gameMode3;
+      console.log(dice11, dice12);
+    }
+
+    if (input == 2) {
+      player1ChosenNo = [player1Dice2] + [player1Dice1];
+      playerTurn = player2Turn;
+      gameMode = gameMode3;
+      console.log(dice11, dice12);
+    }
+
+    return `Player 1 number is ${player1ChosenNo} <br><br> Player 2, please hit submit to throw your dice.`;
+  }
+
+  if (gameMode == gameMode3) {
+    player2Dice1 = dice21;
+    player2Dice2 = dice22;
+    gameMode = gameMode4;
+    return `${playerTurn} <br><br> Dice numbers: ${dice21}, ${dice22}. <br><br> Please choose the order of dice by entering 1 or 2.`;
+  }
+
+  if (gameMode == gameMode4) {
+    if (input == 1) {
+      player2ChosenNo = [player2Dice1] + [player2Dice2];
+    }
+    if (input == 2) {
+      player2ChosenNo = [player2Dice2] + [player2Dice1];
+    }
+
+    if (player1ChosenNo > player2ChosenNo) {
+      winner = `Player 1 Wins!`;
+    }
+    if (player2ChosenNo > player1ChosenNo) {
+      winner = `Player 2 Wins!`;
+    }
+
+    return `Your number is ${player2ChosenNo}. <br><br> ${winner}`;
   }
 };
