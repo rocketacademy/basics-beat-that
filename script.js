5512726a4e53334a61d19c86a0670772d23410a1
kennethongcs <kenneth.ongcs@gmail.com>
2021-10-13 22:14:18 +0800

    changed HTML, CSS, added dice pictures

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+'use strict';
+
+const inputEl = document.querySelector('#input-field');
+const btnSubmit = document.getElementById('submit-button');
+const outputEl = document.getElementById('output-div');
+
+// global variables
+
+// output
+btnSubmit.addEventListener('click', function () {
+  let input = inputEl.value;
+  outputEl.textContent = input;
+  rollDice();
+});
+
+// enter functionality
+inputEl.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    btnSubmit.click();
+  }
+});
+
+const randomNumberGenerator = function () {
+  return Math.trunc(Math.random() * 6 + 1);
+};
+
+// roll dice
+const rollDice = function () {
+  const dice1 = randomNumberGenerator();
+  const dice2 = randomNumberGenerator();
+  outputEl.textContent = `You rolled ${dice1} and ${dice2}`;
 };
