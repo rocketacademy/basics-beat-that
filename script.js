636dfb538ca86ce61b5bfe186fc45a5ade53622a
tabbiho <tabbiho@gmail.com>
2021-10-20 09:39:05 +0800

    Leaderboard & Lowest Combined Number Mode completed

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// Global States
+// Global Variables
 var playerTurn = 1; // 1 = Player 1; 2 = Player 2
 var gamePhase = 1; // 1 = Dice Roll; 2 = Choose Order; 3 = Conclusion
 var player1Rolls = []; // Contains the 2 dice rolls for Player 1
 var player2Rolls = []; // Contains the 2 dice rolls for Player 2
-var playerChoice = 0; // If 1, choose 1st numerical; If 2, choose 2nd numerical
-var finalNumber1 = 0; // Stores final number for Player 1
-var finalNumber2 = 0; // Stores final number for Player 2
+var finalNumbers = []; // Contains the final numbers for all players
 var scorePlayer1 = 0; // Stores running score for Player 1
 var scorePlayer2 = 0; // Stores running score for player 2
+var gameMode = "Choosing Game Mode"; // 2 Choices - Highest Mode or Lowest Mode
 
 // Function: Returns random number from 1 to 6 -inclusive
 var rollDice = function () {
   var randomDecimal = Math.random() * 6;
   var randomInteger = Math.floor(randomDecimal);
+  ß;
   var diceNum = randomInteger + 1;
   return diceNum;
 };
 
 // Function: Takes user input 1 or 2, to generate combined number's value with chosen 1st numerical
 var chosenOrderNum = function (choice, playerRolls) {
+  var chosenNum = 0;
   if (choice == 1) {
     chosenNum = Number(String(playerRolls[0]) + String(playerRolls[1]));
   } else if (choice == 2) {
@@ -29,87 +30,186 @@ var chosenOrderNum = function (choice, playerRolls) {
 
 // Function: Takes player number input, to return message on what numbers were rolled
 var playerXRolls = function (playerNum) {
-  dice1Roll = eval("player" + playerNum + "Rolls[0]");
-  dice2Roll = eval("player" + playerNum + "Rolls[1]");
+  var dice1Roll = eval("player" + playerNum + "Rolls[0]");
+  var dice2Roll = eval("player" + playerNum + "Rolls[1]");
   var message = `You rolled <b>${dice1Roll}</b> for Dice 1 and <b>${dice2Roll}</b> for Dice 2.`;
   return message;
 };
 
-// Function: Returns error message for not picking 1 or 2.
+// Function: Returns error message for not picking Dice 1 or 2
 var errorMessage = function () {
-  return `Please try again. Enter '1' for Dice 1 or '2' for Dice 2.`;
+  return `Please try again. Enter '1' for Dice 1 or '2' for Dice 2.<br><br>${playerXRolls(
+    playerTurn
+  )}<br>Choose the order of the dice (1 or 2).`;
 };
 
-// Function: Generates Scoreboard
-var scoreBoard = function () {
-  score = `<br><br><u>Scoreboard</u><br>Player 1: <b>${scorePlayer1}</b><br>Player 2: <b>${scorePlayer2}</b>`;
+// Function: Generates Leaderboard (Highest Mode - Highest on top; Lowest Mode - Lowest on top)
+var leaderBoard = function () {
+  var score1 = 0;
+  var score2 = 0;
+  if (gameMode == "Highest Mode") {
+    if (scorePlayer1 >= scorePlayer2) {
+      score1 = `Player 1: <b>${scorePlayer1}</b>`;
+      score2 = `Player 2: <b>${scorePlayer2}</b>`;
+    } else if (scorePlayer1 < scorePlayer2) {
+      score1 = `Player 2: <b>${scorePlayer2}</b>`;
+      score2 = `Player 1: <b>${scorePlayer1}</b>`;
+    }
+  } else if (gameMode == "Lowest Mode") {
+    if (scorePlayer1 <= scorePlayer2) {
+      score1 = `Player 1: <b>${scorePlayer1}</b>`;
+      score2 = `Player 2: <b>${scorePlayer2}</b>`;
+    } else if (scorePlayer1 > scorePlayer2) {
+      score1 = `Player 2: <b>${scorePlayer2}</b>`;
+      score2 = `Player 1: <b>${scorePlayer1}</b>`;
+    }
+  }
+  var score = `<br><br><b><u>Leaderboard</u></b><br>${score1}<br>${score2}</b>`;
   return score;
 };
 
+// Function: Refactored Game Phase 1 (Roll)
+var phase1 = function () {
+  eval(`player${playerTurn}Rolls.push(rollDice(),rollDice())`);
+  var message = `Welcome Player ${playerTurn}.<br>${playerXRolls(
+    playerTurn
+  )}<br>Choose the order of the dice (1 or 2).`;
+  gamePhase = 2;
+  return message;
+};
+
+// Function: Refactored Game Phase 2 (Choice)
+var phase2 = function (choice) {
+  finalNumbers.push(chosenOrderNum(choice, eval(`player${playerTurn}Rolls`)));
+  var message = `Player ${playerTurn}, you chose Dice ${choice} first.<br>Your number is <b>${
+    finalNumbers[playerTurn - 1]
+  }</b>`;
+  return message;
+};
+
+// Function: Refactored Game Phase 3 (Conclusion - Default Highest Number)
+var phase3 = function () {
+  message = `Player 1's number is <b>${finalNumbers[0]}</b> and Player 2's number is <b>${finalNumbers[1]}</b>.<br>`;
+  if (finalNumbers[0] > finalNumbers[1]) {
+    message += `<b>Player 1 won</b>.`;
+  } else if (finalNumbers[1] > finalNumbers[0]) {
+    message += `<b>Player 2 won</b>.`;
+  } else if (finalNumbers[0] == finalNumbers[1]) {
+    message += `<b>It's a draw</b>.`;
+  }
+  return message;
+};
+
+// Function: Alternate Game Phase 3 (Conclusion - Lowest Number)
+var phase3Lowest = function () {
+  message = `Player 1's number is <b>${finalNumbers[0]}</b> and Player 2's number is <b>${finalNumbers[1]}</b>.<br>`;
+  if (finalNumbers[0] < finalNumbers[1]) {
+    message += `<b>Player 1 won</b>.`;
+  } else if (finalNumbers[1] < finalNumbers[0]) {
+    message += `<b>Player 2 won</b>.`;
+  } else if (finalNumbers[0] == finalNumbers[1]) {
+    message += `<b>It's a draw</b>.`;
+  }
+  return message;
+};
+
+// Function: Refactored Reset Back to Phase 1
+var reset = function () {
+  scorePlayer1 += finalNumbers[0];
+  scorePlayer2 += finalNumbers[1];
+  var message = `<br><br>Press 'Submit' to play again!`;
+  if (gameMode == "Highest Mode") {
+    message += `<br>Alternatively, enter '<b>Lowest Mode</b>' to switch to Lowest Combined Number Mode.`;
+  } else if (gameMode == "Lowest Mode") {
+    message += `<br>Alternatively, enter '<b>Highest Mode</b>' to switch to Highest Combined Number Mode.`;
+  }
+  message += `<br><i>*do note that swapping game modes resets the Leaderboard*</i>`;
+  message += leaderBoard();
+  gamePhase = 1;
+  player1Rolls = [];
+  player2Rolls = [];
+  finalNumbers = [];
+  return message;
+};
+
+// Function: Highest/Lowest Mode Header
+var header = function () {
+  var message = "";
+  if (gameMode == "Highest Mode") {
+    message = `<b><u>Highest Combined Number Game Mode</u></b><br><br>`;
+  } else if (gameMode == "Lowest Mode") {
+    message = `<b><u>Lowest Combined Number Game Mode</u></b><br><br>`;
+  }
+  return message;
+};
+
+// MAIN Function
 var main = function (input) {
   var myOutputValue = "";
-  // 1st IF: If game is in Phase 3 (Conclusion)
-  if (gamePhase == 3) {
-    myOutputValue = `Player 1's number is <b>${finalNumber1}</b> and Player 2's number is <b>${finalNumber2}</b>.<br>`;
-    if (finalNumber1 > finalNumber2) {
-      myOutputValue += `<b>Player 1 won</b>.`;
-    } else if (finalNumber2 > finalNumber1) {
-      myOutputValue += `<b>Player 2 won</b>.`;
-    } else if (finalNumber1 == finalNumber2) {
-      myOutputValue += `<b>It's a draw</b>.`;
-    }
-    scorePlayer1 += finalNumber1;
-    scorePlayer2 += finalNumber2;
-    myOutputValue += `<br>Press 'Submit' to play again!`;
-    myOutputValue += scoreBoard();
-    gamePhase = 1;
-    player1Rolls = [];
-    player2Rolls = [];
+  // First page for Players to select game mode
+  if (input == "Lowest Mode") {
+    gameMode = "Lowest Mode";
+    return `Welcome to '<b>Lowest Combined Number Mode</b>'. The player with the lowest combined number wins. Press 'Submit' to continue...`;
+  } else if (input == "Highest Mode") {
+    gameMode = "Highest Mode";
+    return `Welcome to '<b>Highest Combined Number Mode</b>'. The player with the highest combined number wins. Press 'Submit' to continue...`;
+  } else if (gameMode == "Choosing Game Mode") {
+    return `Welcome to Beat That!<br><br>Enter either:<br>1. '<b>Highest Mode</b>' for Highest Combined Number Game Mode<br>2. '<b>Lowest Mode</b>' for Lowest Combined Number Game Mode`;
   }
-  // 2nd IF: If game is in 1st Player's Turn (Phase 1 or 2)
+  // 1st IF: If game is in Phase 3 (Conclusion)
+  if (gamePhase == 3 && gameMode == "Highest Mode") {
+    myOutputValue = header();
+    myOutputValue += phase3();
+    myOutputValue += reset();
+  } else if (gamePhase == 3 && gameMode == "Lowest Mode") {
+    myOutputValue = header();
+    myOutputValue += phase3Lowest();
+    myOutputValue += reset();
+  }
+  // Option for Players to swap game modes after every round
+  else if (
+    gamePhase == 1 &&
+    ((gameMode == "Highest Mode" && input == "Lowest Mode") ||
+      (gameMode == "Lowest Mode" && input == "Highest Mode"))
+  ) {
+    if (input == "Lowest Mode") {
+      gameMode = "Lowest Mode";
+      scorePlayer1 = 0;
+      scorePlayer2 = 0;
+      return `Welcome to '<b>Lowest Combined Number Mode</b>'. The player with the lowest combined number wins. Press 'Submit' to continue...`;
+    } else if (input == "Highest Mode") {
+      gameMode = "Highest Mode";
+      scorePlayer1 = 0;
+      scorePlayer2 = 0;
+      return `Welcome to '<b>Highest Combined Number Mode</b>'. The player with the highest combined number wins. Press 'Submit' to continue...`;
+    }
+  }
+  // 2nd IF: If game is in 1st Player's Turn (Phase 1 & 2)
   else if (playerTurn == 1) {
+    myOutputValue = header();
     if (gamePhase == 1) {
-      player1Rolls.push(rollDice(), rollDice());
-      myOutputValue = `Welcome Player 1.<br>${playerXRolls(
-        playerTurn
-      )}<br>Choose the order of the dice (1 or 2).`;
-      gamePhase = 2;
+      myOutputValue += phase1();
     } else if (gamePhase == 2) {
       if (input != 1 && input != 2) {
-        return (
-          errorMessage() +
-          `<br>${playerXRolls(
-            playerTurn
-          )}<br>Choose the order of the dice (1 or 2).`
-        );
+        return header() + errorMessage();
       }
-      playerChoice = input;
-      finalNumber1 = chosenOrderNum(playerChoice, player1Rolls);
-      myOutputValue = `Player 1, you chose Dice ${playerChoice} first.<br>Your number is <b>${finalNumber1}</b>.<br>It is now Player 2's turn. Press 'Submit' to continue...`;
+      myOutputValue += phase2(input);
+      myOutputValue += `<br>It is now Player 2's turn. Press 'Submit' to continue...`;
       playerTurn = 2;
       gamePhase = 1;
     }
   }
-  // 3rd IF: If game is in 2nd Player's Turn (Phase 1 or 2)
+  // 3rd IF: If game is in 2nd Player's Turn (Phase 1 & 2)
   else if (playerTurn == 2) {
+    myOutputValue = header();
     if (gamePhase == 1) {
-      player2Rolls.push(rollDice(), rollDice());
-      myOutputValue = `Welcome Player 2.<br>${playerXRolls(
-        playerTurn
-      )}<br>Choose the order of the dice (1 or 2).`;
-      gamePhase = 2;
+      myOutputValue += phase1();
     } else if (gamePhase == 2) {
       if (input != 1 && input != 2) {
-        return (
-          errorMessage() +
-          `<br>${playerXRolls(
-            playerTurn
-          )}<br>Choose the order of the dice (1 or 2).`
-        );
+        return header() + errorMessage();
       }
-      playerChoice = input;
-      finalNumber2 = chosenOrderNum(playerChoice, player2Rolls);
-      myOutputValue = `Player 2, you chose Dice ${playerChoice} first.<br>Your number is <b>${finalNumber2}</b>.<br>Press 'Submit' to see Final Score.`;
+      myOutputValue += phase2(input);
+      myOutputValue += `<br>Press 'Submit' to see Final Score.`;
       playerTurn = 1;
       gamePhase = 3;
     }
