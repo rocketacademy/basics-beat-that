4b2f854bfad18f6e330ebf64f3ba3a0f990ed273
kangyaowg <kangyaowg@gmail.com>
2021-10-28 02:52:55 +0800

    Added reverse mode and dice count

--- script.js
+++ script.js
@@ -5,18 +5,42 @@ console.log(playerArray);
 var playerResult = [];
 var score = [];
 var playerWinOrder = []
-var numOfDice = 2;
+var numOfDice = 0;
 var step = 0;
 var round = 0;
 var displayPlayer = []
 var displayScore = []
 var workingScore = []
+var mode = 0
+var modeName = 0
+var diceOutput = 0
+var numString = ""
 
 var main = function (input) {
+  //check input is a number
+  if (Number.isNaN(Number(input)) == true) {
+    return 'sorry please enter a number.';
+  }
+
   if (input == 0 && players == 0) {
     return "Player count : 0 <br> Please enter number of Players";
   } else if (input > 0 && players == 0) {
     players = input; //Store number of players
+    return "Please select game mode <br> 1: Normal <br> 2: lower number wins"
+  } else if (input > 0 && mode == 0){
+    if (input == 1){
+      mode = 1
+      modeName = "Normal"
+      return"Normal mode selected <br>Please enter number of dices "
+    }else if (input == 2){
+      mode = 2
+      modeName = "Reversed"
+      return "Reversed mode selected, lower number wins <br> Please enter number of dices"
+    }else {
+      return "Please select game mode <br> 1: Normal <br> 2: lower number wins"
+    }
+  } else if (input > 0 && numOfDice == 0){
+    numOfDice = input
     console.log("players: " + players);
     console.log("playerArray: " + playerArray);
     for (var counter = 0; counter < players; counter += 1) {
@@ -28,43 +52,61 @@ var main = function (input) {
     }
     console.log("A " + playerArray);
     console.log("B " + playerResult);
+    return "Number of Players: " + players + "<br>Mode: "+modeName +"<br>Number of Dice: "+numOfDice+"<br> Player 1 starts, press submit to begin"
   }
 
   // Loop to display dice result and select order and input into result array
   while (playerNum < players) {
     if (playerResult[playerNum] == 0 && step == 0) {
-      console.log(playerArray[0]);
-      console.log(playerArray[1]);
-      console.log(playerArray.length);
-      var dice1 = playerArray[playerNum][0];
-      var dice2 = playerArray[playerNum][1];
-      step = step + 1;
-      return (
-        "Hello Player " +
-        (playerNum + 1) +
-        "<br> You rolled " +
-        dice1 +
-        " for Dice 1 and " +
-        dice2 +
-        " for Dice 2 <br> Choose order of the dice"
-      );
-    } else if (step == 1) {
-      if (input == 1) {
-        playerResult[playerNum] = Number(
-          String(playerArray[playerNum][0]) + String(playerArray[playerNum][1])
-        );
-        console.log("C " + playerResult);
-        step = 0;
-      } else if (input == 2) {
-        playerResult[playerNum] = Number(
-          String(playerArray[playerNum][1]) + String(playerArray[playerNum][0])
-        );
-        console.log("D " + playerResult);
-        step = 0;
-      } else {
-        return "Please enter 1 or 2";
+      //var dice1 = playerArray[playerNum][0];
+      //var dice2 = playerArray[playerNum][1];
+      diceOutput = "Hello Player " + (playerNum + 1) +"<br> Your Dice rolls are "
+      for (var counter = 0;counter < numOfDice; counter+=1){
+        diceOutput = diceOutput + "<br> Dice "+(counter+1)+" : "+playerArray[playerNum][counter]
       }
+      step = step + 1;
+      inputArray = []
+      return (
+        diceOutput + "<br> Player " + (playerNum+1)+ " Please choose order of dice"
+      );
+    } else if (step == 1 && input <= numOfDice && input != 0) {
+      while ((inputArray.length) != numOfDice){
+        if (inputArray.indexOf(input) != -1){
+          return "Dice already chosen, please choose other dice<br>Dice already chosen: " + inputArray +"<br><br>" +diceOutput + "<br> Player " + (playerNum+1)+ " Please choose next order of dice"    // check if Dice already chose
+        }else if(inputArray.length == 0){
+          inputArray.push(input)
+          numString = String(playerArray[playerNum][input-1])
+          console.log("NumString A " + numString)
+          console.log("String(playerArray[playerNum][input]) "+String(playerArray[playerNum][input-1]))
+        }else{
+        inputArray.push(input)
+        numString = String(numString) + String(playerArray[playerNum][input-1])
+        console.log("NumString B" + numString)
+        console.log("String(playerArray[playerNum][input]) "+String(playerArray[playerNum][input-1]))
+        }
+        if (inputArray.length == numOfDice){
+          break
+        }
+        return "Dice chosen: " + inputArray +"<br><br>" +diceOutput + "<br> Player " + (playerNum+1)+ " Please choose next order of dice"
+      }
+      playerResult[playerNum] = Number(numString)
+      // if (input > 0) {
+      //   playerResult[playerNum] = Number(
+      //     String(playerArray[playerNum][0]) + String(playerArray[playerNum][1])
+      //   );
+      //   console.log("C " + playerResult);
+      //   step = 0;
+      // } else if (input == 2) {
+      //   playerResult[playerNum] = Number(
+      //     String(playerArray[playerNum][1]) + String(playerArray[playerNum][0])
+      //   );
+      //   console.log("D " + playerResult);
+      //   step = 0;
+      // } else {
+      //   return "Please enter 1 or 2";
+      // }
       score[playerNum] = score[playerNum] + playerResult[playerNum];
+      step =0
     }
     playerNum = playerNum + 1;
   }
@@ -83,14 +125,14 @@ var main = function (input) {
       "<br>";
   }
   //Reset the variables at the end of every round and recreate dice roll and reset result
-  playerNum = 0;
-  playerArray = [];
-  playerResult = [];
+  // playerNum = 0;
+  // playerArray = [];
+  // playerResult = [];
 
-  for (var counter = 0; counter < players; counter += 1) {
-    playerArray.push(generateDiceArray(numOfDice));
-    playerResult.push(0);
-  }
+  // for (var counter = 0; counter < players; counter += 1) {
+  //   playerArray.push(generateDiceArray(numOfDice));
+  //   playerResult.push(0);
+  // }
   
   //Take Score array, and add player number next to it
   displayScore = []
@@ -102,11 +144,20 @@ var main = function (input) {
     workingPlayer.push(x+1)
   }
   
-  while(displayPlayer.length != players){
-    workingScore.sort((a,b) => a-b)
-    var highestNum = workingScore.pop()
-    displayScore.push(highestNum)
-    displayPlayer.push(score.indexOf(highestNum)+1)
+  if (mode == 1){
+    while(displayPlayer.length != players){
+      workingScore.sort((a,b) => a-b)
+      var highestNum = workingScore.pop()
+      displayScore.push(highestNum)
+      displayPlayer.push(score.indexOf(highestNum)+1)
+    }
+  } else if (mode == 2){
+    while(displayPlayer.length != players){
+      workingScore.sort((a,b) => b-a)
+      var lowestNum = workingScore.pop()
+      displayScore.push(lowestNum)
+      displayPlayer.push(score.indexOf(lowestNum)+1)
+    }
   }
 
 
