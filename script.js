aadd5f839e1250ac56ceadb8be417f8bbbe0f2c6
DLCT13 <daryllct13@gmail.com>
2021-10-28 21:55:51 +0800

    Base completed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 // 2 players in game, and each player takes turn
@@ -24,26 +24,50 @@ var gameMode2 = `Player to select order of dice number`;
 var gameCurrentPlayer = 1;
 var gameCurrentMode = gameMode1;
 
+var playerNumberOne;
+var playerNumberTwo;
+
 //------Below are the functions that are to be used in the main game function-------//
 
-//Function to merge dice numbers together
-var merge2DiceNumbers = 
+//Function to merge dice numbers together dependent on the player
+var merge2DiceNumbers = function (playerInput) {
+  if (gameCurrentPlayer == 1) {
+    var playerMerged = diceArrayPlayerOne;
+  } else {
+    var playerMerged = diceArrayPlayerTwo;
+  }
+  if (playerInput == 1) {
+    var mergedNumber = String(playerMerged[0]) + String(playerMerged[1]);
+  } else {
+    var mergedNumber = String(playerMerged[1]) + String(playerMerged[0]);
+  }
+  console.log(`Player one playermerged ${playerMerged}`);
+  if (gameCurrentPlayer == 1) {
+    playerNumberOne = mergedNumber;
+    return playerNumberOne;
+  } else {
+    playerNumberTwo = mergedNumber;
+    return playerNumberTwo;
+  }
+  return mergedNumber;
+};
 
 //Function of placing number in accordance to player selection
-
 
 //Function of dice rolls for each player and to store into their own arrays
 var playerDiceRolls = function () {
   var playerNewDiceRolls = [rollDice(), rollDice()];
   if (gameCurrentPlayer == 1) {
     diceArrayPlayerOne = playerNewDiceRolls;
+    console.log(`Dice rolls are ${diceArrayPlayerOne}`);
   } else {
     diceArrayPlayerTwo = playerNewDiceRolls;
+    console.log(`Dice rolls are ${diceArrayPlayerTwo}`);
   }
   return playerNewDiceRolls;
 };
 
-//
+//git
 //Function for Dice roll
 var rollDice = function () {
   var randomDecimal = Math.random() * 6;
@@ -55,8 +79,8 @@ var rollDice = function () {
 //Function for determining which player wins
 var winningPlayer = function () {
   if (playerNumberOne > playerNumberTwo) {
-    return 1;
-  } else return 2;
+    return `Player One Wins`;
+  } else return `Player Two Wins`;
 };
 
 // ~~~~~Main Game Function~~~~~
@@ -66,14 +90,27 @@ var main = function (input) {
 
   //If function for game to start with player 1 or 2 depending on the progress of game
   if (gameCurrentMode == gameMode1) {
+    console.log(`game current player is ${gameCurrentPlayer}`);
     var gameDiceRoll = playerDiceRolls();
     //game to change to part 2 of game mode which is selecting of dice number
     gameCurrentMode = gameMode2;
     return `Player ${gameCurrentPlayer}, you have rolled ${diceArrayPlayerOne[0]} and ${diceArrayPlayerOne[1]}. <br>
-    Choose which the order of dice (Enter only 1 or 2 as input) as first dice so as to form the largest number!`
+    Choose which the order of dice (Enter only 1 or 2 as input) as first dice so as to form the largest number!`;
   }
   //Now player will input and game is now in gameMode 2 (selection of order of dice numbers)
-  if (gameCurrentMode == gameMode2){
-    var orderOfDice = 
+  if (gameCurrentMode == gameMode2) {
+    var orderOfDice = merge2DiceNumbers(input);
+    gameCurrentMode = gameMode1;
+    if (gameCurrentPlayer == 1) {
+      gameCurrentPlayer = 2;
+      return `Player 1, you have choosen ${orderOfDice}.`;
+    } else {
+      // add in the winning condition in here before the programme runs back to the start again
+      var winnerOutput = winningPlayer();
+
+      gameCurrentPlayer = 1;
+
+      return winnerOutput;
+    }
   }
 };
