3fe6bb4d5e9d744318e26f607025d420db6ccde0
0besitin <justintpc@hotmail.com>
2021-10-28 19:08:53 +0800

    added score tracker and replay mode

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 //global variables
 var player1 = [];
 var player2 = [];
-var playerTurn = 1;
+var playerTurn = "#1";
 var player1Number = "";
 var player2Number = "";
+var player1Score = [];
+var player2Score = [];
 
 var main = function (input) {
   //Error message
   var myOutputValue = "Invalid Input";
 
   // change mode
-  if (playerTurn == 1 && input == "2" && player2 == "") {
-    playerTurn = 2;
+  if (playerTurn == "#1" && input == "#2") {
+    playerTurn = "#2";
     return "Player 2's Roll. Please roll your dice by pressing Submit";
-  } else if (
-    playerTurn == 2 &&
-    input == "1" &&
-    player1 == "" &&
-    player2Number != ""
-  ) {
-    playerTurn = 1;
-    return "Player 1's Rol1. Please roll your dice by pressing Submit";
+  } else if (playerTurn == "#2" && input == "#1") {
+    playerTurn = "#1";
+    return "Player 1's Roll. Please roll your dice by pressing Submit";
   }
 
   //diceRoll for Player 1 if mode is at 1 and player has not rolled dices
-  if (playerTurn == 1 && player1 == "") {
+  if (playerTurn == "#1" && player1 == "") {
     player1.push(diceRoll());
     player1.push(diceRoll());
+    console.log("player1 rolls");
+    console.log(player1);
     return (
       "Player 1 rolled " +
       player1[0] +
@@ -36,9 +35,11 @@ var main = function (input) {
       "<br> Please select which dice to go first in the number."
     );
     //diceRoll for Player 2 if mode is at 2 and player has not rolled dices
-  } else if (playerTurn == 2 && player2 == "") {
+  } else if (playerTurn == "#2" && player2 == "") {
     player2.push(diceRoll());
     player2.push(diceRoll());
+    console.log("player2 rolls");
+    console.log(player2);
     return (
       "Player 2 rolled " +
       player2[0] +
@@ -49,47 +50,61 @@ var main = function (input) {
     );
   }
   // player 1 select the order of their dice
-  if (playerTurn == 1 && (player1 !== "") & (input == 1)) {
+  if (playerTurn == "#1" && (player1 !== "") & (input == 1)) {
     player1Number = player1[0] + "" + player1[1];
     return (
       "Player 1 has chosen the number " +
       player1Number +
-      "<br><br>If Player 2 has not selected their number, please submit 2 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
+      "<br><br>If Player 2 has not selected their number, please submit #2 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
     );
-  } else if (playerTurn == 1 && (player1 !== "") & (input == 2)) {
-    player1Number = player1[0] + "" + player1[1];
+  } else if (playerTurn == "#1" && (player1 !== "") & (input == 2)) {
+    player1Number = player1[1] + "" + player1[0];
     return (
       "Player 1 has chosen the number " +
       player1Number +
-      "<br><br>If Player 2 has not selected their number, please submit 2 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
+      "<br><br>If Player 2 has not selected their number, please submit #2 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
     );
   }
 
   // player 2 select the order of their dice
-  if (playerTurn == 2 && (player2 !== "") & (input == 1)) {
+  if (playerTurn == "#2" && (player2 !== "") & (input == 1)) {
     player2Number = player2[0] + "" + player2[1];
     return (
       "Player 2 has chosen the number " +
       player2Number +
-      "<br><br>If Player 1 has not selected their number, please submit 1 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
+      "<br><br>If Player 1 has not selected their number, please submit #1 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
     );
-  } else if (playerTurn == 2 && (player1 !== "") & (input == 2)) {
+  } else if (playerTurn == "#2" && (player1 !== "") & (input == 2)) {
     player2Number = player2[1] + "" + player2[0];
     return (
       "Player 2 has chosen the number " +
       player2Number +
-      "<br><br>If Player 1 has not selected their number, please submit 1 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
+      "<br><br>If Player 1 has not selected their number, please submit #1 in the input box. <br><br> If both players have selected their numbers, press Submit to see the results!"
     );
+  }
+
+  // play again
+  if (input == "play again") {
+    player1 = [];
+    player2 = [];
+    playerTurn = "#1";
+    player1Number = "";
+    player2Number = "";
+    myOutputValue = "It is Player 1s Turn again";
   }
 
   // compare numbers
   if (player1Number > player2Number) {
+    player1Score.push(player1Number);
+    player2Score.push(player2Number);
     var myOutputValue =
       "Player 1 wins with a number of " +
       player1Number +
       ". Player 2 had a number of " +
       player2Number;
   } else if (player2Number > player1Number) {
+    player1Score.push(player1Number);
+    player2Score.push(player2Number);
     var myOutputValue =
       "Player 2 wins with a number of " +
       player2Number +
