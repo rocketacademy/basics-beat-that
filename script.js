e1f043a68c4946c5175dbf358870f1ff35b3a47d
Marcus4478 <marcuswyneo@gmail.com>
2021-10-28 23:21:22 +0800

    refactor

--- script.js
+++ script.js
@@ -7,131 +7,185 @@ The player picks the order of the dice they want. For example, if they wanted th
 After both players have rolled and chosen dice order, the player with the higher combined number wins.*/
 
 /* Pseudo code for basic project 2
-when click submit, roll 2 dice, output the 2 dice roll.
+when click submit, roll 2 dice, output the 2 dice roll. (first submit)
 
-player pick 1st dice order by inputting 1 or 2
+player 1 pick 1st dice order by inputting 1 or 2 (second submit)
 
 concat the player guess + remaining as dice roll result
 
-next player
+next player 2
 
-repeat dice roll and order
+repeat dice roll (third submit)
+
+repeat order (fourth submit)
 
 compare results, higher result win
 */
 
-// GLOBAL VARIBLES
-var MODE = "player 1 init"; // init and order
-var PLAYER1 = [];
-var PLAYER2 = [];
-var resultPlayer1 = "";
-var resultPlayer2 = "";
+// GLOBAL VARIABLES
+var mode = "roll dice"; // expect "roll dice" and "order dice"
+var currentPlayer = 1; // number of players
+var playerOne = [];
+var playerOneOrderedDice = "";
+var playerTwo = [];
+var playerTwoOrderedDice = "";
 
 var rollDice = function () {
-  var diceValue = Math.floor(Math.random() * 6) + 1; // return 1 to 6 inclusive
-  console.log(`running function rollDice: ${diceValue}`);
-  console.log("====================");
+  diceValue = Math.floor(Math.random() * 6) + 1; // expect 1 to 6 inclusive
   return diceValue;
 };
 
+var getDiceRoll = function (playerArray) {
+  playerArray[0] = rollDice();
+  playerArray[1] = rollDice();
+  console.log(playerArray);
+  return;
+};
+
+var showRollMessage = function (playerArray) {
+  var message = `Welcome Player ${currentPlayer}
+  <br>
+  You rolled ${playerArray[0]} for Dice 1 and ${playerArray[1]} for Dice 2.
+  <br>
+  Choose the order of the dice.
+  <br>
+  Input 1 or 2`;
+  return message;
+};
+
+var orderDiceValues = function (playerArray, userInput, currentPlayer) {
+  // if user input = 1, concat array[0] and array[1]
+  // if user input = 2, concat array[1] and array[0]
+  if (userInput === "1" && currentPlayer == 1) {
+    playerOneOrderedDice = Number(
+      String(playerArray[0]) + String(playerArray[1])
+    ); // expect integar
+    console.log(userInput, playerOneOrderedDice);
+    return;
+  }
+  if (userInput === "2" && currentPlayer == 1) {
+    playerOneOrderedDice = Number(
+      String(playerArray[1]) + String(playerArray[0])
+    ); // expect integar
+    console.log(userInput, playerOneOrderedDice);
+    return;
+  }
+  if (userInput === "1" && currentPlayer == 2) {
+    playerTwoOrderedDice = Number(
+      String(playerArray[0]) + String(playerArray[1])
+    ); // expect integar
+    console.log(userInput, playerTwoOrderedDice);
+    return;
+  }
+  if (userInput === "2" && currentPlayer == 2) {
+    playerTwoOrderedDice = Number(
+      String(playerArray[1]) + String(playerArray[0])
+    ); // expect integar
+    console.log(userInput, playerTwoOrderedDice);
+    return;
+  }
+};
+
+var showOrderMessage = function (playerOrderedDice, userInput) {
+  message = `Player ${currentPlayer - 1}, you chose Dice ${userInput} first.
+  <br>
+  Your number is ${playerOrderedDice}.
+  <br>
+  It is now Player ${currentPlayer}'s turn.`;
+  return message;
+};
+
+var isDraw = function (playerOneValue, playerTwoValue) {
+  if (playerOneValue == playerTwoValue) {
+    return true;
+  }
+};
+
+var showDrawMessage = function () {
+  message = `Player 1, your number is ${playerOneOrderedDice}.
+  <br>
+  Player 2, your number is ${playerTwoOrderedDice}.
+  <br>
+  It is a draw!`;
+  return message;
+};
+
+var didPlayerOneWin = function (playerOneValue, playerTwoValue) {
+  if (playerOneValue > playerTwoValue) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var showPlayerOneWin = function () {
+  message = `Player 1, your number is ${playerOneOrderedDice}.
+  <br>
+  Player 2, your number is ${playerTwoOrderedDice}.
+  <br>
+  Player 1 wins!`;
+  return message;
+};
+
+var showPlayerTwoWin = function () {
+  message = `Player 1, your number is ${playerOneOrderedDice}.
+  <br>
+  Player 2, your number is ${playerTwoOrderedDice}.
+  <br>
+  Player 2 wins!`;
+  return message;
+};
+
 var main = function (input) {
-  // first load = init, player 1 starts first
-  if (MODE === "player 1 init") {
-    // when submit, roll 2 dice
-    PLAYER1[0] = rollDice();
-    PLAYER1[1] = rollDice();
-    MODE = "player 1 order";
-    return `Welcome Player 1.
-    <br>
-    You rolled ${PLAYER1[0]} for Dice 1 and ${PLAYER1[1]} for Dice 2
-    <br>
-    Choose the order of the dice.
-    <br>
-    Input "12" or "21" for your dice order.`;
+  // first submit
+  // roll 2 dice for player 1
+  if (mode === "roll dice" && currentPlayer === 1) {
+    getDiceRoll(playerOne);
+    mode = "order dice";
+    return showRollMessage(playerOne);
   }
 
-  // mode = order, player 1 choose dice order '12' or '21'
-  if (MODE === "player 1 order") {
-    // check if input is '12' or '21'
-    // TODO user input verification
-    if (input === "12") {
-      resultPlayer1 = String(PLAYER1[0]) + String(PLAYER1[1]);
-      console.log(`running input '12': ${resultPlayer1}`);
-      console.log("====================");
-      MODE = "player 2 init";
-      return `Player 1, you chose Dice 1 first.
-      <br>
-      Your number is ${resultPlayer1}.
-      <br>
-      It is now Player 2's turn.
-      <br>
-      Click Submit`;
-    }
-    if (input === "21") {
-      resultPlayer1 = String(PLAYER1[1]) + String(PLAYER1[0]);
-      console.log(`running input '21': ${resultPlayer1}`);
-      console.log("====================");
-      MODE = "player 2 init";
-      return `Player 1, you chose Dice 2 first.
-      <br>
-      Your number is ${resultPlayer1}.
-      <br>
-      It is now Player 2's turn.
-      <br>
-      Click Submit`;
-    }
+  // second submit
+  // order dice value for player 1
+  if (mode === "order dice" && currentPlayer === 1) {
+    orderDiceValues(playerOne, input, currentPlayer);
+    mode = "roll dice";
+    currentPlayer = 2;
+    return showOrderMessage(playerOneOrderedDice, input);
   }
 
-  // Repeat for Player 2
-  if (MODE === "player 2 init") {
-    // when submit, roll 2 dice
-    PLAYER2[0] = rollDice();
-    PLAYER2[1] = rollDice();
-    MODE = "player 2 order";
-    return `Welcome Player 2.
-    <br>
-    You rolled ${PLAYER2[0]} for Dice 1 and ${PLAYER2[1]} for Dice 2
-    <br>
-    Choose the order of the dice.
-    <br>
-    Input "12" or "21" for your dice order.`;
+  // third submit
+  // roll 2 dice for player 2
+  if (mode === "roll dice" && currentPlayer === 2) {
+    getDiceRoll(playerTwo);
+    mode = "order dice";
+    return showRollMessage(playerTwo);
   }
 
-  // mode = order, player 2 choose dice order '12' or '21'
-  if (MODE === "player 2 order") {
-    // check if input is '12' or '21'
-    // TODO user input verification
-    if (input === "12") {
-      resultPlayer2 = String(PLAYER2[0]) + String(PLAYER2[1]);
-      console.log(`running input '12': ${resultPlayer2}`);
-      console.log("====================");
-      MODE = "compare";
-      return `Player 2, you chose Dice 1 first.
-      <br>
-      Your number is ${resultPlayer2}.`;
-    }
-    if (input === "21") {
-      resultPlayer2 = String(PLAYER2[1]) + String(PLAYER2[0]);
-      console.log(`running input '21': ${resultPlayer2}`);
-      console.log("====================");
-      MODE = "compare";
-      return `Player 2, you chose Dice 2 first.
-      <br>
-      Your number is ${resultPlayer2}.`;
-    }
+  // fourth submit
+  // order dice value for player 2
+  if (mode === "order dice" && currentPlayer === 2) {
+    orderDiceValues(playerTwo, input, currentPlayer);
   }
 
-  if (MODE === "compare") {
-    // assume no draw
-    if (Number(resultPlayer1) > Number(resultPlayer2)) {
-      MODE = "player 1 init";
-      return `Player 1 wins`;
-    } else {
-      MODE = "player 1 init";
-      return `Player 2 wins`;
-    }
+  // all players rolled and ordered
+  // highest number wins
+  // force and check for draw
+  // playerOneOrderedDice = 10;
+  // playerTwoOrderedDice = 10;
+  if (isDraw(playerOneOrderedDice, playerTwoOrderedDice)) {
+    mode = "roll dice";
+    currentPlayer = 1;
+    return showDrawMessage();
   }
-  // TO DO - REFACTOR
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  // check which player wins
+  if (didPlayerOneWin(playerOneOrderedDice, playerTwoOrderedDice)) {
+    mode = "roll dice";
+    currentPlayer = 1;
+    return showPlayerOneWin();
+  } else {
+    mode = "roll dice";
+    currentPlayer = 1;
+    return showPlayerTwoWin();
+  }
 };
