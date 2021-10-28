82f9d152a8c8cc826ae7bb61fe7ae8f2dc9017d5
1Regina <1reginacheong@gmail.com>
2021-10-28 22:44:20 +0800

    refactoring out messages

--- script0.js
+++ script0.js
@@ -1,11 +1,9 @@
 var main = function (input) {
   // let myOutputValue = rollDice();
   // let myOutputValue = makeBigNum(num1, num2) 
-
   let myOutputValue = play2DiceTogether(input)
   return myOutputValue;
 };
-
 
 let rollDice = function() {
   randomNum = (Math.random() * 6) + 1
@@ -27,7 +25,6 @@ let makeBigNum = function(num1, num2){
   return makeNum
 }
 
-
 mode = "roll dice"
 let dice1_1 = 0
 let dice1_2 = 0
@@ -35,20 +32,17 @@ let dice2_1 = 0
 let dice2_2 = 0
 let playersNumbers = []
 console.log("aaaaaa")
+
 let play2DiceTogether = function(input) {
   console.log("bbbbb")
   console.log('start with', mode)
-  if (mode === "roll dice"){
-
-    console.log(`mode is`, mode)
-    mode = "p1 order dice"
+    if (mode === "roll dice"){
     dice1_1 = rollDice()
     dice1_2 = rollDice()
+    console.log(`mode is`, mode)
+    mode = "p1 order dice"
     console.log(`###############`)
-    return `Welcome Player 1. <br><br>
-    You rolled ${dice1_1} for Dice 1 and  ${dice1_2} for Dice 2. <br><br>
-    Choose the order of the dice.`
-
+    return `Welcome ` +  (orderDiceMsg(1,dice1_1,dice1_2))
   }
   console.log("out of the roll dice mode")
   console.log(`mode is`, mode)
@@ -65,11 +59,8 @@ let play2DiceTogether = function(input) {
     playersNumbers.push(makeNum)
     console.log(`Submitted score for plyaer 1 to compare`, playersNumbers)
     mode = "player 2 turn"
-    
     console.log(mode,`@@@@@@@@@@@@@`)
-    return `Player 1, you chose Dice 1 first. <br><br>
-    Your number is ${makeNum}.<br><br>
-    It is now Player 2's turn.` 
+    return createTotalMsg(1,1,makeNum) + `It is now Player 2's turn.` 
   } 
   if (mode === "p1 order dice" && input === '2'){
       mode = "make p1 dice 2 big"
@@ -83,17 +74,14 @@ let play2DiceTogether = function(input) {
       console.log(`Submitted score for plyaer 1 to compare`, playersNumbers)
       mode = "player 2 turn"
       console.log(mode,`%%%%%%%%%%%%%`)
-      return `Player 1, you chose Dice 2 first. <br><br>
-      Your number is ${makeNum}.<br><br>
-      It is now Player 2's turn.` 
+      return createTotalMsg(1,2,makeNum) + `It is now Player 2's turn.` 
     }
   
   if (mode === "player 2 turn"){
     dice2_1 = rollDice()
     dice2_2 = rollDice()
     mode = "player 2 decide"
-    return  `You rolled ${dice2_1} for Dice 1 and ${dice2_2} for Dice 2. <br><br>
-    Choose the order of the dice.`
+    return  orderDiceMsg(2,dice2_1,dice2_2)
   }
   if (mode === "player 2 decide" && input === '1') {
     mode = "make p2 dice 1 big"
@@ -105,8 +93,7 @@ let play2DiceTogether = function(input) {
     // makeNum = makeTens + dice2_2
     playersNumbers.push(makeNum)
     mode = "results"
-    return `Player 2, you chose Dice 1 first. <br><br>
-    Your number is ${makeNum}.<br>`
+    return createTotalMsg(2,1,makeNum)
   }
   if (mode === "player 2 decide" && input === '2') {
     mode = "make p2 dice 2 big"
@@ -117,23 +104,24 @@ let play2DiceTogether = function(input) {
     // makeNum = makeTens + dice2_1
     playersNumbers.push(makeNum)
     mode = "results"
-    return `Player 2, you chose Dice 1 first. <br><br>
-    Your number is ${makeNum}.<br>`
+    return createTotalMsg(2,2,makeNum)
   }
-  if (mode === "results" && (playersNumbers[0]> playersNumbers[1])){
+  let p1total = playersNumbers[0]
+  let p2total = playersNumbers[1]
+  if (mode === "results" && (p1total > p2total)){
     mode = "roll dice"
-    return `Player1 created ${playersNumbers[0]} <br><br>
-            Player2 created ${playersNumbers[1]} <br><br>
-            Player1 wins`
-  }else if (mode === "results" && (playersNumbers[0] < playersNumbers[1])){
+    outcome = createdMsg(1,2,p1total,p2total)
+    msg = outcome + `Player 1 wins`
+  }else if (mode === "results" && (p1total < p2total)){
     mode = "roll dice"        
-    return `Player1 created ${playersNumbers[0]} <br><br>
-            Player2 created ${playersNumbers[1]} <br><br>
-            Player2 wins`
+    outcome = createdMsg(1,2,p1total,p2total)
+    msg = outcome + `Player 2 wins`
   }else{                
             mode = "roll dice"
-            return `It is a draw!`
-        }      
+            msg =  `It is a draw!`
+        }     
+    playersNumbers = []
+    return msg 
   }
 
 let compoundNum = function(input,dice1, dice2){
@@ -148,3 +136,19 @@ let compoundNum = function(input,dice1, dice2){
     return compoundNumber
 }
 
+let orderDiceMsg = function(playerIndex,dice1, dice2) {
+  return `Player ${playerIndex}. <br><br> 
+           You rolled ${dice1} for Dice 1 and  ${dice2} for Dice 2. <br><br>
+           Choose the order of the dice.`
+}
+
+let createTotalMsg = function(playerIndex, diceIndex,makeNum) {
+  return `Player ${playerIndex}, you chose Dice ${diceIndex} first. <br><br>
+          Your number is ${makeNum}.<br><br>`
+}
+
+
+let createdMsg = function(p1, p2, p1total, p2total){
+  return `Player ${p1} created ${p1total} <br><br>
+          Player ${p2} created ${p2total} <br><br>`
+}
