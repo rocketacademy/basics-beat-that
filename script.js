5916bdaaba084056eba77ca41b73d9bdc7667ed4
Nateliew <weeli.tn@gmail.com>
2021-10-29 23:24:00 +0800

    roll dice

--- script.js
+++ script.js
@@ -9,13 +9,6 @@ var guess = "";
 var guess2 = "";
 var userTopGuess = "";
 
-//User press submit button to roll the dice
-
-var userDice = function () {
-  userNumber1 = rollDice();
-  userNumber2 = rollDice();
-};
-
 //Roll dice function
 var rollDice = function () {
   // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
@@ -29,6 +22,12 @@ var rollDice = function () {
   var diceNumber = randomInteger + 1;
 
   return diceNumber;
+};
+
+//User press submit button to roll the dice
+var userDice = function () {
+  userNumber1 = rollDice();
+  userNumber2 = rollDice();
 };
 
 //User choose option 1 or 2
@@ -61,6 +60,7 @@ var main = function (input) {
   if (mode == "start game") {
     mode = "choose number";
     guess = userDice();
+    console.log(guess);
     // guessNum1 = Number(guess);
     myOutputValue = `Player 1: <br> You chose ${userNumber1} and  ${userNumber2}. Please press 1 or 2`;
   }
@@ -75,6 +75,7 @@ var main = function (input) {
   // Computer will return the dice number and ask to choose
   else if (mode == "player 2") {
     guess2 = userDice();
+    console.log(guess2);
     // guessNum2 = Number(guess2);
     myOutputValue = `Player 2: <br> You chose ${userNumber1} and  ${userNumber2}. Please press 1 or 2`;
     console.log(guess2);
