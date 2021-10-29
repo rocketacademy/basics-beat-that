c33ee5d9168667b2ca355ab2426e259c6cd7c9f9
edwinyxt <yixiongtay@gmail.com>
2021-10-29 18:02:43 +0800

    included multi player mode

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
-var gameMode = "playerRoll"; //or "playerSelectOrder" "reportResult"
+var gameMode = "selectNumPlayers"; //or "playerSelectOrder" "reportResult" "playerRoll";
 var playerNum = 1;
-var playerOneChoice = 0;
-var playerTwoChoice = 0;
+var totalPlayerNum = 0;
+var playerChoices = [];
+var playerScores = [];
+var diceRolls = [];
 var concat1 = 0;
 var concat2 = 0;
 
 var main = function (input) {
-  if (gameMode == "playerRoll") {
+  if (gameMode == "selectNumPlayers") {
+    totalPlayerNum = input;
+    if (isNaN(input) || totalPlayerNum == 0) {
+      return "Please input number of players";
+    }
+
+    for (var counter = 0; counter < totalPlayerNum; counter += 1) {
+      playerScores[counter] = 0;
+    }
+    gameMode = "playerRoll";
+    return "Now Player 1 goes first. Click Submit to roll the dice.";
+  }
+
+  while (playerNum < totalPlayerNum) {
+    if (gameMode == "playerRoll") {
+      gameMode = "playerSelectOrder";
+      return rollTwoDice();
+    }
+
+    if (gameMode == "playerSelectOrder") {
+      playerSelectionMessage = storePlayerChoices(input);
+      playerNum += 1;
+      gameMode = "playerRoll";
+      playerSelectionMessage += `<br>It is now Player ${playerNum}'s turn. Click Submit to roll the dice`;
+      return playerSelectionMessage;
+    }
+  }
+
+  if (gameMode == "playerRoll" && playerNum == totalPlayerNum) {
     gameMode = "playerSelectOrder";
     return rollTwoDice();
   }
 
-  if (gameMode == "playerSelectOrder" && playerNum == 1) {
-    var playerSelectionMessage = `Player ${playerNum}, `;
-    playerNum += 1;
-    var nextPlayerMessage = `It is now Player ${playerNum}'s turn. Click Submit to roll the dice`;
-    if (input == 1) {
-      playerOneChoice = concat1;
-      playerSelectionMessage += `you chose Dice 1 first.<br>Your number is ${concat1}.<br>${nextPlayerMessage}`;
-    } else {
-      playerOneChoice = concat2;
-      playerSelectionMessage += `you chose Dice 2 first.<br>Your number is ${concat2}.<br>${nextPlayerMessage}`;
-    }
-    gameMode = "playerRoll";
-    return playerSelectionMessage;
-  }
-
-  if (gameMode == "playerSelectOrder" && playerNum == 2) {
-    var playerSelectionMessage = `Player ${playerNum}, `;
-    if (input == 1) {
-      playerTwoChoice = concat1;
-      playerSelectionMessage += `you chose Dice 1 first.<br>Your number is ${concat1}.<br>`;
-    } else {
-      playerTwoChoice = concat2;
-      playerSelectionMessage += `you chose Dice 2 first.<br>Your number is ${concat2}.<br>`;
-    }
+  if (gameMode == "playerSelectOrder" && playerNum == totalPlayerNum) {
+    playerSelectionMessage = storePlayerChoices(input);
     gameMode = "reportResult";
+    playerSelectionMessage += "<br>Click Submit to see the results";
     return playerSelectionMessage;
   }
 
   if (gameMode == "reportResult") {
-    results = `Player 1 has ${playerOneChoice}, Player 2 has ${playerTwoChoice}`;
+    results = `${playerChoices} ${playerScores}`;
+    // results = `Player 1 has ${playerChoices[0]}, Player 2 has ${playerChoices[1]}, Player 1 has total score ${playerScores[0]}, Player 2 has total score ${playerScores[1]}<br>Click Submit to play another round`;
+    playerNum = 1;
+    gameMode = "playerRoll";
     return results;
   }
 };
@@ -58,4 +70,23 @@ var rollTwoDice = function () {
   concat2 = Number(dice2 + dice1);
   var rollResultMessage = `Welcome ${playerName}.<br>You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br>Choose the order of the dice.<br><br>1. Dice1, Dice2 - ${concat1}<br>2. Dice2, Dice1 - ${concat2}`;
   return rollResultMessage;
+};
+
+var diceRollXTimes = function (x, highOrLowMode) {
+  var randomSixInteger = 1 + Math.floor(Math.random() * 6);
+  return randomSixInteger;
+};
+
+var storePlayerChoices = function (input) {
+  var playerSelectionMessage = `Player ${playerNum}, `;
+  if (input == 1) {
+    playerChoices[playerNum - 1] = concat1;
+    playerSelectionMessage += `you chose Dice 1 first.<br>Your number is ${concat1}.`;
+    playerScores[playerNum - 1] += concat1;
+  } else {
+    playerChoices[playerNum - 1] = concat2;
+    playerSelectionMessage += `you chose Dice 2 first.<br>Your number is ${concat2}.`;
+    playerScores[playerNum - 1] += concat2;
+  }
+  return playerSelectionMessage;
 };
