a5cac39f481c74a0f8203ad4a982a8278b41c1f6
WJLLEE <wayne.jllee@hotmail.com>
2021-10-29 14:53:32 +0800

    Variable Number of Dice

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var highestCombinedNumMode = `Highest Combined Number Mode`;
 var lowestCombinedNumMode = `Lowest Combined Number Mode`;
 var gameMode;
 
+var numDices;
+
 var diceRollMode = `Roll Dice Mode`;
 var diceSelectMode = `Dice Selection Mode`;
 var winnerMode = `Determine Winner Mode`;
@@ -11,10 +13,18 @@ var currPlayer = 1;
 
 var diceRoll = [];
 
-var player1Num;
-var player2Num;
+var playerNum;
+var player1Num = 0;
+var player2Num = 0;
 var player1Score = 0;
 var player2Score = 0;
+
+var resetGame = function () {
+  currPlayer = 1;
+  stageMode = diceRollMode;
+  player1Num = 0;
+  player2Num = 0;
+};
 
 var rollDice = function () {
   var randomInteger = Math.floor(Math.random() * 6);
@@ -23,79 +33,58 @@ var rollDice = function () {
 };
 
 var getDiceRoll = function () {
-  diceRoll = [rollDice(), rollDice()];
   stageMode = diceSelectMode;
-  return `Welcome Player ${currPlayer}. <br>You rolled <strong><u>${diceRoll[0]}</u></strong> for Dice 1 and <strong><u>${diceRoll[1]}</u></strong> for Dice 2. <br>Click Submit to get the combined number.`;
+  var welcomeMessgae = `Welcome Player ${currPlayer}.`;
+  var rolledMessage = [];
+  for (d = 0; d < numDices; d += 1) {
+    diceRoll.push(rollDice());
+    rolledMessage.push(
+      ` <strong><u>${diceRoll[d]}</u></strong> for Dice ${d + 1}`
+    );
+  }
+  return `${welcomeMessgae} <br>You rolled${rolledMessage} <br>Click Submit to get the combined number.`;
 };
 
 var validateSelection = function (selectedNum) {
   return selectedNum == 1 || selectedNum == 2;
 };
 
+var validateNumDices = function (selectedNumDice) {
+  if (selectedNumDice != 0 && selectedNumDice != 1) {
+    return selectedNumDice == selectedNumDice;
+  }
+};
+
 var arrayToNum = function (num1, num2) {
   return Number(String(num1) + String(num2));
 };
 
-/* var getPlayersNum = function (selectedDice) {
-  if (currPlayer == 1) {
-    if (selectedDice == 1) {
-      player1Num = arrayToNum(diceRoll[0], diceRoll[1]);
-    } else {
-      player1Num = arrayToNum(diceRoll[1], diceRoll[0]);
-    }
-    stageMode = diceRollMode;
-    currPlayer = 2;
-    return `Player 1, you chose Dice ${selectedDice} first.<br>Your number is ${player1Num}.<br>It is now Player 2's turn. <br><br>Click Submit to roll dice`;
+var sortArray = function () {
+  if (gameMode == highestCombinedNumMode) {
+    diceRoll = diceRoll.sort(function (a, b) {
+      return b - a;
+    });
+    return diceRoll;
+  } else {
+    return diceRoll.sort();
   }
-  if (currPlayer == 2) {
-    if (selectedDice == 1) {
-      player2Num = arrayToNum(diceRoll[0], diceRoll[1]);
-    } else {
-      player2Num = arrayToNum(diceRoll[1], diceRoll[0]);
-    }
-    stageMode = winnerMode;
-    currPlayer = 1;
-    return `Player 2, you chose Dice ${selectedDice} first.<br>Your number is ${player2Num}.<br><br>Click Submit to view winner!!`;
-  }
-}; */
+};
 
-var getPlayersNumAuto = function (selectedDice) {
+var getPlayersNumAuto = function () {
+  diceRoll = sortArray();
   if (currPlayer == 1) {
-    if (gameMode == highestCombinedNumMode) {
-      if (diceRoll[0] > diceRoll[1]) {
-        player1Num = arrayToNum(diceRoll[0], diceRoll[1]);
-      } else {
-        player1Num = arrayToNum(diceRoll[1], diceRoll[0]);
-      }
-    }
-    if (gameMode == lowestCombinedNumMode) {
-      if (diceRoll[0] > diceRoll[1]) {
-        player1Num = arrayToNum(diceRoll[1], diceRoll[0]);
-      } else {
-        player1Num = arrayToNum(diceRoll[0], diceRoll[1]);
-      }
+    while (diceRoll.length > 0) {
+      player1Num = arrayToNum(player1Num, diceRoll.shift());
     }
     stageMode = diceRollMode;
     currPlayer = 2;
     return `Player 1 <br>Your number is ${player1Num}.<br><br>It is now Player 2's turn. <br>Click Submit to roll dice`;
   }
   if (currPlayer == 2) {
-    if (gameMode == highestCombinedNumMode) {
-      if (diceRoll[0] > diceRoll[1]) {
-        player2Num = arrayToNum(diceRoll[0], diceRoll[1]);
-      } else {
-        player2Num = arrayToNum(diceRoll[1], diceRoll[0]);
-      }
-    }
-    if (gameMode == lowestCombinedNumMode) {
-      if (diceRoll[0] > diceRoll[1]) {
-        player2Num = arrayToNum(diceRoll[1], diceRoll[0]);
-      } else {
-        player2Num = arrayToNum(diceRoll[0], diceRoll[1]);
-      }
+    while (diceRoll.length > 0) {
+      player2Num = arrayToNum(player2Num, diceRoll.shift());
     }
     stageMode = winnerMode;
-    currPlayer = 1;
     return `Player 2 <br>Your number is ${player2Num}.<br><br>Click Submit to view winner!!`;
   }
 };
@@ -136,19 +125,28 @@ var getWinner = function () {
 
 var main = function (input) {
   if (!gameMode) {
-    var gameModeSelection = validateSelection(input);
-    if (gameModeSelection == false) {
+    var gameModeSelected = validateSelection(input);
+    if (gameModeSelected == false) {
       return `Incorrect input! <br><br>Please choose a game mode to start playing! <br>1: Highest Combined Number <br>2: Lowest
         Combined Number`;
     }
-    if (gameModeSelection == true) {
+    if (gameModeSelected == true) {
       if (input == 1) {
         gameMode = highestCombinedNumMode;
-        return `You have choose: ${gameMode}. <br>Click submit to start the game.`;
+        return `You have choose: ${gameMode}. <br>Please select the number of dices (2 or more) use for the game`;
       } else {
         gameMode = lowestCombinedNumMode;
         return `You have choose: ${gameMode}. <br>Click submit to start the game.`;
       }
+    }
+  }
+
+  if (!numDices) {
+    var numDicesSelected = validateNumDices(input);
+    if (numDicesSelected == true) {
+      numDices = input;
+    } else {
+      return `Please select the number of dices (2 or more) use for the game`;
     }
   }
 
@@ -158,6 +156,8 @@ var main = function (input) {
     var newDiceRoll = getDiceRoll();
     return gameModeMessage + newDiceRoll;
   }
+  //Console Log
+  //console.log(`Checkpoint 1: ${diceRoll}`);
 
   if (stageMode == diceSelectMode) {
     var playersNum;
@@ -167,7 +167,7 @@ var main = function (input) {
 
   if (stageMode == winnerMode) {
     var winner = getWinner();
-    stageMode = diceRollMode;
+    resetGame();
     return gameModeMessage + winner;
   }
 };
