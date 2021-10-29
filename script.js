ceb9d40bec77da3db1d7eeefe6332919ace7570d
CheongYuting <cheongyuting.design@gmail.com>
2021-10-29 23:32:04 +0800

    finished base section

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+//let's define player modes
+var currentplayer = 1;
+// let's create a database to store the players' numbers. Don't forget to reset this to 0 whenever you restart the game.
+var playernumber = [];
+var player1number = playernumber[0];
+var player2number = playernumber[1];
 //let's define the game modes
 var rolldicemode = "Roll the dice";
 var chooseordermode = "Choose order of dice";
 var currentGameMode = rolldicemode;
-// let's set a counter for the number of times each player rolled
-var counter = 0;
 // let's create a database to record the dice numbers rolled.
 var diceNumbersRolled = [];
 var diceRoll1 = "";
 var diceRoll2 = "";
+// set output value as a global variable.
+var myOutputValue = "";
 
 //
 //
@@ -16,50 +22,106 @@ var diceRoll2 = "";
 //
 //let's define the main function
 var main = function (input) {
-  var myOutputValue = "";
+  var gamecounter = 0;
   if (currentGameMode == rolldicemode) {
-    // First we need to make sure the dice is rolled twice.
-    while (counter < 2) {
-      counter = counter + 1;
-      //01 Activate the game by rolling the dice
-      var diceNumber = diceRoll();
-      console.log("dicenumber: " + diceNumber);
-      //Store the dice numbers rolled
-      diceNumbersRolled.push(diceNumber);
-    }
-    console.log("dice number data store: " + diceNumbersRolled);
-
-    // Let's assign the dice numbers rolled to the global variables we created above.
-    diceRoll1 = diceNumbersRolled[0];
-    diceRoll2 = diceNumbersRolled[1];
-    console.log("1st dice number: " + diceRoll1);
-    console.log("2nd dice number: " + diceRoll2);
-
-    //02 Let's tell the player what his dice roll is
-    myOutputValue =
-      "You rolled " +
-      diceRoll1 +
-      " and " +
-      diceRoll2 +
-      ". Choose which number you want as the first numeral of your combined number.";
-    console.log("output: " + myOutputValue);
-    // 03 We need to change the game mode to choosing the dice order
+    // We will execute the roll dice game. We need gamecounter as a parameter as the rollDiceGame function uses this variable.
+    myOutputValue = rollDiceGame(gamecounter);
     currentGameMode = chooseordermode;
   }
-  //
-  //
-  // Now let's choose the order of your dice rolled
+
+  // Next, let's choose the order of your dice rolled.
   else if (currentGameMode == chooseordermode) {
-    // If player chooses 1st dice number as first numberal
-    myOutputValue = "Your combined number is " + diceRoll1 + diceRoll2;
+    // If player chooses 1st dice number as first numeral
+    myOutputValue =
+      "Your combined number is " +
+      diceRoll1 +
+      diceRoll2 +
+      ".<br> <br> Click submit to continue.";
+    var combinednumber = String(diceRoll1) + String(diceRoll2);
+
     // If player chooses 2nd dice number as first numeral
     if (input == diceRoll2) {
-      myOutputValue = "Your combined number is " + diceRoll2 + diceRoll1;
+      myOutputValue =
+        "Your combined number is " +
+        diceRoll2 +
+        diceRoll1 +
+        ".<br> <br> Click submit to continue.";
+      combinednumber = String(diceRoll2) + String(diceRoll1);
     }
-    console.log("output: " + myOutputValue);
-  }
-};
 
+    // let's push the combined number of the player into our database
+    playernumber.push(combinednumber);
+    console.log("player numbers array: " + playernumber);
+    console.log("output: " + myOutputValue);
+    console.log("combined number: " + combinednumber);
+    // let's return the game mode to roll dice mode.
+    currentGameMode = rolldicemode;
+    // don't forget to reset the database of diceNumberRolled, because this will affect the value of the combined number which is made of up DiceRoll1 and DiceRoll2.
+    diceNumbersRolled = [];
+
+    // This is how we restart the round for player 2
+    if (currentplayer == 2) {
+      // change game mode to decide which is winner
+      currentGameMode = "Announce Winner";
+    } else {
+      currentplayer = currentplayer + 1;
+    }
+  } else if ((currentGameMode = "Announce Winner")) {
+    myOutputValue = announceWinner();
+  }
+  return myOutputValue;
+};
+//
+//
+//
+//
+//
+//
+var rollDiceGame = function (gamecounter) {
+  console.log("player switch" + currentplayer);
+  // First we need to make sure the dice is rolled twice.
+  while (gamecounter < 2) {
+    gamecounter = gamecounter + 1;
+    //Execute the roll dice function
+    var diceNumber = diceRoll();
+    //Store the dice numbers rolled
+    diceNumbersRolled.push(diceNumber);
+    console.log("push " + diceNumber);
+  }
+  console.log("dice number data store: " + diceNumbersRolled);
+
+  // Let's assign the dice numbers rolled to the global variables we created above.
+  diceRoll1 = diceNumbersRolled[0];
+  diceRoll2 = diceNumbersRolled[1];
+  console.log("1st dice number: " + diceRoll1);
+  console.log("2nd dice number: " + diceRoll2);
+  return (
+    "Alright Player " +
+    currentplayer +
+    "! <br> <br> You rolled " +
+    diceRoll1 +
+    " and " +
+    diceRoll2 +
+    ". <br> <br> Choose which number you want as the first numeral of your combined number."
+  );
+};
+//
+//
+//
+//
+//
+//
+var announceWinner = function () {
+  myOutputValue = "Player 1 wins! To play again, click submit.";
+  if (playernumber[1] > playernumber[0]) {
+    myOutputValue = "Player 2 wins! To play again, click submit.";
+  }
+  currentGameMode = rolldicemode;
+  currentplayer = 1;
+  playernumber = [];
+  return myOutputValue;
+};
+//
 //
 //
 //
@@ -72,3 +134,6 @@ var diceRoll = function () {
   var diceNumber = randomInteger + 1;
   return diceNumber;
 };
+
+// To-dos 1) refactor 2) create game mode for deciding winner
+// Double check parameters for each function
