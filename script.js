3edf07f030f26d27c45a4bb85394c3ed4b6973fa
EmilySinML <02October2021!>
2021-10-29 23:55:23 +0800

    Emily Sin 11-14

--- script.js
+++ script.js
@@ -1,60 +1,121 @@
-var firstMode = "Player rolls dice";
-var secondMode = "Player select combination";
-var gameMode = firstMode;
+//Game Mode
+var INPUTMODE = "Input number of players";
+var ROLLMODE = "Roll dice";
+var ORDERMODE = "Order dice roll";
+var COMPAREMODE = "Compare dice roll";
+var gameMode = INPUTMODE;
 
-var rollDiceFirst = "";
-var rollDiceSecond = "";
+//Global Variable
+var numberOfPlayers = "";
+var playerCounter = 1;
+var myOutputValue = "";
+var diceRollOne = "";
+var diceRollTwo = "";
+var diceRollCombined = "";
+var diceRollArray = [0];
+var playerScore = [0];
 
-var counter = 1;
-var numberOfPlayers = 2;
-var playersArray = [];
+//Message Template
+var PLAYMSG =
+  "Welcome Player " +
+  playerCounter +
+  "<br >You rolled " +
+  diceRollOne +
+  " for Dice 1 and " +
+  diceRollTwo +
+  " for Dice 2.<br >Choose the order of the dice.";
 
 var main = function (input) {
-  while (counter <= numberOfPlayers) {
-    var player = "Player ";
-    if (gameMode == firstMode) {
-      player += counter;
-      rollDiceFirst = rollDice();
-      rollDiceSecond = rollDice();
-      gameMode = secondMode; // Change game mode to First Mode
-      myOutputValue =
-        "Welcome " +
-        player +
-        ".<br >You rolled " +
-        rollDiceFirst +
-        " for Dice 1 and " +
-        rollDiceSecond +
-        " for Dice 2.<br >Choose the order of the dice.";
-      return myOutputValue;
-    } else if (gameMode == secondMode) {
-      player += counter;
-      if (input == "Dice 1") {
-        rollDiceCombi = Number(rollDiceFirst + "" + rollDiceSecond);
-      } else {
-        rollDiceCombi = Number(rollDiceSecond + "" + rollDiceFirst);
-      }
-      playersArray.push(rollDiceCombi);
-      myOutputValue =
-        player +
-        ", you chose " +
-        input +
-        " first. Your number is " +
-        rollDiceCombi +
-        ".<br >";
-      gameMode = firstMode; // Reset game mode to First Mode
-      counter = counter + 1;
-      if (counter == 2) {
-        myOutputValue +=
-          "It is now Player " +
-          counter +
-          "'s turn. Please trigger Submit to continue."; // complicating myOutputValue because life is not complicated enough.
+  //Input
+  if (gameMode == INPUTMODE) {
+    numberOfPlayers = input;
+    var i = 1;
+    while (i <= numberOfPlayers) {
+      playerScore.push(0);
+      i = i + 1;
+    }
+    gameMode = ROLLMODE;
+    return (
+      "Number of players input: " +
+      numberOfPlayers +
+      "<br >Click Submit to begin playing."
+    );
+  }
+  //Roll Order Compare
+  else {
+    while (playerCounter <= numberOfPlayers) {
+      if (gameMode == ROLLMODE) {
+        diceRollOne = rollDice();
+        diceRollTwo = rollDice();
+        gameMode = ORDERMODE;
+        return (
+          "Welcome Player " +
+          playerCounter +
+          "<br >You rolled " +
+          diceRollOne +
+          " for Dice 1 and " +
+          diceRollTwo +
+          " for Dice 2.<br >Choose the order of the dice."
+        );
+      } else if (gameMode == ORDERMODE) {
+        if (input == 1) {
+          diceRollCombined = Number(diceRollOne + "" + diceRollTwo);
+        } else {
+          diceRollCombined = Number(diceRollTwo + "" + diceRollOne);
+        }
+        console.log("diceRollCombined: ", diceRollCombined);
+        diceRollArray.push(diceRollCombined);
+        console.log("diceRollArray: ", diceRollArray);
+        myOutputValue =
+          "Player " +
+          playerCounter +
+          ", you chose to order by Dice " +
+          input +
+          " first. Your final number is: " +
+          diceRollCombined +
+          ". ";
+        if (playerCounter < numberOfPlayers) {
+          gameMode = ROLLMODE;
+          playerCounter = playerCounter + 1;
+          myOutputValue +=
+            "<br >It is now Player " +
+            playerCounter +
+            "'s turn. Press Submit to continue.";
+        } else {
+          gameMode = COMPAREMODE;
+        }
+      } else if (gameMode == COMPAREMODE) {
+        //Find winner for this round
+        i = 0;
+        var winningPlayerID = "";
+        var max = diceRollArray[0];
+        while (i < numberOfPlayers) {
+          i = i + 1;
+          if (max <= diceRollArray[i]) {
+            max = diceRollArray[i];
+            winningPlayerID = i;
+            console.log("max: ", max);
+            console.log("winningPlayerID: ", winningPlayerID);
+          }
+        }
+        playerScore[winningPlayerID] = playerScore[winningPlayerID] + 1;
+        console.log("playerScore:", playerScore);
+        myOutputValue =
+          "Player " +
+          winningPlayerID +
+          " wins this round with " +
+          max +
+          "! <br >Click Submit to continue playing.";
+        gameMode = ROLLMODE;
+        playerCounter = 1;
+        max = "";
+        winningPlayerID = "";
+        diceRollArray = [0];
         return myOutputValue;
       }
+
+      return myOutputValue;
     }
-    myOutputValue =
-      compareDiceRoll(playersArray[0], playersArray[1]) +
-      "The rolls by Player 1 and Player 2 were respectively: " +
-      playersArray;
   }
   return myOutputValue;
 };
@@ -68,16 +129,4 @@ var rollDice = function () {
   // Add 1 to get valid dice rolls of 1 through 6 inclusive.
   var diceNumber = randomInteger + 1;
   return diceNumber;
-};
-
-var compareDiceRoll = function (playerOneRoll, playerTwoRoll) {
-  var compareDiceRollResults = "";
-  if (playerOneRoll == playerTwoRoll) {
-    compareDiceRollResults = "Both players draw!<br >";
-  } else if (playerOneRoll > playerTwoRoll) {
-    compareDiceRollResults = "Player 1 wins!<br >";
-  } else {
-    compareDiceRollResults = "Player 2 wins!<br >";
-  }
-  return compareDiceRollResults;
 };
