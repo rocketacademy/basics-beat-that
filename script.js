b9db79ec57cd3d577b0fc69fee6c2ed7c85f3abd
aqifsariman <79038951+aqifsariman@users.noreply.github.com>
2021-10-31 06:03:04 +0800

    Completed Beat That Game with reversed mode included.

--- script.js
+++ script.js
@@ -1,103 +1,160 @@
-/*
-1. There are 2 players and players take turns.
-
-2. When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
-
-3. The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
-
-4. After both players have rolled and chosen dice order, the player with the higher combined number wins.
-*/
-
 // Global variables
-var currentMode = "Roll the dice";
+var currentMode = "start";
+var reversedMode = false; // adding reversed mode
 var myOutputValue = "";
-var player = "Player 1";
-var player1Results = "";
-var player2Results = "";
+var player = "";
 var player1Score = 0;
 var player2Score = 0;
+var player1DiceOrder = [];
+var player2DiceOrder = [];
+var player1FinalNumb = 0;
+var player2FinalNumb = 0;
 
 // Generate dices
 var diceRollGenerator = function () {
-  var randomDecimal = Math.random() * 6;
-  var randomDiceRoll = Math.floor(randomDecimal) + 1;
-  return randomDiceRoll;
+  return Math.floor(Math.random() * 6) + 1;
+};
+// Specifying the 2 dices for each player
+var diceRoll1Player1 = diceRollGenerator();
+var diceRoll2Player1 = diceRollGenerator();
+var diceRoll1Player2 = diceRollGenerator();
+var diceRoll2Player2 = diceRollGenerator();
+
+// Rolling the dice for player 1
+var player1RollDice = function () {
+  return `Player 1: Dice 1 rolled ${diceRoll1Player1} and Dice 2 rolled ${diceRoll2Player1}. <br><br> Please input 1 or 2 to choose the order of your dice roll.`;
 };
 
-// Specifying the 2 dices
-var diceRoll1 = diceRollGenerator();
-var diceRoll2 = diceRollGenerator();
-
-var orderOfDice = function (input) {
-  if (input == 1) {
-    return `Your number is ${diceRoll1}${diceRoll2}`;
-  }
-  if (input == 2) {
-    return `Your number is ${diceRoll2}${diceRoll1}`;
-  }
+// Order of the dices for player 1
+var userDiceOrderPlayer1 = [];
+var diceOrder1Player1 = function () {
+  userDiceOrderPlayer1.push(diceRoll1Player1, diceRoll2Player1);
+  return `Player 1: Your number is ${(player1FinalNumb = userDiceOrderPlayer1
+    .join("")
+    .trim())}.<br><br> Player 2 press Enter to roll your dice.`; // to combine the 0 and 1 index together
+};
+var diceOrder2Player1 = function () {
+  userDiceOrderPlayer1.push(diceRoll2Player1, diceRoll1Player1);
+  return `Player 1: Your number is ${(player1FinalNumb = userDiceOrderPlayer1
+    .join("")
+    .trim())}. <br><br> Player 2 press Enter to roll your dice.`; // to combine the 0 and 1 index together
 };
 
-// Running the game
-var generateBeatThat = function (input) {
-  if (currentMode == "Roll the dice") {
-    diceRoll1 = diceRollGenerator();
-    diceRoll2 = diceRollGenerator();
-    myOutputValue = `Dice 1 rolled ${diceRoll1} and Dice 2 rolled ${diceRoll2}. <br><br> Please input 1 or 2 to choose the order of your dice roll.`;
-    currentMode = "Dice rolled";
-  } else if (currentMode == "Dice rolled" && player == "Player 1") {
-    player1Results = orderOfDice(input);
-    myOutputValue = `Player 1: ${player1Results}
-      <br><br> Player 2: It is now your turn to roll the dice.<br><br> Press Enter to begin.`;
-    currentMode = "Roll the dice";
-    player = "Player 2";
-  } else if (currentMode == "Dice rolled" && player == "Player 2") {
-    player2Results = orderOfDice(input);
-    myOutputValue = player2Results;
-    currentMode = "results";
-    player = "results";
+// Rolling the dice for player 2
+var player2RollDice = function () {
+  return `Player 2: Dice 1 rolled ${diceRoll1Player2} and Dice 2 rolled ${diceRoll2Player2}. <br><br> Please input 1 or 2 to choose the order of your dice roll.`;
+};
+
+// Order of dices for player 2
+var userDiceOrderPlayer2 = [];
+var diceOrder1Player2 = function () {
+  userDiceOrderPlayer2.push(diceRoll1Player2, diceRoll2Player2);
+  return `Player 2: Your number is ${(player2FinalNumb = userDiceOrderPlayer2
+    .join("")
+    .trim())}.<br><br> Press Enter to view results.`; // to combine the 0 and 1 index together
+};
+
+var diceOrder2Player2 = function () {
+  userDiceOrderPlayer2.push(diceRoll2Player2, diceRoll1Player2);
+  return `Player 2: Your number is ${(player2FinalNumb = userDiceOrderPlayer2
+    .join("")
+    .trim())}. <br><br> Press Enter to view results.`; // to combine the 0 and 1 index together
+};
+
+// Leaderboard display
+var displayLBoard = function () {
+  if (player1Score > player2Score || player1Score == player2Score) {
+    return `LEADERBOARD <br><br> Player 1 Score: ${player1Score} <br><br> Player 2 Score: ${player2Score}<br><br> Press Enter to start another round.`;
+  } else {
+    return `LEADERBOARD <br><br> Player 2 Score: ${player2Score} <br><br> Player 1 Score: ${player1Score}<br><br> Press Enter to start another round.`;
   }
-  if (currentMode == "results" && player == "results") {
-    myOutputValue = `Player 1: ${player1Results} <br><br> Player 2: ${player2Results} <br><br> Press Enter to view results. `;
-    currentMode = "End play";
-    player = "End play";
-  } else if (
-    player1Results > player2Results &&
-    player == "End play" &&
-    currentMode == "End play"
-  ) {
-    myOutputValue = ` Player 1:
-      ${player1Results}. 
-      <br><br>
-    Player 2:
-      ${player2Results}.
-      <br><br> Player 1 wins! <br><br> Press enter to view current score.`;
-    player1Score++;
-    currentMode = "End score";
-  } else if (
-    player2Results > player1Results &&
-    player == "End play" &&
-    currentMode == "End play"
-  ) {
-    myOutputValue = ` Player 1:
-      ${player1Results}. 
-      <br><br>
-    Player 2:
-      ${player2Results}.
-      <br><br> Player 2 wins! <br><br> Press Enter to view current score.`;
-    player2Score++;
-    currentMode = "End score";
-  } else if (currentMode == "End score" && player == "End play") {
-    myOutputValue = `Player 1 Score: ${player1Score} <br><br> Player 2 Score: ${player2Score}<br><br> Press Enter to start another round.`;
-    currentMode = "Restart";
-    player = "Player 1";
-  } else if (currentMode == "Restart" && player == "Player 1") {
-    myOutputValue = `Press Enter to start rolling the dice.`;
-    currentMode = "Roll the dice";
-  }
-  return myOutputValue;
 };
 
 var main = function (input) {
-  var beatThat = generateBeatThat(input);
-  return beatThat;
+  if (currentMode == "start") {
+    myOutputValue = `Choose normal or reversed mode to begin 🎲`;
+  }
+  if (currentMode == "start" && input == "normal") {
+    myOutputValue = `You have chosen NORMAL mode. <br><br> Press enter to start rolling the dice 🎲`;
+    currentMode = "normal mode";
+  } else if (currentMode == "start" && input == "reversed") {
+    myOutputValue = `You have chosen REVERSED mode. <br><br> Press enter to start rolling the dice 🎲`;
+    currentMode = "normal mode";
+    reversedMode = true;
+    // normal mode will roll the dice
+  } else if (currentMode == "normal mode") {
+    diceRoll1Player1 = diceRollGenerator();
+    diceRoll2Player1 = diceRollGenerator();
+    myOutputValue = player1RollDice();
+    currentMode = "player 1 rolled";
+    // input the order
+  } else if (currentMode == "player 1 rolled" && input == 1) {
+    var player1Order = diceOrder1Player1();
+    currentMode = "player 2 start";
+    return player1Order;
+  } else if (currentMode == "player 1 rolled" && input == 2) {
+    var player1Order = diceOrder2Player1();
+    currentMode = "player 2 start";
+    return player1Order;
+
+    // player 2 roll the dice
+  } else if (currentMode == "player 2 start") {
+    diceRoll1Player2 = diceRollGenerator();
+    diceRoll2Player2 = diceRollGenerator();
+    myOutputValue = player2RollDice();
+    currentMode = "player 2 rolled";
+  } else if (currentMode == "player 2 rolled" && input == 1) {
+    var player2Order = diceOrder1Player2();
+    currentMode = "results";
+    return player2Order;
+  } else if (currentMode == "player 2 rolled" && input == 2) {
+    var player2Order = diceOrder2Player2();
+    currentMode = "results";
+    return player2Order;
+
+    // shows result
+  } else if (currentMode == "results" && player1FinalNumb == player2FinalNumb) {
+    myOutputValue = `Its a draw. <br><br> Player 1: ${player1FinalNumb} <br><br> Player 2: ${player2FinalNumb}`;
+    currentMode = "leaderboard";
+  } else if (
+    currentMode == "results" &&
+    player1FinalNumb > player2FinalNumb &&
+    reversedMode == false
+  ) {
+    player1Score++;
+    currentMode = "leaderboard";
+    myOutputValue = `Player 1 wins!<br><br> Player 1: ${player1FinalNumb} <br><br> Player 2: ${player2FinalNumb}`;
+  } else if (
+    currentMode == "results" &&
+    player1FinalNumb < player2FinalNumb &&
+    reversedMode == false
+  ) {
+    player2Score++;
+    myOutputValue = `Player 2 wins!<br><br> Player 1: ${player1FinalNumb} <br><br> Player 2: ${player2FinalNumb}`;
+    currentMode = "leaderboard";
+  } else if (
+    currentMode == "results" &&
+    reversedMode == true &&
+    player1FinalNumb < player2FinalNumb
+  ) {
+    player1Score++;
+    currentMode = "leaderboard";
+    myOutputValue = `Player 1 wins!<br><br> Player 1: ${player1FinalNumb} <br><br> Player 2: ${player2FinalNumb}`;
+  } else if (
+    currentMode == "results" &&
+    reversedMode == true &&
+    player1FinalNumb > player2FinalNumb
+  ) {
+    player2Score++;
+    myOutputValue = `Player 2 wins!<br><br> Player 1: ${player1FinalNumb} <br><br> Player 2: ${player2FinalNumb}`;
+    currentMode = "leaderboard";
+  }
+  // shows leaderboard
+  else if (currentMode == "leaderboard") {
+    myOutputValue = displayLBoard();
+    currentMode = "start";
+    userDiceOrderPlayer1.length = 0; // reset values back to zero
+    userDiceOrderPlayer2.length = 0;
+  }
+  return myOutputValue;
 };
