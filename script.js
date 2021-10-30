ee56249514d471dfecc3116a26c976880a6f0573
READANDMUSE <JOANNE.TANCE@GMAIL.COM>
2021-10-31 00:27:24 +0800

    add base script without determining winner yet

--- script.js
+++ script.js
@@ -6,6 +6,14 @@
 var currentGameMode = "Waiting for players..";
 console.log(currentGameMode);
 
+// Keep track of the current player's number, either 1 or 2.
+// The game starts with Player 1.
+var currPlayer = 1;
+
+// keeping track of each player's chosen numbers
+var playerOneNumber;
+var playerTwoNumber;
+
 // helper functions
 var diceRoll = function () {
   var randomDecimal = Math.random() * 6;
@@ -15,9 +23,7 @@ var diceRoll = function () {
 };
 
 var generateDiceOrder = function () {
-  var randomDecimal = Math.random() * 2;
-  var randomInteger = Math.floor(randomDecimal);
-  var diceOrder = randomInteger + 1; // Add 1 to get valid dice order from 1 to 2 inclusive
+  var diceOrder = Math.ceil() * 2; // Add 1 to get valid dice order from 1 to 2 inclusive
   return diceOrder;
 };
 
@@ -27,53 +33,51 @@ var main = function (input) {
   if (currentGameMode == "Waiting for players..") {
     // switch the mode
 
-    var diceOne = diceRoll();
-    console.log("number for dice one", diceOne);
-    var diceTwo = diceRoll();
-    console.log("number for dice two", diceTwo);
+    playerOneDiceRoll = [diceRoll(), diceRoll()];
+    console.log("dice number for player one", playerOneDiceRoll);
+
     currentGameMode = "Player 1 has rolled the dice";
     console.log(currentGameMode);
 
-    return `Welcome Player 1! You rolled ${diceOne} for Dice 1 and ${diceTwo} for Dice 2. Choose the order of the dice.`;
+    return `Welcome Player 1! You rolled ${playerOneDiceRoll[0]} for Dice 1 and ${playerOneDiceRoll[1]} for Dice 2. Choose the order of the dice.`;
   } else if (currentGameMode == "Player 1 has rolled the dice") {
-    // current mode then no more dicerolls
-
-    var chooseDiceOrder = generateDiceOrder();
-    console.log("dice order", chooseDiceOrder);
-
-    if (chooseDiceOrder == "1") {
+    if (input == 1) {
+      console.log("player 1 dice order", input);
+      currentGameMode = "Player 1 has chosen dice order. Player 2 next";
+      console.log(currentGameMode);
+      return `You have chosen Dice 1 to go first. Your number is ${playerOneDiceRoll[0]}${playerOneDiceRoll[1]}. Now is Player 2's turn!`;
+    }
+    if (input == 2) {
+      console.log("player 1 dice order", input);
       currentGameMode = "Player 1 has chosen dice order. Player 2 next";
       console.log(currentGameMode);
 
-      return `You have chosen Dice 1 to go first. Your number is ${diceOne}${diceTwo}. Now is Player 2's turn!`;
+      return `You have chosen Dice 2 to go first. Your number is ${playerOneDiceRoll[1]}${playerOneDiceRoll[0]}. Now is Player 2's turn!`;
     }
-
-    if (chooseDiceOrder == "2") {
-      currentGameMode = "Player 1 has chosen dice order. Player 2 next";
-      console.log(currentGameMode);
-
-      return `You have chosen Dice 2 to go first. Your number is ${diceTwo}${diceOne}. Now is Player 2's turn!`;
-    }
-
-    return myOutputValue;
   }
 
-  if ((currentGameMode = "Player 2 has rolled the dice!")) {
+  if (currentGameMode == "Player 1 has chosen dice order. Player 2 next") {
+    playerTwoDiceRoll = [diceRoll(), diceRoll()];
+    console.log("dice number for player two", playerTwoDiceRoll);
     currentGameMode = "Player 2 has rolled the dice!";
     console.log(currentGameMode);
-    var myOutputValue = `Welcome Player 2! You rolled ${diceOne} for Dice 1 and ${diceTwo} for Dice 2. Choose the order of the dice.`;
 
-    if (currentGameMode == "Player 2 has rolled the dice!") {
-      if (input == 1) {
-        currentGameMode = "Player 2 has chosen dice order";
-        myOutputValue = `You have chosen Dice 1 to go first. Your number is ${diceOne}${diceTwo}.`;
-      }
+    return `Welcome Player 2! You rolled ${playerTwoDiceRoll[0]} for Dice 1 and ${playerTwoDiceRoll[1]} for Dice 2. Choose the order of the dice.`;
+  } else if (currentGameMode == "Player 2 has rolled the dice!") {
+    if (input == 1) {
+      console.log("player 2 dice order", input);
+      currentGameMode = "Player 2 has chosen dice order";
 
-      if (input == 2) {
-        currentGameMode = "Player 2 has chosen dice order";
-        myOutputValue = `You have chosen Dice 2 to go first. Your number is ${diceTwo}${diceOne}.`;
-      }
+      console.log(currentGameMode);
+      return `You have chosen Dice 1 to go first. Your number is ${playerTwoDiceRoll[0]}${playerTwoDiceRoll[1]}.`;
     }
-    return myOutputValue;
+
+    if (input == 2) {
+      currentGameMode = "Player 2 has chosen dice order";
+      console.log("player 2 dice order", input);
+      return `You have chosen Dice 2 to go first. Your number is ${playerTwoDiceRoll[1]}${playerTwoDiceRoll[0]}.`;
+    }
   }
+
+  return myOutputValue;
 };
