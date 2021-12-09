519cd5464a682cc1abc5c1a8de6ab8711bf6dd43
ALLYESQUE <ALLYESQUE@GMAIL.COM>
2021-12-09 19:25:46 +0800

    added replay mode

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ var ENTERPLAYERNAME = `player enters name`;
 var ROLLDICE = `player rolls dice`;
 var DICEORDER = `player chooses dice order`;
 var COMPARENUMBERS = `compare final nunmbers`;
+var REPLAY = `play again`;
 var gameMode = ENTERPLAYERNAME;
 var playerNames = [];
 var playerDiceNum = [];
 var playerFinalNum = [];
 var isPlayer1turn = true;
+var afterFirstRound = false;
+var winCount = [0, 0];
 
 //players enter name
 var enterPlayerName = function (inputUserName) {
@@ -74,7 +77,11 @@ var getTwoDiceNum = function () {
 var orderDice = function (userChoice) {
   var message = ``;
   var playerChoice = ``;
-  if (gameMode == DICEORDER && isPlayer1turn == true) {
+  if (
+    gameMode == DICEORDER &&
+    isPlayer1turn == true &&
+    afterFirstRound == false
+  ) {
     if (!(userChoice == `1` || userChoice == `2`)) {
       message =
         `Please enter <b>1</b> or <b>2</b>.` +
@@ -93,6 +100,34 @@ var orderDice = function (userChoice) {
         "<br><br>It's the next player's turn now! <br><br>Player 2, please enter your <b>name</b>.";
       console.log(`player 1 num: ` + playerFinalNum[0]);
       gameMode = ENTERPLAYERNAME;
+      console.log("after play 1 choice: " + gameMode);
+      isPlayer1turn = false;
+      return message;
+    }
+  } else if (
+    gameMode == DICEORDER &&
+    isPlayer1turn == true &&
+    afterFirstRound == true
+  ) {
+    if (!(userChoice == `1` || userChoice == `2`)) {
+      message =
+        `Please enter <b>1</b> or <b>2</b>.` +
+        "<br><br>" +
+        `In case you forgot, you rolled a <b>${playerDiceNum[0]}</b> for dice 1 and a <b>${playerDiceNum[1]} </b>for dice 2. `;
+      return message;
+    } else {
+      if (userChoice == `1`) {
+        playerChoice = `${playerDiceNum[0]}` + `${playerDiceNum[1]}`;
+      } else if (userChoice == `2`) {
+        playerChoice = `${playerDiceNum[1]}` + `${playerDiceNum[0]}`;
+      }
+      playerFinalNum.push(playerChoice);
+      message =
+        `<b>${playerNames[0]}</b>, your number is <b>${playerFinalNum[0]}</b>. ` +
+        "<br><br>" +
+        `It's <b>${playerNames[1]}</b>'s turn now, press <b>submit</b> to roll the two dice.`;
+      console.log(`player 1 num: ` + playerFinalNum[0]);
+      gameMode = ROLLDICE;
       console.log("after play 1 choice: " + gameMode);
       isPlayer1turn = false;
       return message;
@@ -127,18 +162,57 @@ var compareFinalNum = function () {
   var message = ``;
   if (gameMode == COMPARENUMBERS) {
     if (playerFinalNum[0] > playerFinalNum[1]) {
+      winCount[0] += 1;
       message = `Congrats <b>${playerNames[0]}</b>, you won!`;
     } else if (playerFinalNum[1] > playerFinalNum[0]) {
+      winCount[1] += 1;
       message = `Congrats <b>${playerNames[1]}</b>, you won!`;
+    } else if ((playerFinalNum[1] = playerFinalNum[0])) {
+      message = `Unbelievable! It's a draw!`;
     }
+
     result =
       "<b>RESULTS:</b> <br><br>" +
       `${playerNames[0]}'s number: ${playerFinalNum[0]} ` +
       "<br>" +
       `${playerNames[1]}'s number: ${playerFinalNum[1]}` +
       "<br><br>";
-    return result + message;
+
+    score =
+      "<br><br><u>Current score:</u><br>" +
+      `${playerNames[0]}'s score: ${winCount[0]} ` +
+      "<br>" +
+      `${playerNames[1]}'s score: ${winCount[1]}` +
+      "<br><br>";
+
+    playAgainMessage = "<br><br>Play again? <b>yes 🥳</b> or <b>no 😔</b>";
+    gameMode = REPLAY;
+    console.log(`after round ends: ${gameMode}`);
+
+    return result + message + score + playAgainMessage;
   }
+};
+
+// Option to play again
+var replayGame = function (userChoice) {
+  var message = ``;
+  if (gameMode == REPLAY && userChoice == `yes`) {
+    message =
+      `Alright!! Let's go!!` +
+      "<br><br>" +
+      `<b>${playerNames[0]}</b>, press the <b>Submit</b> button to roll the two dice.`;
+    playerDiceNum = [];
+    playerFinalNum = [];
+    isPlayer1turn = true;
+    afterFirstRound = true;
+    gameMode = ROLLDICE;
+    console.log(`after player chooses to replay: ${gameMode}`);
+    console.log(`playerDiceNum: ${playerDiceNum}`);
+    console.log(`playerFinalNum: ${playerFinalNum}`);
+  } else if (gameMode == REPLAY && userChoice == `no`) {
+    message = `Aww, alright.... Close this window and hope to see you again! Byeeeee!`;
+  }
+  return message;
 };
 
 var main = function (input) {
@@ -151,6 +225,8 @@ var main = function (input) {
     myOutputValue = orderDice(input);
   } else if (gameMode == COMPARENUMBERS) {
     myOutputValue = compareFinalNum();
+  } else if (gameMode == REPLAY) {
+    myOutputValue = replayGame(input);
   }
   return myOutputValue;
 };
