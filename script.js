22f50d77c86e31bad08cdf09f52266a1d3fe3a06
nathannzq <nathannzq@gmail.com>
2021-12-21 20:55:10 +0800

    blackjack

--- script.js
+++ script.js
@@ -1,202 +1,280 @@
-/* 
-There are 2 players and players take turns.
-When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
-The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
-After both players have rolled and chosen dice order, the player with the higher combined number wins.
+/*
+There will be only two players. One human and one computer (for the Base solution).
+The computer will always be the dealer.
+Each player gets dealt two cards to start.
+The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
+The dealer has to hit if their hand is below 17.
+Each players' score is the total of their card ranks. Jacks/Queen/Kings are 10. Aces can be 1 or 11.
+The player who is closer to, but not above 21 wins the hand.
 */
 
-// Global Var
-player1diceResults = []
-player2diceResults = []
-player1 = ""
-player2 = ""
-player1Number = 1
-player2Number = 2
-// 
+// Global Variables declariation
+var player = [
+  {
+    name: "player",
+    hands: [[]],
+    points: 0, // array of points and wagers corresponding to hands
+    wager: 0,
+    chips: 100,
+    readyState: [false], // array denoting whether the hand is settled
+  },
+];
+// var playerHandValue = checkValue(player[0]);
 
+var computer = [
+  {
+    name: "computer",
+    hands: [[]],
+    points: 0, // array of points and wagers corresponding to hands
+    wager: 0,
+    chips: 100,
+    readyState: [false], // array denoting whether the hand is settled
+  },
+];
+// var computerHandValue = checkValue(computer[0]);
 
-var rollDice = function(){
-  // Create a dice rolling from 1 - 6
-  var randomDice = Math.floor(Math.random() * 6) + 1
-  console.log(`Dice roll: ${randomDice}`)
-  return randomDice
-}
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
 
-var roll2Dice = function(array){
-  firstDice = rollDice()
-  secondDice = rollDice()
-  array.push(firstDice)
-  array.push(secondDice)
-  console.log(`roll2Dice: ${firstDice}, ${secondDice}`)
-  console.log(`array: ${array}`)
-  console.log(`player1diceResults: ${player1diceResults}`)
-  console.log(`player2diceResults: ${player2diceResults}`)
-  return array
-}
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
 
-var playerTurn = function(playerNumber,choice,playerArray){
-  if (playerNumber == 1 && choice == null){
-    player1diceResults = roll2Dice(player1diceResults)
-    myOutputValue = `Player ${playerNumber}, Here is your rolled dice result: <br><br>
-    dice 1 : ${player1diceResults[0]} <br><br>
-    dice 2 : ${player1diceResults[1]} <br><br>
-    Please choose the order of desired dice result. <br>
-    Type 1 and click enter if you wish for dice 1 to be the first number <br>
-    Type 2 and click enter if you wish for dice 2 to be the first number`;
-    return myOutputValue
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
   }
-  else if (playerNumber == 2 && choice == null){
-    player2diceResults = roll2Dice(player2diceResults)
-    myOutputValue = `Player ${playerNumber}, Here is your rolled dice result: <br><br>
-    dice 1 : ${player2diceResults[0]} <br><br>
-    dice 2 : ${player2diceResults[1]} <br><br>
-    Please choose the order of desired dice result. <br>
-    Type 1 and click enter if you wish for dice 1 to be the first number <br>
-    Type 2 and click enter if you wish for dice 2 to be the first number`;
-    return myOutputValue
-  }
-  else if (choice == 1){
-    myOutputValue = `${playerNumber} have chose dice 1 to be the first in order, your result is ${playerArray[0]}${playerArray[1]} <br><br>
-    Click enter for next.`;
 
-    console.log(`player ${playerNumber} dice results: ${playerArray[0]}${playerArray[1]}`);
-    console.log(`user dice order: ${choice}`);
-    return myOutputValue
-  }
-  else if (choice == 2){
-    myOutputValue = `${playerNumber} have chose dice 2 to be the first in order, your result is ${playerArray[1]}${playerArray[0]} <br><br>
-    Click enter for next.`;
+  // Return the completed card deck
+  console.log(cardDeck);
+  return cardDeck;
+};
 
-    console.log(`player ${playerNumber} dice results: ${playerArray[1]}${playerArray[0]}`);
-    console.log(`user dice order: ${choice}`);
-    return myOutputValue
-  }
-}
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
 
-// 3 conditions: 
-// a > b
-// b < a
-// a == b
-var gameOutcome = function () {
-  if (Number(player1diceResults.join("")) > Number(player2diceResults.join(""))) {
-    myOutputValue = `Congratulations ${player1} for winning!!! <br><br>
-    ${player1} BEATS ${player2} with a score of ${Number(player1diceResults.join(""))} against ${Number(player2diceResults.join(""))} <br><br>
-    Click enter to start another new game`;
-    return myOutputValue;
-  } 
-  else if (Number(player2diceResults.join("")) > Number(player1diceResults.join(""))) {
-    myOutputValue = `Congratulations ${player2} for winning!!! <br><br>
-     ${player2} BEATS ${player1} with a score of ${Number(player2diceResults.join(""))} against ${Number(player1diceResults.join(""))}<br><br>
-    Click enter to start another new game`;
-    return myOutputValue;
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
   }
-  else if (Number(player2diceResults.join("")) == Number(player1diceResults.join(""))){
-    myOutputValue = `Issa Draw lubba dubba dub dub <br><br>
-    Click enter to start another new game`;
-    return myOutputValue;
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+// Initialise deck creation and shuffling
+var deck = makeDeck();
+var shuffledDeck = shuffleCards(deck);
+
+// var draw2Cards = function (who) {
+//   who.hands = [shuffledDeck.pop(), shuffledDeck.pop()];
+//   return who;
+// };
+
+// var splitOption = function(){
+
+// }
+
+var checkValue = function (player) {
+  player.points = 0;
+  console.log(player);
+  // getting values of player first hand, if split implemented, do another for loop to change hands
+  // player.hands[0] = [shuffledDeck.pop(), shuffledDeck.pop()];
+  for (var i = 0; i < player.hands[0].length; i += 1) {
+    console.log(`${player.name} cards: ${player.hands[0]}`);
+    var currCardCounter = 0;
+    var acesInHand = 0;
+
+    // getting value of each card from player first hand
+    while (currCardCounter < player.hands[i].length) {
+      console.log(`running card ${currCardCounter}`);
+      var currCard = player.hands[i][currCardCounter];
+      // Deal with aces and picture rank values
+      /* Special case for aces:
+      if 2 aces dealt at start but allow for split
+      else aces will be 11 for 2 cards
+      else aces will be 1 for 3 cards and above
+      */
+      if (currCard.rank == 1) {
+        player.points += 11;
+        acesInHand += 1;
+      } else if (
+        currCard.rank == 11 ||
+        currCard.rank == 12 ||
+        currCard.rank == 13
+      ) {
+        player.points += 10;
+      } else {
+        player.points += currCard.rank;
+      }
+      // Check for if 2 aces dealt at start
+      if (player.hands[i].length == 2 && acesInHand == 2) {
+        player.points = 21;
+        // myOutputValue = `You have 2 aces for a blackjack win! or do you want to split?`;
+        return player;
+        // check condition for split later
+      }
+      if (player.hands[i].length > 2 && acesInHand >= 2) {
+        if (player.points > 21) {
+          var aceCounter = 0;
+          while (aceCounter < acesInHand) {
+            player.points -= 10;
+            aceCounter += 1;
+            if (player.points < 21) {
+              break;
+            }
+          }
+        }
+      }
+      currCardCounter += 1;
+    }
+    return player;
   }
 };
 
+mainGameFunction = `start`;
 
-
-
-currentGameMode = "Welcome to Beat That"
 var main = function (input) {
-  if (currentGameMode == "Welcome to Beat That"){
-    myOutputValue = `Welcome to BEAT THAT!<br><br>
-     Player 1 please enter your name and click ENTER, followed by player 2 name and click ENTER to start the game. <br><br>
-      Game Rules: <br>
-      1. This is a 2 person, player vs player game. <br>
-      2. The game will roll 2 dice and show the dice rolls result. For example, dice 1 rolled 3 and dice 2 rolled 4. <br>
-      3. The player that rolled the dice will pick the order of the dice.(If they wanted the number 43, they would specify that the 2nd dice goes first.) <br>
-      4. After both players have rolled and chosen their dice order, the player with the higher combined number wins.  `;
-
-    currentGameMode = "Enter Player 1 name";
-    console.log(`currentGameMode: ${currentGameMode}`);
-
+  if (mainGameFunction == "start") {
+    myOutputValue = `Welcome to blackjack please press enter to start the game`;
+    mainGameFunction = "draw cards";
     return myOutputValue;
   }
-  // Take in player 1 and 2 names
-  if (currentGameMode == "Enter Player 1 name"){
-    player1 = input
-    currentGameMode = "Enter Player 2 name"
-    console.log(`Player 1: ${player1}`);
-    myOutputValue = `Welcome ${player1}. <br> Player 2, please enter your name click enter. `;
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
+  if (mainGameFunction == "draw cards") {
+    // Draw 2 cards from the top of the deck
+    player[0].hands[0] = [shuffledDeck.pop(), shuffledDeck.pop()];
+    computer[0].hands[0] = [shuffledDeck.pop(), shuffledDeck.pop()];
+    checkValue(player[0]).points;
+    console.log(`Playerhands: ${player[0].points}`);
+    checkValue(computer[0]).points;
+    console.log(`Computerhands: ${computer[0].points}`);
+    // Construct an output string to communicate which cards were drawn
+    var myOutputValue =
+      "Computer had " +
+      computer[0].points +
+      ".<br><br>Player had " +
+      player[0].points +
+      ".<br><br>" +
+      "Player would you like to hit or stand?";
+    mainGameFunction = "waiting instructions";
+    return myOutputValue;
   }
-  if (currentGameMode == "Enter Player 2 name"){
-    player2 = input
-    currentGameMode = "Player 1 turn"
-    console.log(`Player 2: ${player1}`);
-    myOutputValue = `Welcome ${player2}. <br> Player 1, press enter to start your roll `;
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
-  }
-  // 
-  var choiceErrorMsg = function(playerNum){
-    if(playerNum == 1){
-      return `Please enter your choice. <br><br> 
-      dice 1 : ${player1diceResults[0]} <br><br>
-      dice 2 : ${player1diceResults[1]} <br><br>
-      Please choose the order of desired dice result. <br>
-      Type 1 and click enter if you wish for dice 1 to be the first number <br>
-      Type 2 and click enter if you wish for dice 2 to be the first number`}
-    else if (playerNum == 2){
-      return `Please enter your choice. <br><br> 
-      dice 1 : ${player2diceResults[0]} <br><br>
-      dice 2 : ${player2diceResults[1]} <br><br>
-      Please choose the order of desired dice result. <br>
-      Type 1 and click enter if you wish for dice 1 to be the first number <br>
-      Type 2 and click enter if you wish for dice 2 to be the first number`
+  if (mainGameFunction == "waiting instructions") {
+    if (input == "hit") {
+      player[0].hands[0].push(shuffledDeck.pop());
+      checkValue(player[0]).points;
+      console.log(`Playerhands: ${player[0].points}`);
+      checkValue(computer[0]).points;
+      console.log(`Computerhands: ${computer[0].points}`);
+      myOutputValue =
+        "Computer had " +
+        computer[0].points +
+        ".<br><br>Player had " +
+        player[0].points +
+        ".<br><br>" +
+        "Player would you like to hit or stand?";
+      mainGameFunction = "waiting instructions";
+      return myOutputValue;
+    } else if (input == "stand") {
+      myOutputValue = `Player final hand value is ${player[0].points}!<br><br>Computer's turn to draw`;
+      mainGameFunction = "computer turn";
+      console.log("Computer turn!");
+      console.log(`main game function ${mainGameFunction}`);
+      return myOutputValue;
+    } else {
+      myOutputValue = "Please input either hit or stand";
+      mainGameFunction = "waiting instructions";
+      return myOutputValue;
     }
   }
+  if (mainGameFunction == "computer turn") {
+    console.log("In computer turn");
+    if (computer[0].points < 17) {
+      computer[0].hands[0].push(shuffledDeck.pop());
+      checkValue(player[0]).points;
+      console.log(`Playerhands: ${player[0].points}`);
+      checkValue(computer[0]).points;
+      console.log(`Computerhands: ${computer[0].points}`);
+      myOutputValue =
+        "Computer had " +
+        computer[0].points +
+        ".<br><br>Player had " +
+        player[0].points +
+        ".<br><br>" +
+        "Computer thinking whether to draw....";
+      return myOutputValue;
+    }
+  }
+  myOutputValue =
+    "Computer final value is: " +
+    computer[0].points +
+    ".<br><br>Player final value is: " +
+    player[0].points +
+    ".";
+  mainGameFunction = "start";
+  if (computer[0].points > player[0].points && computer[0].points <= 21) {
+    finalOutput = "Computer Wins!<br><br>" + myOutputValue;
+  } else if (computer[0].points < player[0].points && player[0].points <= 21) {
+    finalOutput = "Player Wins!<br><br>" + myOutputValue;
+  } else if (computer[0].points > 21) {
+    finalOutput = "Computer Busts!<br><br>" + myOutputValue;
+  } else if (player[0].points > 21) {
+    finalOutput = "Player Busts!<br><br>" + myOutputValue;
+  } else if (computer[0].points == player[0].points) {
+    finalOutput = "ISSA DRAW!<br><br>" + myOutputValue;
+  }
 
-  // Start player's turn
-  // Player will output dice result
-  // Player will choose which dice to put first
-  // ^Create shared function to do 2 things to be reused for player 2
-  if(currentGameMode == "Player 1 turn"){
-    myOutputValue = playerTurn(1)
-    currentGameMode = "Player 1 choice"
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
-  }
-  if(currentGameMode == "Player 1 choice" && input != ""){
-    player1DiceOrder = input
-    myOutputValue = playerTurn(1, player1DiceOrder,player1diceResults)
-    currentGameMode = "Player 2 turn"
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
-  }
-  else if(currentGameMode == "Player 1 choice" && input == ""){
-    currentGameMode = "Player 1 choice"
-    return choiceErrorMsg(1)
-  }
-  if(currentGameMode == "Player 2 turn"){
-    myOutputValue = playerTurn(2)
-    currentGameMode = "Player 2 choice"
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
-  }
-  if(currentGameMode == "Player 2 choice" && input != ""){
-    player2DiceOrder = input
-    myOutputValue = playerTurn(2, player2DiceOrder,player2diceResults)
-    currentGameMode = "Game Outcome"
-    console.log(`currentGameMode: ${currentGameMode}`);
-    return myOutputValue
-  }
-  else if(currentGameMode == "Player 2 choice" && input == ""){
-    currentGameMode = "Player 2 choice"
-    return choiceErrorMsg(2)
-  }
-  if (currentGameMode == "Game Outcome"){
-    myOutputValue = gameOutcome()
-    console.log(`currentGameMode: ${currentGameMode}`);
-    currentGameMode = "Player 1 turn"
-    // reinitialise array data
-    player1diceResults = []
-    player2diceResults = []
-    return myOutputValue
-  }
+  // Return the fully-constructed output string
+  return finalOutput;
 };
