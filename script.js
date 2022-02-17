ce6ba957dc1d8342c008d5465c319f4a98247709
Floatingtales <dillan.martin@ymail.com>
2022-02-18 06:08:59 +0800

    added a css file, edited html, done minimal work on js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//global constant initialisation
+const HIGHEST_MODE = `highest mode`;
+const LOWEST_MODE = `lowest mode`;
+const STANDARD_MODE = `Standard`;
+const KNOCKOUT_MODE = `Knockout`;
+
+//global variable initialisation
+var playerRolls = [];
+var playerNumber = 0;
+var diceNumber = 2;
+var diceMode = ``;
+var gameMode = ``;
+var currentPlayers = [];
+
+//helper function initialisation
+
+/***
+ * simulates a dice roll
+ * @returns {number} a random number between 1-6
+ */
+var randomizeDiceRoll = function () {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
 var main = function (input) {
   var blorp = "";
   var myOutputValue = "hello world";
