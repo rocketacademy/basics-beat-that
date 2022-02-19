07a82cae89c243ccaa62ffc641e7ce3c10eaa46a
eva.wang <eva.wang81@gmail.com>
2022-02-19 21:15:27 +0800

    added rollDice function

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let mode = "Player one";
 let diceNum1 = "";
 let diceNum2 = "";
 let num_p1, num_p2;
+
+// Generate a random dice number
+function rollDice() {
+  let diceNum = Math.floor(Math.random() * 6) + 1;
+  return diceNum;
+}
