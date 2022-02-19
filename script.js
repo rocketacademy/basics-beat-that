22735cad41fde9a6ab92ae6d7aa158916974cbc0
eva.wang <eva.wang81@gmail.com>
2022-02-19 21:17:25 +0800

    added main function

--- script.js
+++ script.js
@@ -18,3 +18,55 @@ function getOrder(order, num1, num2) {
   if (order == "2") orderedNum = num2 * 10 + num1;
   return orderedNum;
 }
+
+// Get the winner and display
+let main = function (input) {
+  // get two dice number
+  if (input == "" && diceNum1 == "" && diceNum2 == "") {
+    diceNum1 = rollDice();
+    diceNum2 = rollDice();
+    return `Hello ${mode}! Your dice 1 is: ${diceNum1}, dice 2 is: ${diceNum2}. 
+      <br>Which dice goes first, "1" or "2" ?`;
+  }
+
+  if (mode == "Player one") {
+    // Validate player input
+    if (input != "1" && input != "2")
+      return `Your dice 1 is: ${diceNum1}, dice 2 is: ${diceNum2}.
+      <br>Please enter "1" or "2" to get your number.`;
+    num_p1 = getOrder(input, diceNum1, diceNum2);
+    mode = "Player two";
+    diceNum1 = rollDice();
+    diceNum2 = rollDice();
+    return `Hey Player 1, <br>Your number is ${num_p1}. 
+    <br><br>Player 2, it's your turn now! Your dice 1 is: ${diceNum1}, dice 2 is: ${diceNum2}.
+    <br>Which dice goes first, "1" or "2"?`;
+  }
+
+  if (mode == "Player two") {
+    // Validate player input
+    if (input != "1" && input != "2")
+      return `your dice 1 is: ${diceNum1}, dice 2 is: ${diceNum2}
+      <br>Which dice goes first, "1" or "2"`;
+    num_p2 = getOrder(input, diceNum1, diceNum2);
+
+    // Check winner
+    if (num_p1 > num_p2) {
+      let displayMessage = `Hey Player 2, <br> Your Dice 1 is: ${diceNum1}, Dice 2 is: ${diceNum2} 
+      <br>Your number is ${num_p2}, player 1's number is ${num_p1}
+      <hr>Player 1, you are the winner!`;
+      diceNum1 = "";
+      diceNum2 = "";
+      mode = "Player one";
+      return displayMessage;
+    } else {
+      let displayMessage = `Hey Player 2, <br> Your Dice 1 is: ${diceNum1}, Dice 2 is: ${diceNum2} 
+      <br>Your number is ${num_p2}, player 1's number is ${num_p1}
+      <hr>Player 2, you are the winner!`;
+      diceNum1 = "";
+      diceNum2 = "";
+      mode = "Player one";
+      return displayMessage;
+    }
+  }
+};
