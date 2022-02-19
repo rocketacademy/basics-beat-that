d5488b098e4cb885a44f1b8e9618bfeef65a0882
eva.wang <eva.wang81@gmail.com>
2022-02-19 21:16:41 +0800

    added select order function

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function rollDice() {
   let diceNum = Math.floor(Math.random() * 6) + 1;
   return diceNum;
 }
+
+// Get the order your two dice numbers
+function getOrder(order, num1, num2) {
+  let orderedNum;
+  if (order == "1") orderedNum = num1 * 10 + num2;
+  if (order == "2") orderedNum = num2 * 10 + num1;
+  return orderedNum;
+}
