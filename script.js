ddd3c419dbffccb88f0e33daddc441bd03389cae
githubvan <vanessa.eng.ym@gmail.com>
2022-02-19 22:14:30 +0800

    added compare values function. error with invalid options input

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 var gameState = "player1Roll";
 var player1RollRecords = [];
 var player2RollRecords = [];
+var player1PointsRecord = [];
+var player2PointsRecord = [];
 
 // roll dice function
 var rollDice = function () {
@@ -26,11 +28,13 @@ var chooseNumberFunction1 = function (input) {
   if (input == 1) {
     var player1Points =
       String(player1RollRecords[0]) + String(player1RollRecords[1]);
+    player1PointsRecord.push(Number(player1Points));
     outputMessage = `Player 1, your value is ${player1Points}`;
   }
   if (input == 2) {
     var player1Points =
       String(player1RollRecords[1]) + String(player1RollRecords[0]);
+    player1PointsRecord.push(Number(player1Points));
     outputMessage = `Player 1, your value is ${player1Points}`;
   }
   return outputMessage;
@@ -46,11 +50,13 @@ var chooseNumberFunction2 = function (input) {
   if (input == 1) {
     var player2Points =
       String(player2RollRecords[0]) + String(player2RollRecords[1]);
+    player2PointsRecord.push(Number(player2Points));
     outputMessage = `Player 2, your value is ${player2Points}`;
   }
   if (input == 2) {
     var player2Points =
       String(player2RollRecords[1]) + String(player2RollRecords[0]);
+    player2PointsRecord.push(Number(player2Points));
     outputMessage = `Player 2, your value is ${player2Points}`;
   }
   return outputMessage;
@@ -58,7 +64,7 @@ var chooseNumberFunction2 = function (input) {
 
 // MAIN FUNCTION!
 var main = function (input) {
-  //console.log(gameState);
+  //player 1 roll and choose
   if (gameState == "player1Roll") {
     while (player1RollRecords.length < 2) {
       player1RollRecords.push(rollDice());
@@ -74,7 +80,7 @@ var main = function (input) {
     return outputMessage;
   }
 
-  //console.log(gameState);
+  //player 2 roll and choose
   if (gameState == "player2Roll") {
     while (player2RollRecords.length < 2) {
       player2RollRecords.push(rollDice());
@@ -86,6 +92,25 @@ var main = function (input) {
   }
   if (gameState == "player2ChooseNumber") {
     outputMessage = chooseNumberFunction2(input);
+    gameState = "compareValues";
     return outputMessage;
+  }
+  console.log(player1PointsRecord);
+  console.log(player2PointsRecord);
+
+  //compare player 1 and player 2 values
+  if (gameState == "compareValues") {
+    if (Number(player1PointsRecord[0]) > Number(player2PointsRecord[0])) {
+      outputMessage = `Player 1, you have won. <br>
+    Player 1 value: ${player1PointsRecord[0]} <br> 
+    Player 2 value: ${player2PointsRecord[0]}.`;
+      return outputMessage;
+    }
+    if (Number(player1PointsRecord[0]) < Number(player2PointsRecord[0])) {
+      outputMessage = `Player 2, you have won. <br>
+    Player 1 value: ${player1PointsRecord[0]} <br> 
+    Player 2 value: ${player2PointsRecord[0]}.`;
+      return outputMessage;
+    }
   }
 };
