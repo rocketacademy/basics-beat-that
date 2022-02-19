1224a59fd0746a0c04df0ea1481dcd6257df4260
eva.wang <eva.wang81@gmail.com>
2022-02-19 21:14:57 +0800

    declaired variables

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+// Base  (v1 9:20am - 10:20am)
+
+let mode = "Player one";
+let diceNum1 = "";
+let diceNum2 = "";
+let num_p1, num_p2;
