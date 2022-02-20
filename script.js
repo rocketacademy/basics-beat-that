0646bc616f48281bcf8a50ba45f590f50b853094
sianapple <briankoheh@hotmail.com>
2022-02-20 22:53:52 +0800

    Base: update for player 2 array, player 2 dice roll and player 2 conditions. Updated for  winning conditions as well.

--- script.js
+++ script.js
@@ -59,15 +59,18 @@ var diceRollGameModePlayer1 = "Initiate dice roll for player 1";
 var diceOrderGameModePlayer1 = "Choose dice order for player 1";
 var diceRollGameModePlayer2 = "Initiate dice roll for player 2";
 var diceOrderGameModePlayer2 = "Choose dice order for player 2";
+var compareScoreGameMode = "compare score";
 var gameMode = diceRollGameModePlayer1;
-var diceRollArray = [];
+var diceRollArray1 = [];
+var diceRollArray2 = [];
 var chooseNumberArray = [];
+var doubleDigitNumber = ``;
 var counter = 0;
 
 var main = function (input) {
   if (gameMode == diceRollGameModePlayer1) {
     // define variable for PLayer 1's roll dice array
-    var myOutputValue = rollAndStoreDice();
+    var myOutputValue = rollAndStoreDicePlayer1();
 
     //change game mode to dice order game mode
     gameMode = diceOrderGameModePlayer1;
@@ -75,44 +78,119 @@ var main = function (input) {
     return myOutputValue;
   }
 
+  // roll dice and set order for Player 1
   if (gameMode == diceOrderGameModePlayer1) {
     if (input == 1) {
-      var doubleDigitNumber = Number(`${diceRollArray[0]}${diceRollArray[1]}`);
-      chooseNumberArray = chooseNumberArray.push(doubleDigitNumber);
-      console.log(chooseNumberArray);
+      doubleDigitNumber = parseInt(`${diceRollArray1[0]}${diceRollArray1[1]}`);
+      chooseNumberArray.push(doubleDigitNumber);
 
       gameMode = diceRollGameModePlayer2;
-      console.log(gameMode);
       return `Hi Player 1, you have chosen ${doubleDigitNumber}. Now it is Player 2's turn to play.`;
     }
     if (input == 2) {
-      var doubleDigitNumber = Number(`${diceRollArray[1]}${diceRollArray[0]}`);
-      chooseNumberArray = chooseNumberArray.push(doubleDigitNumber);
-      console.log(chooseNumberArray);
+      doubleDigitNumber = parseInt(`${diceRollArray1[1]}${diceRollArray1[0]}`);
+      chooseNumberArray.push(doubleDigitNumber);
 
       gameMode = diceRollGameModePlayer2;
-      console.log(gameMode);
+
       return `Hi Player 1, you have chosen ${doubleDigitNumber}. Now it is Player 2's turn to play.`;
     }
     if (input != 1 || input != 2) {
       return `Hi Player 1, you have not typed in 1 or 2, please type in 1 or 2 <br> <br>
-      You have rolled Dice 1 as ${diceRollArray[0]} and Dice 2 as ${diceRollArray[1]}. <br> <br>`;
+      You have rolled Dice 1 as ${diceRollArray1[0]} and Dice 2 as ${diceRollArray1[1]}. <br> <br>`;
+    }
+  }
+
+  if (gameMode == diceRollGameModePlayer2) {
+    // define variable for PLayer 2's roll dice array
+    var myOutputValue2 = rollAndStoreDicePlayer2();
+
+    //change game mode to dice order game mode
+    gameMode = diceOrderGameModePlayer2;
+
+    return myOutputValue2;
+  }
+
+  // roll dice and set order for Player 2
+  if (gameMode == diceOrderGameModePlayer2) {
+    if (input == 1) {
+      doubleDigitNumber = parseInt(`${diceRollArray2[0]}${diceRollArray2[1]}`);
+      chooseNumberArray.push(doubleDigitNumber);
+
+      console.log(gameMode);
+
+      // move to compare score game mode
+      gameMode = compareScoreGameMode;
+      return `Hi Player 2, you have chosen ${doubleDigitNumber}.`;
+    }
+    if (input == 2) {
+      doubleDigitNumber = parseInt(`${diceRollArray2[1]}${diceRollArray2[0]}`);
+      chooseNumberArray.push(doubleDigitNumber);
+      console.log(chooseNumberArray);
+
+      console.log(gameMode);
+
+      // move to compare score game mode
+      gameMode = compareScoreGameMode;
+      return `Hi Player 2, you have chosen ${doubleDigitNumber}.`;
+    }
+    if (input != 1 || input != 2) {
+      return `Hi Player 2, you have not typed in 1 or 2, please type in 1 or 2 <br> <br>
+      You have rolled Dice 1 as ${diceRollArray2[0]} and Dice 2 as ${diceRollArray2[1]}. <br> <br>`;
+    }
+  }
+
+  // Determine winner
+  if (gameMode == compareScoreGameMode) {
+    // player 1 wins condition
+    if (chooseNumberArray[0] > chooseNumberArray[1]) {
+      // reset to player 1 game mode
+      gameMode = diceRollGameModePlayer1;
+      // show winning message
+      return `Hi Player 1, you have won! Your number is ${chooseNumberArray[0]}, Player 2's number is ${chooseNumberArray[1]}.`;
+    }
+    // player 2 wins condition
+    if (chooseNumberArray[1] > chooseNumberArray[0]) {
+      // reset to player 1 game mode
+      gameMode = diceRollGameModePlayer1;
+      // show winning message
+      return `Hi Player 2, you have won! Your number is ${chooseNumberArray[1]}, Player 1's number is ${chooseNumberArray[0]}.`;
+    }
+    // draw condition
+    if (chooseNumberArray[0] == chooseNumberArray[1]) {
+      // reset to player 1 game mode
+      gameMode = diceRollGameModePlayer1;
+      // show winning message
+      return `It is a Draw. Player 1's number is ${chooseNumberArray[0]}, Player 2's number is ${chooseNumberArray[1]}.`;
     }
   }
 };
 
 // Helper functions below
 
-// roll 2 dice for each player and store the numbers in array function
-var rollAndStoreDice = function () {
+// roll 2 dice for player 1 and store the numbers in array function
+var rollAndStoreDicePlayer1 = function () {
   while (counter < 2) {
     // push rolled dice into array
-    diceRollArray.push(rollDice());
+    diceRollArray1.push(rollDice());
     // increase counter by 1
     counter = counter + 1;
   }
-  console.log(diceRollArray);
-  return `Hi Player 1, you have rolled Dice 1 as ${diceRollArray[0]} and Dice 2 as ${diceRollArray[1]}. <br> <br> Please enter 1 or 2 to select the order of the first digit`;
+  return `Hi Player 1, you have rolled Dice 1 as ${diceRollArray1[0]} and Dice 2 as ${diceRollArray1[1]}. <br> <br> Please enter 1 or 2 to select the order of the first digit`;
+};
+
+// roll 2 dice for player 2 and store the numbers in array function
+var rollAndStoreDicePlayer2 = function () {
+  // reset counter to 0
+  counter = 0;
+  while (counter < 2) {
+    // push rolled dice into array
+    diceRollArray2.push(rollDice());
+    // increase counter by 1
+    counter = counter + 1;
+  }
+
+  return `Hi Player 2, you have rolled Dice 1 as ${diceRollArray2[0]} and Dice 2 as ${diceRollArray2[1]}. <br> <br> Please enter 1 or 2 to select the order of the first digit`;
 };
 
 // roll dice function
