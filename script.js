f99543ade843a9e5063a4c4085adb2d99e56475e
Geeeeps <tgeeping@gmail.com>
2022-02-22 00:12:47 +0800

    1 player base game

--- script.js
+++ script.js
@@ -13,108 +13,93 @@
 
 // If Player 1 combined > Player 2 combined, Player 1 wins. If Player 1 combined < Player 2 combined, Player 2 wins. If Player 1 combined = Player 2 combined, they draw. Restart game.
 
-var gameMode = "enter num of players";
-var counter = 0;
-var playerX = 0;
-var innerGameMode = "";
-var numOfPlayers = 0;
-//var combinedNumArray = [];
+// Player 1 roll dice -> Player 1 choose dice order -> Player 2 roll dice -> Player 2 choose dice order. Store choices and compare to determine winner.
 
-var diceRoll = function () {
+//Game modes: instructions, play the game, player 1 generate dice rolls, player 1 choose dice order, player 2 generate dice rolls, player 2 choose dice order
+//Functions: generate dice rolls, choose dice order
+
+var gameMode = "instructions";
+var INSTRUCTIONS = "instructions";
+var DICEROLL = "dice roll";
+var CHOOSEDICEORDER = "choose order of dice number";
+var diceRoll1 = 0;
+var diceRoll2 = 0;
+var counter = 0;
+var player1CombiArray = [];
+var player2CombiArray = [];
+
+var generateDiceRoll = function () {
   var randomDecimal = Math.random() * 6;
   var randomInteger = Math.floor(randomDecimal) + 1;
   return randomInteger;
 };
 
-var playTheGame = function (numOfPlayers) {
-  myOutputValue = "hello...";
-  innerGameMode = "dice roll";
-  console.log(innerGameMode);
-  counter = 0;
-  playerX = 0;
-  while (counter < numOfPlayers) {
-    console.log(numOfPlayers);
-    if (innerGameMode == "dice roll") {
-      var diceRoll1 = diceRoll();
-      var diceRoll2 = diceRoll();
-      console.log("dice roll 1", diceRoll1);
-      console.log("dice roll 2", diceRoll2);
-      playerX = playerX + 1;
-      console.log("playerX", playerX);
-      innerGameMode = "choose order of dice number";
-      myOutputValue =
-        myOutputValue +
-        "Welcome Player " +
-        playerX +
-        "!<br>" +
-        "<br>" +
-        "Dice 1 rolled " +
-        diceRoll1 +
-        ". <br>" +
-        "Dice 2 rolled " +
-        diceRoll2 +
-        ".<br>" +
-        "<br>" +
-        "Please indicate 'Dice 1' or 'Dice 2' to be the first numeral of the combined number";
-    } else if (innerGameMode == "choose order of dice number") {
-      console.log("inner game mode", innerGameMode);
-      var diceOrderInput = input;
-      if (diceOrderInput == "Dice 1") {
-        //combinedNumArray.push(combinedNum);x
-        myOutputValue =
-          "You have chosen " +
-          diceOrderInput +
-          "to be the first numeral. <br>" +
-          "Your combined number is " +
-          diceRoll1 +
-          diceRoll2 +
-          "<br>" +
-          "<br>" +
-          "Player " +
-          playerX +
-          "- " +
-          diceRoll1 +
-          diceRoll2;
-      }
-      if (diceOrderInput == "Dice 2") {
-        //combinedNumArray.push(combinedNum);
-        myOutputValue =
-          "You have chosen " +
-          diceOrderInput +
-          " to be the first numeral. <br>" +
-          "Your combined number is " +
-          diceRoll2 +
-          diceRoll1 +
-          "<br>" +
-          "<br>" +
-          "Player " +
-          playerX +
-          "- " +
-          diceRoll2 +
-          diceRoll1;
-      }
-    }
-    counter = counter + 1;
-    return myOutputValue;
+var generateDiceOrder = function (input) {
+  if (input == "Dice 1") {
+    console.log("diceRoll1", diceRoll1);
+    console.log("diceRoll2", diceRoll2);
+    var combinedNum = diceRoll1 + "" + diceRoll2;
+    console.log("combined num", combinedNum);
+    player1CombiArray.push(combinedNum);
+    return combinedNum;
+  }
+  if (input == "Dice 2") {
+    var combinedNum = toString(diceRoll2) + toString(diceRoll1);
+    console.log("combined num", combinedNum);
+    player1CombiArray.push(combinedNum);
+    return combinedNum;
   }
 };
+
+// var playTheGame = function (input) {
+//   var myOutputValue = "hello...";
+//   console.log(gameMode);
+//   gameMode = "choose winner";
+// } else if (gameMode == "choose winner") {
+//   if (Number(player1CombiArray[0]) > Number(player2CombiArray[0])) {
+//     myOutputValue = "Player 1 wins this round!";
+//   }
+//   if (Number(player2CombiArray[0]) > Number(player1CombiArray[0])) {
+//     myOutputValue = "Player 2 wins this round!";
+//   }
+//   }
+//   return myOutputValue;
+// };
 
 var main = function (input) {
   var myOutputValue = "";
 
-  if (gameMode == "enter num of players") {
-    numOfPlayers = input;
-    gameMode = "play the game";
+  if (gameMode == INSTRUCTIONS) {
+    gameMode = DICEROLL;
     myOutputValue =
-      "Welcome to the game of Beat That! Player " +
-      playerX +
-      ". Please click submit to begin rolling the dice!";
-  } else if (gameMode == "play the game") {
-    myOutputValue = playTheGame(numOfPlayers);
+      "Welcome to the game of Beat That!" +
+      " Please click submit to begin rolling the dice!";
+  } else if (gameMode == DICEROLL) {
+    diceRoll1 = generateDiceRoll();
+    diceRoll2 = generateDiceRoll();
+    console.log("diceRoll1", diceRoll1);
+    console.log("diceRoll2", diceRoll2);
+    gameMode = CHOOSEDICEORDER;
+    myOutputValue =
+      "You have rolled " +
+      diceRoll1 +
+      " and " +
+      diceRoll2 +
+      ". <br>" +
+      "Please choose 'Dice 1' or 'Dice 2' to be the first numeral of your combined number";
+  } else if (gameMode == CHOOSEDICEORDER) {
+    var diceOrder = generateDiceOrder(input);
+    myOutputValue =
+      "You have chosen " +
+      input +
+      "to be the first numeral. <br>" +
+      "Your combined number is " +
+      diceOrder +
+      "<br>" +
+      "<br>" +
+      "Player 1 - " +
+      player1CombiArray[0];
   }
-  // if ((Number(combinedNumArray[0])) > (Number(combinedNumArray[1]))){
-  //   myOutputValue = "Player " + playerX + " is leading the game!";
-  // }
 
   return myOutputValue;
 };
