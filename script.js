fe8149cc3545804b54d6cdeb4e4a27e42df285f4
babiesq <malikibinali@gmail.com>
2022-02-23 22:44:48 +0800

    changed popping of array to just an empty array

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ var main = function (input) {
     if (input == 2) {
       player1Choice = Number(`${playerRolls[1]}${playerRolls[0]}`);
     }
-    playerRolls.pop();
-    playerRolls.pop();
+    playerRolls = [];
     roundCounter += 1;
     return `Player 1 chose ${player1Choice}.<br><br>Player 2 it is your turn`;
   }
@@ -33,7 +32,7 @@ var main = function (input) {
     roundCounter += 1;
     return `Welcome Player 2.<br><br>You rolled ${playerRolls[0]} for Dice 1 and ${playerRolls[1]} for Dice 2.<br><br>Choose the order of the dice: "1" for ${playerRolls[0]}${playerRolls[1]} or "2" for ${playerRolls[1]}${playerRolls[0]}`;
   }
-  if (roundCounter == 3) {
+  if (roundCounter % 4) {
     if (input != 1 && input != 2) {
       return `Please choose "1" or "2"!`;
     }
@@ -43,20 +42,22 @@ var main = function (input) {
     if (input == 2) {
       player2Choice = Number(`${playerRolls[1]}${playerRolls[0]}`);
     }
-    playerRolls.pop();
-    playerRolls.pop();
+    playerRolls = [];
     roundCounter = 0;
     if (player1Choice > player2Choice) {
       player1Wins += 1;
-      return `Player 2 chose ${player2Choice}. Player 1 chose ${player1Choice}<br><br>Player 1 wins!<br><br>Player 1 wins = ${player1Wins}<br>Player 2 wins = ${player2Wins}<br>Draws= ${draws}`;
+      var outputText = joiner(player2Choice, player1Choice);
+      return outputText + `<br>` + outputText;
     }
     if (player1Choice < player2Choice) {
       player2Wins += 1;
-      return `Player 2 chose ${player2Choice}. Player 1 chose ${player1Choice}<br><br>Player 2 wins!<br><br>Player 1 wins = ${player1Wins}<br>Player 2 wins = ${player2Wins}<br>Draws= ${draws}`;
+      var outputText = joiner(player2Choice, player1Choice);
+      return outputText + `<br>` + outputText;
     }
     if (player1Choice == player2Choice) {
       draws += 1;
-      return `Player 2 chose ${player2Choice}. Player 1 chose ${player1Choice}<br><br>It's a draw!<br><br>Player 1 wins = ${player1Wins}<br>Player 2 wins = ${player2Wins}<br>Draws= ${draws}`;
+      var outputText = joiner(player2Choice, player1Choice);
+      return outputText + `<br>` + outputText;
     }
   }
 };
@@ -69,3 +70,7 @@ var player1Choice = "";
 var player2Choice = "";
 var dice = [1, 2, 3, 4, 5, 6];
 var roundCounter = 0;
+var joiner = function (value1, value2) {
+  myOutput = `Player 2 chose ${value1}. Player 1 chose ${value2}`;
+  return myOutput;
+};
