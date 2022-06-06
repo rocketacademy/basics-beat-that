969217e970f4bf6a4040d23f2659df106e868039
tingtinghan10 <tingtinghan10@gmail.com>
2022-06-07 00:29:08 +0800

    vers2. two players roll and order dice

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
 var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
+var GAME_STATE_COMPARE_SCORES = "GAME_STATE_COMPARE_SCORES";
 var gameState = GAME_STATE_DICE_ROLL;
 
-var playerRolls = [];
+var currentPlayerRolls = [];
+
+var currentPlayer = 1;
+var allPlayersScore = [];
 
 var main = function (input) {
   var outputMessage = "";
-  if (gameState == "GAME_STATE_DICE_ROLL") {
+  if (gameState == GAME_STATE_DICE_ROLL) {
     outputMessage = playerDiceRoll();
     gameState = GAME_STATE_CHOOSE_DICE_ORDER;
-  } else {
+  } else if ((gameState = GAME_STATE_CHOOSE_DICE_ORDER)) {
     outputMessage = getPlayerScore(input);
-    // gameState = GAME_STATE_DICE_ROLL;
+    if (currentPlayer == 1 && !outputMessage.includes("Error")) {
+      currentPlayer = 2;
+      gameState = GAME_STATE_DICE_ROLL;
+      return (
+        outputMessage + "<br><br>It is now Player " + currentPlayer + "'s turn!"
+      );
+    } else if (currentPlayer == 2 && !outputMessage.includes("Error")) {
+      currentPlayer = 1;
+      gameState = GAME_STATE_COMPARE_SCORES;
+      return outputMessage + "<br><br>Press submit to calculate scores!";
+    }
   }
+  // else if ((gameState = GAME_STATE_COMPARE_SCORES)){}
   return outputMessage;
 };
 
@@ -25,31 +40,40 @@ var diceRoll = function () {
 var playerDiceRoll = function () {
   var counter = 0;
   while (counter < 2) {
-    playerRolls.push(diceRoll());
+    currentPlayerRolls.push(diceRoll());
     counter += 1;
   }
   return (
-    "Welcome!<br><br>Your rolled:<br>Dice 1: " +
-    playerRolls[0] +
+    "Welcome, Player " +
+    currentPlayer +
+    "<br><br>Your rolled:<br>Dice 1: " +
+    currentPlayerRolls[0] +
     " | Dice 2: " +
-    playerRolls[1] +
+    currentPlayerRolls[1] +
     ".<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value."
   );
 };
 
 var getPlayerScore = function (playerInput) {
+  var playerScores;
   if (playerInput != 1 && playerInput != 2) {
     return (
       "Error! Please only input '1' or '2' to choose which dice to use as he first digit. <br><br>Your dice rolls are: <br>Dice 1: " +
-      playerRolls[0] +
+      currentPlayerRolls[0] +
       " | Dice 2: " +
-      playerRolls[1] +
+      currentPlayerRolls[1] +
       "."
     );
   } else if (playerInput == 1) {
-    var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
+    var playerScore = Number(
+      String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+    );
   } else {
-    var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
+    var playerScore = Number(
+      String(currentPlayerRolls[1]) + String(currentPlayerRolls[0])
+    );
   }
+  allPlayersScore.push(playerScore);
+  currentPlayerRolls = [];
   return "Your chosen value is: " + playerScore;
 };
