b77b17f06ecbea51ede73dc5ea3f2bdc3f3e776e
Jia Rong <jiarong1202@gmail.com>
2022-06-10 10:58:07 +0800

    Updated Appearance

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ var secondDice = 0;
 var final = 0;
 var playerRolls = [];
 var currentPlayer = 1;
+var playerOne = 0;
+var playerTwo = 0;
+var draw = 0;
 
 // random number generator
 var randomNum = function (max) {
   return Math.floor(Math.random() * max) + 1;
+};
+
+var showStats = function () {
+  return `Player 1 win: ${playerOne} <br> Player 2 win: ${playerTwo} <br> Draw : ${draw} `;
 };
 
 var roll = function () {
@@ -25,28 +32,41 @@ var sequence = function (input) {
     final = Number("" + firstDice + secondDice);
     playerRolls.push(final);
     final = 0;
-    return `You chose Dice 1 first. <br>Your Number is ${
+    return `🎲Player ${currentPlayer}🎲<br><br>You chose Dice 1 first. <br>Your Number is ${
       playerRolls[currentPlayer - 1]
     }.`;
   } else if (input == 2) {
     final = Number("" + secondDice + firstDice);
     playerRolls.push(final);
     final = 0;
-    return `You chose Dice 2 first. <br>Your Number is ${
+    return `🎲Player ${currentPlayer}🎲<br><br>You chose Dice 2 first. <br>Your Number is ${
       playerRolls[currentPlayer - 1]
     }.`;
   } else {
-    return `Please enter '1' or '2' only!<br><br> Dice 1: ${firstDice}<br>Dice 2: ${secondDice}`;
+    return `🎲Player ${currentPlayer}🎲<br><br>Please enter '1' or '2' only!<br><br> Dice 1: ${firstDice}<br>Dice 2: ${secondDice}`;
   }
 };
 var outcome = function () {
   var output = `Player 1 score = ${playerRolls[0]}.<br>Player 2 score = ${playerRolls[1]}.`;
   if (playerRolls[0] > playerRolls[1]) {
-    return output + `<br><br>Player 1 won! <br><br> Press 'q' to quit!`;
+    playerOne += 1;
+    return (
+      output +
+      `<br><br>Player 1 won! <br><br> Press 'q' to quit!<br><br>` +
+      showStats()
+    );
   } else if (playerRolls[0] < playerRolls[1]) {
-    return output + `<br><br>Player 2 won!<br><br> Press 'q' to quit!`;
+    playerTwo += 1;
+    return (
+      output +
+      `<br><br>Player 2 won!<br><br> Press 'q' to quit!<br><br>` +
+      showStats()
+    );
   } else {
-    return output + `<br><br>Draw!<br><br> Press 'q' to quit!`;
+    draw += 1;
+    return (
+      output + `<br><br>Draw!<br><br> Press 'q' to quit!<br><br>` + showStats()
+    );
   }
 };
 
@@ -54,9 +74,13 @@ var main = function (input) {
   if (input != "q") {
     var output = "";
     if (gameState == begin) {
-      roll();
-      gameState = startGame;
-      return `Player ${currentPlayer}<br><br> You have rolled <br> Dice 1: ${firstDice}<br> Dice 2: ${secondDice}. <br><br> Please choose which dice to go first! `;
+      if (!input) {
+        roll();
+        gameState = startGame;
+        return `🎲Player ${currentPlayer}🎲<br><br> You have rolled <br> Dice 1: ${firstDice}<br> Dice 2: ${secondDice}. <br><br> Please choose which dice to go first! `;
+      } else {
+        return `Please press 'roll' to start rolling!`;
+      }
     }
     if (gameState == startGame) {
       output = sequence(input);
@@ -73,7 +97,13 @@ var main = function (input) {
 
     if (gameState == compare) {
       output = outcome();
-      return output;
+      gameState = begin;
+      firstDice = 0;
+      secondDice = 0;
+      final = 0;
+      playerRolls = [];
+      currentPlayer = 1;
+      return output + "<br><br> Click roll to start a new round!";
     }
   } else {
     firstDice = 0;
@@ -82,6 +112,6 @@ var main = function (input) {
     playerRolls = [];
     currentPlayer = 1;
     gameState = begin;
-    return `Thank you for playing! Press roll to start a new round!`;
+    return `Thank you for playing! Press 'roll' to start a new round!`;
   }
 };
