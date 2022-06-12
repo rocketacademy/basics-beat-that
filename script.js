936985f8daf6a9abae3d0443d2dca6edd2b6d7c8
sRanay <jsong2506@gmail.com>
2022-06-12 13:12:47 +0800

    Scoreboard done

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 // Beat That Project
 
 // Global Variable
-const game = new Map();
-game.set("player1ready", false);
-game.set("player2ready", false);
-game.set("player1dice", false);
-game.set("player2dice", false);
-game.set("dice1", 0);
-game.set("dice2", 0);
-game.set("player1Number", 0);
-game.set("player2Number", 0);
-game.set("player1Won", 0);
-game.set("player2Won", 0);
-game.set("gamesPlayed", 0)
-var player1ready = false;
-var player2ready = false;
-var player1dice = false;
-var player2dice = false;
-var dice1;
-var dice2;
-var player1Number;
-var player2Number;
+var [player1Ready, player2Ready, player1Dice, player2Dice] = Array(6).fill(false);
+var [player1WonRounds, player2WonRounds, totalRounds] = Array(3).fill(0);
+var dice1, dice2, player1Number, player2Number;
 
+// Function rollDice
+// Purpose: Use RNG to give to a number between 1 to 6
+// Output: Returns a number between 1 to 6
 var rollDice = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
 
+// Function checkUserInput
+// Purpose: Ensure that the user only enter "1" or "2"
+// Input: User input
+// Output: Returns False if correct Input or True if incorrect Input
 var checkUserInput = function (userInput) {
   if (userInput === "1" || userInput === "2") {
     return false;
@@ -33,6 +23,10 @@ var checkUserInput = function (userInput) {
   return true;
 };
 
+// Function pickOrder
+// Purpose: Concat both dice into 1 String
+// Input: dice1, dice2 and user choice in which order
+// Output: String after combining
 var pickOrder = function (dice1, dice2, choice) {
   if (choice === "1") {
     return "" + dice1 + dice2;
@@ -41,69 +35,88 @@ var pickOrder = function (dice1, dice2, choice) {
   }
 };
 
+// Function resetGame
+// Purpose: Reset the game to the original state
 var resetGame = function () {
-  player1ready = false;
-  player2ready = false;
-  player1dice = false;
-  player2dice = false;
+  player1Ready = false;
+  player2Ready = false;
+  player1Dice = false;
+  player2Dice = false;
 };
 
+// Function createTable
+// Purpose: Create a Scoreboard to be printed at the end of the round
+var createTable = function() {
+  var output = "<table><tr><th>Name</th><th>Score</th></tr><tr><td>";
+  if (player1WonRounds > player2WonRounds || player1WonRounds === player2WonRounds) {
+    output += `Player 1</td><td>${player1WonRounds}</td></tr><tr><td>Player 2</td><td>${player2WonRounds}</td>`;
+  } else {
+    output += `Player 2</td><td>${player2WonRounds}</td></tr><tr><td>Player 1</td><td>${player1WonRounds}</td>`;
+  }
+  output += "</tr></table>"
+  return output;
+};
 
 var main = function (input) {
   let myOutputValue = '';
-  if (player1ready === false) {
+  if (player1Ready === false) {
     dice1 = rollDice();
     dice2 = rollDice();
     myOutputValue += "Welcome Player 1.<br><br>";
     myOutputValue += `You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br><br>Choose the order of the dice.`;
-    player1ready = true;
+    player1Ready = true;
     return myOutputValue;
   }
 
-  if (player1dice === false) {
+  if (player1Dice === false) {
     if (checkUserInput(input)) {
       return "Please return a valid input (1 or 2).";
     }
     player1Number = pickOrder(dice1, dice2, input);
     player1Number = parseInt(player1Number);
-    player1dice = true
-    myOutputValue = "Player 2, please press the submit button to roll dice."
+    player1Dice = true;
+    myOutputValue = "Player 2, please press the submit button to roll dice.";
     return myOutputValue;
   }
 
-  if(player2ready === false) {
+  if(player2Ready === false) {
     dice1 = rollDice();
     dice2 = rollDice();
     myOutputValue += "Welcome Player 2.<br><br>";
     myOutputValue += `You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br><br>Choose the order of the dice.`;
-    player2ready = true;
+    player2Ready = true;
     return myOutputValue;
   }
 
-  if (player2dice === false) {
+  if (player2Dice === false) {
     if (checkUserInput(input)) {
       return "Please return a valid input (1 or 2).";
     }
     player2Number = pickOrder(dice1, dice2, input);
     player2Number = parseInt(player2Number);
-    player2dice = true
+    player2Dice = true;
   }
 
   // Win Condition
   
   if (player1Number > player2Number) {
     myOutputValue += "Player 1 won.<br><br>";
+    player1WonRounds += 1;
   } else if (player2Number > player1Number) {
     myOutputValue += "Player 2 won.<br><br>";
+    player2WonRounds += 1;
   } else {
     myOutputValue += "Draw.<br><br>";
   }
+  totalRounds += 1;
 
-  console.log("Player 1 Number:",player1Number);
-  console.log("Player 2 Number:",player2Number);
+  console.log("Player 1 Number:", player1Number);
+  console.log("Player 2 Number:", player2Number);
 
-  myOutputValue += `Player 1 has ${player1Number} and Player 2 has ${player2Number}<br><br>Press submit to start new game`;
+  myOutputValue += `Player 1 has ${player1Number} and Player 2 has ${player2Number}<br><br>Press submit to start new game<br><br>`
+  myOutputValue += createTable();
   resetGame();
+
 
   return myOutputValue;
 };
