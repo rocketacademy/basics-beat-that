75972d8fcfb0a7c9a9e5da7484f64b1b11a4b01c
SaGe1222 <banico.sgl@gmail.com>
2022-06-15 11:06:27 +0800

    added knockout game

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ var totalScoreBoard = []; //
 var leaderBoard = [];
 var leaderBoardSequence = [];
 
+var newRound = false;
+
 var reset = function () {
   gameMode = "match select";
 
@@ -31,11 +33,17 @@ var reset = function () {
 
   leaderBoard = [];
   leaderBoardSequence = [];
+
+  newRound = false;
 };
 
-//create Random number
+//create Random dice number
 var generateRandomNum = function () {
   return Math.ceil(Math.random() * 6);
+};
+
+var generateCoinToss = function () {
+  return Math.floor(Math.random() * 2);
 };
 
 //sorts the user dice roll from highest to lowest
@@ -101,12 +109,15 @@ var getLowLeaderboards = function () {
 };
 
 //display End of Round game stats
-var gameStats = function () {
+var normalGameStats = function () {
   if (lowOrHigh == "Highest") getHighLeaderboards();
   else getLowLeaderboards();
   var gameStatString = `${matchSelect} Game Stats (${lowOrHigh}): <br>`;
   for (let player = 0; player < leaderBoard.length; player++) {
-    gameStatString += `Player ${leaderBoardSequence[player]}: ${leaderBoard[player]}<br>`;
+    if (player == 0)
+      gameStatString += `<b>Player ${leaderBoardSequence[player]}: ${leaderBoard[player]}</b><br>`;
+    else
+      gameStatString += `Player ${leaderBoardSequence[player]}: ${leaderBoard[player]}<br>`;
   }
   return gameStatString;
 };
@@ -174,7 +185,7 @@ var configLowOrHigh = function (userInput) {
 };
 
 // Game Proper
-var normalGame = function () {
+var gameProper = function () {
   var userNumberStore = []; //random number generated | temporary storage
   for (let num = 0; num < numDices; num++)
     userNumberStore.push(generateRandomNum()); //generated number
@@ -191,41 +202,119 @@ var normalGame = function () {
   if (lowOrHigh == "Highest") orderednum = highestNum(userNumberStore);
   else orderednum = lowestNum(userNumberStore);
 
-  var intUserNum = 0;
+  var intUserNum = 0; //Number of the user
   for (let genNum = 0; genNum < orderednum.length; genNum++) {
     if (intUserNum == 0) intUserNum = Number(orderednum[genNum]);
     else intUserNum = intUserNum * 10 + Number(orderednum[genNum]);
   }
-
   orderedNumString += intUserNum;
 
+  if (matchSelect == "Normal")
+    return normalGame(numString, orderedNumString, intUserNum);
+  else return knockoutGame(numString, orderedNumString, intUserNum);
+};
+
+var normalGame = function (numString, orderedNumString, intUserNum) {
   if (playerTurn == maxPlayers) {
     playerTurn = 0;
     gameRound += 1;
 
     if (totalScoreBoard.length <= 0) {
-      for (let i = 0; i < roundScoreBoard.length; i++) {
+      for (let i = 0; i < roundScoreBoard.length; i++)
         totalScoreBoard[i] = roundScoreBoard[i];
-        console.log(`Player ${i}`, totalScoreBoard[i]);
-      }
     } else {
       for (let i = 0; i < roundScoreBoard.length; i++) {
         totalScoreBoard[i] += roundScoreBoard[i];
         console.log(`Player ${i}`, totalScoreBoard[i]);
       }
     }
-    return `- End of Round ${gameRound - 1} - <br> ${gameStats()}`;
+    return `- End of Round ${
+      gameRound - 1
+    } - <br> ${normalGameStats()} <br><br> Click submit to continue the game`;
   } else {
     roundScoreBoard[playerTurn] = intUserNum;
     playerTurn += 1; //increase player
 
-    return `<b>Round ${gameRound}</b> <br> Welcome Player ${playerTurn}!<br> ${numString} <br> ${orderedNumString}.`;
+    return `<b>Round ${gameRound}</b> <br> Welcome Player ${playerTurn}!<br> ${numString} <br> ${orderedNumString}. <br><br> Click submit for next player turn.`;
   }
 };
 
-var knockoutGame = function () {};
+var highKnockout = function () {
+  if (roundScoreBoard[0] > roundScoreBoard[1]) {
+    leaderBoardSequence.pop();
+    return `<b>Player ${leaderBoardSequence[0]} wins!</b>`;
+  } else if (roundScoreBoard[0] < roundScoreBoard[1]) {
+    leaderBoardSequence.shift();
+    return `<b>Player ${leaderBoardSequence[0]} wins!</b>`;
+  } else {
+    var coinTossWinner = leaderBoardSequence[generateCoinToss()];
+    var coinTossString = `<b>It is a DRAW!</b> <br> 
+      Commenting coin toss (heads) Player ${leaderBoardSequence[0]} (tails) Player ${leaderBoardSequence[1]} <br>
+      Coin Toss Winner: <b>Player ${coinTossWinner}</b>`;
+    if (leaderBoardSequence[0] == coinTossWinner) leaderBoardSequence.pop();
+    else leaderBoardSequence.shift();
+    return coinTossString;
+  }
+};
 
-var gameReset = function (userInput) {
+var lowKnockout = function () {
+  if (roundScoreBoard[0] < roundScoreBoard[1]) {
+    leaderBoardSequence.pop();
+    return `<b>Player ${leaderBoardSequence[0]} wins!</b>`;
+  } else if (roundScoreBoard[0] > roundScoreBoard[1]) {
+    leaderBoardSequence.shift();
+    return `<b>Player ${leaderBoardSequence[0]} wins!</b>`;
+  } else {
+    var coinTossWinner = leaderBoardSequence[generateCoinToss()];
+    var coinTossString = `<b>It is a DRAW!</b> <br> 
+      Commenting coin toss (heads) Player ${leaderBoardSequence[0]} (tails) Player ${leaderBoardSequence[1]} <br>
+      Coin Toss Winner: <b>Player ${coinTossWinner}</b>`;
+    if (leaderBoardSequence[0] == coinTossWinner) leaderBoardSequence.pop();
+    else leaderBoardSequence.shift();
+    return coinTossString;
+  }
+};
+
+var knockoutGame = function (numString, orderedNumString, intUserNum) {
+  if (roundScoreBoard.length == 2) {
+    var winString = "";
+    var battleString = `<b>- Elimination: Match ${gameRound} </b>- <br>
+    Player ${leaderBoardSequence[0]} vs Player ${leaderBoardSequence[1]} <br><br>
+    Player ${leaderBoardSequence[0]}: ${roundScoreBoard[0]} <br>
+    Player ${leaderBoardSequence[1]}: ${roundScoreBoard[1]} <br><br>`;
+
+    if (lowOrHigh == "Highest") winString = highKnockout();
+    else winString = lowKnockout();
+
+    roundScoreBoard = [];
+    gameRound += 1;
+    newRound = true;
+    return battleString + winString;
+  } else if (playerTurn == maxPlayers) {
+    var winString = `<b>Knockout Champion</b> <br>
+    Congratulations Player ${leaderBoardSequence[0]} <br><br>
+    The game will now reset.`;
+    reset();
+    return winString;
+  } else {
+    var currPlayer = playerTurn;
+    if (!newRound) {
+      playerTurn += 1; //increase player
+      leaderBoardSequence.push(playerTurn);
+      currPlayer = playerTurn;
+    } else {
+      currPlayer = leaderBoardSequence[0];
+      newRound = false;
+    }
+    roundScoreBoard.push(intUserNum);
+    if (roundScoreBoard.length == 2) {
+      return `<b>Knockout: Match ${gameRound}</b> <br>Player ${currPlayer} Ready!<br> ${numString} <br> ${orderedNumString}. <br><br> Click submit for player elimination.`;
+    } else
+      return `<b>Knockout: Match ${gameRound}</b> <br>Player ${currPlayer} Ready!<br> ${numString} <br> ${orderedNumString}. <br><br> Click submit for next player turn.`;
+  }
+};
+
+var gameResetPermission = function (userInput) {
   var yOrN = userInput.toLowerCase();
   console.log(yOrN);
   if (yOrN == "y") {
@@ -244,13 +333,10 @@ var main = function (input) {
     prevGameMode = gameMode;
     gameMode = "reset";
     return "<b>Are you sure you want to reset?</b> (y/n) <br> All records will reset and will go back to match selection.";
-  } else if (gameMode == "reset") return gameReset(input);
+  } else if (gameMode == "reset") return gameResetPermission(input);
   else if (gameMode == "match select") return configMatchSelect(input);
   else if (gameMode == "player select") return configPlayerSelect(input);
   else if (gameMode == "dice select") return configDiceSelect(input);
   else if (gameMode == "low or high") return configLowOrHigh(input);
-  else if (gameMode == "game proper") {
-    if (matchSelect == "Normal") return normalGame();
-    else return knockoutGame();
-  }
+  else if (gameMode == "game proper") return gameProper();
 };
