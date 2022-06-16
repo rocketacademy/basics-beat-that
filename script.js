b082e58405d8f674ebc17cc0bf03a301d0f7bad3
BerylKwok <fishnetvanilla@gmail.com>
2022-06-16 20:13:14 +0800

    beat that base

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+//There is player 1
+//There is player 2,
+//Diceroll code
+//There is an addition of numbers
+//
+
+var gamemode = "Yellow";
+var player1 = [0, 0];
+var player2 = [0, 0];
+
+var diceroll = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var randomNumber = randomInteger + 1;
+  return randomNumber;
+};
+
+//when game mode is in player 1,
+//dice automatically rolls twice and tells player 1 what is their outcome for dice 1 and dice 2
+var player1Diceroll = function () {
+  player1.push(diceroll[0]);
+  player1.push(diceroll[1]);
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  //The user types R the mode switches to red
+  if (input == "r") {
+    gamemode = "Please click on submit button to start";
+  }
+  //The user types G the mode switches to green
+  if (input == "g") {
+    gamemode = "green";
+  }
+  var myOutputValue = "Hello";
   return myOutputValue;
 };
