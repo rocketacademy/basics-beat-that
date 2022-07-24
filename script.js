94b4f14f7b239ae1b6f56159660a4864ee8ac2a5
darrenlowjq <darrenlowjq@gmail.com>
2022-07-25 00:21:34 +0800

    added global variables and dice concatenation function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var diceRoll = function () {
   return Math.ceil(Math.random() * 6) + 1;
 };
+
+var player1Result = 0;
+var player2Result = 0;
+
+var concatNum = function (firstNum, secondNum) {
+  return firstNum * 10 + secondNum;
+};
