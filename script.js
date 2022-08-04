d0780246b5e51a11f0fbb09f6f8a606df8de7c58
esthercocolee <esther.leejx@gmail.com>
2022-08-04 18:09:38 +0800

    display only player 1's dice results

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //default player game mode
-var mode = "waiting";
+var gameMode = "waiting";
+var playerMode = "Player 1";
 
 //create random number
 var randomNumber = function () {
@@ -7,43 +8,53 @@ var randomNumber = function () {
   return numberRolled;
 };
 
-//array to store dice number rolled by player
-var playerRolls = [];
+//array to store dice number rolled by player 1 & player 2
+var player1Rolls = [];
+var player2Rolls = [];
 
 var main = function (input) {
   var myOutputValue = "";
-  if (input == "") {
-    myOutputValue = `Welcome! Please enter "Player 1" or "Player 2" to begin.`;
+  var gameMode = "waiting";
+  if (gameMode == "waiting") {
+    if (input == "") {
+      return `Welcome! Player 1 will roll the dice first. <br>To roll the dice, please enter "start"`;
+    }
   }
-  //switch to player 1
-  if (input == "Player 1") {
-    myOutputValue = `Welcome Player 1! <br>To roll the dice, please enter "start". <br> To switch players, please enter "Player 2".`;
-  }
-  //switch to player 2
-  if (input == "Player 2") {
-    myOutputValue = `Welcome Player 2! <br>To roll the dice, please enter "start". <br> To switch players, please enter "Player 1".`;
-  }
+  //player 1 roll dice
   if (input == "start") {
-    mode = "start";
-    var dice1 = randomNumber();
-    playerRolls.push(dice1);
-    var dice2 = randomNumber();
-    playerRolls.push(dice2);
-    myOutputValue = `You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br>Choose the order of the dice by typing "1" or "2".`;
+    gameMode = "start";
   }
+  if (gameMode == "start") {
+    var player1Dice1 = randomNumber();
+    player1Rolls.push(player1Dice1);
+    var player1Dice2 = randomNumber();
+    player1Rolls.push(player1Dice2);
+    myOutputValue = `You rolled ${player1Dice1} for Dice 1 and ${player1Dice2} for Dice 2.<br>Choose the order of the dice by typing "1" or "2".`;
+  }
+  // //show dice results for option 1
   if (input == 1) {
-    var playerNumber = Number(String(playerRolls[0]) + String(playerRolls[1]));
-    console.log(playerRolls[0]);
-    console.log(playerRolls[1]);
-    myOutputValue = `Player 1, you chose Dice 1 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn.`;
+    var playerNumber = Number(
+      String(player1Rolls[0]) + String(player1Rolls[1])
+    );
+    console.log(player1Rolls[0]);
+    console.log(player1Rolls[1]);
+    myOutputValue = `Player 1, you chose Dice 1 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn. Enter "start" to begin Player 2's turn.`;
   }
+  // //show dice results for option 2
   if (input == 2) {
-    var playerNumber = Number(String(playerRolls[1]) + String(playerRolls[0]));
-    console.log(playerRolls[1]);
-    console.log(playerRolls[0]);
-    myOutputValue = `Player 1, you chose Dice 2 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn.`;
+    var playerNumber = Number(
+      String(player1Rolls[1]) + String(player1Rolls[0])
+    );
+    console.log(player1Rolls[1]);
+    console.log(player1Rolls[0]);
+    myOutputValue = `Player 1, you chose Dice 2 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn. Enter "start" to begin Player 2's turn.`;
   }
+  // if ((playerMode = "Player 2")) {
+  //   var player2Dice1 = randomNumber();
+  //   player2Rolls.push(player2Dice1);
+  //   var player2Dice2 = randomNumber();
+  //   player2Rolls.push(player2Dice2);
+  //   myOutputValue = `You rolled ${player2Dice1} for Dice 1 and ${player2Dice2} for Dice 2.<br>Choose the order of the dice by typing "1" or "2".`;
+  // }
   return myOutputValue;
 };
-
-//You should get the random number outside of the guessing function and add it through a parameter.
