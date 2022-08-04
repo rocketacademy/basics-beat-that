696d492587e44086effc475e71d120bb80c8386e
esthercocolee <esther.leejx@gmail.com>
2022-08-04 15:23:19 +0800

    show player 1's final number

--- script.js
+++ script.js
@@ -7,17 +7,8 @@ var randomNumber = function () {
   return numberRolled;
 };
 
-// //random number stored in dice 1
-// var dice1 = firstNumber(randomNumber);
-
-// //random number stored in dice 2
-// var dice2 = secondNumber(randomNumber);
-
-// //create random dice 2
-// var dice2 = function () {
-//   var randomDice2 = Math.floor(Math.random() * 6 + 1);
-//   return randomDice2;
-// };
+//array to store dice number rolled by player
+var playerRolls = [];
 
 var main = function (input) {
   var myOutputValue = "";
@@ -35,14 +26,24 @@ var main = function (input) {
   if (input == "start") {
     mode = "start";
     var dice1 = randomNumber();
+    playerRolls.push(dice1);
     var dice2 = randomNumber();
+    playerRolls.push(dice2);
     myOutputValue = `You rolled ${dice1} for Dice 1 and ${dice2} for Dice 2.<br>Choose the order of the dice by typing "1" or "2".`;
   }
-  if (input == "1") {
-    myOutputValue = `Player 1, you chose Dice 1 first. <br>Your number is ${dice1}${dice2}. <br>It is now Player 2's turn.`;
+  if (input == 1) {
+    var playerNumber = Number(String(playerRolls[0]) + String(playerRolls[1]));
+    console.log(playerRolls[0]);
+    console.log(playerRolls[1]);
+    myOutputValue = `Player 1, you chose Dice 1 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn.`;
   }
-  if (input == "2") {
-    myOutputValue = `Player 1, you chose Dice 2 first. <br>Your number is ${dice2}${dice1}. <br>It is now Player 2's turn.`;
+  if (input == 2) {
+    var playerNumber = Number(String(playerRolls[1]) + String(playerRolls[0]));
+    console.log(playerRolls[1]);
+    console.log(playerRolls[0]);
+    myOutputValue = `Player 1, you chose Dice 2 first. <br>Your number is ${playerNumber}. <br>It is now Player 2's turn.`;
   }
   return myOutputValue;
 };
+
+//You should get the random number outside of the guessing function and add it through a parameter.
