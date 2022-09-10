0664f569c2f04fa7212b236ebcd9e9bca97fa57b
Yabing <sherry.liuyabing@gmail.com>
2022-09-10 23:36:51 +0800

    Implement multiple players

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
-var leaderBoard = {
-  "Player 1": 0,
-  "Player 2": 0,
-};
+var players = 2;
+var leaderBoard = {};
 
-var main = function (dice, mode) {
+var main = function (players, dice, mode) {
   setTimeout(function () {
-    updateOutput(playGame(dice, mode));
+    updateOutput(playGame(players, dice, mode));
   }, 1500);
   return `Rolling ${dice} dice... <br> <img src="https://thumbs.gfycat.com/SecondTartCygnet-max-1mb.gif" alt="Something's happening, give it a sec..."  height="150" />`;
 };
 
-var playGame = function (dice, mode) {
-  var player1Roll = [];
-  var player2Roll = [];
-  counter = 0;
-  while (counter < dice) {
-    player1Roll.push(rollDice());
-    player2Roll.push(rollDice());
-    counter += 1;
+var playGame = function (players, dice, mode) {
+  var playersRolls = [];
+  var playersCounter = 0;
+  while (playersCounter < players) {
+    var playerRolls = [];
+    var diceCounter = 0;
+    while (diceCounter < dice) {
+      playerRolls.push(rollDice());
+      diceCounter += 1;
+    }
+    playersRolls.push(playerRolls);
+    playersCounter += 1;
+    console.log(`A player just rolled ${playerRolls}`);
   }
-  console.log(`Player 1 rolled ${player1Roll}`);
-  console.log(`Player 2 rolled ${player2Roll}`);
+  console.log(`Dice rolls ${playersRolls}`);
 
   if (mode == "lowest-combined") {
-    player1Num = lowestCombinedMode(player1Roll);
-    player2Num = lowestCombinedMode(player2Roll);
+    var playersRollsCombined = lowestCombinedMode(playersRolls);
   }
 
   if (mode == "highest-combined") {
-    player1Num = highestCombinedMode(player1Roll);
-    player2Num = highestCombinedMode(player2Roll);
+    var playersRollsCombined = highestCombinedMode(playersRolls);
+  }
+  console.log(`everyone's score: ${playersRollsCombined}`);
+
+  var winningScore = Math.max(...playersRollsCombined);
+  winningPlayer =
+    "Player " + (playersRollsCombined.indexOf(winningScore.toString()) + 1);
+
+  if ([...new Set(playersRollsCombined)].length < playersRollsCombined.length) {
+    return `Your scores are ${playersRollsCombined}. It's a draw! Roll again. <br> <img src="https://i.gifer.com/1Jxj.gif" alt="Shrug"  height="150" />`;
   }
 
-  var playerRolls = `Player 1 rolled ${player1Num} and Player 2 rolled ${player2Num}.`;
-  if (player1Num > player2Num) {
-    var winner = "Player 1";
-  }
-  if (player1Num < player2Num) {
-    var winner = "Player 2";
-  }
-  if (player1Num == player2Num) {
-    return `It's a draw! Roll again. <br> <img src="https://i.gifer.com/1Jxj.gif" alt="Shrug"  height="150" />`;
-  }
-  var result = `${playerRolls} ${winner} wins! <br> <img src="https://i.pinimg.com/originals/00/24/b9/0024b92df56e60d2706ed908b31ad870.gif" alt="Congrats to the winner!"  height="150" />`;
-  updateLeaderboard(winner);
+  var result = `Your scores are ${playersRollsCombined}. ${winningPlayer} wins! <br> <img src="https://i.pinimg.com/originals/00/24/b9/0024b92df56e60d2706ed908b31ad870.gif" alt="Congrats to the winner!"  height="150" />`;
   console.log(leaderBoard);
+  updateLeaderboard(winningPlayer);
   return result;
 };
 
 // Combine dice rolls into largest possible integer
-var highestCombinedMode = function (rolls) {
-  playerNum = parseInt(rolls.sort().reverse().join(""));
-  return playerNum;
+var highestCombinedMode = function (playersRolls) {
+  var combined = [];
+  var counter = 0;
+  while (counter < playersRolls.length) {
+    combined.push(playersRolls[counter].sort().reverse().join(""));
+    counter += 1;
+  }
+  return combined;
 };
 
 // Combine dice rolls into smallest possible integer
-var lowestCombinedMode = function (rolls) {
-  playerNum = parseInt(rolls.sort().join(""));
-  return playerNum;
+var lowestCombinedMode = function (playersRolls) {
+  var combined = [];
+  var counter = 0;
+  while (counter < playersRolls.length) {
+    combined.push(playersRolls[counter].sort().join(""));
+    counter += 1;
+  }
+  return combined;
 };
 
 // Generate random number between 1 and 6
@@ -65,16 +74,28 @@ var rollDice = function () {
   return Math.ceil(Math.random() * 6);
 };
 
-// Overwrite "rolling..." with round outcome
+// Overwrite "rolling..." with the outcome of the round
 var updateOutput = function (input) {
   var outputBox = document.querySelector("#output-div");
   outputBox.innerHTML = input;
 };
 
-var updateLeaderboard = function (winner) {
-  leaderBoard[winner] += 1;
-  var player1Score = document.querySelector("#player-1-wins");
-  var player2Score = document.querySelector("#player-2-wins");
-  player1Score.innerHTML = `Player 1: ${leaderBoard["Player 1"]}`;
-  player2Score.innerHTML = `Player 2: ${leaderBoard["Player 2"]}`;
+var generateLeaderboard = function (players) {
+  var counter = 0;
+  var leaderBoard = {};
+  while (counter < players) {
+    leaderBoard[`Player ${counter + 1}`] = 0;
+    counter += 1;
+  }
+  return leaderBoard;
+};
+
+var updateLeaderboard = function (winningPlayer) {
+  leaderBoard[winningPlayer] += 1;
+  var scoreSpace = document.querySelector("#scores");
+  var output = "";
+  for (var entry in leaderBoard) {
+    output += `${entry}: ${leaderBoard[entry]}<br>`;
+  }
+  scoreSpace.innerHTML = output;
 };
