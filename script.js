47b2fe2502d0d0d63c7931884e6327ada5ab98e1
unknown <ahjonchua@gmail.com>
2023-01-05 20:42:55 +0800

    dice roll 1

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
+var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
+var gameState = GAME_STATE_DICE_ROLL;
+
+var playerRolls = [];
+
+var rollDice = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = rollDice();
   return myOutputValue;
 };
