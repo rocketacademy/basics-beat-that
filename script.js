dc8b2a8ceb668702a8224758474b6638c175023f
Alayne1 <alaynelxr@gmail.com>
2023-02-21 21:52:23 +0800

    Added main functionalities, lacks handling of draw situations

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ After both players have rolled and chosen dice order, the player with the higher
 
 time log
 18/2: 1 hour
+21/2: 
 */
 
 // input order of dice
@@ -16,62 +17,120 @@ time log
 
 // declare game mode constant variables
 var GAME_MODE_ROLL_DICE = "ROLL_DICE";
-var GAME_MODE_PLAYER1_NUM_DICE = "ENTER_ORDER_1";
-var GAME_MODE_PLAYER2_NUM_DICE = "ENTER_ORDER_2";
+var GAME_MODE_ORDER_DICE = "ENTER_ORDER";
 
 // initialise game mode to enter num dice mode
 var gameMode = GAME_MODE_ROLL_DICE;
 
-// create global arrays for player 1 and player 2
-var player1DiceRolled = [];
-var player2DiceRolled = [];
+// create strings for player 1 and player 2 combination
+var player1Choice = "";
+var player2Choice = "";
 
-// Generate a random integer from 1 to 6
+// create array to store number combinations
+var diceArray = [];
 
+// Create placeholder for dice sequence
+var firstDice = "";
+var secondDice = "";
+
+// number of players in game and start with Player 1
+var numPlayers = 2;
+var currPlayer = 1;
+
+// helper function: rollDice. Generate a random integer from 1 to 6
 var rollDice = function () {
-  // return 2;
+  //return "2";
   var choicesInDice = 6;
   var randomDecimal = Math.random() * choicesInDice;
   var randomInteger = Math.floor(randomDecimal);
   var rolledDice = randomInteger + 1;
-  return rolledDice;
+  return rolledDice.toString();
 };
 
-// sequence of events:
-// auto rolls 2 dice for player 1
-// player 1 enter choice of sequence
-// add the numbers 6 + 5 = 65.
-// auto rolls 2 dice for player 1
-// player 2 enter choice of sequence
-// add the numbers 3 + 1 = 31.
-// compare the two values then return the winner.
-// restart game
+// helper function: startOver
+var startOver = function () {
+  gameMode = GAME_MODE_ROLL_DICE;
+  firstDice = "";
+  secondDice = "";
+  currPlayer = 1;
+};
 
-var main = function () {
-  if (gameMode == GAME_MODE_ROLL_DICE) {
-    var diceRoll1 = rollDice();
-    player1DiceRolled.push(diceRoll1);
+// helper function: combiDiceOrder. Puts the dice in player's chosen order
+var combiDiceOrder = function (input) {
+  if (input == 1) {
+    return firstDice + secondDice;
+  }
+  return secondDice + firstDice;
+};
 
-    var diceRoll2 = rollDice();
-    player1DiceRolled.push(diceRoll2);
+// helper function:
 
-    console.log(diceRoll1);
-    console.log(diceRoll2);
-    console.log(player1DiceRolled);
+var main = function (input) {
+  if (gameMode == GAME_MODE_ROLL_DICE && currPlayer <= numPlayers) {
+    firstDice = rollDice();
+    secondDice = rollDice();
 
-    gameMode = GAME_MODE_PLAYER1_NUM_DICE;
-    myOutputValue =
-      "Hello, you rolled " +
-      diceRoll1 +
+    /*
+    console.log("this is the first dice", firstDice, typeof firstDice);
+    secondDice = rollDice();
+    console.log(
+      "this is the second dice rolled",
+      secondDice,
+      typeof secondDice
+    );
+    */
+
+    // Switch modes
+    gameMode = GAME_MODE_ORDER_DICE;
+
+    var genericMessage =
+      "Hello, Player " +
+      currPlayer +
+      " you rolled " +
+      firstDice +
       " for dice 1 and " +
-      diceRoll2 +
+      secondDice +
       " for dice 2. Choose the order of the dice by entering '1' or '2' which dice to be first!";
+    var myOutputValue = genericMessage;
     return myOutputValue;
   }
-  //if (gameMode == GAME_MODE_PLAYER1_NUM_DICE) {
-  //var choiceIs = player1DiceRolled[index];
-  // return choiceIs;
-  //}
 
-  return "quick end";
+  if (gameMode == GAME_MODE_ORDER_DICE) {
+    if (input == 1 || input == 2) {
+      player1Choice = combiDiceOrder(input);
+      console.log("this is player1Choice", player1Choice);
+
+      diceArray.push(player1Choice);
+      console.log(diceArray);
+
+      // adds 1 so it moves the turn to the next plater
+      currPlayer += 1;
+      gameMode = GAME_MODE_ROLL_DICE;
+      return (
+        "You chose " +
+        "Dice" +
+        input +
+        " first. Your dice combination is: " +
+        player1Choice
+      );
+    }
+    // Input validation if input is not 1 or 2
+    return "Enter 1 or 2 to select your dice order.";
+  }
+
+  // calculate maximum
+  var highScore = Math.max(...diceArray);
+  var winningPlayer = diceArray.indexOf(highScore.toString()) + 1;
+  console.log("higher score: " + highScore);
+  console.log("winning player", winningPlayer);
+
+  // initiate startOver
+  startOver();
+  return (
+    "This is the high score " +
+    highScore +
+    "by Player" +
+    winningPlayer +
+    " Start game again."
+  );
 };
