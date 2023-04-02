091929b6156393da6a1e40dabced1b89dbccffe0
ryanlerler <lyp2726@outlook.com>
2023-04-02 16:15:25 +0800

    remove use of array and complete player 2

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var mode = "waiting for click";
-var isANewRound = true;
-var player1Num1 = [];
-var player1Num2 = [];
+var isANewPlayer1Round = true;
+var isANewPlayer2Round = true;
+var player1Num1 = 0;
+var player1Num2 = 0;
+var player2Num1 = 0;
+var player2Num2 = 0;
 
 var getRandomInteger = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
 
 var main = function (input) {
+  var myOutputValue;
+  // player 1
   if (mode == "waiting for click") {
     if (input) {
       myOutputValue = 'Please hit the "Submit" button to continue.';
@@ -16,28 +21,77 @@ var main = function (input) {
     }
   }
   if (mode == "player1 roll") {
-    if (isANewRound) {
-      player1Num1.push(getRandomInteger());
-      player1Num2.push(getRandomInteger());
+    if (isANewPlayer1Round) {
+      player1Num1 = getRandomInteger();
+      player1Num2 = getRandomInteger();
       myOutputValue = `Player 1's turn: <br> 
     You rolled ${player1Num1} for Dice 1 and ${player1Num2} for Dice 2. <br>
     You are about to concatenate the two digits rolled to create the largest possible number. <br>
     Enter '1' if you would like the digit in Dice 1 to be in the tens place. <br>
     Enter '2' if you would like the digit in Dice 2 to be in the tens place. <br>`;
       mode = "player1 choose dice";
-      isANewRound = false;
+      isANewPlayer1Round = false;
     }
   } else if (mode == "player1 choose dice") {
     var player1Choice = input;
     if (player1Choice == 1) {
-      myOutputValue = `You choose Dice 1. <br>
-      Your number is ${player1Num1}${player1Num2}.`;
+      myOutputValue = `Player 1, you choose Dice 1 first. <br>
+      Your number is ${player1Num1}${player1Num2}. <br>
+      Player 2's turn now! <br>
+      Please hit the "Submit" button to continue.`;
     } else if (player1Choice == 2) {
-      myOutputValue = `You choose Dice 2. <br>
-      Your number is ${player1Num2}${player1Num1}.`;
+      myOutputValue = `Player 1, you choose Dice 2 first. <br>
+      Your number is ${player1Num2}${player1Num1}. <br>
+      Player 2's turn now! <br>
+      Please hit the "Submit" button to continue.`;
     } else {
       myOutputValue = `Please enter '1' if you want ${player1Num1} or enter '2' if you want ${player1Num2} to be in the tens place.`;
     }
+    if (player1Choice == 1 || player1Choice == 2) {
+      mode = "waiting for click 2";
+    }
+  }
+
+  // player 2
+  else if (mode == "waiting for click 2") {
+    if (input) {
+      myOutputValue = `Player 2's turn now! Please hit the "Submit" button to continue.`;
+    } else {
+      mode = "player2 roll";
+    }
+  }
+  if (mode == "player2 roll") {
+    if (isANewPlayer2Round) {
+      player2Num1 = getRandomInteger();
+      player2Num2 = getRandomInteger();
+      myOutputValue = `Player 2's turn: <br> 
+    You rolled ${player2Num1} for Dice 1 and ${player2Num2} for Dice 2. <br>
+    You are about to concatenate the two digits rolled to create the largest possible number. <br>
+    Enter '1' if you would like the digit in Dice 1 to be in the tens place. <br>
+    Enter '2' if you would like the digit in Dice 2 to be in the tens place. <br>`;
+      mode = "player2 choose dice";
+      isANewPlayer2Round = false;
+    }
+  } else if (mode == "player2 choose dice") {
+    var player2Choice = input;
+    if (player2Choice == 1) {
+      myOutputValue = `Player 2, you choose Dice 1 first. <br>
+      Your number is ${player2Num1}${player2Num2}.`;
+    } else if (player2Choice == 2) {
+      myOutputValue = `Player 2, you choose Dice 2 first.  <br>
+      Your number is ${player2Num2}${player2Num1}.`;
+    } else {
+      myOutputValue = `Please enter '1' if you want ${player2Num1} or enter '2' if you want ${player2Num2} to be in the tens place.`;
+    }
+    if (player2Choice == 1 || player2Choice == 2) {
+      mode = "result";
+    }
+  }
+
+  // result
+  else if (mode == 'result'){
+    
   }
   return myOutputValue;
 };
+
