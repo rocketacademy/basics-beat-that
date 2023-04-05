293d8cd03c5fd702dd993e360167577368bff30d
Farhan <107352602+elfarhanno@users.noreply.github.com>
2023-04-05 12:33:04 +0800

    Key in instructions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+There are 2 players and players take turns
+.
+When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
+
+The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
+
+After both players have rolled and chosen dice order, the player with the higher combined number wins.
+*/
+
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
