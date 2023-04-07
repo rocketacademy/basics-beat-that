8497b29dbfd89410cf0c8f4f21fd9fb99f2414a4
Farhan <107352602+elfarhanno@users.noreply.github.com>
2023-04-07 23:54:24 +0800

    Completed normal mode

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ The player picks the order of the dice they want. For example, if they wanted th
 After both players have rolled and chosen dice order, the player with the higher combined number wins.
 */
 
+var gameState = "pageLoad"
+var player1Roll = [];
+var player2Roll = [];
+var currentPlayerRoll = 1;
+var currentPlayer = 1;
+var player1Score = 0;
+var player2Score = 0;
+var player1TotalWins = 0;
+var player2TotalWins = 0;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = diceRoll();
   return myOutputValue;
 };
+
+//function for random dice roll
+var diceRoll = function() {
+  var randNum1to6 = Math.ceil(Math.random() * 6);
+  return randNum1to6
+  console.log(randNum1to6)
+}
+
+var getPlayerRolls = function() {
+  diceroll1 = diceRoll()
+}
