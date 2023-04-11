a4f6601258db293796915e5c153e3f8d37272828
KS66M <ksong329@gmail.com>
2023-04-11 22:22:58 +0800

    create for loop to store dice roll numbers of 2 players in array

--- script.js
+++ script.js
@@ -4,10 +4,35 @@
 // Store each player's dice rolls in an array. When each player rolls dice, use a loop to place n dice roll values in that player's array, where n is the number of dice the players specified at the beginning of the round. Output each player's dice roll values.
 // Auto-generate the optimal combined number based on each player's dice rolls to determine the winner of that round.
 
+let randomDiceNumberOfPlayer1inArray = [];
+let randomDiceNumberOfPlayer2inArray = [];
+
 let rollDice = function () {
   let randomDecimal = Math.random() * 6;
   let diceNumber = Math.floor(randomDecimal) + 1;
   return diceNumber;
 };
 
-var main = function (input) {};
+//Create for loop to place n dice roll values in that player's array. Output each player's dice roll values.
+let main = function (input) {
+  let output = "";
+  for (let i = 0; i < input; i += 1) {
+    let randomDiceNumberOfPlayer1 = rollDice();
+    let randomDiceNumberOfPlayer2 = rollDice();
+    randomDiceNumberOfPlayer1inArray.push(randomDiceNumberOfPlayer1);
+    randomDiceNumberOfPlayer2inArray.push(randomDiceNumberOfPlayer2);
+    console.log(
+      "randomDiceNumberOfPlayer1inArray",
+      randomDiceNumberOfPlayer1inArray
+    );
+    console.log(
+      "randomDiceNumberOfPlayer2inArray",
+      randomDiceNumberOfPlayer2inArray
+    );
+    output = `Hi, Player 1! Your dice number are: ${randomDiceNumberOfPlayer1inArray} <br> Player 2, your dice number are: ${randomDiceNumberOfPlayer2inArray} <br> Please click submit.`;
+  }
+
+  //Auto-generate the optimal combined number based on each player's dice rolls to determine the winner of that round.
+
+  return output;
+};
