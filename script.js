843b3df32eff8b64979c26f1c2ec15ef490992dd
KS66M <ksong329@gmail.com>
2023-04-11 21:31:30 +0800

    Create helper function to generate random dice number

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+// Variable Number of Dice
+// Create a new version of Beat That that rolls two or more dice per player.
+// At the beginning of each round, ask the players how many dice they would like to play with. Both players will roll the same number of dice each round.
+// Store each player's dice rolls in an array. When each player rolls dice, use a loop to place n dice roll values in that player's array, where n is the number of dice the players specified at the beginning of the round. Output each player's dice roll values.
+// Auto-generate the optimal combined number based on each player's dice rolls to determine the winner of that round.
+
+let rollDice = function () {
+  let randomDecimal = Math.random() * 6;
+  let diceNumber = Math.floor(randomDecimal) + 1;
+  return diceNumber;
 };
+
+var main = function (input) {};
