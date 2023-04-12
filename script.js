43eeb96594e0b7ddd1c91e675e3197f89adf9c3f
ryanlerler <lyp2726@outlook.com>
2023-04-12 09:47:51 +0800

    refactor codes, decompose,

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // More Comfortable
-// Include base, score, leaderboard, lowest combined number mode, auto-generate combined number features
+// Include base, score, leaderboard, lowest combined number mode, auto-generate combined number, variable number of dice features
 
 // GLOBAL VARIABLES
+const CHOOSE_DICECOUNT = "CHOOSE_DICECOUNT";
 const ROLL = "ROLL";
 const AUTO_GENERATE_NUMBER = "AUTO_GENERATE_NUMBER";
 const LEADERBOARD = "LEADERBOARD";
 const HIGHEST_COMBINED_NUMBER_MODE = "1";
 const LOWEST_COMBINED_NUMBER_MODE = "2";
-let programState = ROLL;
+let programState = CHOOSE_DICECOUNT;
 let gameMode = "";
+let diceCount = 0;
 let currentPlayerDiceNumbers = [];
 let currentPlayer = 1;
 let player1FinalNumEachRound = [];
@@ -18,17 +20,17 @@ const rollOneDice = function () {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-const rollTwoDice = function () {
-  for (i = 0; i < 2; i += 1) {
+const rollMultipleDice = function (diceCount) {
+  let rollStateMessage = `Player ${currentPlayer}'s turn: <br>`;
+  for (i = 0; i < diceCount; i += 1) {
     currentPlayerDiceNumbers.push(rollOneDice());
+    let diceId = i + 1;
+    rollStateMessage += `You rolled ${currentPlayerDiceNumbers[i]} for Dice ${diceId}. <br>`;
   }
-  let rollStateMessage = `Player ${currentPlayer}'s turn: <br>
-    You rolled ${currentPlayerDiceNumbers[0]} for Dice 1 and ${currentPlayerDiceNumbers[1]} for Dice 2. <br>
-    Please hit the "Submit" button to continue. <br>`;
   if (gameMode === HIGHEST_COMBINED_NUMBER_MODE) {
-    rollStateMessage += `Computer is about to automatically concatenate the two digits rolled to create the <b> largest </b> possible number.`;
+    rollStateMessage += `Computer is about to automatically concatenate the ${diceCount} digits rolled to create the <b> largest </b> possible number. Please hit the "Submit" button to continue.`;
   } else if (gameMode === LOWEST_COMBINED_NUMBER_MODE) {
-    rollStateMessage += `Computer is about to automatically concatenate the two digits rolled to create the <b> smallest </b> possible number.`;
+    rollStateMessage += `Computer is about to automatically concatenate the ${diceCount} digits rolled to create the <b> smallest </b> possible number. Please hit the "Submit" button to continue.`;
   }
   return rollStateMessage;
 };
@@ -131,16 +133,24 @@ const resetGame = function () {
 const main = function (input) {
   let myOutputValue;
   if (!gameMode) {
-    if (input != "1" && input != "2") {
-      return `Please choose a game mode: enter '1' for highest combined number mode or '2' for lowest combined number mode.`;
-    } else if (input === "1") {
+    if (input !== "h" && input !== "l") {
+      return `Please choose a game mode: enter 'h' for highest combined number mode or 'l' for lowest combined number mode.`;
+    } else if (input === "h") {
       gameMode = HIGHEST_COMBINED_NUMBER_MODE;
-    } else if (input === "2") {
+    } else if (input === "l") {
       gameMode = LOWEST_COMBINED_NUMBER_MODE;
     }
   }
-  if (programState === ROLL) {
-    myOutputValue = rollTwoDice();
+  if (programState === CHOOSE_DICECOUNT) {
+    if (!input || Number.isNaN(Number(input))) {
+      return `Please enter a number to choose how many dice to roll.`;
+    } else {
+      diceCount = input;
+      myOutputValue = `You have chosen to roll ${diceCount} dice.`;
+      programState = ROLL;
+    }
+  } else if (programState === ROLL) {
+    myOutputValue = rollMultipleDice(diceCount);
     programState = AUTO_GENERATE_NUMBER;
   } else if (programState === AUTO_GENERATE_NUMBER) {
     myOutputValue = autoGenerateNumber();
@@ -159,6 +169,168 @@ const main = function (input) {
   }
   return myOutputValue;
 };
+
+// // More Comfortable
+// // Include base, score, leaderboard, lowest combined number mode, auto-generate combined number features
+
+// // GLOBAL VARIABLES
+// const ROLL = "ROLL";
+// const AUTO_GENERATE_NUMBER = "AUTO_GENERATE_NUMBER";
+// const LEADERBOARD = "LEADERBOARD";
+// const HIGHEST_COMBINED_NUMBER_MODE = "1";
+// const LOWEST_COMBINED_NUMBER_MODE = "2";
+// let programState = ROLL;
+// let gameMode = "";
+// let currentPlayerDiceNumbers = [];
+// let currentPlayer = 1;
+// let player1FinalNumEachRound = [];
+// let player2FinalNumEachRound = [];
+
+// const rollOneDice = function () {
+//   return Math.floor(Math.random() * 6) + 1;
+// };
+
+// const rollTwoDice = function () {
+//   for (i = 0; i < 2; i += 1) {
+//     currentPlayerDiceNumbers.push(rollOneDice());
+//   }
+//   let rollStateMessage = `Player ${currentPlayer}'s turn: <br>
+//     You rolled ${currentPlayerDiceNumbers[0]} for Dice 1 and ${currentPlayerDiceNumbers[1]} for Dice 2. <br>
+//     Please hit the "Submit" button to continue. <br>`;
+//   if (gameMode === HIGHEST_COMBINED_NUMBER_MODE) {
+//     rollStateMessage += `Computer is about to automatically concatenate the two digits rolled to create the <b> largest </b> possible number.`;
+//   } else if (gameMode === LOWEST_COMBINED_NUMBER_MODE) {
+//     rollStateMessage += `Computer is about to automatically concatenate the two digits rolled to create the <b> smallest </b> possible number.`;
+//   }
+//   return rollStateMessage;
+// };
+
+// const autoGenerateNumber = function () {
+//   let currentPlayerFinalNum;
+//   // First digit is greater
+//   if (
+//     (gameMode === HIGHEST_COMBINED_NUMBER_MODE &&
+//       currentPlayerDiceNumbers[0] > currentPlayerDiceNumbers[1]) ||
+//     (gameMode === LOWEST_COMBINED_NUMBER_MODE &&
+//       currentPlayerDiceNumbers[1] > currentPlayerDiceNumbers[0])
+//   ) {
+//     currentPlayerFinalNum =
+//       String(currentPlayerDiceNumbers[0]) + String(currentPlayerDiceNumbers[1]);
+//   }
+
+//   // Second digit is greater
+//   else if (
+//     (gameMode === HIGHEST_COMBINED_NUMBER_MODE &&
+//       currentPlayerDiceNumbers[1] > currentPlayerDiceNumbers[0]) ||
+//     (gameMode === LOWEST_COMBINED_NUMBER_MODE &&
+//       currentPlayerDiceNumbers[0] > currentPlayerDiceNumbers[1])
+//   ) {
+//     currentPlayerFinalNum =
+//       String(currentPlayerDiceNumbers[1]) + String(currentPlayerDiceNumbers[0]);
+//   }
+
+//   // NEED TO ACCOUNT FOR THE CHANCES THAT THE DIGITS MAY BE EQUAL IN ANY MODE. OR ELSE, currentPlayerFinalNum MAY BE NAN.
+//   else if (currentPlayerDiceNumbers[0] === currentPlayerDiceNumbers[1]) {
+//     currentPlayerFinalNum =
+//       String(currentPlayerDiceNumbers[0]) + String(currentPlayerDiceNumbers[1]);
+//   }
+
+//   // Transfer the final num to another variable for each player separately for LEADERBOARD later
+//   if (currentPlayer === 1) {
+//     player1FinalNumEachRound.push(Number(currentPlayerFinalNum));
+//   } else if (currentPlayer === 2) {
+//     player2FinalNumEachRound.push(Number(currentPlayerFinalNum));
+//   }
+//   return `Player ${currentPlayer}, your number is ${currentPlayerFinalNum}. <br>
+//           Player 2's turn now! <br>
+//           Please hit the "Submit" button to continue.`;
+// };
+
+// const showLeaderboard = function () {
+//   // Calculate the sum of all player 1 scores
+//   let player1Sum = 0;
+//   for (i = 0; i < player1FinalNumEachRound.length; i += 1) {
+//     player1Sum += player1FinalNumEachRound[i];
+//   }
+
+//   // Calculate the sum of all player 2 scores
+//   let player2Sum = 0;
+//   for (i = 0; i < player2FinalNumEachRound.length; i += 1) {
+//     player2Sum += player2FinalNumEachRound[i];
+//   }
+
+//   // Player 1 is leading
+//   if (
+//     (gameMode === HIGHEST_COMBINED_NUMBER_MODE && player1Sum > player2Sum) ||
+//     (gameMode === LOWEST_COMBINED_NUMBER_MODE && player1Sum < player2Sum)
+//   ) {
+//     return `Player 1's score: ${player1Sum} | Player 2's score: ${player2Sum} <br>
+//           Press Submit to play again. <br> <br>
+//           <b> ------ LEADERBOARD ------ </b> <br>
+//           Player 1 is currently winning!!! <br>
+//           Player 1: ${player1Sum} <br>
+//           Player 2: ${player2Sum}`;
+//   }
+
+//   // Player 2 is leading
+//   else if (
+//     (gameMode === HIGHEST_COMBINED_NUMBER_MODE && player1Sum < player2Sum) ||
+//     (gameMode === LOWEST_COMBINED_NUMBER_MODE && player1Sum > player2Sum)
+//   ) {
+//     return `Player 1's score: ${player1Sum} | Player 2's score: ${player2Sum} <br>
+//           Press Submit to play again. <br> <br>
+//           <b> ------ LEADERBOARD ------ </b> <br>
+//           Player 2 is currently winning!!! <br>
+//           Player 2: ${player2Sum} <br>
+//           Player 1: ${player1Sum}`;
+//   }
+
+//   // Draw condition for any mode
+//   else if (player1Sum === player2Sum) {
+//     return `Player 1's score: ${player1Sum} | Player 2's score: ${player2Sum} <br>
+//           Press Submit to play again. <br> <br>
+//           <b> ------ LEADERBOARD ------ </b> <br>
+//           DRAW! No one is currently winning!!!`;
+//   }
+// };
+
+// const resetGame = function () {
+//   programState = ROLL;
+//   currentPlayerDiceNumbers = [];
+//   currentPlayer = 1;
+// };
+
+// const main = function (input) {
+//   let myOutputValue;
+//   if (!gameMode) {
+//     if (input != "1" && input != "2") {
+//       return `Please choose a game mode: enter '1' for highest combined number mode or '2' for lowest combined number mode.`;
+//     } else if (input === "1") {
+//       gameMode = HIGHEST_COMBINED_NUMBER_MODE;
+//     } else if (input === "2") {
+//       gameMode = LOWEST_COMBINED_NUMBER_MODE;
+//     }
+//   }
+//   if (programState === ROLL) {
+//     myOutputValue = rollTwoDice();
+//     programState = AUTO_GENERATE_NUMBER;
+//   } else if (programState === AUTO_GENERATE_NUMBER) {
+//     myOutputValue = autoGenerateNumber();
+//     if (currentPlayer === 1) {
+//       currentPlayer = 2;
+//       programState = ROLL;
+//       currentPlayerDiceNumbers = [];
+//     } else if (currentPlayer === 2) {
+//       myOutputValue = `${myOutputValue} <br> Let's see who the winner is!!!`;
+//       programState = LEADERBOARD;
+//     }
+//   } else if (programState === LEADERBOARD) {
+//     myOutputValue = showLeaderboard();
+//     // AFTER showing the result, game resets continuously without refreshing the browser
+//     resetGame();
+//   }
+//   return myOutputValue;
+// };
 
 // --- BASE ---
 // --- SECOND ATTEMPT ---
