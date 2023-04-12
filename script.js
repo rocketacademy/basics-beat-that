2fbac9174156f96189925a903c455c31ec098bb1
AngDeLi <iledgna@hotmail.com>
2023-04-12 22:51:33 +0800

    changed code to vary number of users in game.

--- script.js
+++ script.js
@@ -1,107 +1,97 @@
+//User keys in the number of players they want.
 var rollDice = function () {
   var randomDiceNumber = Math.floor(Math.random() * 6 + 1);
   return randomDiceNumber;
 };
-
-var player1Score = 0;
-var player2Score = 0;
-var diceNumber = 0;
-var generatedPlayer1Number = [];
-var generatedPlayer2Number = [];
-var generatedHighest = [];
-var generated2Highest = [];
-var rollcombination1 = [];
-var rollcombination2 = [];
+var playerDice1 = 0;
+var playerDice2 = 0;
 var gameMode = "start";
-
-var convertArrayToNumber = function (diceNumber, array) {
-  var myOutputValue = "";
-  for (var i = 0; i < diceNumber; i += 1) {
-    var number = array[i];
-    var myOutputValue = myOutputValue + number;
-  }
-  var convertToNumber = Number(myOutputValue);
-  return convertToNumber;
-};
+var numberOfPlayers = 0;
+var playerScore = 0;
+var x = 0;
+var recordOfNumbers = [];
 
 var main = function (input) {
   if (gameMode == "start") {
-    generatedPlayer1Number = [];
-    generatedPlayer2Number = [];
-    generatedHighest = [];
-    generated2Highest = [];
-    rollcombination1 = [];
-    rollcombination2 = [];
-    var myOutputValue = ` Enter the number of dice you want for the game.`;
-    gameMode = "number of dice";
-    return myOutputValue;
-  }
-  if (gameMode == "number of dice") {
-    diceNumber = input;
-    if (isNaN(input) || Number(input) <= 0) {
-      var myOutputValue = `Please enter a valid input number.`;
+    x = 0;
+    recordOfNumbers = [];
+    if (isNaN(input) || input <= 0) {
+      var myOutputValue = `Please enter a valid number of players.`;
+      return myOutputValue;
     } else {
-      gameMode = "player1roll";
-      var myOutputValue = `There will be ${input} dice for each player in this game. Good luck.`;
+      numberOfPlayers = input;
+      var myOutputValue = `You will have ${input} players in this game. Player 1 will go first.<br>Rules of the game: Click start to initiate the roll of 2 dice. Then choose your first number from the random roll of the 2 dice to form a 2 digit number.<br>Player with the highest value will win.<br>Click submit to continue.`;
+      gameMode = "playerRoll";
+      return myOutputValue;
     }
-    return myOutputValue;
   }
-  if (gameMode == "player1roll") {
-    for (var n = diceNumber; n > 0; n -= 1) {
-      var randomDice = rollDice();
-      generatedPlayer1Number.push(randomDice);
-      rollcombination1.push(randomDice);
+  if (gameMode == "playerRoll") {
+    while (x < numberOfPlayers) {
+      playerDice1 = rollDice();
+      playerDice2 = rollDice();
+      var myOutputValue = `Welcome Player ${
+        Number(x) + 1
+      }. <br>You rolled ${playerDice1} for Dice 1 and ${playerDice2} for Dice 2. <br>Choose the order of your dice by keying in the number of the dice you want to be the first.`;
+      gameMode = "playerToChoose";
+      return myOutputValue;
     }
-    console.log(generatedPlayer1Number);
-    for (var n = diceNumber; n > 0; n -= 1) {
-      var largestNumber = Number(Math.max(...generatedPlayer1Number));
-      generatedPlayer1Number.splice(
-        generatedPlayer1Number.indexOf(largestNumber),
-        1
-      );
-      generatedHighest.push(largestNumber);
+    if (x == numberOfPlayers) {
+      gameMode = "conclusion";
+      var myOutputValue = `We will now conclude the winner. Please click submit to continue.`;
+      return myOutputValue;
     }
-    console.log(generatedHighest);
-    var myOutputValue = `Welcome Player 1. <br>You rolled ${rollcombination1}. <br>Your auto-generated highest combination for the numbers is ${generatedHighest}`;
-    gameMode = "player2roll";
-    return myOutputValue;
+  }
+  if (gameMode == "playerToChoose") {
+    if (Number(input) !== playerDice1 && Number(input) !== playerDice2) {
+      var myOutputValue = `Please key in a valid number that you rolled. Either ${playerDice1} or ${playerDice2}.`;
+      return myOutputValue;
+    }
+    if (Number(input) == playerDice1) {
+      var myOutputValue = `Player ${Number(x) + 1}, you chose Dice 1 first.
+Your number is ${
+        playerDice1.toString() + playerDice2.toString()
+      }. <br>Click submit to continue.`;
+      playerScore = Number(playerDice1.toString() + playerDice2.toString());
+      console.log(playerScore);
+      recordOfNumbers.push(playerScore);
+      gameMode = "playerRoll";
+      x += 1;
+      return myOutputValue;
+    }
+    if (Number(input) == playerDice2) {
+      var myOutputValue = `Player ${Number(x) + 1}, you chose Dice 2 first.
+Your number is ${
+        playerDice2.toString() + playerDice1.toString()
+      }. <br>Click submit to continue.`;
+      playerScore = Number(playerDice2.toString() + playerDice1.toString());
+      console.log(playerScore);
+      recordOfNumbers.push(playerScore);
+      gameMode = "playerRoll";
+      x += 1;
+      return myOutputValue;
+    }
   }
 
-  if (gameMode == "player2roll") {
-    for (var n = diceNumber; n > 0; n -= 1) {
-      var randomDice = rollDice();
-      generatedPlayer2Number.push(randomDice);
-      rollcombination2.push(randomDice);
-    }
-    console.log(generatedPlayer2Number);
-    for (var n = diceNumber; n > 0; n -= 1) {
-      var largestNumber = Number(Math.max(...generatedPlayer2Number));
-      generatedPlayer2Number.splice(
-        generatedPlayer2Number.indexOf(largestNumber),
-        1
-      );
-      generated2Highest.push(largestNumber);
-    }
-    console.log(generated2Highest);
-    var myOutputValue = `Welcome Player 2. <br>You rolled ${rollcombination2}. <br>Your auto-generated highest combination for the numbers is ${generated2Highest}`;
-    gameMode = "conclusion";
-    return myOutputValue;
-  }
   if (gameMode == "conclusion") {
-    player1Score = convertArrayToNumber(diceNumber, generatedHighest);
-    player2Score = convertArrayToNumber(diceNumber, generated2Highest);
-    console.log(player1Score);
-    console.log(player2Score);
-    if (player2Score > player1Score) {
-      var statement = `Since Player 1 Number is ${player1Score}, and Player 2 number is ${player2Score}. Player 2 wins. `;
+    var largestNumber = Math.max(...recordOfNumbers);
+    var myOutputValue = `The 2-digit record of all players in order from player 1 to player ${x} are as follows: ${recordOfNumbers}. <br>Largest number being ${largestNumber}.<br>Congratulations to player ${
+      Number(recordOfNumbers.indexOf(largestNumber)) + 1
+    }.`;
+    var repetitionCounter = 0;
+    var largestNumberPlayers = [];
+    for (var i = 0; i < recordOfNumbers.length; i += 1) {
+      if (largestNumber == recordOfNumbers[i]) {
+        repetitionCounter += 1;
+        largestNumberPlayers.push(i + 1);
+        console.log(largestNumberPlayers);
+        console.log(repetitionCounter);
+      }
     }
-    if (player1Score > player2Score) {
-      var statement = `Since Player 1 Number is ${player1Score}, and Player 2 number is ${player2Score}. Player 1 wins. `;
+    if (repetitionCounter >= 2) {
+      var myOutputValue = `The 2-digit record of all players in order from player 1 to player ${x} are as follows: ${recordOfNumbers}. <br>Largest number being ${largestNumber}.<br>Congratulations to players ${largestNumberPlayers}.`;
     }
-    if (player1Score == player2Score) {
-      var statement = `Since Player 1 Number is ${player1Score}, and Player 2 number is ${player2Score}. It's a draw. `;
-    }
+
     gameMode = "start";
-    return statement + `To restart the game, click submit to continue.`;
+    return myOutputValue + `<br>To restart the game, click submit to continue.`;
   }
 };
