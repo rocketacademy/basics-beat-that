0cd8f1fe9e25ffa5e0e490cb19e9497a3a8ddd14
KS66M <ksong329@gmail.com>
2023-04-12 21:10:25 +0800

    added if statement to determine the combined number of which player is greater

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let rollDice = function () {
 //Create for loop to place n dice roll values in that player's array. Output each player's dice roll values.
 let main = function (input) {
   let output = "";
+  let gameState = "";
   for (let i = 0; i < input; i += 1) {
     let randomDiceNumberOfPlayer1 = rollDice();
     let randomDiceNumberOfPlayer2 = rollDice();
@@ -29,10 +30,45 @@ let main = function (input) {
       "randomDiceNumberOfPlayer2inArray",
       randomDiceNumberOfPlayer2inArray
     );
-    output = `Hi, Player 1! Your dice number are: ${randomDiceNumberOfPlayer1inArray} <br> Player 2, your dice number are: ${randomDiceNumberOfPlayer2inArray} <br> Please click submit.`;
+    output = `Hi, Player 1! Your dice number are: ${randomDiceNumberOfPlayer1inArray} <br> Player 2, your dice number are: ${randomDiceNumberOfPlayer2inArray} <br> Please enter "c" to determine which player is winner.`;
   }
-
+  //when user enter "c", it will determine which player is winner.
   //Auto-generate the optimal combined number based on each player's dice rolls to determine the winner of that round.
-
+  if (input == "c") {
+    gameState =
+      "GAME STATE_COMPARE THE COMBINED DICE NUMBER BETWEEN TWO PLAYER";
+  }
+  if (
+    gameState ==
+    "GAME STATE_COMPARE THE COMBINED DICE NUMBER BETWEEN TWO PLAYER"
+  ) {
+    //Sort array for dice no in descending order
+    randomDiceNumberOfPlayer1inArray.sort((a, b) => b - a);
+    randomDiceNumberOfPlayer2inArray.sort((a, b) => b - a);
+    //Combine dice no into a single string using reduce method
+    let combinedNoOfPlayer1InString = randomDiceNumberOfPlayer1inArray.reduce(
+      (acc, curr) => acc + curr,
+      ""
+    );
+    let combinedNoOfPlayer2InString = randomDiceNumberOfPlayer2inArray.reduce(
+      (acc, curr) => acc + curr,
+      ""
+    );
+    console.log("combinedNoOfPlayer1InString:", combinedNoOfPlayer1InString);
+    console.log("combinedNoOfPlayer2InString:", combinedNoOfPlayer2InString);
+    //Convert combined dice number in string to number
+    //Create if.else statement to compare combined dice number btw 2 player
+    let combinedNoOfPlayer1 = Number(combinedNoOfPlayer1InString);
+    let combinedNoOfPlayer2 = Number(combinedNoOfPlayer2InString);
+    console.log(combinedNoOfPlayer1);
+    console.log(combinedNoOfPlayer2);
+    if (combinedNoOfPlayer1 > combinedNoOfPlayer2) {
+      output = `Player 1, you won!. Since Player 1's combined number is ${combinedNoOfPlayer1}, player 2's combined number is ${combinedNoOfPlayer2}.<br>Please click submit to reset the game.`;
+    } else if (combinedNoOfPlayer2 > combinedNoOfPlayer1) {
+      output = `Player 2, you won!. Since Player 1's combined number is ${combinedNoOfPlayer2}, player 2's combined number is ${combinedNoOfPlayer1}.<br>Please click submit to reset the game.`;
+    }
+  }
+  //Create if statement for reset mode
+  //ADD INPUT VALIDATION LATER
   return output;
 };
