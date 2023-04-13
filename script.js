862c074ab06da5752002b3afd60e25253f045650
ashferoz <ash.safrina@gmail.com>
2023-04-13 13:27:35 +0800

    player is now able to form their own number combination

--- script.js
+++ script.js
@@ -23,13 +23,42 @@ let randomDiceRoll = function () {
   let randomNum = Math.random() * 6;
   let randomInteger = Math.floor(randomNum);
   let diceNum = randomInteger + 1;
-  console.log(`randomDiceRoll output:`, diceNum);
   return diceNum;
+};
+
+// player chosen number
+
+let playerChosenNumber = function (playerInput) {
+  if (playerInput == 1) {
+    let playerString1 = String(playerRolls[0]) + String(playerRolls[1]);
+    let playerNumber1 = Number(playerString1);
+    return `You chose to go with this number combination: ${playerNumber1}`;
+  }
+  if (playerInput == 2) {
+    let playerString2 = String(playerRolls[1]) + String(playerRolls[0]);
+    let playerNumber2 = Number(playerString2);
+    return `Interesting... 👀 <br /><br />You have chosen to form the number: ${playerNumber2}`;
+  }
 };
 
 // ==== MAIN FUNCTION ==== //
 let main = function (input) {
   let output = ``;
   if (gameSate == `GAME_STATE_DICE_ROLL`) {
+    // roll dice twice
+    for (let i = 0; i < 2; i = i += 1) {
+      let DiceNum1 = randomDiceRoll();
+      let DiceNum2 = randomDiceRoll();
+      // push dice numbers in player array
+      playerRolls.push(DiceNum1, DiceNum2);
+    }
+
+    // change game mode
+    gameSate = `CHOOSE NUMBER ORDER`;
+
+    return `✨ Here are the numbers you got! ✨ <br /><br />🎲 Dice 1 = ${playerRolls[0]} <br />🎲 Dice 2 = ${playerRolls[1]} <br /><br />Choose which dice to form the first number "1" or "2".`;
+  } else if (gameSate == `CHOOSE NUMBER ORDER`) {
+    output = playerChosenNumber(input);
+    return output;
   }
 };
