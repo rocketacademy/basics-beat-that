770f3353c0049c07ce9a560ddbf8ab6ecc005374
chxshire22 <akuindra22@gmail.com>
2023-05-27 17:08:45 +0800

    make one turn function to not repeat. dont work.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let diceRoll = function () {
 //to store the dice numbers, and other global variables 
 let numOfPlayers = 2; // min num of players 
 let currentPlayer; 
+let roundMaxNumber;
 
 let turns = 0; 
 
@@ -17,7 +18,7 @@ let playerNum;
 let playerNumCombo = []; // index 0 and 1 reserved for player 1, 2. use push to add player 3 or 4
 let playerScore = []; //store player scores. 
 
-let gameState = "";
+let turnActive = "roll";
 
 document.getElementById("output-div").innerHTML = "Player 1, roll the dice";
 // document.getElementById("submit-button").innerHTML = "ROLL";
@@ -39,37 +40,42 @@ let initiate = function (input){
 
 let oneTurnEvents = function(input){
   // document.getElementById("input-field").disabled = false;
-  if (gameState == "") {
+  if (turnActive == "roll" && numOfPlayers%turns != 0) {
     rolledNums = [];
     for (let j = 0; j < 2; j++) {
       rolledNums.push(diceRoll());
       console.log(rolledNums);
     }
-    gameState = "player choose";
+    turnActive = "player choose";
     document.getElementById("submit-button").innerHTML = "Submit";
     return `The numbers you rolled are ${rolledNums[0]} and ${rolledNums[1]} 
     <br> Choose which of the numbers is to go first`
   } 
-  else if (gameState == "player choose") {
+  else if (turnActive == "player choose") {
     // document.getElementById("submit-button").innerHTML = "Continue";
     input = Number(input);
     if (input == 2) {
       playerNum = [rolledNums[1], rolledNums[0]].join("");//FLIP
       console.log(playerNum)
       playerNumCombo.push(playerNum);
-      console.log(playerNumCombo)
+      console.log("playernumcombo:",playerNumCombo)
+      turns++
+      turnActive = "roll"
       return `Your rolls combined is ${playerNum}.`;
     } else if (input == 1) {
       playerNum = rolledNums.join("");
       console.log(playerNum);
       playerNumCombo.push(playerNum);
       console.log("playernumcombo:",playerNumCombo);
+      turns++
+      turnActive = "roll"
       return `Your rolls combined is ${playerNum}.`; //NO FLIP
     } else {
       return `That is not a valid input, please choose a number either 1 or 2`;
     }
   } // need to move this to the beginning of the if statement
   }
+
 
 
 // will need a while = True loop to keep store of highscores, and stats of each player
