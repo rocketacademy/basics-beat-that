9ef73263bf51953bfa3429dd3a076cfe9cd509be
AGAN86 <khgan001@gmail.com>
2023-05-28 17:45:57 +0800

    Fork and cloned and added in the diceroll function

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
+};
+
+//Function to simulate dice rolling
+//Input not required
+var diceRoll = function () {
+  //Library command to generate random decimal from 0 to 5.999999
+  var randomDec = Math.random() * 6;
+  //Library command to round down the decimal to an integer
+  var randomInt = Math.floor(randomDec);
+  //For random result to be min 1 and max 6
+  var diceNum = randomInt + 1;
+  return diceNum;
 };
