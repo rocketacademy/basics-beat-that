a337a4d27c8e6bb0f1153395860ce6ed820e9dc0
AGAN86 <khgan001@gmail.com>
2023-05-28 18:56:44 +0800

    Added another global variable to store player one’s dice rolls; Updated default output value to be instructions and rules of the game; Added an else if block to include a for loop to roll dice twice for player one; Within the else if, the dice rolls will be saved to the global array for player one; It will also output the dice roll results to the player;

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-//Global Variable to control game modes
+//Global Variables
+
+//Toggle gamemodes
 stateManager = "instructions";
+//Store first player's dice rolls
+playerOneResults = [];
 
 var main = function (input) {
+  //Local Variable to output instructions of the game upon first click of submit
+  var myOutputValue = `🎲<strong>WELCOME TO THE BEAT THAT DICE GAME!</strong>🎲<br><br>
+  
+  Rules:<br>
+    1. There are 2 players and players take turns.<br><br>
+    2. When a player clicks Submit, the game rolls 2 dice and shows the dice rolls,<br>for example 3 and 6.<br><br>
+    3. The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first.<br><br>
+    4. After both players have rolled and chosen dice order, the player with the higher combined number wins.<br><br>
+    
+    Please click submit to roll the dice`;
+
   //if block to give instruction to player and change the mode to player one
   if (stateManager == "instructions") {
-    stateManager == "playerOne";
-    return `Please click submit to roll the dice`;
+    stateManager = "playerOne";
+    return myOutputValue;
   }
-
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  //if block to roll dice 2 times and save it to playerOneResults global variable
+  else if (stateManager == "playerOne") {
+    for (counter = 0; counter < 2; counter += 1) {
+      rollDice = diceRoll();
+      playerOneResults.push(rollDice);
+    }
+    myOutputValue = `🎲<strong>WELCOME PLAYER 1</strong>🎲<br><br>
+    You rolled ${playerOneResults[0]} for dice one and ${playerOneResults[1]} for dice two.<br><br>
+    Enter "1" or "2" for the respective dice roll to be the first number of your score.<br><br>
+    Hint:<br>
+    Choose the bigger roll for a higher winning chance!`;
+    return myOutputValue;
+  }
 };
 
 //Function to simulate dice rolling
