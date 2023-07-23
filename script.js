0fb76e75a6049ff63a4431116f07c1e5622f07cc
Mannushka <zaimar1910@gmail.com>
2023-07-23 22:26:55 +0800

    version 2: two players roll and order dice

--- script.js
+++ script.js
@@ -11,9 +11,12 @@
 //Global variables:
 var GAME_STATE_ROLL_DICE = "GAME_STATE_ROLL_DICE ";
 var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
+var GAME_STATE_COMPARE_SCORES = "GAME_STATE_COMPARE_SCORES";
 var gameState = GAME_STATE_ROLL_DICE;
-
-var playerRolls = []; //this array will store the player rolls
+var currentPlayer = 1;
+var currentPlayerRolls = []; //this array will store the currrent player rolls
+var allPlayersScore = [];
+var playerFinalValue = 0;
 //Helper function "Roll the dice"
 var rollDice = function () {
   var randomDecimal = Math.random() * 6;
@@ -26,15 +29,17 @@ var rollDice = function () {
 var rollDiceForPlayer = function () {
   var counter = 0;
   while (counter < 2) {
-    playerRolls.push(rollDice()); // adding dice rolls to the player rolls array on line 16
+    currentPlayerRolls.push(rollDice()); // adding dice rolls to the player rolls array on line 16
     counter = counter + 1;
   }
-  console.log("dice rolls for player 1 ", playerRolls);
+  console.log("dice rolls for current player ", currentPlayerRolls);
   return (
-    "Welcome Player 1. You rolled " +
-    playerRolls[0] +
+    "Welcome Player " +
+    currentPlayer +
+    ". You rolled " +
+    currentPlayerRolls[0] +
     " and " +
-    playerRolls[1] +
+    currentPlayerRolls[1] +
     ". Now please key in 1 or 2 to choose the corresponding dice as the first digit of your final value."
   );
 };
@@ -46,25 +51,37 @@ var getPlayerFinalValue = function (input) {
     console.log("Input is not 1 and not 2", input != 1 && input != 2);
     return (
       "Error! Please only key in 1 or 2 to choose which dice to use as the first digit. Your current dice rolls are " +
-      playerRolls[0] +
+      currentPlayerRolls[0] +
       " as dice 1 and " +
-      playerRolls[1] +
+      currentPlayerRolls[1] +
       " as dice 2."
     );
   }
   //condition 1 "if input == 1"
   if (input == 1) {
     console.log("input == 1 ", input == 1);
-    var playerFinalValue = Number(`${playerRolls[0]}` + `${playerRolls[1]}`);
-    return "Your final value is " + playerFinalValue + ".";
+    playerFinalValue = Number(
+      `${currentPlayerRolls[0]}` + `${currentPlayerRolls[1]}`
+    );
   }
 
   //condition 2 "if input == 2"
   if (input == 2) {
     console.log("input == 2 ", input == 2);
-    var playerFinalValue = Number(`${playerRolls[1]}` + `${playerRolls[0]}`);
-    return "Your final value is " + playerFinalValue + ".";
+    playerFinalValue = Number(
+      `${currentPlayerRolls[1]}` + `${currentPlayerRolls[0]}`
+    );
   }
+  allPlayersScore.push(playerFinalValue);
+  currentPlayerRolls = [];
+  console.log("current player rolls:", currentPlayerRolls);
+  return (
+    "Player " +
+    currentPlayer +
+    ", your final value is " +
+    playerFinalValue +
+    "."
+  );
 };
 var main = function (input) {
   console.log("Game state: ", gameState);
@@ -78,8 +95,27 @@ var main = function (input) {
     //cnange the game state
     gameState = GAME_STATE_CHOOSE_DICE_ORDER;
     console.log("game state is choose the dice order ", gameState);
-  } else if (gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
+    return myOutputMessage;
+  }
+  if (gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
+    console.log(
+      "game state: choose the dice order ",
+      gameState == GAME_STATE_CHOOSE_DICE_ORDER
+    );
     myOutputMessage = getPlayerFinalValue(input);
   }
-  return myOutputMessage;
+  if (currentPlayer == 1) {
+    console.log("current player is 1: ", currentPlayer == 1);
+    currentPlayer = 2;
+    gameState = GAME_STATE_ROLL_DICE;
+    console.log("game state: ", gameState);
+    return myOutputMessage + " It is now player's 2 turn.";
+  }
+
+  if (currentPlayer == 2) {
+    console.log("current player is 2: ", currentPlayer == 2);
+    gameState = GAME_STATE_COMPARE_SCORES;
+    console.log("game state: ", gameState);
+    return myOutputMessage + " Please click 'submit' to see the final score.";
+  }
 };
