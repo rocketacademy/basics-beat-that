3ca9eb2123f4209a6f5b2f27633f7729596d5ec4
ianlauyin <ianlauyin@gmail.com>
2023-08-24 23:45:48 +0800

    Completed the style changing in index.html

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 //Global Variable
 var userName = [];
 var userNumber = 0;
+var currentGameMode = 0; //Game Mode 0 for not in game , 1 for , 2 for
 
 var userAdded = function (user) {
   userNumber += 1;
   userName.push(user);
-  return `${user}, welcome to the game. There is ${userNumber} number of player now!`;
+  return `${
+    userName[userNumber - 1]
+  }, welcome to the game. Here is ${userNumber} number of player now!`;
+  //Add Player List*******
+  //Limit the player number******
 };
+
+var gameModeSelect = function (gameMode) {};
 
 var main = function (input) {
   return;
