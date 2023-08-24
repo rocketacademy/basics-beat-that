37ef5a9ec0ef353ad44de8a670fc9733bda50c43
ianlauyin <ianlauyin@gmail.com>
2023-08-24 22:48:36 +0800

    little change in the html and userAdded function added in script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+//Global Variable
+var userName = [];
+var userNumber = 0;
+
+var userAdded = function (user) {
+  userNumber += 1;
+  userName.push(user);
+  return `${user}, welcome to the game. There is ${userNumber} number of player now!`;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  return;
 };
