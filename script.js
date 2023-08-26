274e215b77121a649054bd2a3bf738ef91f41605
ianlauyin <ianlauyin@gmail.com>
2023-08-26 14:49:12 +0800

    Prevent the user playing without 2 or more player and no game mode

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 //Global Variable
 var userName = [];
+var userScore = [];
 var userNumber = 0;
-var currentGameMode = 0; //Game Mode 0 for not in game , 1 for , 2 for
+var currentGameMode = 0;
 
 var userAdded = function (user) {
   userNumber += 1;
   userName.push(user);
   return `${
     userName[userNumber - 1]
-  }, welcome to the game. Here is ${userNumber} number of player now!`;
+  }, welcome to the game. Here is ${userNumber} player now!`;
   //Add Player List*******
   //Limit the player number******
 };
 
-var gameModeSelect = function (gameMode) {};
+var gameModeSelect = function (gameMode) {
+  currentGameMode = gameMode;
+  return `You choose ${currentGameMode}!Nice choice!<br>Let's start the game by pressing the play button!🤩🤩🤩`;
+};
 
-var main = function (input) {
-  return;
+var main = function () {
+  if (userNumber < 2) {
+    return "Here is not enough player to start the game.🥲<br> Please find more friend to play the game with you.";
+  }
+
+  if (currentGameMode == "") {
+    return "You have not choose the game mode yet.🤨<br>Please choose what game mode you want to play before starting the game.<br>The game mode rule is explained in the bottom of this website.😆😆";
+  }
+
+  return "Test";
 };
