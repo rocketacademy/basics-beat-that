2e0eb93f23f6615910e92dae950036269d32b425
sthsee <samanthasee65@gmail.com>
2023-08-27 12:58:11 +0800

    did rollDice function and playerOneArray to test things out

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+/*
+write function to roll dice.
+
+main function should have 
+for player 1:
+a loop that rolls dice twice, and save number into array,
+call the numbers in array to display to user,
+allow user to enter order of dice rolls in array,
+for player 2:
+same operations
+then:
+draw, 1>2, or 2>1
+reset everything
+*/
+
+var playerOneArray = [];
+
+var rollDice = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.ceil(randomDecimal);
+  return randomInteger;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
+  var diceRollCounter = 0;
+  while (diceRollCounter < 2) {
+    playerOneArray.push(rollDice());
+    diceRollCounter = diceRollCounter + 1;
+  }
+  myOutputValue = myOutputValue + playerOneArray;
   return myOutputValue;
 };
