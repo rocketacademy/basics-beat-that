ed7b5ecb75a9ea784c787fd8420919c40cd1058b
sthsee <samanthasee65@gmail.com>
2023-08-27 13:25:22 +0800

    able to receive user's input to order the numbers and return final number of Player 1

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ reset everything
 */
 
 var playerOneArray = [];
+var playerTwoArray = [];
+var playerOneFinalNumber = 0;
 
 var rollDice = function () {
   var randomDecimal = Math.random() * 6;
@@ -22,12 +24,25 @@ var rollDice = function () {
 };
 
 var main = function (input) {
-  var myOutputValue = "hello world";
-  var diceRollCounter = 0;
-  while (diceRollCounter < 2) {
-    playerOneArray.push(rollDice());
-    diceRollCounter = diceRollCounter + 1;
+  var myOutputValue = "";
+
+  if (playerOneArray.length == 0) {
+    var diceRollCounter = 0;
+    while (diceRollCounter < 2) {
+      playerOneArray.push(rollDice());
+      diceRollCounter = diceRollCounter + 1;
+    }
+    myOutputValue = `Player 1 has rolled ${playerOneArray[0]} and ${playerOneArray[1]}.<br>Which dice (1 or 2) would you choose to be placed as the first digit?`;
+  } else if (playerOneArray.length != 0) {
+    if (input == 1) {
+      var playerOneFinalNumber =
+        playerOneArray[0].toString() + playerOneArray[1].toString();
+      myOutputValue = `Player 1 has chosen dice 1 to be placed as the first digit.<br>Player 1's number is ${playerOneFinalNumber}.`;
+    } else if (input == 2) {
+      var playerOneFinalNumber =
+        playerOneArray[1].toString() + playerOneArray[0].toString();
+      myOutputValue = `Player 1 has chosen dice 2 to be placed as the first digit.<br>Player 1's number is ${playerOneFinalNumber}.`;
+    }
   }
-  myOutputValue = myOutputValue + playerOneArray;
   return myOutputValue;
 };
