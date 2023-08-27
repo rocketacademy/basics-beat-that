002a54d4035e2868ebf915dcbb472e93cb4a19af
magiicloud <hongyun_rc@outlook.com>
2023-08-27 13:40:24 +0800

    inital game play mechanics added

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 var playerOneName = "";
 var playerTwoName = "";
 
+//game mechanics
+var playGame = function (diceChoice) {
+  //player rolls 2 dice and shows the dice roll and convert it to string to concatenate later
+  var diceRollOne = rollDice().toString();
+  console.log(`dice one = ${diceRollOne}`);
+  var diceRollTwo = rollDice().toString();
+  console.log(`dice two = ${diceRollTwo}`);
+  var combinedDice = "";
+  //player pick the order to concatenate
+  if (diceChoice == 1) {
+    combinedDice = diceRollOne + diceRollTwo;
+    return combinedDice;
+  }
+  combinedDice = diceRollTwo + diceRollOne;
+  return combinedDice;
+};
+
 //roll dice function
 var rollDice = function () {
   var randomValue = Math.random() * 6;
@@ -11,6 +28,6 @@ var rollDice = function () {
 };
 
 var main = function (input) {
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  var playerOnePlay = playGame(input);
+  return playerOnePlay;
 };
