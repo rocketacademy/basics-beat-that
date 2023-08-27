cfab29c66b89a2df925d78ba5e6291a6646849de
magiicloud <hongyun_rc@outlook.com>
2023-08-27 13:23:13 +0800

    added roll dice function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+//roll dice function
+var rollDice = function () {
+  var randomValue = Math.random() * 6;
+  var randomInteger = Math.floor(randomValue);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
