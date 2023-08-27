d5a5dbcff65854ecaf59eb573e316f7a4c52b20b
sthsee <samanthasee65@gmail.com>
2023-08-27 15:06:08 +0800

    computer will auto-order players' dice rolls to form largest number

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ write function to roll dice.
 main function should have 
 for player 1:
 a loop that rolls dice twice, and save number into array,
-call the numbers in array to display to user,
-allow user to enter order of dice rolls in array,
+auto sort array
+display final number to user,
 for player 2:
 same operations
 then:
@@ -27,43 +27,37 @@ var rollDice = function () {
 var main = function (input) {
   var myOutputValue = "";
 
+  // if no player1 dice rolls detected
   if (playerOneArray.length == 0) {
     var playerOneDiceRollCounter = 0;
     while (playerOneDiceRollCounter < 2) {
       playerOneArray.push(rollDice());
       playerOneDiceRollCounter = playerOneDiceRollCounter + 1;
     }
-    myOutputValue = `Player 1 has rolled ${playerOneArray[0]} and ${playerOneArray[1]}.<br>Which dice (1 or 2) would you choose to be placed as the first digit?`;
-  } else if (playerOneArray.length != 0 && playerOneFinalNumber == 0) {
-    if (input == 1) {
-      playerOneFinalNumber =
-        playerOneArray[0].toString() + playerOneArray[1].toString();
-      myOutputValue = `Player 1 has chosen dice 1 to be placed as the first digit.<br>Player 1's number is ${playerOneFinalNumber}.`;
-    } else if (input == 2) {
-      playerOneFinalNumber =
-        playerOneArray[1].toString() + playerOneArray[0].toString();
-      myOutputValue = `Player 1 has chosen dice 2 to be placed as the first digit.<br>Player 1's number is ${playerOneFinalNumber}.`;
-    }
-    myOutputValue =
-      myOutputValue +
-      `<br><br> Player 2, please press Submit for your dice roll.`;
+    myOutputValue = `Player 1 has rolled ${playerOneArray[0]} and ${playerOneArray[1]}.`;
+    playerOneArray.sort(function (a, b) {
+      return b - a;
+    });
+    playerOneFinalNumber =
+      playerOneArray[0].toString() + playerOneArray[1].toString();
+    myOutputValue += `<br>Player 1's number is ${playerOneFinalNumber}.
+    <br><br> Player 2, please press Submit for your dice roll.`;
+
+    // else if Player 1's number is not 0, and no player2 dice rolls detected
   } else if (playerOneFinalNumber != 0 && playerTwoArray.length == 0) {
     var playerTwoDiceRollCounter = 0;
     while (playerTwoDiceRollCounter < 2) {
       playerTwoArray.push(rollDice());
       playerTwoDiceRollCounter = playerTwoDiceRollCounter + 1;
     }
-    myOutputValue = `Player 2 has rolled ${playerTwoArray[0]} and ${playerTwoArray[1]}.<br>Which dice (1 or 2) would you choose to be placed as the first digit?`;
-  } else if (playerTwoArray.length != 0 && playerTwoFinalNumber == 0) {
-    if (input == 1) {
-      playerTwoFinalNumber =
-        playerTwoArray[0].toString() + playerTwoArray[1].toString();
-      myOutputValue = `Player 2 has chosen dice 1 to be placed as the first digit.<br>Player 2's number is ${playerTwoFinalNumber}.`;
-    } else if (input == 2) {
-      playerTwoFinalNumber =
-        playerTwoArray[1].toString() + playerTwoArray[0].toString();
-      myOutputValue = `Player 2 has chosen dice 2 to be placed as the first digit.<br>Player 2's number is ${playerTwoFinalNumber}.`;
-    }
+    myOutputValue = `Player 2 has rolled ${playerTwoArray[0]} and ${playerTwoArray[1]}.`;
+    playerTwoArray.sort(function (a, b) {
+      return b - a;
+    });
+    playerTwoFinalNumber =
+      playerTwoArray[0].toString() + playerTwoArray[1].toString();
+    myOutputValue += `<br>Player 2's number is ${playerTwoFinalNumber}.`;
+
     if (playerOneFinalNumber == playerTwoFinalNumber) {
       myOutputValue =
         myOutputValue +
