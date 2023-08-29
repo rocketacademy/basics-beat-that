1fd33c1fbc60867a5985023981cc55fa742377cc
zwjryan <zwjryan@gmail.com>
2023-08-29 19:29:49 +0800

    added a function to generate a random integer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// generate a random integer between 1 to 6
+var randomDiceRoll = function () {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
 var main = function (input) {
   var myOutputValue = "hello world";
   console.log(randomDiceRoll());
