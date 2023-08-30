acbf355e2bd6293c11f92665952d9466f16ab613
zwjryan <zwjryan@gmail.com>
2023-08-30 16:11:09 +0800

    working base game with score added player 2 logic added score to keep track of higher combined number win added gamereset to continue playing game without refreshing the browser added function to store base game

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
 var GAME_STATE_DICE_ORDER = "GAME_STATE_DICE_ORDER";
+var GAME_STATE_COMPARE_SCORE = "GAME_STATE_COMPARE_SCORE";
 var modes = GAME_STATE_DICE_ROLL;
 var playerRolls = [];
+var currentPlayer = 1;
+var trackScore = [];
 
 // generate a random integer between 1 to 6
 var randomInteger = function () {
@@ -16,39 +19,75 @@ var randomDiceRoll = function () {
     playerRolls.push(diceRoll);
     counter += 1;
   }
-  modes = GAME_STATE_DICE_ORDER;
-  return `dices roll! you rolled ${playerRolls[0]} and ${playerRolls[1]}.`;
+  return `welcome player ${currentPlayer}. <br><br> you rolled ${playerRolls[0]} for dice 1 and ${playerRolls[1]} for dice 2. <br><br> choose the order of the dice by inputting "1" or "2".`;
 };
 
 var numberComparison = function (playerInput) {
-  if (playerInput != 1 && playerInput != 2) {
-    console.log(`if input != 1 && input != 2`);
-    return `choose the order of the dice as the first numeral of the combined number.`;
-  }
+  var combinedNumber = "";
   if (playerInput == 1) {
-    console.log(`if input == 1`);
-    var combinedNumber = Number(
-      String(playerRolls[0]) + String(playerRolls[1])
-    );
-    return `you chose dice 1 first. your combined number is ${combinedNumber}.`;
+    combinedNumber = Number(String(playerRolls[0]) + String(playerRolls[1]));
   }
   if (playerInput == 2) {
-    console.log(`if input == 2`);
-    var combinedNumber = Number(
-      String(playerRolls[1]) + String(playerRolls[0])
-    );
-    return `you chose dice 2 first. your combined number is ${combinedNumber}.`;
+    combinedNumber = Number(String(playerRolls[1]) + String(playerRolls[0]));
+  }
+  trackScore.push(combinedNumber);
+  playerRolls = [];
+  return `player ${currentPlayer} chose dice ${playerInput} first. <br><br> your combined number is ${combinedNumber}.<br><br>`;
+};
+
+var compareScore = function () {
+  var myOutputValue = `player 1 score: ${trackScore[0]} | player 2 score: ${trackScore[1]}.<br>`;
+  if (trackScore[0] > trackScore[1]) {
+    myOutputValue = `${myOutputValue} <br> player 1 wins.`;
+  }
+  if (trackScore[0] < trackScore[1]) {
+    myOutputValue = `${myOutputValue} <br> player 2 wins.`;
+  }
+  if (trackScore[0] == trackScore[1]) {
+    myOutputValue = `${myOutputValue} <br> draw game. `;
+  }
+  return `${myOutputValue} <br><br> ${gameResetAlert()}`;
+};
+
+var gameReset = function () {
+  currentPlayer = 1;
+  modes = GAME_STATE_DICE_ROLL;
+  trackScore = [];
+};
+
+var gameResetAlert = function () {
+  return `click on "Submit" to play again.`;
+};
+
+var baseGameBeatThat = function (playerInput) {
+  var myOutputValue = "";
+  if (modes == GAME_STATE_DICE_ROLL) {
+    myOutputValue = randomDiceRoll();
+    modes = GAME_STATE_DICE_ORDER;
+    return myOutputValue;
+  }
+  if (modes == GAME_STATE_DICE_ORDER) {
+    if (playerInput != 1 && playerInput != 2) {
+      return `error message. <br><br>  you rolled ${playerRolls[0]} for dice 1 and ${playerRolls[1]} for dice 2. <br><br> choose the order of the dice as the first numeral of the combined number.`;
+    }
+    myOutputValue = numberComparison(playerInput);
+    if (currentPlayer == 1) {
+      currentPlayer = 2;
+      modes = GAME_STATE_DICE_ROLL;
+      return `${myOutputValue} player 2's turn.`;
+    }
+    if (currentPlayer == 2) {
+      modes = GAME_STATE_COMPARE_SCORE;
+    }
+    return `${myOutputValue} calculating scores . . . <br><br> click on "Submit" to continue.`;
+  }
+  if (modes == GAME_STATE_COMPARE_SCORE) {
+    myOutputValue = compareScore();
+    gameReset();
+    return myOutputValue;
   }
 };
 
 var main = function (input) {
-  var myOutputValue = "";
-  if (modes == GAME_STATE_DICE_ROLL) {
-    console.log(modes);
-    myOutputValue = randomDiceRoll();
-    console.log(randomDiceRoll());
-  }
-  console.log(modes);
-  myOutputValue = numberComparison(input);
-  return myOutputValue;
+  return baseGameBeatThat(input);
 };
