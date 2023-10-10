ae7381f9333bc93cc186c53b88d2c5217a2a293a
lesterc013 <lesterc013@gmail.com>
2023-10-10 21:55:02 +0800

    Added game states to store player one name, then move to roll player one dice

--- script.js
+++ script.js
@@ -5,15 +5,27 @@
 // After both players have rolled and chosen dice order, the player with the higher combined number wins.
 
 // Pseudocode
-// 1st player clicks submit, a helper function will roll dice which returns a number between 1 to 6 and store this number in first index of playerOneDiceRolls array. roll dice again and store this number in second index of playerOneDiceRolls array
-// then, we need to ask the player to specify which order they want the dice to be in
+// first input is player will enter their name, and then
+// a helper function will roll dice which returns a number between 1 to 6 and store this number in first index of playerOneDiceRolls array. roll dice again and store this number in second index of playerOneDiceRolls array
+// the game state needs to then change to ask user to input how they want to sequence the dice rolls
 
 // Make the dice roll variables global because we will need to switch game states
 var playerOneDiceRolls = [];
+var gameState = "storePlayerOneName";
+var playerOneName = "";
 
-var main = function () {
-  for (var i = 0; i < 2; i += 1) playerOneDiceRolls[i] = rollDice(); // rolls dice twice and stores it in the corresponding index of the array
-  var myOutputValue = playerOneDiceRolls;
+var main = function (input) {
+  var myOutputValue = "";
+  if (gameState == "storePlayerOneName") {
+    playerOneName = input; // only take input and put it into playerOneName if gameState is "Player One enter name"
+    myOutputValue = `Welcome to Beat That! ${playerOneName}. Press "Submit" again to roll your dice!`;
+    gameState = "rollPlayerOneDice";
+  } else if (gameState == "rollPlayerOneDice") {
+    for (var i = 0; i < 2; i += 1) playerOneDiceRolls[i] = rollDice(); // rolls dice twice and stores it in the corresponding index of the array
+    myOutputValue = `You rolled ${playerOneDiceRolls[0]} for Dice One and ${playerOneDiceRolls[1]} for Dice Two.<br> Choose the order of your dice`;
+    gameState = "askPlayerOneChoice";
+    console.log(gameState);
+  }
   return myOutputValue;
 };
 
