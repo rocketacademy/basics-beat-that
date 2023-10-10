3d11aebcb1d24735ea9823a862ce06aac305cd87
lesterc013 <lesterc013@gmail.com>
2023-10-10 21:13:42 +0800

    Added rules and pseudocode for player's first dice roll

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Basics - Beat That Rules and Brief Flow
+// There are 2 players and players take turns.
+// When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
+// The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
+// After both players have rolled and chosen dice order, the player with the higher combined number wins.
+
+// Pseudocode
+// 1st player clicks submit, a helper function will roll dice which returns a number between 1 to 6 and store this number in playerOneFirstDiceRoll. roll dice again and store this number in playerOneSecondDiceRoll
+// then, we need to ask the player to specify which order they want the dice to be in
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
