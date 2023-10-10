e3bde9c3b6f3f58c33ba716b55de5ca4f5a0aca0
jasperteo <jaspertzj@outlook.sg>
2023-10-11 04:17:28 +0800

    added game logic for player 1

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
+//Declare Global Variables
+let player1Roll,
+  player2Roll,
+  gameMessage,
+  player1Number,
+  player2Number,
+  diceOrder;
+
+//Main Function
+let main = function (input, myOutputValue) {
+  if (!player1Roll) {
+    player1Roll = rollTwoDice();
+    gameMessage = `Welcome Player 1.<br>You rolled ${player1Roll[0]} for Dice 1 and ${player1Roll[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first`;
+  } else if (player1Roll && !player2Roll) {
+    diceOrder = Number(input);
+    switch (diceOrder) {
+      case 1:
+        player1Number = `${player1Roll[0]}` + `${player1Roll[1]}`;
+        break;
+      case 2:
+        player1Number = `${player1Roll[1]}` + `${player1Roll[0]}`;
+        break;
+      default:
+        return `Invalid input.<br>You rolled ${player1Roll[0]} for Dice 1 and ${player1Roll[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first`;
+    }
+    gameMessage = `Player 1, you chose Dice ${diceOrder} first.<br>Your number is ${player1Number}.<br>It is now Player 2's turn.`;
+  }
+  myOutputValue = gameMessage;
   return myOutputValue;
 };
+
+let rollTwoDice = () => [
+  Math.floor(Math.random() * 6) + 1,
+  Math.floor(Math.random() * 6) + 1,
+];
