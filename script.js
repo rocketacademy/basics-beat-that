a4363514662c329afdb084db3424d9d1cecb3f78
lesterc013 <lesterc013@gmail.com>
2023-10-10 21:29:29 +0800

    Added for loop to roll dice twice for player one

--- script.js
+++ script.js
@@ -8,8 +8,12 @@
 // 1st player clicks submit, a helper function will roll dice which returns a number between 1 to 6 and store this number in playerOneFirstDiceRoll. roll dice again and store this number in playerOneSecondDiceRoll
 // then, we need to ask the player to specify which order they want the dice to be in
 
+// Make the dice roll variables global because we will need to switch game states
+var playerOneDiceRolls = [];
+
 var main = function () {
-  var myOutputValue = rollDice();
+  for (var i = 0; i < 2; i += 1) playerOneDiceRolls[i] = rollDice(); // rolls dice twice and stores it in the corresponding index of the array
+  var myOutputValue = playerOneDiceRolls;
   return myOutputValue;
 };
 
