d4a0f339c3384ffc0bae9b404948a37691b48198
jasperteo <jaspertzj@outlook.sg>
2023-10-12 05:14:44 +0800

    rewrote some parts for readability

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Declare Global Variables
-let numberRolled,
+let currentNumberRolled,
   gameMessage,
   player1Number,
   player2Number,
-  diceOrder,
+  currentDiceOrder,
   player1Score = 0,
   player2Score = 0;
 
@@ -11,26 +11,28 @@ let numberRolled,
 let main = function (input, myOutputValue) {
   if (player1Number && player2Number) {
     evalutateWinner();
-    player1Number = null;
-    player2Number = null;
-    diceOrder = null;
-    numberRolled = null;
-  } else if (!player1Number && !numberRolled) {
-    numberRolled = rollTwoDice();
-    gameMessage = `Welcome Player 1.<br>You rolled ${numberRolled[0]} for Dice 1 and ${numberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first`;
-  } else if (!player1Number && numberRolled) {
-    diceOrder = Number(input);
-    player1Number = combineDiceNumber();
-    gameMessage = `Player 1, you chose Dice ${diceOrder} first.<br>Your number is ${player1Number}.<br>It is now Player 2's turn.`;
-    diceOrder = null;
-    numberRolled = null;
-  } else if (!player2Number && !numberRolled) {
-    numberRolled = rollTwoDice();
-    gameMessage = `Welcome Player 2.<br>You rolled ${numberRolled[0]} for Dice 1 and ${numberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first.`;
-  } else if (!player2Number && numberRolled) {
-    diceOrder = Number(input);
-    player2Number = combineDiceNumber();
-    gameMessage = `Player 2, you chose Dice ${diceOrder} first.<br>Your number is ${player2Number}. Press sumbmit to reveal winner.`;
+    resetGame();
+  } else if (!player1Number && !currentNumberRolled) {
+    currentNumberRolled = rollTwoDice();
+    gameMessage = `Welcome Player 1.<br>You rolled ${currentNumberRolled[0]} for Dice 1 and ${currentNumberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first`;
+  } else if (!player1Number && currentNumberRolled) {
+    if (input == `1` || input == `2`) {
+      currentDiceOrder = Number(input);
+      player1Number = combineDiceNumber();
+      gameMessage = `Player 1, you chose Dice ${currentDiceOrder} first.<br>Your number is ${player1Number}.<br>It is now Player 2's turn.`;
+      semiResetGame();
+    } else
+      gameMessage = `Invalid input.<br>You rolled ${currentNumberRolled[0]} for Dice 1 and ${currentNumberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first.`;
+  } else if (!player2Number && !currentNumberRolled) {
+    currentNumberRolled = rollTwoDice();
+    gameMessage = `Welcome Player 2.<br>You rolled ${currentNumberRolled[0]} for Dice 1 and ${currentNumberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first.`;
+  } else if (!player2Number && currentNumberRolled) {
+    if (input == `1` || input == `2`) {
+      currentDiceOrder = Number(input);
+      player2Number = combineDiceNumber();
+      gameMessage = `Player 2, you chose Dice ${currentDiceOrder} first.<br>Your number is ${player2Number}. Press sumbmit to reveal winner.`;
+    } else
+      gameMessage = `Invalid input.<br>You rolled ${currentNumberRolled[0]} for Dice 1 and ${currentNumberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first.`;
   }
   myOutputValue = gameMessage;
   return myOutputValue;
@@ -44,31 +46,38 @@ let rollTwoDice = () => [
 
 //Concatenate the rolls to form new number
 function combineDiceNumber() {
-  switch (diceOrder) {
+  switch (currentDiceOrder) {
     case 1:
-      return Number(`${numberRolled[0]}` + `${numberRolled[1]}`);
+      return Number(`${currentNumberRolled[0]}` + `${currentNumberRolled[1]}`);
     case 2:
-      return Number(`${numberRolled[1]}` + `${numberRolled[0]}`);
-    default:
-      return `Invalid input.<br>You rolled ${numberRolled[0]} for Dice 1 and ${numberRolled[1]} for Dice 2.<br>Choose the order of the dice, type "1" for Dice 1 first and type "2" for Dice 2 first.`;
+      return Number(`${currentNumberRolled[1]}` + `${currentNumberRolled[0]}`);
   }
 }
 
 //Compare both numbers to determine winner
 function evalutateWinner() {
-  switch (true) {
-    case player1Number > player2Number:
-      gameMessage = `Winner is Player 1 with ${player1Number} over Player 2 with ${player2Number}.`;
-      break;
-    case player1Number < player2Number:
-      gameMessage = `Winner is Player 2 with ${player2Number} over Player 1 with ${player1Number}.`;
-      break;
-    case player1Number == player2Number:
-      gameMessage = `It is a draw with both players getting ${player1Number}.`;
-      break;
-    default:
-      return `Error with evaluating winner.`;
-  }
+  gameMessage =
+    player1Number > player2Number
+      ? `Winner is Player 1 with ${player1Number} over Player 2 with ${player2Number}.`
+      : player1Number < player2Number
+      ? `Winner is Player 2 with ${player2Number} over Player 1 with ${player1Number}.`
+      : player1Number == player2Number
+      ? `It is a draw with both players getting ${player1Number}.`
+      : `Error with evaluating winner.`;
+}
+
+//Reset entire game state
+function resetGame() {
+  player1Number = null;
+  player2Number = null;
+  currentDiceOrder = null;
+  currentNumberRolled = null;
+}
+
+//Reset game state for next player
+function semiResetGame() {
+  currentDiceOrder = null;
+  currentNumberRolled = null;
 }
 
 function scoreRecord() {}
