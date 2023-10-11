b459964eb018bfafe7bd2fc784fd9f217e196d98
benleong94 <bendemeerben@gmail.com>
2023-10-11 13:15:22 +0800

    Reorganized the html, css and js files

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var button = document.querySelector("#submit-button");
+button.addEventListener("click", function () {
+  var input = document.querySelector("#input-field");
+  var result = main(input.value);
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+  input.value = "";
+});
+
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
