0be16c1b83a38558fdfd875c277e5b758b2d0c89
lesterc013 <lesterc013@gmail.com>
2023-10-17 23:00:00 +0800

    Commented out old main. Added new main to use as base. Added pseudocode for Auto-Generate Combined Number AGCN

--- script-more-comfortable.js
+++ script-more-comfortable.js
@@ -51,8 +51,31 @@
 // insert another gameState for checkResultLowest which outputs winner based on lowest combined score instead
 // then we switch the gameState accordingly
 
+// Auto-Generate Combined Number
+/* Update the game to auto-generate the highest (or lowest) combined number from dice rolls. For example, for dice rolls [6, 3] in Lowest Combined Number mode, the game would auto-generate the combined number 36. */
+// means i dont even need the askPlayerOneChoice and askPlayerTwoChoice game states already?
+
+// HTML code to tell user to input "normal" or "lowest" to start the game
+// need to have a starting gameState = "start"
+// 1. starting gameState will ask userChoice = input for "normal" or "lowest" game mode
+// user validation
+// helper function to return the next gameState = "normalRollDice" or "lowestRollDice"
+// output will be `You chose ${userChoice} game mode. Press Submit to roll the dice.`
+
+// 2. gameState both "normalRollDice" and "lowestRollDice" will then roll two dices using for loop
+// the dice value will store in playerXDiceRolls
+// based on the game mode, use Math.max or Math.min(...array) first, then the other second to get the 1st and 2nd numbers spread operator to spread the array in the Math method.
+// Store 1st number in playerXFirstNum, 2nd number in playerXSecondNum
+// Concatenate and convert to the final number using helper function. Store in playerXFinalNumber
+// update the running score using helper function
+// output will be `Player 1 rolled X for Dice 1 and Y for Dice 2. <br> Player 1 auto-gen number is XY.<br> Player 1 running score is XXX. <br> <br> Player 2 rolled A for Dice 1 and B for Dice 2 <br> Player 2 auto-gen number is AB. <br> Player 2 running score is YYY <br> <br> Press Submit to find winner based on "normal"/"lowest" game state`
+// change gameState to "checkResultNormal" or "checkResultLowest"
+
+// 3. can take the already used checkResult code
+// output the leader and the leaderboard
+
 // initialise the initial gameState
-var gameState = "rollPlayerOneDice";
+// var gameState = "rollPlayerOneDice";
 
 // global playerOne variables so we can access them throughout the different gameStates
 var playerOneDiceRolls = [];
@@ -175,6 +198,119 @@ var main = function (input) {
   }
   return myOutputValue;
 };
+
+// old main up till Lowest Combined Number Mode
+// var main = function (input) {
+//   // "refresh" myOutputValue everytime we restart the app i.e. pressing submit
+//   var myOutputValue = "";
+//   if (gameState == "rollPlayerOneDice") {
+//     // for loop to roll two dices and store the dice rolls in playerOneDiceRolls array
+//     for (var i = 0; i < 2; i += 1) playerOneDiceRolls[i] = rollDice();
+//     // reassign myOutputValue to show Player 1 their rolls and ask them to choose which dice to be first
+//     myOutputValue = `Welcome Player 1.<br>You rolled ${playerOneDiceRolls[0]} for Dice One and ${playerOneDiceRolls[1]} for Dice Two.<br>Choose the order of your dice by inputting 1 for Dice One first or 2 for Dice Two first`;
+//     gameState = "askPlayerOneChoice";
+//   } else if (gameState == "askPlayerOneChoice") {
+//     // assign input to the diceNumberChosen
+//     var diceNumberChosen = input;
+//     // input validation - if user inputs a valid diceNumberChosen
+//     if (diceNumberChosen == 1 || diceNumberChosen == 2) {
+//       // function to return the number in the tens position
+//       var playerOneNumberInTens = returnNumberInTensPosition(
+//         diceNumberChosen,
+//         playerOneDiceRolls
+//       );
+//       // function to return the number in the ones position
+//       var playerOneNumberInOnes = returnNumberInOnesPosition(
+//         diceNumberChosen,
+//         playerOneDiceRolls
+//       );
+//       // function to concatenate the numbers and then return it as a Number type
+//       playerOneFinalNumber = returnFinalNumber(
+//         playerOneNumberInTens,
+//         playerOneNumberInOnes
+//       );
+//       // function to update playerOneRunningScore
+//       playerOneRunningScore = updateRunningScore(
+//         playerOneRunningScore,
+//         playerOneFinalNumber
+//       );
+//       // reassign myOutputValue to tell playerOne the final number and their running score. And tell Player 2 to resubmit to roll the dice
+//       myOutputValue = `You chose Dice ${diceNumberChosen} first. <br> Your final number this round is ${playerOneFinalNumber}.<br>Your running score is ${playerOneRunningScore}.<br> Press submit to roll Player 2's dice`;
+//       gameState = "rollPlayerTwoDice";
+//     } else {
+//       // user validation if the user inputs wrong diceNumberChosen
+//       myOutputValue = `Please enter a valid input of either 1 or 2 only. <br>You rolled ${playerOneDiceRolls[0]} for Dice One and ${playerOneDiceRolls[1]} for Dice Two.<br>Choose the order of your dice`;
+//     }
+//   } else if (gameState == "rollPlayerTwoDice") {
+//     for (var i = 0; i < 2; i += 1) playerTwoDiceRolls[i] = rollDice();
+//     myOutputValue = `Player 2,<br>You rolled ${playerTwoDiceRolls[0]} for Dice One and ${playerTwoDiceRolls[1]} for Dice Two.<br>Choose the order of your dice`;
+//     gameState = "askPlayerTwoChoice";
+//   } else if (gameState == "askPlayerTwoChoice") {
+//     var diceNumberChosen = input;
+//     // input validation
+//     if (diceNumberChosen == 1 || diceNumberChosen == 2) {
+//       var playerTwoNumberInTens = returnNumberInTensPosition(
+//         diceNumberChosen,
+//         playerTwoDiceRolls
+//       );
+//       var playerTwoNumberInOnes = returnNumberInOnesPosition(
+//         diceNumberChosen,
+//         playerTwoDiceRolls
+//       );
+//       playerTwoFinalNumber = returnFinalNumber(
+//         playerTwoNumberInTens,
+//         playerTwoNumberInOnes
+//       );
+//       playerTwoRunningScore = updateRunningScore(
+//         playerTwoRunningScore,
+//         playerTwoFinalNumber
+//       );
+//       myOutputValue = `You chose Dice ${diceNumberChosen} first. <br> Your final number this round is ${playerTwoFinalNumber}.<br>Your running score is ${playerTwoRunningScore}.<br> Now, choose the game state. Enter "normal" for the normal game state or "lowest" for lowest combined number game state.`;
+//       // change this to the intermediate step to ask user for which checkResult they want
+//       gameState = "askUserChoiceForGameState";
+//     } else {
+//       myOutputValue = `Please enter a valid input of either 1 or 2 only.<br> You rolled ${playerTwoDiceRolls[0]} for Dice One and ${playerTwoDiceRolls[1]} for Dice Two.<br>Choose the order of your dice`;
+//     }
+//     // intermediate gameState to ask user to switch
+//   } else if (gameState == "askUserChoiceForGameState") {
+//     var userChoice = input;
+//     // user validation
+//     if (userChoice == "normal" || userChoice == "lowest") {
+//       gameState = chooseNextGameState(userChoice);
+//       myOutputValue = `You have chosen ${userChoice} as the next game state.<br> Press "Submit" once more to see the results based on your choice`;
+//     } else
+//       myOutputValue = `Please enter a valid choice of "normal" or "lowest" only`;
+//   }
+//   // normal gameState
+//   else if (gameState == "checkResultNormal") {
+//     // function to check who is the leader in terms of running score
+//     leader = checkLeader(playerOneRunningScore, playerTwoRunningScore);
+//     // function to display leaderboard
+//     leaderboard = displayLeaderboard(
+//       playerOneRunningScore,
+//       playerTwoRunningScore
+//     );
+//     myOutputValue = `Player 1's number this round is ${playerOneFinalNumber}.<br>
+//     Player 2's final number this round is ${playerTwoFinalNumber}<br>
+//     Current Leader based on normal game state is ${leader}.<br><br>${leaderboard}<br><br>Press Submit again to reroll from Player 1`;
+//     gameState = "rollPlayerOneDice";
+//   }
+//   // lowest gameState
+//   else if (gameState == "checkResultLowest") {
+//     // function to check who is the "winner" in terms of lowest running score
+//     lowestLeader = checkLowest(playerOneRunningScore, playerTwoRunningScore);
+//     // function to display leaderboard
+//     lowestLeaderboard = displayLowestLeaderboard(
+//       playerOneRunningScore,
+//       playerTwoRunningScore
+//     );
+//     myOutputValue = `Player 1's number this round is ${playerOneFinalNumber}.<br>
+//     Player 2's final number this round is ${playerTwoFinalNumber}<br>
+//     Current Leader based on lowest combined number game state is ${lowestLeader}.<br><br>${lowestLeaderboard}<br><br>Press Submit again to reroll from Player 1`;
+//     gameState = "rollPlayerOneDice";
+//   }
+//   return myOutputValue;
+// };
 
 // Dice Roll Helper Function - returns a random number between 1 to 6
 var rollDice = function () {
