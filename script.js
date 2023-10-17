b5cccfe6247986ba8b67be88a9bb344f177d5038
jasperteo <jaspertzj@outlook.sg>
2023-10-17 17:12:00 +0800

    added leaderboard sort

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 //Declare Global Variables
-let currentNumberRolled = [],
-  gameMessage,
-  scoreTable = [],
-  scoreRecord = [],
-  gameMode,
+let gameMode,
   numberOfDice,
   numberOfPlayers,
-  playerCounter = 0,
-  currentNumber;
+  gameMessage,
+  currentNumberRolled = [],
+  scoreTable = [],
+  scoreRecord = [],
+  scoreRecordSorted = [],
+  playerCounter = 0;
 
 //Main Function
 let main = function (input, myOutputValue) {
   if (numberOfDice && numberOfPlayers && gameMode) {
     if (playerCounter < numberOfPlayers) {
-      playerLogic();
-      recordScore();
+      playerRound();
       semiResetGame();
       playerCounter++;
     } else {
@@ -45,44 +44,34 @@ let main = function (input, myOutputValue) {
     } else
       gameMessage = "Invalid input, type in the number of players (integer).";
   } else gameMessage = "Error in main function.";
-  myOutputValue = gameMessage + `<br><br>${leaderboard()}`;
+  sortLeaderboard();
+  myOutputValue = gameMessage + `<br><br>${displaySortedLeaderboard()}`;
   return myOutputValue;
 };
 
-//Generate leaderboard
-function leaderboard(record = "") {
-  for (let i = 0; i < scoreRecord.length; i++) {
-    record += `Player ${i + 1}: ${scoreRecord[i]}<br>`;
-  }
-  return record;
-}
-
-//Determine winner
-function winEvaluation() {
-  switch (gameMode) {
-    case "H":
-      let maxValue = Math.max(...scoreTable);
-      let maxIndex = scoreTable.indexOf(maxValue);
-      return `Player ${maxIndex + 1} won with ${maxValue}.`;
-    case "L":
-      let minValue = Math.min(...scoreTable);
-      let minIndex = scoreTable.indexOf(maxValue);
-      return `Player ${minIndex + 1} won with ${minValue}.`;
-  }
-}
-
-//Generate a number for player
-function playerLogic() {
+//Generate a number for player and record score
+function playerRound() {
   currentNumberRolled = rollDice();
   let displayRoll = [...currentNumberRolled];
   sortDiceNumber();
-  currentNumber = combineDiceNumber();
+  let currentNumber = combineDiceNumber();
+  scoreTable[playerCounter] += currentNumber;
+  scoreRecord[playerCounter] += scoreTable[playerCounter];
   gameMessage = `Welcome Player ${
     playerCounter + 1
   }.<br>You rolled: ${displayRoll}<br>Your number is ${currentNumber}.`;
 }
 
-//Insertion sort algorithm
+//Generate rolls from number of dice
+function rollDice() {
+  let outputArray = [];
+  for (let i = 0; i < numberOfDice; i++) {
+    outputArray.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return outputArray;
+}
+
+//Insertion sort algorithm for dice number
 function sortDiceNumber() {
   let i, j, holdIndex;
   switch (gameMode) {
@@ -108,46 +97,104 @@ function sortDiceNumber() {
 }
 
 //Concatenate the rolls to form new number
-const combineDiceNumber = (concatenatedNumber = "") => {
+function combineDiceNumber() {
+  let concatenatedNumber = "";
   for (let i = 0; i < currentNumberRolled.length; i++) {
     concatenatedNumber += currentNumberRolled[i];
   }
   return Number(concatenatedNumber);
-};
+}
 
-//Generate rolls from number of dice
-const rollDice = (outputArray = []) => {
-  for (let i = 0; i < numberOfDice; i++) {
-    outputArray.push(Math.floor(Math.random() * 6) + 1);
+//Generate leaderboard from highest to lowest
+function displaySortedLeaderboard() {
+  record = "";
+  for (let [player, score] of scoreRecordSorted) {
+    record += `Player ${Number(player) + 1}: ${score}<br>`;
   }
-  return outputArray;
-};
+  return record;
+}
 
-//Generates score table
-const recordScore = () => {
-  scoreTable[playerCounter] += currentNumber;
-  scoreRecord[playerCounter] += scoreTable[playerCounter];
-};
+//Insertion Sort for leaderboard
+function sortLeaderboard() {
+  let i, j, holdIndex;
+  scoreRecordSorted = Object.entries({ ...scoreRecord });
+  switch (gameMode) {
+    case "H":
+      for (i = 1; i < scoreRecordSorted.length; i++) {
+        holdIndex = scoreRecordSorted[i];
+        for (j = i - 1; j >= 0 && scoreRecordSorted[j][1] < holdIndex[1]; j--) {
+          scoreRecordSorted[j + 1] = scoreRecordSorted[j];
+        }
+        scoreRecordSorted[j + 1] = holdIndex;
+      }
+      break;
+    case "L":
+      for (i = 1; i < scoreRecordSorted.length; i++) {
+        holdIndex = scoreRecordSorted[i];
+        for (j = i - 1; j >= 0 && scoreRecordSorted[j][1] > holdIndex[1]; j--) {
+          scoreRecordSorted[j + 1] = scoreRecordSorted[j];
+        }
+        scoreRecordSorted[j + 1] = holdIndex;
+        break;
+      }
+  }
+}
+
+//Determine winner
+function winEvaluation() {
+  switch (gameMode) {
+    case "H":
+      let maxValue = Math.max(...scoreTable);
+      let maxIndex = scoreTable.indexOf(maxValue);
+      return `Player ${maxIndex + 1} won with ${maxValue}.`;
+    case "L":
+      let minValue = Math.min(...scoreTable);
+      let minIndex = scoreTable.indexOf(maxValue);
+      return `Player ${minIndex + 1} won with ${minValue}.`;
+  }
+}
+
+// //Find max/min value and index of it
+// function winEvaluate() {
+//   switch (gameMode) {
+//     case "H":
+//       let maxIndex = 0;
+//       for (let i = 1; i < scoreTable.length; i++) {
+//         if (scoreTable[i] > scoreRecord[maxIndex]) {
+//           maxIndex = i;
+//         }
+//       }
+//       let maxValue = scoreRecord[maxIndex];
+//       return `Player ${maxIndex + 1} won with ${maxValue}.`;
+//     case "L":
+//       let minIndex = 0;
+//       for (let i = 1; i < scoreTable.length; i++) {
+//         if (scoreTable[i] < scoreRecord[minIndex]) {
+//           minIndex = i;
+//         }
+//       }
+//       let minValue = scoreRecord[minIndex];
+//       return `Player ${minIndex + 1} won with ${minValue}.`;
+//   }
+// }
 
 //Reset game state for next player
-const semiResetGame = () => {
-  currentNumberRolled = null;
-  currentNumber = null;
-};
+const semiResetGame = () => (currentNumberRolled = null);
 
 //Reset entire game state
-const resetGame = () => {
+function resetGame() {
   currentNumberRolled = null;
-  currentNumber = null;
   numberOfDice = null;
   playerCounter = 0;
   for (let i = 0; i < scoreTable.length; i++) {
     scoreTable[i] = 0;
   }
-};
+}
 
-// //Fill array function
-// function fillArray() {}
-
-// //Find max/min value and index of it
-// function evaluate() {}
+//Fill array function
+// function fillScoreArrays() {
+//   for (let i = 0; i < numberOfPlayers; i++) {
+//     scoreTable.push(0);
+//     scoreRecord.push(0);
+//   }
+// }
