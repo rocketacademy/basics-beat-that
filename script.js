12d78bcd53b47d6a2afb9d92421e1714a822482b
samlee-zy <samlee.zy95@gmail.com>
2023-12-03 19:22:16 +1100

    Sam's submission of Project 2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+// Author: Samuel Lee
+// Last Updated: 03/12/2023
+
 // =========== GLOBAL VARIABLES ==========
-var player1Name = "";
-var player2Name = "";
-var player1Dice1 = 0;
-var player1Dice2 = 0;
-var player1Number = 0;
-var player2Dice1 = 0;
-var player2Dice2 = 0;
-var player2Number = 0;
-var journeyCounter = 0;
+
+// GAME MODES
+var DEFAULT_MODE = "default";
+var PLAYER_COUNT = "playercount";
+var PLAYER_NAMES = "inputplayernames";
+var GAME_MODE = "gametime";
+var RESULT_MODE = "results";
+var TALLY_TIME = "tallyresults";
+var gamemode = PLAYER_COUNT;
+
+// GLOBAL VARIABLES
+var nameCounter = 0;
+var playerNames = [];
+var diceResults = [];
+var gameScore = [];
+var numberOfPlayers = 0;
+var diceRoll_1 = 0;
+var diceRoll_2 = 0;
+var currentPlayer = 0;
+var previousPlayer = 0;
 
 // =========== HELPER FUNCTIONS ==========
 // Function that helps autogenerate the dice roll values
@@ -32,79 +46,131 @@ var nameString = function (input) {
 };
 
 // Initial Dice rolls for each player and determines which player is currently going through this linear journey
-var playerInput = function (input, counter) {
-  if (counter == 0) {
-    player1Name = input;
-    player1Dice1 = diceRoll();
-    player1Dice2 = diceRoll();
-    journeyCounter++; // journeyCounter == 1 here
-    return `Player 1's name is ${player1Name}. <br>For Dice 1, you rolled: ${player1Dice1}.<br>For Dice 2, you rolled: ${player1Dice2}.<br> Please enter '1' for Dice 1 to be first, or '2' for Dice 2 to be the first.`;
-  } else {
-    player2Name = input;
-    player2Dice1 = diceRoll();
-    player2Dice2 = diceRoll();
-    journeyCounter++;
-    return `Player 2's name is ${player2Name}. <br>For Dice 1, you rolled: ${player2Dice1}.<br>For Dice 2, you rolled: ${player2Dice2}.<br> Please enter '1' for Dice 1 to be first, or '2' for Dice 2 to be the first.`;
-  }
+var playerInput = function (currentPlayer) {
+  console.log("Entering PlayerInput function");
+  diceRoll_1 = diceRoll();
+  diceRoll_2 = diceRoll();
+  gamemode = RESULT_MODE;
 };
 
 // Function that assists in calculating the player's final Dice results after selecting their Dice positions
-var playerResult = function (input, counter) {
-  if (counter == 1) {
-    if (input == 1) {
-      journeyCounter++;
-      player1Number = player1Dice1.toString() + player1Dice2.toString();
-      return `${player1Name}, you chose Dice 1 first.<br> Your number is now: ${player1Number}.<br> It is now Player 2's turn.`;
-    } else {
-      journeyCounter++;
-      player1Number = player1Dice2.toString() + player1Dice1.toString();
-      return `${player1Name}, you chose Dice 2 first.<br> Your number is now: ${player1Number}.<br> It is now Player 2's turn.`;
-    }
-  } else {
-    if (input == 1) {
-      player2Number = player2Dice1.toString() + player2Dice2.toString();
-      if (Number(player1Number) > Number(player2Number)) {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player1Name} has the greater value, ${player1Name} WINS!`;
-      } else if (Number(player1Number) < Number(player2Number)) {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player2Name} has the greater value, ${player2Name} WINS!`;
-      } else {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player1Name} and ${player2Name} has the same number, it's a DRAW!`;
-      }
-      return `${player2Name}, you chose Dice 1 first.<br> Your number is now: ${player2Number}.`;
-    } else {
-      player2Number = player2Dice2.toString() + player2Dice1.toString();
-      if (Number(player1Number) > Number(player2Number)) {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player1Name} has the greater value, ${player1Name} WINS!`;
-      } else if (Number(player1Number) < Number(player2Number)) {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player2Name} has the greater value, ${player2Name} WINS!`;
-      } else {
-        return `${player1Name} number: ${player1Number}.<br>${player2Name} number: ${player2Number}.<br>As ${player1Name} and ${player2Name} has the same number, it's a DRAW!`;
-      }
-    }
+var playerResult = function (input) {
+  if (input == 1) {
+    diceResults[currentPlayer] = diceRoll_1.toString() + diceRoll_2.toString();
+    previousPlayer = currentPlayer;
+    currentPlayer++;
+    gamemode = GAME_MODE;
+  } else if (input == 2) {
+    diceResults[currentPlayer] = diceRoll_2.toString() + diceRoll_1.toString();
+    previousPlayer = currentPlayer;
+    currentPlayer++;
+    gamemode = GAME_MODE;
   }
+  return `${playerNames[previousPlayer]}, you chose Dice ${input} first.<br> Your number is now: <b>${diceResults[previousPlayer]}</b>.<br>Player ${playerNames[currentPlayer]} turn. <br> Please press <b>Submit</b> to start your roll`;
+};
+
+// Identifying the highest number in the array, and returns the index of the array so we can determine the winner
+var playerTally = function (playerScores) {
+  // Convert strings to integers
+  const numericArray = playerScores.map(Number);
+
+  // Find the maximum value in the array
+  const maxValue = Math.max(...numericArray);
+
+  // Find the index of the maximum value
+  const indexOfMaxValue = numericArray.indexOf(maxValue);
+
+  return indexOfMaxValue;
 };
 
 // =========== MAIN FUNCTION ==========
 var main = function (input) {
+  // Identifies the input and restarts the game if 'exit' is typed
   var usrInput = input;
-  if (journeyCounter == 0) {
-    if (nameString(usrInput) !== true) {
-      return `Please only input characters into the textbox! ${usrInput} is not a valid input`;
+  if (usrInput == "exit") {
+    location.reload(true);
+  }
+
+  // Determines the number of players, playing the game
+  if (gamemode == PLAYER_COUNT) {
+    console.log("Entering PLAYER_COUNT gamemode");
+    numberOfPlayers = usrInput;
+    for (var i = 0; i < numberOfPlayers; i++) {
+      gameScore[i] = 0;
     }
-    return playerInput(usrInput, journeyCounter);
+    gamemode = PLAYER_NAMES;
+
+    return `Please enter the player's names. Once you add 1 player's name, press submit. Continue to do so until all player's names have been submitted.`;
   }
-  console.log(journeyCounter);
-  if (journeyCounter == 1) {
-    return playerResult(input, journeyCounter);
+
+  // Once player types start, the game mode is switched on!
+  if (usrInput.toLowerCase() == "start" && playerNames.length != 0) {
+    console.log(`Entering GAME_MODE gamemode`);
+    gamemode = GAME_MODE;
   }
-  if (journeyCounter == 2) {
-    if (nameString(usrInput) !== true) {
-      return `Please only input characters into the textbox! ${usrInput} is not a valid input`;
+
+  // If reroll is typed, then we go into the next round
+  if (usrInput.toLowerCase() == "reroll" && playerNames.length != 0) {
+    console.log(`Entering GAME_MODE gamemode`);
+    gamemode = GAME_MODE;
+    diceResults = [];
+    currentPlayer = 0;
+    previousPlayer = 0;
+  }
+
+  // The moment the number of turns have reached the number of players or more, we get the final results
+  if (currentPlayer > numberOfPlayers) {
+    console.log(`Entering TALLY_TIME gamemode`);
+    gamemode = TALLY_TIME;
+    var indexOfWinner = playerTally(diceResults);
+    var storeOutputString = "";
+    for (var i = 0; i < numberOfPlayers; i++) {
+      if (i == indexOfWinner) {
+        gameScore[i] += 1;
+      }
+      storeOutputString += `Player <b>${playerNames[i]}</b> dice roll: <b>${diceResults[i]}</b> | current score: <b>${gameScore[i]}</b><br>`;
     }
-    return playerInput(usrInput, journeyCounter);
+    return (
+      storeOutputString +
+      `<br>🎊 Player <b>${playerNames[indexOfWinner]}</b> wins!🥳<br> <br> Type <b>'reroll'</b> to play again<br>Type <b>'exit'</b> to restart this game!`
+    );
   }
-  if (journeyCounter == 3) {
-    journeyCounter++;
-    return playerResult(input, journeyCounter);
+
+  // If the gamemode == GAME_MODE, operate as such:
+  if (gamemode == GAME_MODE) {
+    console.log(`Entering GAME_MODE gamemode - playerInput`);
+    console.log(`Current Player: ${playerNames[currentPlayer]}`);
+    playerInput(currentPlayer);
+    return `<b>${playerNames[previousPlayer]} rolls</b>. <br>For Dice 1, you rolled: ${diceRoll_1}.<br>For Dice 2, you rolled: ${diceRoll_2}.<br> Please enter '1' for Dice 1 to be first, or '2' for Dice 2 to be the first.`;
+  }
+
+  // Gets the results based on User's selection of which Dice goes first
+  if (gamemode == RESULT_MODE) {
+    console.log(`Entering RESULT_MODE gamemode`);
+    console.log(`Current Player: ${playerNames[currentPlayer]}`);
+
+    var saveResult = playerResult(usrInput);
+    if (currentPlayer == numberOfPlayers) {
+      currentPlayer++;
+      return `${playerNames[previousPlayer]}, you chose Dice ${input} first.<br> Your number is now: <b>${diceResults[previousPlayer]}</b>! <br><br>Press the <b>Submit</b> button to see who won!`;
+    }
+    return saveResult;
+  }
+
+  // Assuming that this is where we add the player's names.
+  if (gamemode == PLAYER_NAMES) {
+    var storePlayerNames = "";
+    console.log("Entering PLAYER_NAMES gamemode");
+    while (nameCounter <= numberOfPlayers) {
+      playerNames[nameCounter] = usrInput;
+      nameCounter++;
+      if (nameCounter >= numberOfPlayers) {
+        for (var i = 0; i < playerNames.length; i++) {
+          storePlayerNames += `Player ${i + 1} name: ${playerNames[i]}<br>`;
+        }
+        return storePlayerNames + `Type <b>'start'</b> to begin the game!`;
+      }
+      return `Please add next player's name`;
+    }
   }
 };
