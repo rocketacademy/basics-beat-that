3378e2d629efb5686f75315c510813e3ad5acd0c
CapitolJazz <capitoljazzakat@gmail.com>
2024-02-04 16:06:05 +0800

    vers02. two players roll and order dice

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var GAME_STATE_DICE_ROLL = 'GAME_STATE_DICE_ROLL';
-var GAME_STATE_CHOOSE_DICE_ROLL = 'GAME_STATE_CHOOSE_DICE_ROLL';
+var GAME_STATE_CHOOSE_DICE_ORDER = 'GAME_STATE_CHOOSE_DICE_ORDER';
+var GAME_STATE_COMPARE_SCORES = 'GAME_STATE_COMPARE_SCORES';
 var gameState = GAME_STATE_DICE_ROLL;
 
-var playerRolls = [];
+var currentPlayerRolls = []; 
+
+var currentPlayer = 1;
+var allPlayersScore = []; 
 
 var rollDice = function(){
   console.log('Control flow: start of rollDice()')
@@ -16,50 +20,69 @@ var rollDiceForPlayer = function(){
   console.log('Control flow: start of rollDiceForPlayer()');
   var counter = 0;
   while (counter < 2){
-    playerRolls.push(rollDice());
+    currentPlayerRolls.push(rollDice());
     counter = counter + 1;
   }
 
-  console.log('rollDiceForPLayer changes, playerRolls:', playerRolls);
-  return "Welcome<br><br>You rolled:<br>Dice 1:" + playerRolls[0] + "| Dice 2:" + playerRolls[1] + ".<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value."
+  console.log('rollDiceForPLayer changes, currentPlayerRolls:', currentPlayerRolls);
+  return "Welcome, Player "+ currentPlayer + "<br><br>You rolled:<br>Dice 1:" + currentPlayerRolls[0] + "| Dice 2:" + currentPlayerRolls[1] + ".<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value."
 };
 
 var getPlayerScore = function(playerInput){
 
+  var playerScore;
+
   if(playerInput != 1 && playerInput != 2){
       console.log('Control flow: input validation, valid input... Not 1 & not 2');
-      return"Error! Please only input '1' or '2' to choose which dice to use as the first digit.<br><br>Your dice rolls are:<br>Dice 1: " + playerRolls[0] + "| Dice 2: " + playerRolls[1] + "."; 
+      return"Error! Please only input '1' or '2' to choose which dice to use as the first digit.<br><br>Your dice rolls are:<br>Dice 1: " + currentPlayerRolls[0] + "| Dice 2: " + currentPlayerRolls[1] + "."; 
     }
 
     if(playerInput == 1){
-      var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
+      var playerScore = Number(String(currentPlayerRolls[0]) + String(currentPlayerRolls[1]));
       return "Your chosen value is:" + playerScore;
     }
 
     if(playerInput == 2){
-      var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
-      return "Your chosen value is:" + playerScore;
+      var playerScore = Number(String(currentPlayerRolls[1]) + String(currentPlayerRolls[0]));
+
+      allPlayersScore.push(playerScore);
+
+      curren = [];
+      return "Player "+ currentPlayer + ", your chosen value is: " + playerScore;
     }
 };
 
 var main = function (input) {
   console.log('Checking game state on submit click:', gameState);
-  var myOutputValue = '';
+  console.log('Checking currentPlayer on submit click:', currentPlayer);
+  var outputMessage = '';
 
   if(gameState == GAME_STATE_DICE_ROLL){
     console.log ('Control flow: gameState == GAME_STATE_DICE_ROLL');
-  }
   
-  outputMessage = rollDiceForPlayer();
-  gameState = GAME_STATE_CHOOSE_DICE_ROLL;
+    outputMessage = rollDiceForPlayer();
 
-  if(gameState == GAME_STATE_CHOOSE_DICE_ROLL){
+    gameState = GAME_STATE_CHOOSE_DICE_ORDER;
+    return outputMessage;
+  }
+
+  if(gameState == GAME_STATE_CHOOSE_DICE_ORDER){
     console.log('Control flow: gameState == GAME_STATE_CHOOSE_DICE_ROLL');
 
     outputMessage = getPlayerScore(input);
-    return outputMessage
 
+    if(currentPlayer == 1){
+      console.log("Control flow: end of player 1's turn, now player 2's turn");
+      currentPlayer = 2;
+      gameState = GAME_STATE_DICE_ROLL;
+      return outputMessage + "<br><br>It is now player 2's turn!";
+    }
+
+    if(currentPlayer == 2){
+      console.log("Control flow: end of player 2's turn, Next submit click will calculate score.");
+      gameState = GAME_STATE_COMPARE_SCORES;
+      return outputMessage + "<br><br>Press submit to calculate scores.";
+    }
   }
 
-  return myOutputValue;
-};
+}
