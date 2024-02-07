68f052493e9addd4eaf927ded35a8395f80102d7
yinglingtan <yingling.tan00@gmail.com>
2024-02-08 00:57:00 +0800

    ver 3

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// ===== Requirements ===== //
-// 1) there are 2 players and players take turns
-// 2) when a player clicks submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6
-// 3) the player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first
-// 4) after both players have rolled and chosen dice order, the player with the higher combined number wins
-
-// ==== problem breakdown and planing ==== //
-// ver 1. rolls 2 dice and turns the output for 1 player. That player chooses the dice order and get the correct return output.
-// ver 2. refactored code to include player 2
-//    - global variables for currentPlayer; allPlayersScore
-//    - refactor outputMessages to interact with each player, 1 and 2
-//    - write logic for player 1 to go first then player 2, and finally point towards comparing score
-// ver 3. implement comparing dice scores and declare winner
-// ver 4. reset the game so that the players can play continually without refreshing the browser page
-
 // Global Variables
 var GAME_STATE_DICE_ROLL = "GAME_STATE_DICE_ROLL";
 var GAME_STATE_CHOOSE_DICE_ORDER = "GAME_STATE_CHOOSE_DICE_ORDER";
@@ -22,7 +7,7 @@ var gameState = GAME_STATE_DICE_ROLL;
 var currentPlayerRolls = [];
 
 var currentPlayer = 1;
-var allPlayersScore = [];
+var allPlayerScore = [];
 
 // Helper Function
 var rollDice = function () {
@@ -51,16 +36,16 @@ var rollDiceForPlayer = function () {
   return (
     "Welcome, Player " +
     currentPlayer +
-    "<br><br>You rolled:<br>Dice 1; " +
+    "<br><br>You rolled:<br>Dice 1: " +
     currentPlayerRolls[0] +
-    " | Dice 2:" +
+    " | Dice 2: " +
     currentPlayerRolls[1] +
     ".<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value."
   );
 };
 
 var getPlayerScore = function (playerInput) {
-  var playerScores;
+  var playerScore;
   // input validation
   if (playerInput != 1 && playerInput != 2) {
     console.log(
@@ -91,11 +76,33 @@ var getPlayerScore = function (playerInput) {
     );
   }
   // Store playerScore in array
-  allPlayersScore.push(playerScore);
+  allPlayerScore.push(playerScore);
 
   // clear current player rolls array
   currentPlayerRolls = [];
   return "Player" + currentPlayer + ", your chosen value is: " + playerScore;
+};
+
+var comparePlayerScore = function () {
+  var outputMessage =
+    "Player 1 score: " +
+    allPlayerScore[0] +
+    "<br>Player 2 score: " +
+    allPlayerScore[1];
+
+  // player 1 wins
+  if (allPlayerScore[0] > allPlayerScore[1]) {
+    outputMessage = outputMessage + "<br><br>Player 1 wins!";
+  }
+  // player 2 wins
+  if (allPlayerScore[0] < allPlayerScore[1]) {
+    outputMessage = outputMessage + "<br><br>Player 2 wins!";
+  }
+  // tie
+  if (allPlayerScore[0] == allPlayerScore[1]) {
+    outputMessage = outputMessage + "<br><br>It's a tie!";
+    return outputMessage;
+  }
 };
 
 var main = function (input) {
@@ -140,25 +147,7 @@ var main = function (input) {
   if (gameState == GAME_STATE_COMPARE_SCORES) {
     console.log("Control flow: gameState == GAME_STATE_COMPARE_SCORES");
 
-    outputMessage =
-      "Player 1 score: " +
-      allPlayersScore[0] +
-      "<br>Player 2 score: " +
-      allPlayersScore[1];
-
-    // player 1 wins
-    if (allPlayerScore[0] > allPlayersScore[1]) {
-      outputMessage = outputMessage + "<br><br>Player 1 wins!";
-    }
-    // player 2 wins
-    if (allPlayerScore[0] > allPlayersScore[1]) {
-      outputMessage = outputMessage + "<br><br>Player 2 wins!";
-    }
-    // tie
-    if (allPlayersScore[0] == allPlayersScore(1)) {
-      outputMessage = outputMessage + "<br><br>It's a tie!";
-    }
-
+    outputMessage = comparePlayerScore();
     return outputMessage;
   }
 };
