d789485860cd9f0c5ed6cfbceb04ac1262b02dc9
franticenigma <kc.u.cuenca@gmail.com>
2024-02-07 14:49:56 +0800

    version 3: compare player scores

--- script.js
+++ script.js
@@ -85,6 +85,23 @@ var getPlayerScore = function (playerInput) {
   return `Player ${currentPlayer}, your chosen value is: ${playerScore}`;
 };
 
+var comparePlayersScores = function () {
+  var compareMessage = `Player 1 score: ${allPlayersScore[0]} <br> Player 2 score: ${allPlayersScore[1]}`;
+  // player 1 wins
+  if (allPlayersScore[0] > allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br><br> Player 1 wins!`;
+  }
+  // player 2 wins
+  if (allPlayersScore[0] < allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br><br> Player 2 wins!`;
+  }
+  // tie
+  if (allPlayersScore[0] == allPlayersScore[1]) {
+    compareMessage = `${compareMessage} <br><br> It's a tie!`;
+  }
+  return compareMessage;
+};
+
 var main = function (input) {
   console.log(`Checking game state on submit click: `, gameState);
   console.log(`Checking currentPlayer on submit click: `, currentPlayer);
@@ -119,5 +136,11 @@ var main = function (input) {
       gameState = GAME_STATE_COMPARE_SCORES;
       return `${outputMessage} Press submit to calculate scores!`;
     }
+  }
+  if (gameState == GAME_STATE_COMPARE_SCORES) {
+    console.log(`Control flow: gameState == GAME_STATE_COMPARE_SCORES`);
+
+    outputMessage = comparePlayersScores();
+    return outputMessage;
   }
 };
