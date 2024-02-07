4da8b3e8b92fd1378be06cf3cb2fbfd916183f47
yinglingtan <yingling.tan00@gmail.com>
2024-02-07 12:26:19 +0800

    vers 1. one player rolls and orders dice

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ var playerRolls = [];
 
 // Helper Function
 var rollDice = function () {
+  console.log("Control flow: start of rollDice()");
+  // Random decimal between 0 and 6
   var randomDecimal = Math.random() * 6;
+  // Random integer from 1 to 6
   var randomInteger = Math.floor(randomDecimal) + 1;
 
   console.log("rollDice output, randomInteger: ", randomInteger);
@@ -36,49 +39,49 @@ var rollDiceForPlayer = function () {
 
   console.log("rollDiceForPlayer changes, playerRolls: ", playerRolls);
   return (
-    "Welcome<br><br>You rolled: <br>Dice 1: " +
+    "Welcome<br><br>You rolled:<br>Dice 1; " +
     playerRolls[0] +
-    " | Dice 2: " +
+    " | Dice 2:" +
     playerRolls[1] +
     ".<br><br>Now, please input either '1' or '2' to choose the corresponding dice to be used as the first digit of your final value."
   );
 };
 
-var getPlayerScore = function (input) {
+var getPlayerScore = function (playerInput) {
   // input validation
-  if (input != 1 && input != 2) {
+  if (playerInput != 1 && playerInput != 2) {
     console.log(
-      "Control flow: input validation, invalid input... is NOT 1 AND NOT 2"
+      "Control flow: input validation, invalid inout... NOT 1 AND NOT 2"
     );
-    return (
-      "Error! Please only input '1' or '' to choose which dice to use as first digit.<br><br>Your dice rolls are: <br>Dice 1: " +
+    return;
+    "Error! Please only input '1' or '' to choose which dice to use as the first digit.<br><br>Your dice rolls are:<br>Dice 1: " +
       playerRolls[0] +
       " | Dice 2: " +
       playerRolls[1] +
-      "."
-    );
+      ".";
   }
   // input == 1
-  if (input == 1) {
+  if (playerInput == 1) {
     console.log("Control flow: input == 1");
     var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
     return "Your chosen value is: " + playerScore;
   }
+
   // input == 2
-  if (input == 2) {
+  if (playerInput == 2) {
     console.log("Control flow: input == 2");
-    var playerScore = Number(String(playerRolls[0]) + String(playerRolls[0]));
+    var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
     return "Your chosen value is: " + playerScore;
   }
 };
-
 var main = function (input) {
   console.log("Checking game state on submit click: ", gameState);
-  var OutputMessage = "";
+  var myOutputMessage = "";
+
   if (gameState == GAME_STATE_DICE_ROLL) {
     console.log("Control flow: gameState == GAME_STATE_DICE_ROLL");
 
-    // Display disce rolled as output message
+    // Display dice rolled as output message
     outputMessage = rollDiceForPlayer();
 
     // Change the game state
@@ -89,9 +92,8 @@ var main = function (input) {
   if (gameState == GAME_STATE_CHOOSE_DICE_ORDER) {
     console.log("Control flow: gameState == GAME_STATE_CHOOSE_DICE_ORDER");
 
-    // Call playerScore function
-    outputMessage = getPlayerScore(playerInput);
-    gameState = GAME_STATE_DICE_ROLL;
+    // Cal playerScore function
+    outputMessage = getPlayerScore(input);
     return outputMessage;
   }
 };
