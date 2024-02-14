bc4aa0ea6e924b78c0330285b56c7c1ac86f8439
Jia <sinjh1097@gmail.com>
2024-02-15 02:23:12 +0800

    complete game before tracking number of wins

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 //Calculate number of wins
 
 //2 players
-var player1 = "player1";
-var player2 = "player2";
+var player1 = "Player 1";
+var player2 = "Player 2";
 var player1DiceRoll = [];
 var player2DiceRoll = [];
 var player1Number;
@@ -15,7 +15,7 @@ var player2Number;
 //2 game modes, start with first player
 var modeRollDice = "modeRollDice";
 var modeChooseDiceOrder = "modeChooseDiceOrder";
-var currentPlayer = "player1";
+var currentPlayer = "Player 1";
 
 //Game starts with rolling of dice
 var gameMode = modeRollDice;
@@ -27,22 +27,20 @@ var rollSingleDice = function () {
 var rollDiceAndAssign = function () {
   var getDiceNumber = [rollSingleDice(), rollSingleDice()];
 
-  if (currentPlayer === "player1") {
+  if (currentPlayer === "Player 1") {
     player1DiceRoll = getDiceNumber;
   } else {
     player2DiceRoll = getDiceNumber;
   }
   return getDiceNumber;
 };
-var getDiceNumber = rollDiceAndAssign();
-console.log("Player 1's dice numbers are", player1DiceRoll);
 
 //Player picks the order of the numbers rolled (1 for first number first, 2 for second number first)
 //orderChosen is the user input (1 or 2)
 
 var getPlayerNumber = function (orderChosen) {
   var diceArray;
-  if (currentPlayer === "player1") {
+  if (currentPlayer === "Player 1") {
     diceArray = player1DiceRoll;
   } else {
     diceArray = player2DiceRoll;
@@ -54,21 +52,68 @@ var getPlayerNumber = function (orderChosen) {
     playerNumber = Number(String(diceArray[1]) + String(diceArray[0]));
   }
 
-  if (currentPlayer === 1) {
+  if (currentPlayer === "Player 1") {
     player1Number = playerNumber;
   } else {
     player2Number = playerNumber;
   }
   return playerNumber;
 };
-var orderChosen = 2;
-var player1Number = getPlayerNumber(orderChosen);
-console.log("Player 1's number:", player1Number);
 
+//determine winner
 var determineWinner = function () {
   if (player1Number > player2Number) {
-    return "player1";
+    return "Player 1";
   } else {
-    return "player2";
+    return "Player 2";
+  }
+};
+
+//yay, we are into the main function!
+
+var main = function (input) {
+  //Roll dice first
+  if (gameMode === modeRollDice) {
+    var getDiceNumber = rollDiceAndAssign();
+
+    gameMode = modeChooseDiceOrder;
+    var diceRollMessage = `Welcome ${currentPlayer}! <br> 
+Your dice numbers are: <br> 
+Dice 1: ${getDiceNumber[0]} <br>
+Dice 2: ${getDiceNumber[1]} <br> 
+Indicate your choice of order by entering 1 or 2 to assume the first value of the combined number.`;
+    return diceRollMessage;
+  }
+
+  //Combine numbers based on chosen order and show user combined number
+  if (gameMode === modeChooseDiceOrder) {
+    var orderChosen = Number(input);
+    if (isNaN(orderChosen) || (orderChosen !== 1 && orderChosen !== 2)) {
+      var returnErrorMessage =
+        "Please enter either 1 or 2 to indicate your preferred order.";
+      return returnErrorMessage;
+    }
+  }
+  var playerNumber = getPlayerNumber(orderChosen);
+  var combinedNumberMessage = `${currentPlayer}, you chose Dice ${orderChosen} as your first value. <br> 
+    Your number is therefore ${playerNumber}.<br>`;
+  if (currentPlayer === "Player 1") {
+    currentPlayer = "Player 2";
+    gameMode = modeRollDice;
+    return (
+      combinedNumberMessage +
+      "It is now Player 2's turn. Press submit to roll your dice."
+    );
+  }
+
+  //Determine winner and reset game
+  if (currentPlayer === "Player 2") {
+    var winningPlayer = determineWinner();
+    currentPlayer = "Player 1";
+    gameMode = modeRollDice;
+    var winningMessage = `${winningPlayer} has won. <br> 
+      Player 1's number is ${player1Number} while Player 2's number is ${player2Number}. <br> 
+      Press submit to play again.`;
+    return winningMessage;
   }
 };
